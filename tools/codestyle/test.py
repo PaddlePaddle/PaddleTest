5da5965c3049c913700876522ff11ae0cf26688d
zhengtianyu <zhengtianyu@MacintoshdeMacBook-Pro.local>
2021-07-06 14:41:56 +0800

    add

---
+++ tools/codestyle/test.py
@@ -0,0 +1,9 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+
+
+s = "zhengitnayu@baidu.com"
+
+ip = r"(([01]{0,1}\d{0,1}\d|2[0-4]\d|25[0-5])\.){3}([01]{0,1}\d{0,1}\d|2[0-4]\d|25[0-5])"
+a = "password"
