fb6117da9c9b8be6911e1910e214e995fbf8f69c
Divano <DDDivano@outlook.com>
2021-07-07 16:24:30 +0800

    add framework case

---
+++ framework/api/nn/README.md
@@ -0,0 +1,41 @@
+# 网络层测试套件
+## 依赖安装
+* pip install pytest
+
+## 执行命令
+**命令默认会选择test_开头的文件进行执行**
+* pytest --disable-warnings . 执行全部
+* pytest --disable-warnings -sv . 执行全部并打印其中print输出，用户debug=True时候的调试
+* pytest . --html=report.html 生成Html测试报告，名字是report.html
+* pytest --disable-warnings -sv xx.py::test_xx 执行xx.py下的test_xx测试用例
+
+## 规范
+每个API对应一个文件
+
+## 使用方式
+**参考test_example.py 或者 test_roll.py**
+* 需要编写一个类继承APICase类，并且重写hook方法，hook方法主要需要包含self.types定义，同时也可选择性改写基类功能配置参数。
+* docstring 写传入的参数列表，方便直观看到传入参数
+* 实例化对象 obj。
+* 编写case，指定输入输出，调用obj.run即可。
+* 通用测试，例如check dtype调用 obj.base即可。
+
+## 基本用法
+* obj.run()执行专用case。
+* obj.base()执行dtype等基础检测Case。
+* obj.exception()执行异常检测Case，异常检测使用pytest.raise实现。默认检测c++异常，采用字符串匹配，异常类型是字符串。如果需要检测python异常，异常类型就是python异常类型，mode参数设置成为"python"
+
+## 全局参数用法
+* self.no_grad_var (tuple/set/list) 用来设置tensor类型的param反向截断，如果不设置，可能在反向check出现keyerror。
+* self.debug=True 用于编写case的时候调试用，会将全部的log都打印出来，包括错误异常。默认值：False
+* self.static=True和self.dygraph=True用于指定跑该类型网络，默认均为True，设置False为不跑该类型网络。
+* self.enable_backward=False用来截断反向网络，主要是调试前向，以及某些无反向API的测试，默认为True。
+
+## 特殊情况参数设置
+比如在某些情况，dtype支持除float32，float64以外的类型，同时该api存在反向，正常执行因为int，bool类型不存在反向会报错，那么需要单独重新定义参数，有两种方法实现。第一种：在指定case里单独指定参数，比如obj.types=[]，同时执行完case之后需要把参数还原回去，防止下个case参数异常。第二种：单独定义指定相关Case的对象obj2，利用obj2进行一些特殊行为的Case测试。
+
+## 不兼容情况
+需要单独编写case，不依赖框架，具体写法可以参考test_meshgrid1
+
+## FAQ
+目前功能不完善，有bug联系dddivano@outlook.com。
---
+++ framework/api/nn/test_conv2d.py
@@ -0,0 +1,543 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+"""
+/***************************************************************************
+*
+* Copyright (c) 2020 Baidu.com, Inc. All Rights Reserved
+* @file test_conv2d.py
+* @author zhengtianyu
+* @date 2020-08-28 16:30:26
+* @brief test_conv2d
+*
+**************************************************************************/
+"""
+from apibase import APIBase
+from apibase import randtool
+import pytest
+import paddle
+import paddle.fluid as fluid
+import numpy as np
+
+
+class TestConv2d(APIBase):
+    """
+    test
+    """
+
+    def hook(self):
+        """
+        implement
+        """
+        self.types = [np.float32]
+        self.delta = 1e-3 * 5
+        # self.debug = True
+        # self.static = True
+        # enable check grad
+        # self.enable_backward = True
+
+
+obj = TestConv2d(paddle.nn.Conv2D)
+
+
+@pytest.mark.p0
+def test_conv2d_base():
+    """
+    base
+    """
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = 1
+    padding = 0
+    res = np.array([[[[12.5633, 10.7230], [13.3298, 13.2501]]], [[[14.4928, 12.5433], [15.1694, 13.6606]]]])
+    obj.base(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d():
+    """
+    default
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = 1
+    padding = 0
+    res = np.array([[[[12.5633, 10.7230], [13.3298, 13.2501]]], [[[14.4928, 12.5433], [15.1694, 13.6606]]]])
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d1():
+    """
+    kernel_size = [2, 2]
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [2, 2]
+    stride = 1
+    padding = 0
+    res = np.array(
+        [
+            [[[5.6780, 3.9777, 4.8305], [4.8986, 5.0738, 5.9837], [5.1103, 7.2506, 7.0629]]],
+            [[[5.2631, 5.0306, 6.2066], [7.6067, 5.8608, 5.8187], [7.2613, 6.7396, 6.0788]]],
+        ]
+    )
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d2():
+    """
+    kernel_size = [2, 2], out_channels = 3
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 3
+    kernel_size = [2, 2]
+    stride = 1
+    padding = 0
+    res = np.array(
+        [
+            [
+                [[5.6780, 3.9777, 4.8305], [4.8986, 5.0738, 5.9837], [5.1103, 7.2506, 7.0629]],
+                [[5.6780, 3.9777, 4.8305], [4.8986, 5.0738, 5.9837], [5.1103, 7.2506, 7.0629]],
+                [[5.6780, 3.9777, 4.8305], [4.8986, 5.0738, 5.9837], [5.1103, 7.2506, 7.0629]],
+            ],
+            [
+                [[5.2631, 5.0306, 6.2066], [7.6067, 5.8608, 5.8187], [7.2613, 6.7396, 6.0788]],
+                [[5.2631, 5.0306, 6.2066], [7.6067, 5.8608, 5.8187], [7.2613, 6.7396, 6.0788]],
+                [[5.2631, 5.0306, 6.2066], [7.6067, 5.8608, 5.8187], [7.2613, 6.7396, 6.0788]],
+            ],
+        ]
+    )
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d3():
+    """
+    kernel_size = [2, 2] stride = 2
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = 2
+    padding = 0
+    res = np.array([[[[12.5633]]], [[[14.4928]]]])
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d4():
+    """
+    kernel_size = [2, 2] stride = 2 padding=1
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = 2
+    padding = 1
+    res = np.array([[[[5.6780, 6.7046], [7.8561, 13.2501]]], [[[5.2631, 8.0467], [10.6586, 13.6606]]]])
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d5():
+    """
+    kernel_size = [2, 2] stride = 2 padding=0 groups=3
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 6
+    kernel_size = [3, 3]
+    stride = 2
+    padding = 0
+    groups = 3
+    res = np.array(
+        [
+            [[[4.3166]], [[4.3166]], [[4.6029]], [[4.6029]], [[3.6437]], [[3.6437]]],
+            [[[3.6364]], [[3.6364]], [[5.5694]], [[5.5694]], [[5.2870]], [[5.2870]]],
+        ]
+    )
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        groups=groups,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d6():
+    """
+    kernel_size = [3, 3] stride = 1 padding=1 w=0.7 b=-0.3
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 2
+    kernel_size = [3, 3]
+    stride = 1
+    padding = 0
+    res = np.array(
+        [
+            [[[8.4943, 7.2061], [9.0308, 8.9750]], [[8.4943, 7.2061], [9.0308, 8.9750]]],
+            [[[9.8450, 8.4803], [10.3186, 9.2624]], [[9.8450, 8.4803], [10.3186, 9.2624]]],
+        ]
+    )
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        weight_attr=fluid.initializer.ConstantInitializer(value=0.7),
+        bias_attr=fluid.initializer.ConstantInitializer(value=-0.3),
+    )
+
+
+def test_conv2d7():
+    """
+    kernel_size = [3, 3] stride = 1 padding=1 w=0.7 b=-0.3 data_format="NHWC"
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4]).transpose(0, 2, 3, 1)
+    in_channels = 3
+    out_channels = 2
+    kernel_size = [3, 3]
+    stride = 1
+    padding = 0
+    data_format = "NHWC"
+    res = np.array(
+        [
+            [[[8.4943, 7.2061], [9.0308, 8.9750]], [[8.4943, 7.2061], [9.0308, 8.9750]]],
+            [[[9.8450, 8.4803], [10.3186, 9.2624]], [[9.8450, 8.4803], [10.3186, 9.2624]]],
+        ]
+    ).transpose(0, 2, 3, 1)
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        data_format=data_format,
+        weight_attr=fluid.initializer.ConstantInitializer(value=0.7),
+        bias_attr=fluid.initializer.ConstantInitializer(value=-0.3),
+    )
+
+
+def test_conv2d8():
+    """
+    padding_mode = "reflect"
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = [2, 1]
+    padding = 1
+    padding_mode = "reflect"
+    res = np.array(
+        [
+            [[[10.8965, 11.4650, 10.1772, 10.7457], [11.1693, 13.3298, 13.2501, 15.4106]]],
+            [[[11.5616, 13.0999, 12.1508, 13.6891], [15.1975, 15.1694, 13.6606, 13.6325]]],
+        ]
+    )
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        padding_mode=padding_mode,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d9():
+    """
+    padding_mode = "replicate"
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = [2, 1]
+    padding = 1
+    padding_mode = "replicate"
+    res = np.array(
+        [
+            [[[14.0708, 11.8796, 9.9366, 10.3045], [12.3989, 13.3298, 13.2501, 14.4000]]],
+            [[[11.6388, 12.2609, 11.9894, 13.6088], [16.7781, 15.1694, 13.6606, 13.7325]]],
+        ]
+    )
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        padding_mode=padding_mode,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d10():
+    """
+    padding_mode = "circular"
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = [2, 1]
+    padding = 1
+    padding_mode = "circular"
+    res = np.array(
+        [
+            [[[13.4084, 12.6462, 12.4637, 13.7390], [12.3192, 13.3298, 13.2501, 14.4797]]],
+            [[[13.2020, 12.9375, 13.1067, 14.2273], [15.2694, 15.1694, 13.6606, 15.2412]]],
+        ]
+    )
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        padding_mode=padding_mode,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d11():
+    """
+    padding_mode = "zeros" dilation = 2
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = [2, 1]
+    padding = 1
+    dilation = 2
+    padding_mode = "zeros"
+    res = np.array([[[[6.3869, 6.1435]]], [[[6.4830, 6.3120]]]])
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        padding_mode=padding_mode,
+        dilation=dilation,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d12():
+    """
+    padding_mode = "zeros" dilation = [2, 2]
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = [2, 1]
+    padding = 1
+    dilation = [2, 2]
+    padding_mode = "zeros"
+    res = np.array([[[[6.3869, 6.1435]]], [[[6.4830, 6.3120]]]])
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        padding_mode=padding_mode,
+        dilation=dilation,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d13():
+    """
+    padding_mode = "zeros" dilation = (2, 2)
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = [2, 1]
+    padding = 1
+    dilation = (2, 2)
+    padding_mode = "zeros"
+    res = np.array([[[[6.3869, 6.1435]]], [[[6.4830, 6.3120]]]])
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        padding_mode=padding_mode,
+        dilation=dilation,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d14():
+    """
+    padding_mode = "zeros" dilation = (2, 2)  padding = (1, 2)
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = [2, 1]
+    padding = (1, 2)
+    dilation = (2, 2)
+    padding_mode = "zeros"
+    res = np.array([[[[6.1435, 6.3869, 6.1435, 6.3869]]], [[[6.3120, 6.4830, 6.3120, 6.4830]]]])
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        padding_mode=padding_mode,
+        dilation=dilation,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
+
+
+def test_conv2d15():
+    """
+    padding_mode = "zeros" dilation = (2, 2)  padding = [1, 2]
+    """
+    np.random.seed(obj.seed)
+    x = randtool("float", 0, 1, [2, 3, 4, 4])
+    in_channels = 3
+    out_channels = 1
+    kernel_size = [3, 3]
+    stride = [2, 1]
+    padding = [1, 2]
+    dilation = (2, 2)
+    padding_mode = "zeros"
+    res = np.array([[[[6.1435, 6.3869, 6.1435, 6.3869]]], [[[6.3120, 6.4830, 6.3120, 6.4830]]]])
+    obj.run(
+        res=res,
+        data=x,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        padding_mode=padding_mode,
+        dilation=dilation,
+        weight_attr=fluid.initializer.ConstantInitializer(value=1),
+        bias_attr=fluid.initializer.ConstantInitializer(value=0),
+    )
