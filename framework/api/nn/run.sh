5d8a7647b60a5aa1efb7c3e49150bb4275af1b5e
Divano <DDDivano@outlook.com>
2021-07-08 11:27:06 +0800

    optimize run script

--- framework/api/nn/run.sh
+++ framework/api/nn/run.sh
@@ -2,6 +2,9 @@ pip3.7 install pytest
 export FLAGS_call_stack_level=2
 cases=`find . -name "test*.py" | sort`
 ignore=""
+bug=0
+
+echo "============ failed cases =============" >> result.txt
 for file in ${cases}
 do
     echo ${file}
@@ -11,6 +14,9 @@ do
         python3.7 -m pytest ${file}
         if [ $? -ne 0 ]; then
             echo ${file} >> result.txt
+            bug=`expr ${bug} + 1`
         fi
     fi
 done
+
+echo "total bugs: "${bug} >> result.txt
--- framework/api/run.sh
+++ framework/api/run.sh
@@ -1,5 +1,8 @@
 home=$PWD
 # nn
 cd nn
+rm -rf ./result.txt
+echo "[nn cases result]" >> result.txt
 bash ./run.sh
+cat ./result.txt
 cd $home
