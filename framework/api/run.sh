5aab36ea267fd97e899518a7a9df2ab082a0e2fd
Divano <DDDivano@outlook.com>
2021-07-08 15:07:18 +0800

    add base run script

--- framework/api/run.sh
+++ framework/api/run.sh
@@ -6,3 +6,11 @@ echo "[nn cases result]" >> result.txt
 bash ./run.sh
 cat ./result.txt
 cd $home
+
+# optimizer
+cd optimizer
+rm -rf ./result.txt
+echo "[optimizer cases result]" >> result.txt
+bash ./run.sh
+cat ./result.txt
+cd $home
