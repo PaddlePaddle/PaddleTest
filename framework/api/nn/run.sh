e1ca60add2f2bab40015501c6ee3e3890fc2784d
Divano <DDDivano@outlook.com>
2021-07-08 10:01:33 +0800

    run.sh

---
+++ framework/api/nn/run.sh
@@ -0,0 +1,13 @@
+pip3.7 install pytest
+export FLAGS_call_stack_level=2
+cases=`find . -name "test*.py" | sort`
+ignore=""
+for file in ${cases}
+do
+    echo ${file}
+    if [[ ${ignore} =~ ${file##*/} ]]; then
+        echo "跳过"
+    else
+        python3.7 -m pytest ${file} --alluredir=report
+    fi
+done
