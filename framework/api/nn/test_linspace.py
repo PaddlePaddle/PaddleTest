362f2b4160e10d077e620f0e572c8d834a23bcbe
Zeref996 <53218160+Zeref996@users.noreply.github.com>
2021-07-20 17:08:42 +0800

    add case (#31)

---
+++ framework/api/nn/test_linspace.py
@@ -0,0 +1,203 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+"""
+test linspace
+"""
+from apibase import APIBase
+import paddle
+import pytest
+import numpy as np
+
+
+class TestLinspace(APIBase):
+    """
+    test linspace
+    """
+
+    def hook(self):
+        """
+        implement
+        """
+        #start and stop only support float32, float64 tensor
+        self.types = [np.float32, np.float64]
+        self.enable_backward = False
+        self.debug = True
+
+
+obj = TestLinspace(paddle.linspace)
+
+
+@pytest.mark.api_base_linspace_vartype
+def test_linspace_base():
+    """
+    base
+    """
+    start = 3.5
+    stop = 9.6
+    num = 5
+    res = np.linspace(start=start, stop=stop, num=num)
+    obj.base(res=res, start=start, stop=stop, num=num)
+
+
+@pytest.mark.api_base_linspace_parameters
+def test_linspace1():
+    """
+    start=tensor,stop=num
+    """
+    start = 2.9 + np.arange(1)
+    stop = 9.6
+    num = 5
+    res = np.array([2.9, 4.575, 6.25, 7.925, 9.6])
+    obj.run(res=res, start=start, stop=stop, num=num, dtype='float64')
+
+
+@pytest.mark.api_base_linspace_parameters
+def test_linspace2():
+    """
+    start=tensor,stop=tensor
+    """
+    start = 3.0 + np.arange(1)
+    stop = 8.3 + np.arange(1)
+    num = 2
+    res = np.linspace(start=3.0, stop=8.3, num=num)
+    obj.run(res=res, start=start, stop=stop, num=num, dtype='float64')
+
+
+@pytest.mark.api_base_linspace_exception
+def test_linspace3():
+    """
+    num=0
+    """
+    start = 3
+    stop = 9
+    num = 0
+    obj.exception(mode='c', etype='Error', start=start, stop=stop, num=num)
+
+
+@pytest.mark.api_base_linspace_parameters
+def test_linspace4():
+    """
+    num=1
+    """
+    start = 3
+    stop = 9
+    num = 1
+    res = np.linspace(start=start, stop=stop, num=num)
+    obj.run(res=res, start=start, stop=stop, num=num)
+
+
+@pytest.mark.api_base_linspace_exception
+def test_linspace5():
+    """
+    TypeError:num=float
+    Cannot catch exception(it's a bug)
+    """
+    start = 3
+    stop = 9
+    num = 1.2
+    obj.exception(mode='python', etype=TypeError, star=start, stop=stop, num=num)
+
+
+@pytest.mark.api_base_linspace_parameters
+def test_linspace6():
+    """
+    start>stop
+    """
+    start = 9
+    stop = 3
+    num = 5
+    res = np.linspace(start=start, stop=stop, num=num)
+    obj.run(res=res, start=start, stop=stop, num=num)
+
+
+@pytest.mark.api_base_linspace_parameters
+def test_linspace7():
+    """
+    dtype=np.dtype
+    """
+    start = 3
+    stop = 9
+    num = 5
+    dtype = np.float64
+    res = np.linspace(start=start, stop=stop, num=num)
+    obj.run(res=res, start=start, stop=stop, num=num, dtype=dtype)
+
+
+@pytest.mark.api_base_linspace_parameters
+def test_linspace8():
+    """
+    dtype=float32
+    """
+    start = 3
+    stop = 9
+    num = 5
+    dtype = 'float32'
+    res = np.linspace(start=start, stop=stop, num=num)
+    obj.run(res=res, start=start, stop=stop, num=num, dtype=dtype)
+
+
+@pytest.mark.api_base_linspace_parameters
+def test_linspace9():
+    """
+    dtype=float64
+    """
+    start = 3
+    stop = 9
+    num = 5
+    dtype = 'float64'
+    res = np.linspace(start=start, stop=stop, num=num)
+    obj.run(res=res, start=start, stop=stop, num=num, dtype=dtype)
+
+
+@pytest.mark.api_base_linspace_exception
+def test_linspace10():
+    """
+    TypeError:dtype=bool
+    """
+    start = 3
+    stop = 9
+    num = 5
+    dtype = 'bool'
+    obj.exception(mode='c', etype='NotFoundError', start=start, stop=stop, num=num, dtype=dtype)
+
+
+class TestLinspace1(APIBase):
+    """
+    test linspace
+    """
+
+    def hook(self):
+        """
+        implement
+        """
+        #num only support int32 tensor
+        self.types = [np.int32]
+        self.enable_backward = False
+
+
+obj1 = TestLinspace1(paddle.linspace)
+
+
+@pytest.mark.api_base_linspace_parameters
+def test_linspace11():
+    """
+    num=tensor
+    """
+    start = 4.5
+    stop = 8
+    num = 2 + np.arange(1)
+    res = np.linspace(start=start, stop=stop, num=2)
+    obj1.run(res=res, start=start, stop=stop, num=num)
+
+
+@pytest.mark.api_base_linspace_parameters
+def test_linspace12():
+    """
+    input and stop is int32 tensor
+    """
+    start = 1 + np.arange(1)
+    stop = 8 + np.arange(1)
+    num = 2
+    res = np.array([1, 8])
+    obj1.run(res=res, start=start, stop=stop, num=num)
---
+++ framework/api/nn/test_multinomial.py
@@ -0,0 +1,147 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+"""
+test multinomial
+"""
+import paddle
+import pytest
+import numpy as np
+import paddle.fluid as fluid
+from apibase import compare
+
+if fluid.is_compiled_with_cuda() is True:
+    devices = ['gpu', 'cpu']
+else:
+    devices = ['cpu']
+if fluid.is_compiled_with_cuda() is True:
+    places = [fluid.CPUPlace(), fluid.CUDAPlace(0)]
+else:
+    places = [fluid.CPUPlace()]
+dtypes = [np.float32, np.float64]
+value_dtypes = [np.int16, np.int32, np.int64]
+
+seed = 33
+
+
+@pytest.mark.api_base_multinomial_vartype
+def test_multinomial_dygraph1():
+    """
+    x=1-D Tensor,num_samples=1
+    """
+    paddle.disable_static()
+    for device in devices:
+        for t in dtypes:
+            np.random.seed(seed)
+            paddle.set_device(device)
+            x = np.random.random(6).astype(t)
+            res = paddle.multinomial(paddle.to_tensor(x), name=None)
+            assert np.allclose(res.shape, [1], atol=0.005, rtol=0.05, equal_nan=True)
+    paddle.enable_static()
+
+
+@pytest.mark.api_base_multinomial_parameters
+def test_multinomial_dygraph2():
+    """
+    x=1-D Tensor,num_samples=5,replacement=True
+    """
+    paddle.disable_static()
+    for device in devices:
+        for t in dtypes:
+            np.random.seed(seed)
+            paddle.set_device(device)
+            x = np.random.random(7).astype(t)
+            res = paddle.multinomial(paddle.to_tensor(x), num_samples=4, name=None)
+            assert np.allclose(res.shape, [4], atol=0.005, rtol=0.05, equal_nan=True)
+    paddle.enable_static()
+
+
+@pytest.mark.api_base_multinomial_parameters
+def test_multinomial_dygraph3():
+    """
+    x=2-D Tensor,num_samples=5,replacement=True
+    """
+    paddle.disable_static()
+    for device in devices:
+        for t in dtypes:
+            np.random.seed(seed)
+            paddle.set_device(device)
+            x = np.random.random([2, 7]).astype(t)
+            res = paddle.multinomial(paddle.to_tensor(x), num_samples=5, replacement=True, name=None)
+            assert np.allclose(res.shape, [2, 5], atol=0.005, rtol=0.05, equal_nan=True)
+    paddle.enable_static()
+
+
+@pytest.mark.api_base_multinomial_vartype
+def test_multinomial_static1():
+    """
+    x=1-D Tensor,num_samples=1
+    :return:
+    """
+    paddle.enable_static()
+    for device in devices:
+        for t in dtypes:
+            np.random.seed(seed)
+            paddle.set_device(device)
+            main_program = fluid.Program()
+            startup_program = fluid.Program()
+            x = np.random.random([6]).astype(t)
+            feed = {"x": x}
+            with fluid.unique_name.guard():
+                with fluid.program_guard(main_program=main_program, startup_program=startup_program):
+                    x = paddle.static.data(name="x", shape=x.shape, dtype=t)
+                    res = paddle.multinomial(x=x, name=None)
+                    exe = fluid.Executor()
+                    exe.run(startup_program)
+                    static_out = exe.run(main_program, feed=feed, fetch_list=[res])
+                    assert np.allclose(np.array(static_out).shape, [1], atol=0.005, rtol=0.05, equal_nan=True)
+
+
+@pytest.mark.api_base_multinomial_parameters
+def test_multinomial_static2():
+    """
+    x=1-D Tensor,num_samples=8,replacement=True
+    :return:
+    """
+    paddle.enable_static()
+    for device in devices:
+        for t in dtypes:
+            np.random.seed(seed)
+            paddle.set_device(device)
+            main_program = fluid.Program()
+            startup_program = fluid.Program()
+            x = np.random.random([5]).astype(t)
+            feed = {"x": x}
+            with fluid.unique_name.guard():
+                with fluid.program_guard(main_program=main_program, startup_program=startup_program):
+                    x = paddle.static.data(name="x", shape=x.shape, dtype=t)
+                    res = paddle.multinomial(x=x, num_samples=8, replacement=True, name=None)
+                    exe = fluid.Executor()
+                    exe.run(startup_program)
+                    static_out = exe.run(main_program, feed=feed, fetch_list=[res])
+                    assert np.allclose(np.array(static_out).shape, [1, 8], atol=0.005, rtol=0.05, equal_nan=True)
+
+
+@pytest.mark.api_base_multinomial_parameters
+def test_multinomial_static3():
+    """
+    x=2-D Tensor,num_samples=5,replacement=True
+    :return:
+    """
+    paddle.enable_static()
+    for device in devices:
+        for t in dtypes:
+            np.random.seed(seed)
+            paddle.set_device(device)
+            main_program = fluid.Program()
+            startup_program = fluid.Program()
+            x = np.random.random([6, 1]).astype(t)
+            feed = {"x": x}
+            with fluid.unique_name.guard():
+                with fluid.program_guard(main_program=main_program, startup_program=startup_program):
+                    x = paddle.static.data(name="x", shape=x.shape, dtype=t)
+                    res = paddle.multinomial(x=x, num_samples=5, replacement=True, name=None)
+                    exe = fluid.Executor()
+                    exe.run(startup_program)
+                    static_out = exe.run(main_program, feed=feed, fetch_list=[res])
+                    assert np.allclose(np.array(static_out).shape, [1, 6, 5], atol=0.005, rtol=0.05, equal_nan=True)
