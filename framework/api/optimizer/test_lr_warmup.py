d7f48c572f749bee9e70a2b417a6456f14dea428
neao <gzx1990@gmail.com>
2021-08-06 16:55:21 +0800

    fix

--- framework/api/optimizer/test_lr_warmup.py
+++ framework/api/optimizer/test_lr_warmup.py
@@ -12,7 +12,7 @@ start_lr = 0
 end_lr = 0.5
 warmup_steps = 20
 learning_rate = 0.5
-    
+
 
 def test_warmup_1():
     """
@@ -25,8 +25,8 @@ def test_warmup_1():
         exp = start_lr + (end_lr - start_lr) * i / warmup_steps
         assert exp == scheduler_1.get_lr()
         scheduler_1.step()
-        
-       
+
+
 def test_warmup_2():
     """
     test step > warmup_step
@@ -35,17 +35,17 @@ def test_warmup_2():
     scheduler_2 = paddle.optimizer.lr.LinearWarmup(
         learning_rate=learning_rate,
         warmup_steps=warmup_steps,
-        start_lr=start_lr, 
-        end_lr=end_lr, 
-        last_epoch=last_epoch, 
-        verbose=False
+        start_lr=start_lr,
+        end_lr=end_lr,
+        last_epoch=last_epoch,
+        verbose=False,
     )
     assert learning_rate == scheduler_2.get_lr() 
 
 
 def test_warmup_3():
     """
-    test warmup_steps <=0 
+    test warmup_steps <=0
     """
     try:
         scheduler_3 = paddle.optimizer.lr.LinearWarmup(
