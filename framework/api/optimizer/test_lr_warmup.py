f48721e16fa7edf412bbf1d16b582a6587e16272
neao <gzx1990@gmail.com>
2021-08-06 16:39:24 +0800

    fix style

--- framework/api/optimizer/test_lr_warmup.py
+++ framework/api/optimizer/test_lr_warmup.py
@@ -7,7 +7,6 @@ test lr LinearWarmup case
 
 
 import paddle
-import pytest
 
 start_lr = 0
 end_lr = 0.5
@@ -19,11 +18,11 @@ def test_warmup_1():
     """
     test warmup base test
     """
-    scheduler_1 = paddle.optimizer.lr.LinearWarmup(\
-        learning_rate=learning_rate, warmup_steps=warmup_steps,\
-        start_lr=start_lr, end_lr=end_lr, verbose=False)
-    for i in range(0,20):
-        exp = start_lr + (end_lr - start_lr) * i / warmup_steps 
+    scheduler_1 = paddle.optimizer.lr.LinearWarmup(
+        learning_rate=learning_rate, warmup_steps=warmup_steps, start_lr=start_lr, end_lr=end_lr, verbose=False
+    )
+    for i in range(0, 20):
+        exp = start_lr + (end_lr - start_lr) * i / warmup_steps
         assert exp == scheduler_1.get_lr()
         scheduler_1.step()
         
@@ -33,9 +32,14 @@ def test_warmup_2():
     test step > warmup_step
     """
     last_epoch = warmup_steps + 1
-    scheduler_2 = paddle.optimizer.lr.LinearWarmup(\
-        learning_rate=learning_rate, warmup_steps=warmup_steps,\
-        start_lr=start_lr, end_lr=end_lr, last_epoch=last_epoch, verbose=False)
+    scheduler_2 = paddle.optimizer.lr.LinearWarmup(
+        learning_rate=learning_rate,
+        warmup_steps=warmup_steps,
+        start_lr=start_lr, 
+        end_lr=end_lr, 
+        last_epoch=last_epoch, 
+        verbose=False
+    )
     assert learning_rate == scheduler_2.get_lr() 
 
 
@@ -44,9 +48,8 @@ def test_warmup_3():
     test warmup_steps <=0 
     """
     try:
-        print("begin")
-        scheduler_3 = paddle.optimizer.lr.LinearWarmup(\
-            learning_rate=learning_rate, warmup_steps=0,\
-            start_lr=start_lr, end_lr=end_lr, verbose=True)
+        scheduler_3 = paddle.optimizer.lr.LinearWarmup(
+            learning_rate=learning_rate, warmup_steps=0, start_lr=start_lr, end_lr=end_lr, verbose=True
+        )
     except AssertionError as error:
         assert """'warmup_steps' must be a positive integer""" in str(error)
