6d9015c1b5544d47b4a573075cc576adaeeea0fb
quanxiang-liu <qxliunj@163.com>
2021-08-10 16:11:59 +0800

    add clas gan xly P0 case

---
+++ models/PaddleGAN/CE/linux/scripts/EDVR/train_2card.sh
@@ -0,0 +1,20 @@
+export FLAGS_cudnn_deterministic=True
+
+cur_path=`pwd`
+echo $cur_path
+
+cd /workspace/task/PaddleGAN
+sed -i 's/use_flip: True/use_flip: False/g' configs/edvr_m_wo_tsa.yaml #将 use_flip 字段替换为 Fasle
+sed -i 's/use_rot: True/use_rot: False/g' configs/edvr_m_wo_tsa.yaml #将 use_rot 字段替换为 Fasle
+rm -rf data
+ln -s /home/data/cfs/models_ce/PaddleGAN data
+if [ ! -d "log" ]; then
+  mkdir log
+fi
+if [ ! -d "../log" ]; then
+  mkdir ../log
+fi
+python -m pip install -r requirements.txt
+python -m paddle.distributed.launch tools/main.py -c configs/edvr_m_wo_tsa.yaml -o total_iters=100 dataset.train.use_flip=False dataset.train.use_rot=False  log_config.interval=20 log_config.visiual_interval=999999 snapshot_config.interval=999999 > log/edvr_2card.log 2>&1
+cat log/edvr_2card.log | grep " INFO: Iter: 100/100" > ../log/edvr_2card.log
+
