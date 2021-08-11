392c732b297a44143050907d67c7ee1432ed51ff
wangye707 <wangye19@baidu.com>
2021-08-11 07:30:41 +0000

    添加检测库启动脚本

--- inference/python_api_test/run.sh
+++ inference/python_api_test/run.sh
@@ -27,3 +27,31 @@ else
   echo "error!"
   exit 8
 fi
+
+cd test_det_model
+rm -rf ./result.txt
+echo "[det model inference cases result]" >> result.txt
+bash ./run.sh
+class_model=$?
+echo ${class_model}
+cat ./result.txt
+cd ..
+
+# result
+echo "=============== result ================="
+if [ `expr ${class_model}` -eq 0 ]; then
+  result=`find . -name "result.txt"`
+  for file in ${result}
+    do
+      cat ${file}
+    done
+  echo "success!"
+else
+  result=`find . -name "result.txt"`
+  for file in ${result}
+    do
+      cat ${file}
+    done
+  echo "error!"
+  exit 8
+fi
