04c696b8f228c9dfbaf6d61c76d6d0301fc64057
quanxiang-liu <qxliunj@163.com>
2021-08-11 17:00:05 +0800

    change code

--- models/PaddleClas/CE/linux/scripts/ResNet50/train_1card.sh
+++ models/PaddleClas/CE/linux/scripts/ResNet50/train_1card.sh
@@ -10,7 +10,14 @@ sed -ie '/flip_code/d'  ppcls/configs/ImageNet/ResNet/ResNet50.yaml
 
 rm -rf dataset
 ln -s /home/data/cfs/models_ce/PaddleClas dataset
-mkdir log
+if [ ! -d "log" ]; then
+  mkdir log
+fi
+if [ ! -d "../log" ]; then
+  mkdir ../log
+fi
 python -m pip install -r requirements.txt
 python tools/train.py -c ppcls/configs/ImageNet/ResNet/ResNet50.yaml -o Global.epochs=2 -o DataLoader.Train.sampler.shuffle=False -o DataLoader.Train.sampler.batch_size=4 -o DataLoader.Eval.sampler.batch_size=4 > log/resnet50_1card.log 2>&1
+cat log/resnet50_1card.log
+cat log/resnet50_1card.log | grep Train | grep Avg | grep 'Epoch 2/2'
 cat log/resnet50_1card.log | grep Train | grep Avg | grep 'Epoch 2/2' > ../log/resnet50_1card.log
