8a4036e1f9d961ce2e63260c6a1f20df8651f30f
quanxiang-liu <qxliunj@163.com>
2021-08-11 16:42:36 +0800

    change path

--- models/PaddleClas/CE/linux/scripts/ResNet50/train_2card.sh
+++ models/PaddleClas/CE/linux/scripts/ResNet50/train_2card.sh
@@ -3,7 +3,7 @@ export FLAGS_cudnn_deterministic=True
 cur_path=`pwd`
 echo $cur_path
 
-cd /workspace/PaddleGAN/ce/Paddle_Cloud_CE/src/task/PaddleClas
+cd /workspace/PaddleClas/ce/Paddle_Cloud_CE/src/task/PaddleClas
 sed -i 's/RandCropImage/ResizeImage/g'  ppcls/configs/ImageNet/ResNet/ResNet50.yaml
 sed -ie '/RandFlipImage/d'  ppcls/configs/ImageNet/ResNet/ResNet50.yaml
 sed -ie '/flip_code/d'  ppcls/configs/ImageNet/ResNet/ResNet50.yaml
