f1b11ecae3b18c510c3ddec317dcc4bbba68fb91
wangye707 <wangye19@baidu.com>
2021-08-11 13:01:17 +0000

    更新run.sh启动脚本

--- inference/python_api_test/run.sh
+++ inference/python_api_test/run.sh
@@ -4,54 +4,11 @@ cd test_class_model
 rm -rf ./result.txt
 echo "[class model inference cases result]" >> result.txt
 bash ./run.sh
-class_model=$?
-echo ${class_model}
 cat ./result.txt
 cd ..
-
-# result
-echo "=============== result ================="
-if [ `expr ${class_model}` -eq 0 ]; then
-  result=`find . -name "result.txt"`
-  for file in ${result}
-    do
-      cat ${file}
-    done
-  echo "success!"
-else
-  result=`find . -name "result.txt"`
-  for file in ${result}
-    do
-      cat ${file}
-    done
-  echo "error!"
-  exit 8
-fi
 
 cd test_det_model
 rm -rf ./result.txt
-echo "[det model inference cases result]" >> result.txt
+echo "[class model inference cases result]" >> result.txt
 bash ./run.sh
-class_model=$?
-echo ${class_model}
 cat ./result.txt
-cd ..
-
-# result
-echo "=============== result ================="
-if [ `expr ${class_model}` -eq 0 ]; then
-  result=`find . -name "result.txt"`
-  for file in ${result}
-    do
-      cat ${file}
-    done
-  echo "success!"
-else
-  result=`find . -name "result.txt"`
-  for file in ${result}
-    do
-      cat ${file}
-    done
-  echo "error!"
-  exit 8
-fi
