b24763188b05676395bf03a60d041d823cea3658
wangye707 <wangye19@baidu.com>
2021-08-12 11:39:37 +0000

    修改启动脚本逻辑

--- inference/python_api_test/run.sh
+++ inference/python_api_test/run.sh
@@ -4,53 +4,28 @@ cd test_class_model
 rm -rf ./result.txt
 echo "[Class model inference cases result]" >> result.txt
 bash ./run.sh
-class_model=$?
-echo ${class_model}
+bug=$?
 cat ./result.txt
 cd ..
-# result
-echo "=============== result ================="
-if [ `expr ${class_model}` -eq 0 ]; then
-  result=`find . -name "result.txt"`
-  for file in ${result}
-    do
-      cat ${file}
-    done
+echo "===============Class model result ================="
+if [ `expr ${bug}` -eq 0 ]; then
   echo "success!"
 else
-  result=`find . -name "result.txt"`
-  for file in ${result}
-    do
-      cat ${file}
-    done
   echo "error!"
   exit 8
 fi
-
-
 
 cd test_det_model
 rm -rf ./result.txt
 echo "[Detection model inference cases result]" >> result.txt
 bash ./run.sh
-class_model=$?
-echo ${class_model}
+bug=$?
 cat ./result.txt
-# result
-echo "=============== result ================="
-if [ `expr ${class_model}` -eq 0 ]; then
-  result=`find . -name "result.txt"`
-  for file in ${result}
-    do
-      cat ${file}
-    done
+cd ..
+echo "===============Detection model result ================="
+if [ `expr ${bug}` -eq 0 ]; then
   echo "success!"
 else
-  result=`find . -name "result.txt"`
-  for file in ${result}
-    do
-      cat ${file}
-    done
   echo "error!"
   exit 8
 fi
