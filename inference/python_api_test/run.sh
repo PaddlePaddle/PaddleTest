9078fe0718a7fa77ac515121710cf4c0f23afe7c
wangye707 <wangye19@baidu.com>
2021-08-12 03:34:36 +0000

    更新run.sh

--- inference/python_api_test/run.sh
+++ inference/python_api_test/run.sh
@@ -2,13 +2,55 @@ home=$PWD
 # base
 cd test_class_model
 rm -rf ./result.txt
-echo "[class model inference cases result]" >> result.txt
+echo "[Class model inference cases result]" >> result.txt
 bash ./run.sh
+class_model=$?
+echo ${class_model}
 cat ./result.txt
 cd ..
+# result
+echo "=============== result ================="
+if [ `expr ${class_model}` -eq 0 ]; then
+  result=`find . -name "result.txt"`
+  for file in ${result}
+    do
+      cat ${file}
+    done
+  echo "success!"
+else
+  result=`find . -name "result.txt"`
+  for file in ${result}
+    do
+      cat ${file}
+    done
+  echo "error!"
+  exit 8
+fi
+
+
 
 cd test_det_model
 rm -rf ./result.txt
-echo "[class model inference cases result]" >> result.txt
+echo "[Detection model inference cases result]" >> result.txt
 bash ./run.sh
+class_model=$?
+echo ${class_model}
 cat ./result.txt
+# result
+echo "=============== result ================="
+if [ `expr ${class_model}` -eq 0 ]; then
+  result=`find . -name "result.txt"`
+  for file in ${result}
+    do
+      cat ${file}
+    done
+  echo "success!"
+else
+  result=`find . -name "result.txt"`
+  for file in ${result}
+    do
+      cat ${file}
+    done
+  echo "error!"
+  exit 8
+fi
