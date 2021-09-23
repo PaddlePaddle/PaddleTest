abc11ddb2705b616cc94399fb163fb74110dab18
Pcjmmc <544546992@qq.com>
2021-09-23 12:45:58 +0800

    fix NLP GPT-2 case bug

--- models/PaddleNLP/CE/scripts/GPT-2/linux_py37.sh
+++ models/PaddleNLP/CE/scripts/GPT-2/linux_py37.sh
@@ -5,4 +5,4 @@ cur_path=`pwd`
 code_path=$cur_path/../../models_repo/examples/language_model/data_tools/
 cd $code_path
 sed -i 's/python3 -m pybind11/python3.7 -m pybind11/g'  Makefile
-sed -i 's/python3-config --extension-suffix/shell python3.7m-config --extension-suffix/g' Makefile
+sed -i 's/python3-config --extension-suffix/python3.7m-config --extension-suffix/g' Makefile
