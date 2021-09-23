4f24a4b30fd3e0dbe0c35b0ea7e2f6b4282dd34b
ZhangYulongg <1272816783@qq.com>
2021-09-23 11:34:38 +0800

    add pcpvt_base

--- inference/python_api_test/test_class_model/run.sh
+++ inference/python_api_test/test_class_model/run.sh
@@ -1,5 +1,5 @@
 export FLAGS_call_stack_level=2
-cases="./test_resnet50.py ./test_resnet50_slim.py ./test_tnt_small.py"
+cases="./test_resnet50.py ./test_resnet50_slim.py ./test_tnt_small.py ./test_pcpvt_base.py"
 ignore=""
 bug=0
 
