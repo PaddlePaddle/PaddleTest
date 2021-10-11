5e9318cecef6c7f3cfe969f9a741690b3bf707c5
SmirnovKol <1945340016@qq.com>
2021-10-11 13:34:11 +0800

    add pytest of paddle.nn.CosineSimilarity

--- framework/api/nn/test_cosine_similarity.py
+++ framework/api/nn/test_cosine_similarity.py
@@ -45,7 +45,7 @@ def compute_cosine_similarity(x1, x2, axis=1, eps=1e-8):
 @pytest.mark.api_nn_CosineSimilarity_vartype
 def test_cosine_similarity_base():
     """
-    base
+    base, check int data type of input
     """
     x1 = randtool("int", -10, 10, [5, 10])
     x2 = randtool("int", -10, 10, [5, 10])
@@ -81,7 +81,8 @@ def test_cosine_similarity():
 @pytest.mark.api_nn_CosineSimilarity_parameters
 def test_cosine_similarity2():
     """
-    modify value range
+    modify value range, the elements of input tensor have
+    value range [-10000, 10000]
     """
     x1 = randtool("float", -10000, 10000, [5, 10])
     x2 = randtool("float", -10000, 10000, [5, 10])
@@ -141,6 +142,43 @@ def test_cosine_similarity5():
     x2 = randtool("float", -10, 10, [5, 10])
     axis = 1
     eps = 1e-4
+    expected_result = compute_cosine_similarity(x1, x2, axis=axis, eps=eps)
+
+    x1 = paddle.to_tensor(x1)
+    x2 = paddle.to_tensor(x2)
+    result = paddle.nn.CosineSimilarity(axis=axis, eps=eps)(x1, x2)
+
+    compare(result, expected_result)
+
+
+@pytest.mark.api_nn_CosineSimilarity_parameters
+def test_cosine_similarity6():
+    """
+    axis takes minus value
+    """
+    x1 = randtool("float", -10, 10, [5, 6, 7, 8, 10])
+    x2 = randtool("float", -10, 10, [5, 6, 7, 8, 10])
+    axis = -2
+    eps = 1e-8
+    expected_result = compute_cosine_similarity(x1, x2, axis=axis, eps=eps)
+
+    x1 = paddle.to_tensor(x1)
+    x2 = paddle.to_tensor(x2)
+    result = paddle.nn.CosineSimilarity(axis=axis, eps=eps)(x1, x2)
+
+    compare(result, expected_result)
+
+
+@pytest.mark.api_nn_CosineSimilarity_parameters
+def test_cosine_similarity7():
+    """
+    the two input tensors don't have the same shape,
+    but are broadcastable and don't have the same data type
+    """
+    x1 = randtool("float", -10, 10, [5, 6, 7, 8, 10])
+    x2 = randtool("int", -10, 10, [7, 8, 10])
+    axis = -1
+    eps = 1e-8
     expected_result = compute_cosine_similarity(x1, x2, axis=axis, eps=eps)
 
     x1 = paddle.to_tensor(x1)
