2b1c6d7d641d031c647436c51aa200487264cfc0
songqi <gsq7474741@icloud.com>
2021-10-11 14:18:19 +0800

    add cos_sim test cases v4

--- framework/api/nn/test_cosinesimilarity.py
+++ framework/api/nn/test_cosinesimilarity.py
@@ -21,7 +21,7 @@ class TestCosineSimilarity(APIBase):
         """
         implement
         """
-        self.types = [np.float32, np.float64]
+        self.types = [np.int8, np.float32, np.float64]
         # self.enable_backward=False
         # self.debug = True
         # self.static = True
@@ -75,7 +75,7 @@ def test_cosinesimilarity_base():
 def test_cosinesimilarity():
     """
     input shape
-    test on [3,8,2]
+    test on 3_D input
     """
     x1 = randtool("float", -10, 10, [3, 8, 2])
     x2 = randtool("float", -10, 10, [3, 8, 2])
@@ -106,10 +106,10 @@ def test_cosinesimilarity2():
     axis (int): Dimension of vectors to compute cosine similarity. Default is 1.
     test on -1, 0, 1
     """
-    x1 = randtool("float", -10, 10, [1, 5])
-    x2 = randtool("float", -10, 10, [1, 5])
+    x1 = randtool("float", -10, 10, [1, 5, 2])
+    x2 = randtool("float", -10, 10, [1, 5, 2])
 
-    axis_list = [-1, 0, 1]
+    axis_list = [-2, -1, 0, 1, 2]
     eps = 1e-8
 
     for axis in axis_list:
@@ -164,35 +164,55 @@ def test_cosinesimilarity3():
 def test_cosinesimilarity4():
     """
     input value
-    test on case 1, 2
-    case 1:
-    x1 = [[-502.97974512, -100.0491579, -178.11839401, -479.40061823, 740.79137669]]
-    x2 = [[-629.92014568, -960.67714914, 906.50406301, 360.90160946, -26.8237469]]
-    expect result = [0.03752203]
-    case 2:
-    x1 = [[-50.29797451 -10.00491579 -17.8118394  -47.94006182  74.07913767]]
-    x2 = [[-62.99201457 -96.06771491  90.6504063   36.09016095  -2.68237469]]
-    expect result = [0.03752203]
+    test on different shape
     """
-    x1_list = [
-        [[-502.97974512, -100.0491579, -178.11839401, -479.40061823, 740.79137669]],
-        [[-50.29797451, -10.00491579, -17.8118394, -47.94006182, 74.07913767]],
-    ]
-
-    x2_list = [
-        [[-629.92014568, -960.67714914, 906.50406301, 360.90160946, -26.8237469]],
-        [[-62.99201457, -96.06771491, 90.6504063, 36.09016095, -2.68237469]],
-    ]
-    res_list = [[0.03752203], [0.03752203]]
+    x1 = randtool("float", -100, 100, [5, 1, 2, 3, 4])
+    x2 = randtool("float", -100, 100, [2, 3, 4])
 
     axis = 1
     eps = 1e-8
 
-    for i in range(res_list.__len__()):
-        x1 = paddle.to_tensor(x1_list[i])
-        x2 = paddle.to_tensor(x2_list[i])
+    w12 = np.sum(np.multiply(x1, x2), axis=axis)
+    w1 = np.sum(np.multiply(x1, x1), axis=axis)
+    w2 = np.sum(np.multiply(x2, x2), axis=axis)
+    n12 = np.sqrt(np.clip(w1 * w2, a_min=eps * eps, a_max=None))
+    cos_sim = w12 / n12
 
-        cos_sim_func = paddle.nn.CosineSimilarity(axis=axis, eps=eps)
-        paddle_res = cos_sim_func(x1, x2).numpy()
+    res = cos_sim
 
-        compare(paddle_res, res_list[i])
+    x1_tensor = paddle.to_tensor(x1)
+    x2_tensor = paddle.to_tensor(x2)
+
+    cos_sim_func = paddle.nn.CosineSimilarity(axis=axis, eps=eps)
+    paddle_res = cos_sim_func(x1_tensor, x2_tensor).numpy()
+
+    compare(paddle_res, res)
+
+
+@pytest.mark.api_nn_CosineSimilarity_parameters
+def test_cosinesimilarity5():
+    """
+    input value
+    test on different shape and axis set at 0
+    """
+    x1 = randtool("float", -100, 100, [5, 1, 3])
+    x2 = randtool("float", -100, 100, [1, 3])
+
+    axis = 0
+    eps = 1e-8
+
+    w12 = np.sum(np.multiply(x1, x2), axis=axis)
+    w1 = np.sum(np.multiply(x1, x1), axis=axis)
+    w2 = np.sum(np.multiply(x2, x2), axis=axis)
+    n12 = np.sqrt(np.clip(w1 * w2, a_min=eps * eps, a_max=None))
+    cos_sim = w12 / n12
+
+    res = cos_sim
+
+    x1_tensor = paddle.to_tensor(x1)
+    x2_tensor = paddle.to_tensor(x2)
+
+    cos_sim_func = paddle.nn.CosineSimilarity(axis=axis, eps=eps)
+    paddle_res = cos_sim_func(x1_tensor, x2_tensor).numpy()
+
+    compare(paddle_res, res)
