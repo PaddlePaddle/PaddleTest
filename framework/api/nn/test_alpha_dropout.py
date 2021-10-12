974e64441bd9b8c16a358ae0027351e61495d45e
spade <spadeaiverxin@163.com>
2021-10-12 13:15:45 +0800

    fix test bug

--- framework/api/nn/test_alpha_dropout.py
+++ framework/api/nn/test_alpha_dropout.py
@@ -5,14 +5,14 @@
     test AlphaDropout
 """
 import logging
-
 from paddle.fluid import layers
-
 from apibase import APIBase, compare, compare_grad
 from apibase import randtool
 import paddle
 import pytest
 import numpy as np
+
+RANDOM_SEED = 100
 
 
 class TestAlphaDropout(APIBase):
@@ -22,7 +22,7 @@ class TestAlphaDropout(APIBase):
 
     def hook(self):
         self.types = [np.float32, np.float64]
-        self.seed = 100
+        self.seed = RANDOM_SEED
         self.enable_backward = False
 
     # 重写base run，确保动态图和静态图的随机数产出相同
@@ -146,11 +146,151 @@ class TestAlphaDropout(APIBase):
                     no_grad_var=self.no_grad_var,
                 )
 
+    def run(self, res, data=None, **kwargs):
+        """
+        run
+        Args:
+            res: expect result
+            **kwargs: kwargs
+
+        Returns:
+            Assertion
+        """
+        # 取默认type
+        if self.dtype is None:
+            if np.float64 in self.types:
+                self.dtype = np.float64
+            else:
+                self.dtype = self.types[0]
+        if self.debug:
+            for place in self.places:
+                self.place = place
+                logging.info("[Place] is ===============================>>>>>>>>" + str(self.place))
+                # start run paddle dygraph
+                if self.dygraph:
+                    paddle.disable_static(self.place)
+                    if str(self.place) == "CPUPlace":
+                        paddle.set_device("cpu")
+                    else:
+                        paddle.set_device("gpu:0")
+                    logging.info("[start] run " + self.__class__.__name__ + " dygraph")
+                    paddle.seed(self.seed)
+                    self._check_params(res, data, **kwargs)
+                    dygraph_forward_res = self._dygraph_forward()
+                    logging.info("dygraph forward result is :")
+                    if isinstance(dygraph_forward_res, (list)):
+                        compare(dygraph_forward_res, res, self.delta, self.rtol)
+                        logging.info(dygraph_forward_res)
+                    else:
+                        compare(dygraph_forward_res.numpy(), res, self.delta, self.rtol)
+                        logging.info(dygraph_forward_res.numpy())
+                    if self.enable_backward:
+                        dygraph_backward_res = self._dygraph_backward(dygraph_forward_res)
+                        logging.info("[dygraph grad]")
+                        logging.info(dygraph_backward_res)
+                    paddle.enable_static()
+                if self.static:
+
+                    # start run paddle static
+                    logging.info("[start] run " + self.__class__.__name__ + " static")
+                    if self.enable_backward:
+                        static_forward_res, static_backward_res = self._static_forward(res, data, **kwargs)
+                        logging.info("static forward result is :")
+                        logging.info(static_forward_res)
+                        logging.info("[static grad]")
+                        logging.info(static_backward_res)
+                    else:
+                        static_forward_res = self._static_forward(res, data, **kwargs)
+                        logging.info("static forward result is :")
+                        logging.info(static_forward_res)
+                    compare(static_forward_res, res, self.delta, self.rtol)
+                    # start run torch
+                if self.enable_backward:
+                    paddle.seed(100)
+                    grad = self.compute_grad(res, data, **kwargs)
+                    logging.info("[numeric grad]")
+                    logging.info(grad)
+                    if self.static and self.dygraph:
+                        compare_grad(
+                            static_backward_res, dygraph_backward_res, mode="both", no_grad_var=self.no_grad_var
+                        )
+                    if self.dygraph:
+                        compare_grad(
+                            dygraph_backward_res,
+                            grad,
+                            mode="dygraph",
+                            delta=self.delta,
+                            rtol=self.rtol,
+                            no_grad_var=self.no_grad_var,
+                        )
+                    if self.static:
+                        compare_grad(
+                            static_backward_res,
+                            grad,
+                            mode="static",
+                            delta=self.delta,
+                            rtol=self.rtol,
+                            no_grad_var=self.no_grad_var,
+                        )
+        else:
+            for place in self.places:
+                self.place = place
+                paddle.disable_static(self.place)
+                if str(self.place) == "CPUPlace":
+                    paddle.set_device("cpu")
+                else:
+                    paddle.set_device("gpu:0")
+                logging.info("[Place] is ===============================>>>>>>>>" + str(self.place))
+                # start run paddle dygraph
+                logging.info("[start] run " + self.__class__.__name__ + " dygraph")
+                paddle.disable_static(self.place)
+                paddle.seed(self.seed)
+                self._check_params(res, data, **kwargs)
+                dygraph_forward_res = self._dygraph_forward()
+                if isinstance(dygraph_forward_res, (list)):
+                    compare(dygraph_forward_res, res, self.delta, self.rtol)
+                else:
+                    compare(dygraph_forward_res.numpy(), res, self.delta, self.rtol)
+                if self.enable_backward:
+                    dygraph_backward_res = self._dygraph_backward(dygraph_forward_res)
+                paddle.enable_static()
+                # start run paddle static
+                paddle.seed(100)
+                logging.info("[start] run " + self.__class__.__name__ + " static")
+                if self.enable_backward:
+                    static_forward_res, static_backward_res = self._static_forward(res, data, **kwargs)
+                else:
+                    static_forward_res = self._static_forward(res, data, **kwargs)
+                compare(static_forward_res, res, self.delta, self.rtol)
+                # start run torch
+                if self.enable_backward:
+                    grad = self.compute_grad(res, data, **kwargs)
+                    compare_grad(static_backward_res, dygraph_backward_res, mode="both", no_grad_var=self.no_grad_var)
+                    compare_grad(
+                        dygraph_backward_res,
+                        grad,
+                        mode="dygraph",
+                        delta=self.delta,
+                        rtol=self.rtol,
+                        no_grad_var=self.no_grad_var,
+                    )
+                    compare_grad(
+                        static_backward_res,
+                        grad,
+                        mode="static",
+                        delta=self.delta,
+                        rtol=self.rtol,
+                        no_grad_var=self.no_grad_var,
+                    )
+
 
 obj = TestAlphaDropout(paddle.nn.AlphaDropout)
+paddle.seed(RANDOM_SEED)
+np_random_tensor = layers.uniform_random([2, 3], dtype="float32", min=0.0, max=1.0)
+np_random_tensor = np_random_tensor.numpy()
 
 
-def numpy_alpha_dropout(x, p, training=True):
+def numpy_alpha_dropout(x, p, random_tensor, training=True):
     """
     numpy version alpha dropout
     """
@@ -171,8 +311,7 @@ def numpy_alpha_dropout(x, p, training=True):
 
         dtype = x.dtype
         input_shape = x.shape
-        random_tensor = layers.uniform_random(input_shape, dtype="float32", min=0.0, max=1.0)
-        random_tensor = random_tensor.numpy()
+
         p = np.ones(input_shape, dtype="float32") * p
         keep_mask = np.greater_equal(random_tensor, p)
         keep_mask = keep_mask.astype(dtype)
@@ -194,7 +333,7 @@ def test_alpha_dropout_base():
     x = randtool("float", 0, 2, [2, 3])
     p = 0.5
     paddle.seed(100)
-    res = numpy_alpha_dropout(x, p)
+    res = numpy_alpha_dropout(x, p, random_tensor=np_random_tensor)
     obj.base(res, data=x)
 
 
@@ -206,12 +345,8 @@ def test_alpha_dropout1():
     x = randtool("float", 0, 2, [2, 3])
     paddle.seed(100)
     p = 0.5  # defult is 0.5
-    res = numpy_alpha_dropout(x, p)
-
-    paddle.seed(100)
-    func = paddle.nn.AlphaDropout()
-    paddle_res = func(paddle.to_tensor(x))
-    compare(res, paddle_res)
+    res = numpy_alpha_dropout(x, p, random_tensor=np_random_tensor)
+    obj.run(res, x)
 
 
 @pytest.mark.api_nn_AlphaDropout_parameters
@@ -222,11 +357,8 @@ def test_alpha_dropout2():
     x = randtool("float", 0, 2, [2, 3])
     paddle.seed(100)
     p = 1.0  # defult is 0.5
-    res = numpy_alpha_dropout(x, p)
-    func = paddle.nn.AlphaDropout(p=p)
-    paddle.seed(100)
-    paddle_res = func(paddle.to_tensor(res))
-    compare(res, paddle_res)
+    res = numpy_alpha_dropout(x, p, random_tensor=np_random_tensor)
+    obj.run(res, x)
 
 
 @pytest.mark.api_nn_AlphaDropout_parameters
@@ -237,35 +369,32 @@ def test_alpha_dropout2():
     x = randtool("float", 0, 2, [2, 3])
     paddle.seed(100)
     p = 0.0  # defult is 0.5
-    res = numpy_alpha_dropout(x, p)
-    func = paddle.nn.AlphaDropout(p=p)
-    paddle.seed(100)
-    paddle_res = func(paddle.to_tensor(res))
-    compare(res, paddle_res)
+    res = numpy_alpha_dropout(x, p, random_tensor=np_random_tensor)
+    obj.run(res, x)
 
 
-# @pytest.mark.api_nn_AlphaDropout_parameters
-# def test_alpha_dropout2():
-#     """
-#     p = -1
-#     """
-#     x = randtool("float", 0, 2, [2, 3])
-#     obj.exception(etype=ValueError, mode="python", data=x, p=-1)
+@pytest.mark.api_nn_AlphaDropout_parameters
+def test_alpha_dropout2():
+    """
+    p = -1
+    """
+    x = randtool("float", 0, 2, [2, 3])
+    obj.exception(etype=ValueError, mode="python", data=x, p=-1)
 
 
-# @pytest.mark.api_nn_AlphaDropout_parameters
-# def test_alpha_dropout2():
-#     """
-#     p = 2, 使用exception接口
-#     """
-#     x = randtool("float", 0, 2, [2, 3])
-#     obj.exception(etype=ValueError, mode="python", data=x, p=-2)
-#
-#
-# @pytest.mark.api_nn_AlphaDropout_parameters
-# def test_alpha_dropout2():
-#     """
-#     p = '1'
-#     """
-#     x = randtool("float", 0, 2, [2, 3])
-#     obj.exception(etype=TypeError, mode="python", data=x, p="1")
+@pytest.mark.api_nn_AlphaDropout_parameters
+def test_alpha_dropout2():
+    """
+    p = 2, 使用exception接口
+    """
+    x = randtool("float", 0, 2, [2, 3])
+    obj.exception(etype=ValueError, mode="python", data=x, p=-2)
+
+
+@pytest.mark.api_nn_AlphaDropout_parameters
+def test_alpha_dropout2():
+    """
+    p = '1'
+    """
+    x = randtool("float", 0, 2, [2, 3])
+    obj.exception(etype=TypeError, mode="python", data=x, p="1")
