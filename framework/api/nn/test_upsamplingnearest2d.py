d7dce4d6dd3715fa49c87d2db6d75b500f972ebc
Nemo <85602470+GeYuYao-hub@users.noreply.github.com>
2021-10-16 15:38:37 +0800

    Update test_upsamplingnearest2d.py

--- framework/api/nn/test_upsamplingnearest2d.py
+++ framework/api/nn/test_upsamplingnearest2d.py
@@ -46,7 +46,8 @@ def upsample_2d(img, scale_factor=None, size=None, data_format="NCHW"):
         if (
             str(type(scale_factor)) == "<class 'tuple'>" 
             or str(type(scale_factor)) == "<class 'list'>" 
-            or str(type(scale_factor)) == "<class 'numpy.ndarray'>)"):
+            or str(type(scale_factor)) == "<class 'numpy.ndarray'>)"
+            ):
             size = [h_in * scale_factor[0], w_in * scale_factor[1]]
         else:
             size = [h_in * scale_factor, w_in * scale_factor]
