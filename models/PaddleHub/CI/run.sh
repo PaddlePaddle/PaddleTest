a5d0ff419f57aced928cb2319c81b15a9905d686
Zeref996 <825276847@qq.com>
2021-10-18 17:42:12 +0800

    fix file run.sh, run_mode=model

--- models/PaddleHub/CI/run.sh
+++ models/PaddleHub/CI/run.sh
@@ -3,12 +3,12 @@
 cur_path=`pwd`
 PY_CMD=$1
 
-for module in $(cat CI_model_list.txt):
+for module in $(cat CI_model_list.txt)
 do
 $PY_CMD test_${module}.py 2>&1 | tee -a results.log
 done
 
-num=`cat $cur_path/results.log | grep "failed" | wc -l`
+num=`cat $cur_path/results.log | grep "Error" | wc -l`
 if [ "${num}" -gt "0" ];then
 exit 1
 else
