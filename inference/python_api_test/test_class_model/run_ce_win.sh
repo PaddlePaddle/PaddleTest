d6afc21de550e54fd1c8c954937914a0fa2519de
ZhangYulongg <1272816783@qq.com>
2021-10-25 19:43:02 +0800

    add windows CE shell

---
+++ inference/python_api_test/test_class_model/run_ce_win.sh
@@ -0,0 +1,25 @@
+export FLAGS_call_stack_level=2
+cases=`find . -name "test*.py" | sort`
+ignore="test_swin_transformer_gpu.py \
+        test_swin_transformer_trt_fp16.py \
+        test_swin_transformer_trt_fp32.py
+        "
+bug=0
+
+echo "============ failed cases =============" >> result.txt
+for file in ${cases}
+do
+    echo ${file}
+    if [[ ${ignore} =~ ${file##*/} ]]; then
+        echo "跳过"
+    else
+        python -m pytest -m jetson --disable-warnings -v ${file}
+        if [ $? -ne 0 ]; then
+            echo ${file} >> result.txt
+            bug=`expr ${bug} + 1`
+        fi
+    fi
+done
+
+echo "total bugs: "${bug} >> result.txt
+exit ${bug}
