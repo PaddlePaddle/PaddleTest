7532b92ec24bb10aedf97ecc7c13ad9c445f67d4
quanxiang-liu <qxliunj@163.com>
2021-10-28 20:37:31 +0800

    change clas p0 ce case yml,run_mode=model

--- models/PaddleClas/CE/linux/scripts/ResNet50_vd/train_2card.sh
+++ models/PaddleClas/CE/linux/scripts/ResNet50_vd/train_2card.sh
@@ -7,5 +7,12 @@ rm -rf dataset
 ln -s ${Data_path} dataset
 mkdir log
 python -m pip install -r requirements.txt
-python -m paddle.distributed.launch tools/train.py -c ppcls/configs/ImageNet/ResNet/ResNet50_vd.yaml  -o Global.epochs=2 -o Global.eval_during_train=False -o DataLoader.Train.sampler.shuffle=False > log/resnet50_vd_2card.log 2>&1
-cat log/resnet50_vd_2card.log | grep Avg | grep 'Epoch 2/2' > ../log/resnet50_vd_2card.log
+python -m paddle.distributed.launch tools/train.py -c ppcls/configs/ImageNet/ResNet/ResNet50_vd.yaml  \
+    -o Global.epochs=5  \
+    -o Global.seed=1234 \
+    -o DataLoader.Train.loader.num_workers=0 \
+    -o DataLoader.Train.sampler.shuffle=False  \
+    -o Global.eval_during_train=False  \
+    -o Global.save_interval=5 \
+    -o DataLoader.Train.sampler.batch_size=4 > log/resnet50_vd_2card.log 2>&1
+cat log/resnet50_vd_2card.log | grep Avg | grep 'Epoch 5/5' > ../log/resnet50_vd_2card.log
--- models/PaddleClas/CE/linux/scripts/ViT_large_patch16_384/train_1card.sh
+++ models/PaddleClas/CE/linux/scripts/ViT_large_patch16_384/train_1card.sh
@@ -1,12 +1,13 @@
 export FLAGS_cudnn_deterministic=True
 cd ${Project_path}
-sed -i 's/RandCropImage/ResizeImage/g' ppcls/configs/ImageNet/VisionTransformer/ViT_large_patch16_384.yaml
-sed -ie '/RandFlipImage/d' ppcls/configs/ImageNet/VisionTransformer/ViT_large_patch16_384.yaml
-sed -ie '/flip_code/d' ppcls/configs/ImageNet/VisionTransformer/ViT_large_patch16_384.yaml
+sed -i 's/RandCropImage/ResizeImage/g' ppcls/configs/ImageNet/VisionTransformer/ViT_small_patch16_224.yaml
+sed -ie '/RandFlipImage/d' ppcls/configs/ImageNet/VisionTransformer/ViT_small_patch16_224.yaml
+sed -ie '/flip_code/d' ppcls/configs/ImageNet/VisionTransformer/ViT_small_patch16_224.yaml
+sed -i 's/learning_rate:/learning_rate: 0.0001 #/g' ppcls/configs/ImageNet/VisionTransformer/ViT_small_patch16_224.yaml #将 学习率调低为0.0001
 
 rm -rf dataset
 ln -s ${Data_path} dataset
 mkdir log
 python -m pip install -r requirements.txt
-python tools/train.py -c ppcls/configs/ImageNet/VisionTransformer/ViT_large_patch16_384.yaml -o Global.epochs=2 -o DataLoader.Train.sampler.shuffle=False -o DataLoader.Train.sampler.batch_size=4 -o DataLoader.Eval.sampler.batch_size=4 -o Optimizer.lr.learning_rate=0.001 > log/ViT_large_patch16_384_1card.log 2>&1
-cat log/ViT_large_patch16_384_1card.log | grep Train | grep Avg | grep 'Epoch 2/2' > ../log/ViT_large_patch16_384_1card.log
+python tools/train.py -c ppcls/configs/ImageNet/VisionTransformer/ViT_small_patch16_224.yaml -o Global.epochs=2 -o DataLoader.Train.sampler.shuffle=False -o DataLoader.Train.sampler.batch_size=4 -o DataLoader.Eval.sampler.batch_size=4 -o Optimizer.lr.learning_rate=0.001 > log/ViT_small_patch16_224_1card.log 2>&1
+cat log/ViT_small_patch16_224_1card.log | grep Train | grep Avg | grep 'Epoch 2/2' > ../log/ViT_small_patch16_224_1card.log
