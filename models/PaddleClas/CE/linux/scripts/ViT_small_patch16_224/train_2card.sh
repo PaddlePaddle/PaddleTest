e721bbe4bc6adec630b4e18a0d2d03df8c27a374
quanxiang-liu <qxliunj@163.com>
2021-11-01 21:27:25 +0800

    change & add clase ce case,run_mode=model

--- models/PaddleClas/CE/linux/scripts/ViT_small_patch16_224/train_2card.sh
+++ models/PaddleClas/CE/linux/scripts/ViT_small_patch16_224/train_2card.sh
@@ -1,20 +1,44 @@
 export FLAGS_cudnn_deterministic=True
 cd ${Project_path}
-sed -i 's/RandCropImage/ResizeImage/g'  ppcls/configs/ImageNet/VisionTransformer/ViT_small_patch16_224.yaml
-sed -ie '/RandFlipImage/d'  ppcls/configs/ImageNet/VisionTransformer/ViT_small_patch16_224.yaml
-sed -ie '/flip_code/d'  ppcls/configs/ImageNet/VisionTransformer/ViT_small_patch16_224.yaml
-sed -i 's/learning_rate:/learning_rate: 0.0001 #/g' ppcls/configs/ImageNet/VisionTransformer/ViT_small_patch16_224.yaml #将 学习率调低为0.0001
+
+line=ppcls/configs/ImageNet/VisionTransformer/ViT_small_patch16_224.yaml
+filename=${line##*/}
+#echo $filename
+model=${filename%.*}
+echo $model
+
+sed -i 's/RandCropImage/ResizeImage/g' $line
+sed -ie '/RandFlipImage/d' $line
+sed -ie '/flip_code/d' $line
+sed -i 's/learning_rate:/learning_rate: 0.0001 #/g' $line #将 学习率调低为0.0001
+
+if [ -d "output" ]; then
+   rm -rf output
+else
+   python -m pip install -r requirements.txt
+fi
 
 rm -rf dataset
 ln -s ${Data_path} dataset
 mkdir log
-python -m pip install -r requirements.txt
-python -m paddle.distributed.launch tools/train.py -c ppcls/configs/ImageNet/VisionTransformer/ViT_small_patch16_224.yaml  \
+python -m paddle.distributed.launch tools/train.py -c $line  \
     -o Global.epochs=5  \
     -o Global.seed=1234 \
+    -o Global.output_dir=output \
     -o DataLoader.Train.loader.num_workers=0 \
     -o DataLoader.Train.sampler.shuffle=False  \
     -o Global.eval_during_train=False  \
     -o Global.save_interval=5 \
-    -o DataLoader.Train.sampler.batch_size=4 > log/ViT_small_patch16_224_2card.log 2>&1
-cat log/ViT_small_patch16_224_1card.log | grep Train | grep Avg | grep 'Epoch 5/5' > ../log/ViT_small_patch16_224_2card.log
+    -o DataLoader.Train.sampler.batch_size=4 > log/${model}_2card.log 2>&1
+cat log/${model}_2card.log | grep Train | grep Avg | grep 'Epoch 5/5' > ../log/${model}_2card.log
+
+params_dir=$(ls output)
+echo "######  params_dir"
+echo $params_dir
+
+if [ -f "output/$params_dir/latest.pdparams" ];then
+   echo -e "\033[33m training of ${model}  successfully!\033[0m"
+else
+   cat log/${model}_2card.log
+   echo -e "\033[31m training of ${model} failed!\033[0m"
+fi
