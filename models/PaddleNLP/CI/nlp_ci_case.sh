3610a3aab57c9cc65907e16ca69e47ef7c3e3c62
zjjlivein <1834792141@qq.com>
2021-11-15 11:30:17 +0800

    save log2bos run_mode=model

--- models/PaddleNLP/CI/nlp_ci_case.sh
+++ models/PaddleNLP/CI/nlp_ci_case.sh
@@ -924,7 +924,11 @@ for p0case in ${P0case_list[*]};do
 done
 echo -e "\033[35m ---- end run P0case  \033[0m"
 
-cd ${nlp_dir}/logs
+cd ${nlp_dir}
+wget https://paddle-qa.bj.bcebos.com/paddlenlp/bos_conf.py
+wget https://paddle-qa.bj.bcebos.com/paddlenlp/upload.py
+python upload.py logs
+cd logs
 FF=`ls *_FAIL*|wc -l`
 if [ "${FF}" -gt "0" ];then
     exit 1
