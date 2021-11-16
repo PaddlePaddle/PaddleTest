133a3b2d26482b5474f481ef639aa3de3e8cb288
quanxiang-liu <qxliunj@163.com>
2021-11-16 20:07:35 +0800

    fix win ce case bat6 & win yaml&run_mode=model

--- models/PaddleClas/CE/scripts/grep_win_log.bat
+++ models/PaddleClas/CE/scripts/grep_win_log.bat
@@ -19,33 +19,33 @@ echo "train_win_gpu"|findstr %2 >nul
 if !errorlevel! equ 0 (
     echo '#####train_win_gpu'
     type %log_path%\!model!_train.log | findstr 5/5 | findstr Avg
-    type %log_path%\!model!_train.log | findstr 5/5 | findstr Avg > ../%log_path%\!model!_train.log
+    type %log_path%\!model!_train.log | findstr 5/5 | findstr Avg > ..\%log_path%\!model!_train.log
 )
 
 echo "eval_win"|findstr %2 >nul
 if !errorlevel! equ 0 (
     echo '#####eval_win'
     type %log_path%\!model!_train.log | findstr 5/5 | findstr Avg
-    type %log_path%\!model!_train.log | findstr 5/5 | findstr Avg > ../%log_path%\!model!_eval.log
+    type %log_path%\!model!_train.log | findstr 5/5 | findstr Avg > ..\%log_path%\!model!_eval.log
 )
 
 echo "infer_win"|findstr %2 >nul
 if !errorlevel! equ 0 (
     echo '#####infer_win'
     type %log_path%\!model!_infer.log | findstr infer_exit_code
-    type %log_path%\!model!_infer.log | findstr infer_exit_code > ../%log_path%\!model!_infer.log
+    type %log_path%\!model!_infer.log | findstr infer_exit_code > ..\%log_path%\!model!_infer.log
 )
 
 echo "export_win"|findstr %2 >nul
 if !errorlevel! equ 0 (
     echo '#####export_win'
     type %log_path%\!model!_export.log | findstr export_exit_code
-    type %log_path%\!model!_export.log | findstr export_exit_code > ../%log_path%\!model!_export.log
+    type %log_path%\!model!_export.log | findstr export_exit_code > ..\%log_path%\!model!_export.log
 )
 
 echo "predict_win"|findstr %2 >nul
 if !errorlevel! equ 0 (
     echo '#####predict_win'
     type %log_path%\!model!_predict.log | findstr predict_exit_code
-    type %log_path%\!model!_predict.log | findstr predict_exit_code > ../%log_path%\!model!_predict.log
+    type %log_path%\!model!_predict.log | findstr predict_exit_code > ..\%log_path%\!model!_predict.log
 )
