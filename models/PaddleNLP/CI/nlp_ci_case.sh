4127c2696a7b596ac3f3e5288945a3112db45440
zhang junjun <1834792141@qq.com>
2021-11-19 18:52:31 +0800

    fix bos (#360)
    * fix bos run_mode=model
    
    * fix bos run_mode=model
    
    * fix bos run_mode=model
    
    * fix bos run_mode=model

--- models/PaddleNLP/CI/nlp_ci_case.sh
+++ models/PaddleNLP/CI/nlp_ci_case.sh
@@ -924,11 +924,10 @@ for p0case in ${P0case_list[*]};do
 done
 echo -e "\033[35m ---- end run P0case  \033[0m"
 
-cd ${nlp_dir}
+cd ${nlp_dir}/
+cp -r /ssd1/paddlenlp/bos/* ./
 tar -zcvf logs.tar logs/
 mkdir upload && mv logs.tar upload
-wget https://paddle-qa.bj.bcebos.com/paddlenlp/bos_conf.py
-wget https://paddle-qa.bj.bcebos.com/paddlenlp/upload.py
 python upload.py upload
 cd logs
 FF=`ls *_FAIL*|wc -l`
