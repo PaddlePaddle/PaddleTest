362810c8dc325a03dd7c22fe6c3977a166bf6be3
pollyyan <602478544@qq.com>
2021-11-29 21:13:30 +0800

    add ocr win mac CE & add ocr CI unique & add t2s CI/CE & test=model

---
+++ models/PaddleSpeech_t2s/CE/linux/scripts/speedyspeech/baker/train_1card.sh
@@ -0,0 +1,45 @@
+export FLAGS_cudnn_deterministic=True
+cd ${Project_path}
+
+rm -rf ~/datasets
+ln -s ${Data_path}/train_data ~/datasets
+if [ ! -d "../log" ]; then
+  mkdir ../log
+fi
+python -m pip install --ignore-installed -r requirements.txt
+
+cd examples/csmsc/tts2
+source path.sh
+source ${MAIN_ROOT}/utils/parse_options.sh
+conf_path=conf/default.yaml
+train_output_path=exp/default
+# data preprocess
+if [ ! -f "baker_alignment_tone.tar.gz" ]; then
+  wget https://paddlespeech.bj.bcebos.com/MFA/BZNSYP/with_tone/baker_alignment_tone.tar.gz
+  tar xf baker_alignment_tone.tar.gz
+fi
+sed -i "s/python3/python/g" ./local/preprocess.sh
+rm -rf dump
+./local/preprocess.sh ${conf_path} > preprocess.log 2>&1
+if [ $? -eq 0 ];then
+   cat preprocess.log
+   echo -e "\033[33m data preprocess of speedyspeech successfully! \033[0m"
+else
+   cat preprocess.log
+   echo -e "\033[31m data preprocess of speedyspeech failed! \033[0m"
+fi
+
+# train
+sed -i "s/max_epoch: 300/max_epoch: 10/g" ${conf_path}
+sed -i "s/python3/python/g" ./local/train.sh
+rm -rf exp
+./local/train.sh ${conf_path} ${train_output_path} > train_1card.log 2>&1
+if [ $? -eq 0 ];then
+   cat train_1card.log
+   echo -e "\033[33m training_1card of speedyspeech successfully! \033[0m"
+else
+   cat train_1card.log
+   echo -e "\033[31m training_1card of speedyspeech failed! \033[0m"
+fi
+sed -i "s/max_epoch:10/max_epoch: 300/g" ${conf_path}
+cat train_1card.log | grep "3060/3060" | awk 'BEGIN{FS=","} {print $6}' > ../../../../log/speedyspeech_baker_1card.log
