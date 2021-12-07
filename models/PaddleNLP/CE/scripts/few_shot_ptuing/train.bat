6adf1e200fcf74e1846eb149178248dab77598a2
Divano <dddivano@outlook.com>
2021-12-07 15:20:57 +0800

    Revert "升级NLP case ，test=model"

--- models/PaddleNLP/CE/scripts/few_shot_ptuing/train.bat
+++ models/PaddleNLP/CE/scripts/few_shot_ptuing/train.bat
@@ -10,7 +10,7 @@ cd models_repo\examples\few_shot\p-tuning\
 if not exist predict_output md predict_output
 
 if "%3"=="iflytek" (
-    python -u -m paddle.distributed.launch --gpus %2 ptuning.py --task_name %3 --device %1 --p_embedding_num 2 --save_dir "checkpoints/%3" --batch_size 4 --learning_rate 1E-4 --epochs 1 --save_steps 20 --max_seq_length 512 > %logpath%/train_%3_%1.log 2>&1
+    python -u -m paddle.distributed.launch --gpus %2 ptuning.py --task_name %3 --device %1 --p_embedding_num 1 --save_dir "checkpoints/%3" --batch_size 8 --learning_rate 1E-4 --epochs 1 --save_steps 20 --max_seq_length 512 > %logpath%/train_%3_%1.log 2>&1
 ) else if "%3"=="csldcp" (
     python -u -m paddle.distributed.launch --gpus %2 ptuning.py --task_name %3 --device %1 --p_embedding_num 1 --save_dir "checkpoints/%3" --batch_size 2 --learning_rate 1E-4 --epochs 1 --save_steps 20 --max_seq_length 512 > %logpath%/train_%3_%1.log 2>&1
 ) else (
