9684c2026d36224f969dbdd99dc83e2243c2e988
shjNT <sunhongjiang@baidu.com>
2021-12-09 14:39:24 +0800

    close and onehot

---
+++ framework/api/nn/test_functional_one_hot.py
@@ -0,0 +1,61 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+"""
+test_functional_one_hot
+"""
+from apibase import APIBase
+import paddle
+import pytest
+import numpy as np
+
+
+class TestFunctionalOneHot(APIBase):
+    """
+    test
+    """
+
+    def hook(self):
+        """
+        implement
+        """
+        self.types = [np.int32, np.int64]
+        # self.debug = True
+        # self.static = True
+        # enable check grad
+        self.enable_backward = False
+
+
+obj = TestFunctionalOneHot(paddle.nn.functional.one_hot)
+
+
+def cal_one_hot(x, num_classes):
+    """
+    calculate one_hot
+    """
+    res = []
+    for i in x.flatten():
+        tmp = [0] * num_classes
+        tmp[i] = 1
+        res.append(tmp)
+    return np.array(res).reshape(list(x.shape) + [num_classes])
+
+
+@pytest.mark.api_nn_one_hot_vartype
+def test_one_hot_base():
+    """
+    base
+    """
+    x_data = np.random.randint(0, 6, (2, 3))
+    res = cal_one_hot(x_data, 10)
+    obj.base(res=res, x=x_data, num_classes=10)
+
+
+@pytest.mark.api_nn_one_hot_parameters
+def test_one_hot0():
+    """
+    default
+    """
+    x_data = np.random.randint(0, 6, (2, 3, 4, 6))
+    res = cal_one_hot(x_data, 10)
+    obj.run(res=res, x=x_data, num_classes=10)
---
+++ framework/api/paddlebase/test_isclose.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+
+"""
+test_isclose
+"""
+
+from apibase import APIBase
+
+import paddle
+import pytest
+import numpy as np
+
+
+class TestIsClose(APIBase):
+    """
+    test isclose
+    """
+
+    def hook(self):
+        """
+        implement
+        """
+        self.types = [np.float32, np.float64]
+        # self.debug = True
+        # enable check grad
+        self.enable_backward = False
+
+
+obj = TestIsClose(paddle.isclose)
+
+
+@pytest.mark.api_base_isclose_vartype
+def test_isclose_base():
+    """
+    base
+    """
+    x = np.array([10000.0, 1e-07])
+    y = np.array([10000.1, 1e-08])
+    res = np.isclose(x, y)
+    obj.base(res=res, x=x, y=y)
+
+
+@pytest.mark.api_base_isclose_parameters
+def test_isclose0():
+    """
+    x, y: multiple dimension
+    """
+    x = np.random.rand(3, 4, 5) * 100
+    y = x + 0.001
+    res = np.isclose(x, y)
+    obj.run(res=res, x=x, y=y)
+
+
+@pytest.mark.api_base_isclose_parameters
+def test_isclose1():
+    """
+     equal_nan=True
+    """
+    x = np.array([10000.0, 1e-07, np.NAN, 1.0, 3.0, 0.0])
+    y = np.array([10000.01, 1e-06, np.NAN, np.NAN, 3.0, np.NAN])
+    res = np.isclose(x, y)
+    obj.run(res=res, x=x, y=y)
