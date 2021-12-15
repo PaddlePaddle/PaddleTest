f8a7144c07a4841a94ababa678ad33db19317368
Pcjmmc <544546992@qq.com>
2021-12-15 19:40:39 +0800

    新增NLP PP-MiniLM case， test=model

---
+++ models/PaddleNLP/CE/scripts/PP-MiniLM/infer.bat
@@ -0,0 +1,19 @@
+@echo off
+cd ../..
+
+if not exist log\PP-MiniLM md log\PP-MiniLM
+set logpath=%cd%\log\PP-MiniLM
+cd models_repo\examples\model_compression\pp-minilm\inference
+
+set NAME=%3
+
+for %%i in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do call set NAME=%%NAME:%%i=%%i%%
+
+python infer.py --task_name %NAME% --model_path  ../quantization/%NAME%_quant_models/mse4/int8  --int8 --collect_shape
+python infer.py --task_name %NAME%  --model_path  ../quantization/%NAME%_quant_models/mse4/int8  --int8 > %logpath%/infer_%3_%4_%5_%1.log 2>&1
+
+if %ERRORLEVEL% == 1 (
+    echo "exit_code: 1.0" >> %logpath%/infer_%3_%4_%5_%1.log
+) else (
+    echo "exit_code: 0.0" >> %logpath%/infer_%3_%4_%5_%1.log
+)
