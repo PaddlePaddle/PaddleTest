c3b0aa8b6af0a5de435ba497ee11f4e056c009b0
wangye707 <39162106+wangye707@users.noreply.github.com>
2021-12-22 18:47:50 +0800

    Update test_yolov3_trt_fp16.py

--- inference/python_api_test/test_det_model/test_yolov3_trt_fp16.py
+++ inference/python_api_test/test_det_model/test_yolov3_trt_fp16.py
@@ -143,7 +143,9 @@ def test_trt_fp16_more_bz():
 
         output_data_dict = {"save_infer_model/scale_0.tmp_1": scale_0, "save_infer_model/scale_1.tmp_1": scale_1}
         test_suite.load_config(model_file="./yolov3/model.pdmodel", params_file="./yolov3/model.pdiparams")
-        test_suite.trt_more_bz_test(input_data_dict, output_data_dict, repeat=1, delta=6e-2, precision="trt_fp16")
+        test_suite.trt_more_bz_test(
+            input_data_dict, output_data_dict, repeat=1, delta=6e-2, precision="trt_fp16",  result_sort=True
+        )
 
         del test_suite  # destroy class to save memory
 
@@ -196,7 +198,7 @@ def test_jetson_trt_fp16_more_bz():
         output_data_dict = {"save_infer_model/scale_0.tmp_1": scale_0, "save_infer_model/scale_1.tmp_1": scale_1}
         test_suite.load_config(model_file="./yolov3/model.pdmodel", params_file="./yolov3/model.pdiparams")
         test_suite.trt_more_bz_test(
-            input_data_dict, output_data_dict, repeat=1, max_batch_size=10, delta=6e-2, precision="trt_fp16"
+            input_data_dict, output_data_dict, repeat=1, max_batch_size=10, delta=6e-2, precision="trt_fp16",  result_sort=True
         )
 
         del test_suite  # destroy class to save memory
