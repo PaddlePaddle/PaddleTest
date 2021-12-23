9bcbb56e0981c12a2fc59503cb7bfbb314bba47e
shjNT <sunhongjiang@baidu.com>
2021-12-23 15:33:22 +0800

    fix randint_like

--- framework/api/paddlebase/test_randint_like.py
+++ framework/api/paddlebase/test_randint_like.py
@@ -12,124 +12,75 @@ import pytest
 import numpy as np
 
 
-class TestRandintLike(APIBase):
+def cal_randint_like(x, dtype="float32", low=0, high=None):
     """
-    test randint_like
+    calculate api
     """
+    x = x.astype(dtype)
+    xp = paddle.to_tensor(x)
+    dynamic_res = paddle.randint_like(xp, low=low, high=high)
 
-    def hook(self):
-        """
-        implement
-        """
-        self.types = [np.float32, np.float64, np.float16, np.float32, np.float64]
-        # self.debug = True
-        # enable check grad
-        self.enable_backward = False
+    paddle.enable_static()
+    main_program, startup_program = paddle.static.Program(), paddle.static.Program()
+    with paddle.utils.unique_name.guard():
+        with paddle.static.program_guard(main_program=main_program, startup_program=startup_program):
+            data0 = paddle.static.data(name="s0", shape=x.shape, dtype=dtype)
+            feed = {"s0": x}
+            out = paddle.randint_like(data0, low=low, high=high)
 
-
-obj = TestRandintLike(paddle.randint_like)
-
-
-@pytest.mark.api_nn_randint_like_vartype
-def test_randint_like_base():
-    """
-    base
-    """
-    x = np.zeros((4,))
-    res = np.array([3.0, 2.0, 2.0, -4.0])
-    obj.base(res=res, x=x, low=-4, high=4)
+            exe = paddle.static.Executor()
+            exe.run(startup_program)
+            static_res = exe.run(main_program, feed=feed, fetch_list=[out])
+    paddle.disable_static()
+    assert dynamic_res.numpy().shape == static_res[0].shape
+    return dynamic_res.numpy()
 
 
 @pytest.mark.api_nn_randint_like_parameters
 def test_randint_like0():
     """
-    default
+    base
     """
-    x = np.zeros((4, 5))
-    res = np.array(
-        [
-            [3.0, 2.0, 2.0, -4.0, 0.0],
-            [-4.0, -2.0, 3.0, 2.0, 1.0],
-            [-2.0, -2.0, 1.0, 1.0, 0.0],
-            [3.0, 0.0, -2.0, -1.0, -1.0],
-        ]
-    )
-    obj.run(res=res, x=x, low=-4, high=4)
+    x = np.zeros((4,))
+    res = cal_randint_like(x, low=-4, high=4)
+    assert res.shape == (4,)
 
 
 @pytest.mark.api_nn_randint_like_parameters
 def test_randint_like1():
     """
-    x: 3-d tensor
+    default
     """
-    x = np.zeros((2, 4, 5))
-    res = np.array(
-        [
-            [
-                [3.0, 2.0, 2.0, -4.0, 0.0],
-                [-4.0, -2.0, 3.0, 2.0, 1.0],
-                [-2.0, -2.0, 1.0, 1.0, 0.0],
-                [3.0, 0.0, -2.0, -1.0, -1.0],
-            ],
-            [
-                [-2.0, 1.0, 1.0, 2.0, -3.0],
-                [-4.0, -1.0, -4.0, 1.0, -3.0],
-                [-4.0, 3.0, -4.0, 1.0, -4.0],
-                [-1.0, 0.0, -4.0, -3.0, -1.0],
-            ],
-        ]
-    )
-    obj.run(res=res, x=x, low=-4, high=4)
+    x = np.zeros((4, 5))
+    res = cal_randint_like(x, low=-4, high=4)
+    assert res.shape == (4, 5)
 
 
 @pytest.mark.api_nn_randint_like_parameters
 def test_randint_like2():
     """
-    x: 4-d tensor
+    x: 3-d tensor
     """
-    x = np.zeros((2, 2, 4, 5))
-    res = np.array(
-        [
-            [
-                [
-                    [3.0, 2.0, 2.0, -4.0, 0.0],
-                    [-4.0, -2.0, 3.0, 2.0, 1.0],
-                    [-2.0, -2.0, 1.0, 1.0, 0.0],
-                    [3.0, 0.0, -2.0, -1.0, -1.0],
-                ],
-                [
-                    [-2.0, 1.0, 1.0, 2.0, -3.0],
-                    [-4.0, -1.0, -4.0, 1.0, -3.0],
-                    [-4.0, 3.0, -4.0, 1.0, -4.0],
-                    [-1.0, 0.0, -4.0, -3.0, -1.0],
-                ],
-            ],
-            [
-                [
-                    [3.0, -4.0, -2.0, 1.0, -1.0],
-                    [2.0, -2.0, -1.0, 2.0, 0.0],
-                    [-1.0, -2.0, 2.0, 0.0, -3.0],
-                    [-2.0, -1.0, 0.0, -2.0, 2.0],
-                ],
-                [
-                    [0.0, -2.0, 3.0, 3.0, 1.0],
-                    [-1.0, 0.0, 3.0, 0.0, -2.0],
-                    [1.0, 0.0, -4.0, 3.0, 2.0],
-                    [-4.0, -1.0, -1.0, 1.0, -3.0],
-                ],
-            ],
-        ]
-    )
-    obj.run(res=res, x=x, low=-4, high=4)
+    x = np.zeros((2, 4, 5))
+    res = cal_randint_like(x, low=-4, high=4)
+    assert res.shape == (2, 4, 5)
 
 
 @pytest.mark.api_nn_randint_like_parameters
-def test_randint_like0():
+def test_randint_like3():
+    """
+    x: 4-d tensor
+    """
+    x = np.zeros((2, 2, 4, 5))
+    res = cal_randint_like(x, low=-4, high=4)
+    assert res.shape == (2, 2, 4, 5)
+
+
+@pytest.mark.api_nn_randint_like_parameters
+def test_randint_like4():
     """
     high=None
     """
     x = np.zeros((4, 5))
-    res = np.array(
-        [[3.0, 2.0, 2.0, 0.0, 0.0], [0.0, 2.0, 3.0, 2.0, 1.0], [2.0, 2.0, 1.0, 1.0, 0.0], [3.0, 0.0, 2.0, 3.0, 3.0]]
-    )
-    obj.run(res=res, x=x, low=4)
+    res = cal_randint_like(x, low=4)
+    assert res.shape == (4, 5)
