9cba8c5bdd536cb7e680779ecffa0926b30bccbf
Zeref996 <825276847@qq.com>
2021-12-24 19:19:28 +0800

    add onnx case, test=models

---
+++ models/Paddle2ONNX/Seg2ONNX/infer_for_onnx.py
@@ -0,0 +1,419 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+"""
+infer predict
+"""
+
+import argparse
+import codecs
+import os
+import sys
+# import auto_log
+
+LOCAL_PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(LOCAL_PATH, "..", ".."))
+
+import yaml
+import numpy as np
+from paddle.inference import create_predictor, PrecisionType
+from paddle.inference import Config as PredictConfig
+
+import paddleseg.transforms as T
+from paddleseg.cvlibs import manager
+from paddleseg.utils import get_sys_env, logger, get_image_list
+from paddleseg.utils.visualize import get_pseudo_color_map
+
+
+def use_auto_tune(args):
+    """
+    base
+    """
+    return (
+        hasattr(PredictConfig, "collect_shape_range_info")
+        and hasattr(PredictConfig, "enable_tuned_tensorrt_dynamic_shape")
+        and args.device == "gpu"
+        and args.use_trt
+        and args.enable_auto_tune
+    )
+
+
+class DeployConfig:
+    """
+    base
+    """
+
+    def __init__(self, path):
+        """
+        base
+        """
+        with codecs.open(path, "r", "utf-8") as file:
+            self.dic = yaml.load(file, Loader=yaml.FullLoader)
+
+        self._transforms = self._load_transforms(self.dic["Deploy"]["transforms"])
+        self._dir = os.path.dirname(path)
+
+    @property
+    def transforms(self):
+        """
+        base
+        """
+        return self._transforms
+
+    @property
+    def model(self):
+        """
+        base
+        """
+        return os.path.join(self._dir, self.dic["Deploy"]["model"])
+
+    @property
+    def params(self):
+        """
+        base
+        """
+        return os.path.join(self._dir, self.dic["Deploy"]["params"])
+
+    def _load_transforms(self, t_list):
+        """
+        base
+        """
+        com = manager.TRANSFORMS
+        transforms = []
+        for t in t_list:
+            ctype = t.pop("type")
+            transforms.append(com[ctype](**t))
+
+        return T.Compose(transforms)
+
+
+def auto_tune(args, imgs, img_nums):
+    """
+    Use images to auto tune the dynamic shape for trt sub graph.
+    The tuned shape saved in args.auto_tuned_shape_file.
+    Args:
+        args(dict): input args.
+        imgs(str, list[str]): the path for images.
+        img_nums(int): the nums of images used for auto tune.
+    Returns:
+        None
+    """
+    logger.info("Auto tune the dynamic shape for GPU TRT.")
+
+    assert use_auto_tune(args)
+
+    if not isinstance(imgs, (list, tuple)):
+        imgs = [imgs]
+    num = min(len(imgs), img_nums)
+
+    cfg = DeployConfig(args.cfg)
+    pred_cfg = PredictConfig(cfg.model, cfg.params)
+    pred_cfg.enable_use_gpu(100, 0)
+    if not args.print_detail:
+        pred_cfg.disable_glog_info()
+    pred_cfg.collect_shape_range_info(args.auto_tuned_shape_file)
+
+    predictor = create_predictor(pred_cfg)
+    input_names = predictor.get_input_names()
+    input_handle = predictor.get_input_handle(input_names[0])
+
+    for i in range(0, num):
+        data = np.array([cfg.transforms(imgs[i])[0]])
+        input_handle.reshape(data.shape)
+        input_handle.copy_from_cpu(data)
+        try:
+            predictor.run()
+        except:
+            logger.info(
+                "Auto tune fail. Usually, the error is out of GPU memory, "
+                "because the model and image is too large. \n"
+            )
+            del predictor
+            if os.path.exists(args.auto_tuned_shape_file):
+                os.remove(args.auto_tuned_shape_file)
+            return
+
+    logger.info("Auto tune success.\n")
+
+
+class Predictor:
+    """
+    base
+    """
+
+    def __init__(self, args):
+        """
+        Prepare for prediction.
+        The usage and docs of paddle inference, please refer to
+        https://paddleinference.paddlepaddle.org.cn/product_introduction/summary.html
+        """
+        self.args = args
+        self.cfg = DeployConfig(args.cfg)
+
+        self._init_base_config()
+
+        if args.device == "cpu":
+            self._init_cpu_config()
+        else:
+            self._init_gpu_config()
+
+        self.predictor = create_predictor(self.pred_cfg)
+
+        # if hasattr(args, "benchmark") and args.benchmark:
+        #
+        #     pid = os.getpid()
+        #     self.autolog = auto_log.AutoLogger(
+        #         model_name=args.model_name,
+        #         model_precision=args.precision,
+        #         batch_size=args.batch_size,
+        #         data_shape="dynamic",
+        #         save_path=None,
+        #         inference_config=self.pred_cfg,
+        #         pids=pid,
+        #         process_name=None,
+        #         gpu_ids=0,
+        #         time_keys=["preprocess_time", "inference_time", "postprocess_time"],
+        #         warmup=0,
+        #         logger=logger,
+        #     )
+
+    def _init_base_config(self):
+        """
+        base
+        """
+        self.pred_cfg = PredictConfig(self.cfg.model, self.cfg.params)
+        if not self.args.print_detail:
+            self.pred_cfg.disable_glog_info()
+        self.pred_cfg.enable_memory_optim()
+        self.pred_cfg.switch_ir_optim(True)
+
+    def _init_cpu_config(self):
+        """
+        Init the config for x86 cpu.
+        """
+        logger.info("Use CPU")
+        self.pred_cfg.disable_gpu()
+        if self.args.enable_mkldnn:
+            logger.info("Use MKLDNN")
+            # cache 10 different shapes for mkldnn
+            self.pred_cfg.set_mkldnn_cache_capacity(10)
+            self.pred_cfg.enable_mkldnn()
+        self.pred_cfg.set_cpu_math_library_num_threads(self.args.cpu_threads)
+
+    def _init_gpu_config(self):
+        """
+        Init the config for nvidia gpu.
+        """
+        logger.info("Use GPU")
+        self.pred_cfg.enable_use_gpu(100, 0)
+        precision_map = {"fp16": PrecisionType.Half, "fp32": PrecisionType.Float32, "int8": PrecisionType.Int8}
+        precision_mode = precision_map[self.args.precision]
+
+        if self.args.use_trt:
+            logger.info("Use TRT")
+            self.pred_cfg.enable_tensorrt_engine(
+                workspace_size=1 << 30,
+                max_batch_size=1,
+                min_subgraph_size=50,
+                precision_mode=precision_mode,
+                use_static=False,
+                use_calib_mode=False,
+            )
+
+            if use_auto_tune(self.args) and os.path.exists(self.args.auto_tuned_shape_file):
+                logger.info("Use auto tuned dynamic shape")
+                allow_build_at_runtime = True
+                self.pred_cfg.enable_tuned_tensorrt_dynamic_shape(
+                    self.args.auto_tuned_shape_file, allow_build_at_runtime
+                )
+            else:
+                logger.info("Use manual set dynamic shape")
+                min_input_shape = {"x": [1, 3, 100, 100]}
+                max_input_shape = {"x": [1, 3, 2000, 3000]}
+                opt_input_shape = {"x": [1, 3, 512, 1024]}
+                self.pred_cfg.set_trt_dynamic_shape_info(min_input_shape, max_input_shape, opt_input_shape)
+
+    def run(self, imgs):
+        """
+        base
+        """
+        if not isinstance(imgs, (list, tuple)):
+            imgs = [imgs]
+
+        num = len(imgs)
+        input_names = self.predictor.get_input_names()
+        input_handle = self.predictor.get_input_handle(input_names[0])
+        output_names = self.predictor.get_output_names()
+        output_handle = self.predictor.get_output_handle(output_names[0])
+        results = []
+        args = self.args
+
+        if not os.path.exists(args.save_dir):
+            os.makedirs(args.save_dir)
+
+        for i in range(0, num, args.batch_size):
+            if args.benchmark:
+                self.autolog.times.start()
+            data = np.array([self._preprocess(img) for img in imgs[i : i + args.batch_size]])
+            # print('***' * 30)
+            # print(data)
+            np.save(os.path.join(args.model_name, args.with_argmax, "input_np", "input_" + str(i)), data)
+            # print('***' * 30)
+            input_handle.reshape(data.shape)
+            input_handle.copy_from_cpu(data)
+            if args.benchmark:
+                self.autolog.times.stamp()
+
+            self.predictor.run()
+
+            results = output_handle.copy_to_cpu()
+            if args.benchmark:
+                self.autolog.times.stamp()
+
+            # print('+++' * 30)
+            # print(results)
+            # print('infer_out shape:', results.shape)
+            print("{} No.{} {}/input infer_result:".format(args.model_name, i, args.with_argmax))
+            print(results)
+            print(results.shape)
+            print("*****" * 30)
+            np.save(os.path.join(args.model_name, args.with_argmax, "infer_output_np", "output_" + str(i)), results)
+            # print('+++' * 30)
+            results = self._postprocess(results)
+
+            # print('===' * 30)
+            # print(results)
+            # print('infer_out_after_postprocess shape:', results.shape)
+            # print('===' * 30)
+            if args.benchmark:
+                self.autolog.times.end(stamp=True)
+            self._save_imgs(results, imgs)
+
+        logger.info("Finish")
+
+    def _preprocess(self, img):
+        """
+        base
+        """
+        return self.cfg.transforms(img)[0]
+
+    def _postprocess(self, results):
+        """
+        base
+        """
+        if self.args.with_argmax:
+            results = np.argmax(results, axis=1)
+        return results
+
+    def _save_imgs(self, results, imgs):
+        """
+        base
+        """
+        for i in range(results.shape[0]):
+            result = get_pseudo_color_map(results[i])
+            basename = os.path.basename(imgs[i])
+            basename, _ = os.path.splitext(basename)
+            basename = f"{basename}.png"
+            result.save(os.path.join(self.args.save_dir, basename))
+
+
+def parse_args():
+    """
+    base
+    """
+    parser = argparse.ArgumentParser(description="Test")
+    parser.add_argument("--config", dest="cfg", help="The config file.", default=None, type=str, required=True)
+    parser.add_argument(
+        "--image_path",
+        dest="image_path",
+        help="The directory or path or file list of the images to be predicted.",
+        type=str,
+        default=None,
+        required=True,
+    )
+    parser.add_argument(
+        "--batch_size", dest="batch_size", help="Mini batch size of one gpu or cpu.", type=int, default=1
+    )
+    parser.add_argument(
+        "--save_dir", dest="save_dir", help="The directory for saving the predict result.", type=str, default="./output"
+    )
+    parser.add_argument(
+        "--device", choices=["cpu", "gpu"], default="gpu", help="Select which device to inference, defaults to gpu."
+    )
+
+    parser.add_argument(
+        "--use_trt",
+        default=False,
+        type=eval,
+        choices=[True, False],
+        help="Whether to use Nvidia TensorRT to accelerate prediction.",
+    )
+    parser.add_argument(
+        "--precision", default="fp32", type=str, choices=["fp32", "fp16", "int8"], help="The tensorrt precision."
+    )
+    parser.add_argument(
+        "--enable_auto_tune",
+        default=False,
+        type=eval,
+        choices=[True, False],
+        help="Whether to enable tuned dynamic shape. We uses some images to collect "
+        "the dynamic shape for trt sub graph, which avoids setting dynamic shape manually.",
+    )
+    parser.add_argument(
+        "--auto_tuned_shape_file",
+        type=str,
+        default="auto_tune_tmp.pbtxt",
+        help="The temp file to save tuned dynamic shape.",
+    )
+
+    parser.add_argument("--cpu_threads", default=10, type=int, help="Number of threads to predict when using cpu.")
+    parser.add_argument(
+        "--enable_mkldnn",
+        default=False,
+        type=eval,
+        choices=[True, False],
+        help="Enable to use mkldnn to speed up when using cpu.",
+    )
+
+    parser.add_argument(
+        "--benchmark",
+        type=eval,
+        default=False,
+        help="Whether to log some information about environment, model, configuration and performance.",
+    )
+    parser.add_argument(
+        "--model_name", default="", type=str, help="When `--benchmark` is True, the specified model name is displayed."
+    )
+    parser.add_argument(
+        "--with_argmax", default="", type=str, help="When `--benchmark` is True, the specified model name is displayed."
+    )
+    parser.add_argument(
+        "--print_detail", dest="print_detail", help="Print GLOG information of Paddle Inference.", action="store_true"
+    )
+
+    return parser.parse_args()
+
+
+def main(args):
+    """
+    base
+    """
+    imgs_list, _ = get_image_list(args.image_path)
+
+    if use_auto_tune(args):
+        tune_img_nums = 10
+        auto_tune(args, imgs_list, tune_img_nums)
+
+    predictor = Predictor(args)
+    predictor.run(imgs_list)
+
+    if use_auto_tune(args) and os.path.exists(args.auto_tuned_shape_file):
+        os.remove(args.auto_tuned_shape_file)
+
+    if args.benchmark:
+        predictor.autolog.report()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
