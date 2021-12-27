48df12782a05b8533a60c6c2c6b9707a80761cfe
Zeref996 <825276847@qq.com>
2021-12-27 17:47:55 +0800

    fix jit api run.sh, test=model

--- framework/e2e/jit/api/run.sh
+++ framework/e2e/jit/api/run.sh
@@ -18,7 +18,46 @@ test_is_empty.py \
 test_is_tensor.py \
 test_isfinite.py \
 test_isinf.py \
-test_isnan.py"
+test_isnan.py \
+
+test_arange.py \
+test_clip.py \
+test_conj.py \
+test_diagonal.py \
+test_digamma.py \
+test_divide.py \
+test_empty_like.py \
+test_erf.py \
+test_expm1.py \
+test_exp.py \
+test_kron.py \
+test_lgamma.py \
+test_linspace.py \
+test_log10.py \
+test_log1p.py \
+test_log2.py \
+test_log.py \
+test_logsumexp.py \
+test_mean.py \
+test_mm.py \
+test_pow.py \
+test_rsqrt.py \
+test_scale.py \
+test_sinh.py \
+test_sin.py \
+test_sqrt.py \
+test_square.py \
+test_stanh.py \
+test_std.py \
+test_sum.py \
+test_tanh_.py \
+test_tanh.py \
+test_tan.py \
+test_trace.py \
+test_var.py \
+test_floor_divide.py \
+test_min.py \
+test_prod.py"
 bug=0
 
 echo "============ failed cases =============" >> result.txt
