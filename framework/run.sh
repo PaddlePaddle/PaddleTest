799206e1089d578339afeecf597522f0e26daa7e
Zeref996 <825276847@qq.com>
2021-12-27 17:26:21 +0800

    add jit frame run.sh, test=model

--- framework/run.sh
+++ framework/run.sh
@@ -6,24 +6,18 @@ python3.7 -m pip install scipy
 
 # api
 cd api
-#rm -rf ./result.txt
-#echo "[api cases result]" >> result.txt
 bash ./run.sh
 api=$?
 echo ${api}
-#cat ./result.txt
 cd $home
 
 
 # jit
 cd e2e
 cd jit
-#rm -rf ./result.txt
-#echo "[nn cases result]" >> result.txt
 bash ./run.sh
 jit=$?
 echo ${jit}
-#cat ./result.txt
 cd $home
 
 
