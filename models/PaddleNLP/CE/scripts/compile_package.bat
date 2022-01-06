9b7f6082b3e23bc95bd6dfadb1ca7ef36cdce562
Pcjmmc <544546992@qq.com>
2022-01-06 12:51:03 +0800

    fix nlp case, test=model

--- models/PaddleNLP/CE/scripts/compile_package.bat
+++ models/PaddleNLP/CE/scripts/compile_package.bat
@@ -4,8 +4,6 @@ python -m pip uninstall Pillow
 python -m pip install Pillow==8.4.0
 cd models_repo
 
-del /s /q  C:\Python39\Lib\site-packages\~umpy-1.21.5.dist-info
-
 python setup.py bdist_wheel
 
 for %%i in (".\dist\*.whl") do (
