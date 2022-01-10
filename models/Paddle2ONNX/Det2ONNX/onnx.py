f34a2269abe22f12d3b89fcd5473a5483027e06a
Zeref996 <825276847@qq.com>
2022-01-10 20:19:13 +0800

    add Det 2 onnx, test=model

--- models/Paddle2ONNX/Det2ONNX/onnx.py
+++ models/Paddle2ONNX/Det2ONNX/onnx.py
@@ -20,6 +20,58 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+
+def base_onnx():
+    """
+
+    :return:
+    """
+    image = np.load(os.path.join(input_path, "input_" + str(i) + ".npy"))
+    im_shape = np.load(os.path.join(input_path, "im_shape_" + str(i) + ".npy"))
+    scale_factor = np.load(os.path.join(input_path, "scale_factor_" + str(i) + ".npy"))
+    inputs_dict = {}
+    sess = rt.InferenceSession(model_path)
+    inputs_dict[sess.get_inputs()[0].name] = im_shape
+    inputs_dict[sess.get_inputs()[1].name] = image
+    inputs_dict[sess.get_inputs()[2].name] = scale_factor
+    print(inputs_dict)
+    onnx_result = sess.run(None, input_feed=inputs_dict)
+    print("{} No.{} input onnx_result:".format(args.model_name, i))
+    print(onnx_result[0])
+    print(onnx_result[0].shape)
+    print("*****" * 30)
+    np.save(os.path.join(output_path, "output_" + str(i) + ".npy"), onnx_result[0])
+
+
+def picodet_onnx():
+    """
+
+    :return:
+    """
+    image = np.load(os.path.join(input_path, "input_" + str(i) + ".npy"))
+    inputs_dict = {}
+    sess = rt.InferenceSession(model_path)
+    inputs_dict[sess.get_inputs()[0].name] = image
+    print(inputs_dict)
+    onnx_result = sess.run(None, input_feed=inputs_dict)
+    print("{} No.{} input onnx_result:".format(args.model_name, i))
+    print(onnx_result[0])
+    print(onnx_result[0].shape)
+    print("*****" * 30)
+    np.save(os.path.join(output_path, "output_" + str(i) + ".npy"), onnx_result[0])
+
+
+models_zoo = {
+    "models/yolov3_darknet53_270e_coco": base_onnx,
+    "models/yolov3_mobilenet_v1_ssld_270e_coco": base_onnx,
+    "models/yolov3_mobilenet_v3_large_270e_coco": base_onnx,
+    "models/yolov3_r34_270e_coco": base_onnx,
+    "models/yolov3_r50vd_dcn_270e_coco": base_onnx,
+    "models/ppyolo_tiny_650e_coco": base_onnx,
+    "models/picodet_l_416_coco": picodet_onnx,
+    "models/fairmot_dla34_30e_1088x608": base_onnx,
+}
+
 if __name__ == "__main__":
     if args.use_gpu is False:
         paddle.set_device("cpu")
@@ -33,18 +85,4 @@ if __name__ == "__main__":
     input_list = os.listdir(input_path)
 
     for i in range(int(len(input_list) / 3)):
-        image = np.load(os.path.join(input_path, "input_" + str(i) + ".npy"))
-        im_shape = np.load(os.path.join(input_path, "im_shape_" + str(i) + ".npy"))
-        scale_factor = np.load(os.path.join(input_path, "scale_factor_" + str(i) + ".npy"))
-        inputs_dict = {}
-        sess = rt.InferenceSession(model_path)
-        inputs_dict[sess.get_inputs()[0].name] = im_shape
-        inputs_dict[sess.get_inputs()[1].name] = image
-        inputs_dict[sess.get_inputs()[2].name] = scale_factor
-        print(inputs_dict)
-        onnx_result = sess.run(None, input_feed=inputs_dict)
-        print("{} No.{} input onnx_result:".format(args.model_name, i))
-        print(onnx_result[0])
-        print(onnx_result[0].shape)
-        print("*****" * 30)
-        np.save(os.path.join(output_path, "output_" + str(i) + ".npy"), onnx_result[0])
+        models_zoo[args.model_name]()
