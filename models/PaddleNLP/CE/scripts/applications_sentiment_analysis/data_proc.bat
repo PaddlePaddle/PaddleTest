744f8852b6011a8dfc2b1277b1bf8e2aae9d2d86
Pcjmmc <544546992@qq.com>
2022-01-13 11:07:15 +0800

    fix NLP case, test=model

--- models/PaddleNLP/CE/scripts/applications_sentiment_analysis/data_proc.bat
+++ models/PaddleNLP/CE/scripts/applications_sentiment_analysis/data_proc.bat
@@ -3,9 +3,9 @@ cd ../..
 
 cd models_repo\applications\sentiment_analysis\
 
-if not checkpoints/ext_checkpoints md checkpoints/ext_checkpoints
-if not checkpoints/cls_checkpoints md checkpoints/cls_checkpoints
-if not checkpoints/pp_checkpoints  md checkpoints/pp_checkpoints
+if not exist checkpoints/ext_checkpoints md checkpoints/ext_checkpoints
+if not exist checkpoints/cls_checkpoints md checkpoints/cls_checkpoints
+if not exist checkpoints/pp_checkpoints  md checkpoints/pp_checkpoints
 if not exist data md data
 cd data
 python -m wget https://bj.bcebos.com/v1/paddlenlp/data/ext_data.tar.gz
