146d6e8eb895e98291f261e9bd0f499e6ead326d
quanxiang-liu <qxliunj@163.com>
2022-01-25 14:41:16 +0800

    fix clas release2.3 change clas win model list, test=model

--- models/PaddleClas/CI/linux/PaddleClas_CI_release23.sh
+++ models/PaddleClas/CI/linux/PaddleClas_CI_release23.sh
@@ -93,10 +93,11 @@ python -m pip install --ignore-installed --upgrade \
    pip -i https://mirror.baidu.com/pypi/simple
 python -m pip install  --ignore-installed paddleslim \
    -i https://mirror.baidu.com/pypi/simple
-python -m pip install --ignore-installed dataset/visualdl-2.2.1-py3-none-any.whl \
-   -i https://mirror.baidu.com/pypi/simple
+# python -m pip install --ignore-installed dataset/visualdl-2.2.1-py3-none-any.whl \
+#    -i https://mirror.baidu.com/pypi/simple #已更新至2.2.3
 python -m pip install  -r requirements.txt  \
    -i https://mirror.baidu.com/pypi/simple
+
 
 rm -rf models_list
 rm -rf models_list_all
