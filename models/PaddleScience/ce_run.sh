d3dfd5d7893b478efa6dfbaffd57cfca0f4f32a2
shjNT <sunhongjiang@baidu.com>
2022-02-09 19:31:16 +0800

    change ce run_mode=model

--- models/PaddleScience/ce_run.sh
+++ models/PaddleScience/ce_run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 root_dir=$PWD
-cases=`find . -name "*.py" | sort`
-ignore="ce_win.py"
-bug=0
+cases=`find ./examples -name "*.py" | sort`
+cases1=`find ./api -name test_*.py" | sort`
+
+ignore=""
+example_bug=0
+api_bug=0
 
 echo "" >  ${root_dir}/result.txt
 echo "========= bug file list =========" > ${root_dir}/result.txt
+
+echo "====examples bugs:" > ${root_dir}/result.txt
 for file in ${cases}
 do
 file_name=`basename $file`
@@ -20,12 +25,38 @@ echo ${file_name}
         python ${file_name} --alluredir=report
         if [ $? -ne 0 ]; then
             echo ${file_name} >> ${root_dir}/result.txt
-            bug=`expr ${bug} + 1`
+            example_bug=`expr ${bug} + 1`
         fi
         cd -
     fi
 echo ============================= ${file_name}  end! =============================
 done
-echo "total bugs: "${bug} >> ${root_dir}/result.txt
+
+echo "examples bug numbers: "${example_bug} >> ${root_dir}/result.txt
+echo ************************** models test finished ************************** >> ${root_dir}/result.txt
+
+
+echo "====api bugs:" >> ${root_dir}/result.txt
+for file in ${cases1}
+do
+
+echo ${file_name} =========
+    if [[ ${ignore} =~ ${file_name} ]]; then
+        echo "skip"
+    else
+        python -m pytest ${file_name} --alluredir=report
+        if [ $? -ne 0 ]; then
+            echo ${file_name} >> ${root_dir}/result.txt
+            api_bug=`expr ${bug} + 1`
+        fi
+        cd -
+    fi
+echo ============================= ${file_name}  end! =============================
+done
+
+echo "examples bug numbers: "${example_bug} >> ${root_dir}/result.txt
+
+bug=$[$example_bug+$api_bug]
+echo "total bug: $bug"
 cat ${root_dir}/result.txt
 exit ${bug}
