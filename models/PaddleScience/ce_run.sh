f03af434961b0433d908642978d272ffe63a71c3
shjNT <88530238+shjNT@users.noreply.github.com>
2022-02-10 14:23:13 +0800

    fix ce run_mode=model (#677)

--- models/PaddleScience/ce_run.sh
+++ models/PaddleScience/ce_run.sh
@@ -11,7 +11,7 @@ api_bug=0
 echo "" >  ${root_dir}/result.txt
 echo "========= bug file list =========" > ${root_dir}/result.txt
 
-echo "====examples bugs:" >> ${root_dir}/result.txt
+echo "**** examples bugs ****" >> ${root_dir}/result.txt
 for file in ${cases}
 do
 file_name=`basename $file`
@@ -31,24 +31,23 @@ echo ${file_name}
     fi
 echo ============================= ${file_name}  end! =============================
 done
-
+echo ************************** models test finished **************************
 echo "examples bug numbers: "${example_bug} >> ${root_dir}/result.txt
-echo "************************** models test finished **************************" >> ${root_dir}/result.txt
 
 
-echo "====api bugs:" >> ${root_dir}/result.txt
+echo "**** api bugs ****" >> ${root_dir}/result.txt
 for file in ${cases1}
 do
 file_name=`basename $file`
 file_dir=`dirname $file`
 
-echo ${file_name} =========
+echo ${file_name}
     if [[ ${ignore} =~ ${file_name} ]]; then
         echo "skip"
     else
 	echo $PWD
 	cd ${file_dir}
-        python -m pytest ${file_name} --alluredir=report
+        python -m pytest ${file_name}
         if [ $? -ne 0 ]; then
             echo ${file_name} >> ${root_dir}/result.txt
             api_bug=`expr ${api_bug} + 1`
@@ -57,9 +56,8 @@ echo ${file_name} =========
     fi
 echo ============================= ${file_name}  end! =============================
 done
-
+echo *************************** api test finished ****************************
 echo "api bug numbers: "${api_bug} >> ${root_dir}/result.txt
-echo "************************** api test finished **************************" >> ${root_dir}/result.txt
 
 bug=$[$example_bug+$api_bug]
 echo "total bug: $bug"
