021cd051e278329ebd9e23dc9019bce42a65db41
iamWHTWD <28421344+iamWHTWD@users.noreply.github.com>
2022-03-02 20:31:47 +0800

    Update run_slim_CPU_case.bat

--- models/PaddleSlim/CE/windows/run_slim_CPU_case.bat
+++ models/PaddleSlim/CE/windows/run_slim_CPU_case.bat
@@ -121,7 +121,8 @@ for /f "delims=" %%i in (' find /C "FAIL" result.log ') do set result=%%i
 echo %result:~-1%
 
 for /f "delims=" %%i in (' echo %result:~-1% ') do set exitcode=%%i
-echo -----exitcode:%exitcode%---------
+echo -----fail case:%exitcode%---------
+echo -----exit code:%exitcode%---------
 exit %exitcode%
 
 echo -----------------run P0case:end -------------------------
