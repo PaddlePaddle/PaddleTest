87c924ae0b264d3a6a4bbd0947536abb8644dd2b
shjNT <88530238+shjNT@users.noreply.github.com>
2022-03-04 17:42:37 +0800

    add loss curve to ce run_mode=model (#755)

--- models/PaddleScience/ce_run.sh
+++ models/PaddleScience/ce_run.sh
@@ -2,22 +2,19 @@
 
 root_dir=$PWD
 cases=`find ./examples -name "*.py" | sort`
-cases1=`find ./api -name "test_*.py" | sort`
 
 ignore=""
 example_bug=0
-api_bug=0
 
 echo "" >  ${root_dir}/result.txt
 echo "========= bug file list =========" > ${root_dir}/result.txt
 
-echo "**** examples bugs ****" >> ${root_dir}/result.txt
 for file in ${cases}
 do
 file_name=`basename $file`
 file_dir=`dirname $file`
 
-echo ${file_name}
+echo ============================= ${file_name} start ============================
     if [[ ${ignore} =~ ${file_name} ]]; then
         echo "skip"
     else
@@ -32,35 +29,20 @@ echo ${file_name}
     fi
 echo ============================= ${file_name}  end! =============================
 done
-echo ************************** models test finished **************************
-echo "examples bug numbers: "${example_bug} >> ${root_dir}/result.txt
 
-
-echo "**** api bugs ****" >> ${root_dir}/result.txt
-for file in ${cases1}
+#generate loss curve
+cd ./examples
+log_files=`find . -name "*.log" | sort`
+for file in ${log_files}
 do
 file_name=`basename $file`
-file_dir=`dirname $file`
 
-echo ${file_name}
-    if [[ ${ignore} =~ ${file_name} ]]; then
-        echo "skip"
-    else
-	echo $PWD
-	cd ${file_dir}
-        python -m pytest ${file_name}
-        if [ $? -ne 0 ]; then
-            echo ${file_name} >> ${root_dir}/result.txt
-            api_bug=`expr ${api_bug} + 1`
-        fi
-        cd -
-    fi
-echo ============================= ${file_name}  end! =============================
+mv ${file} .
+python log.py ${file_name}
+
 done
-echo *************************** api test finished ****************************
-echo "api bug numbers: "${api_bug} >> ${root_dir}/result.txt
 
-bug=$[$example_bug+$api_bug]
+bug=$example_bug
 echo "total bug: $bug"
 cat ${root_dir}/result.txt
 exit ${bug}
