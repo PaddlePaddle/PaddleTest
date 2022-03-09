d6d70ebc66bf1f8dd26215c7655034f0d88c5c73
Pcjmmc <544546992@qq.com>
2022-03-09 14:06:09 +0800

    调整NLP，减少batch_size, test=model

--- models/PaddleNLP/CE/scripts/ernie-gen/infer.bat
+++ models/PaddleNLP/CE/scripts/ernie-gen/infer.bat
@@ -7,7 +7,7 @@ set logpath=%cd%\log\ernie-gen
 
 cd models_repo\examples\text_generation\ernie-gen\
 
-python -u ./predict.py --model_name_or_path ernie-1.0 --max_encode_len 24 --max_decode_len 72 --batch_size 48 --init_checkpoint tmp\model_30\model_state.pdparams --device %1 > %logpath%/infer_%1.log 2>&1
+python -u ./predict.py --model_name_or_path ernie-1.0 --max_encode_len 24 --max_decode_len 72 --batch_size 4 --init_checkpoint tmp\model_30\model_state.pdparams --device %1 > %logpath%/infer_%1.log 2>&1
 
 if %ERRORLEVEL% == 1 (
     echo "exit_code: 1.0" >> %logpath%/infer_%1.log
