3a62c2090615207c07fc6fc4058d3ed66b5af29c
ZhangYulongg <1272816783@qq.com>
2022-03-10 11:15:12 +0800

    test=model

--- models/PaddleClas/Full_Chain/upload.sh
+++ models/PaddleClas/Full_Chain/upload.sh
@@ -31,10 +31,10 @@ paddle_commit=`git rev-parse HEAD`
 cd -
 model_name=`cat ${config_file} | grep model_name | awk -F ":" '{print $NF}' | head -n 1`
 echo ${model_name}
-output_dir="test_tipc/output/norm_train_gpus_0,1_autocast_null/${model_name}"
+output_dir="test_tipc/output/${model_name}/norm_train_gpus_0,1_autocast_null"
 # special model
 if [ ${model_name} == "GeneralRecognition_PPLCNet_x2_5" ]; then
-    output_dir="test_tipc/output/norm_train_gpus_0,1_autocast_null/RecModel"
+    output_dir="test_tipc/output/RecModel/norm_train_gpus_0,1_autocast_null"
 fi
 echo ${output_dir}
 if [ ! -d ${output_dir} ]; then
@@ -44,9 +44,9 @@ fi
 # copy model files
 upload_dir="${output_dir}_upload"
 mkdir -p ${upload_dir}
-\cp test_tipc/output/norm_train_gpus_0,1_autocast_null/inference.pdiparams ${upload_dir}
-\cp test_tipc/output/norm_train_gpus_0,1_autocast_null/inference.pdiparams.info ${upload_dir}
-\cp test_tipc/output/norm_train_gpus_0,1_autocast_null/inference.pdmodel ${upload_dir}
+\cp ${output_dir}/inference.pdiparams ${upload_dir}
+\cp ${output_dir}/inference.pdiparams.info ${upload_dir}
+\cp ${output_dir}/inference.pdmodel ${upload_dir}
 \cp ${config_file} ${upload_dir}
 
 # upload model
@@ -54,7 +54,7 @@ model_tar_name="${time_stamp}^${REPO}^${model_name}^${paddle_commit}^${repo_comm
 models_link_file="tipc_models_url.txt"
 model_url="https://paddle-qa.bj.bcebos.com/fullchain_ce_test/${model_tar_name}"
 path_suffix=${upload_dir##*/}
-tar -zcvf ${model_tar_name} -C test_tipc/output/norm_train_gpus_0,1_autocast_null ${path_suffix}
+tar -zcvf ${model_tar_name} -C test_tipc/output/${model_name} ${path_suffix}
 python2 ${push_file} ${model_tar_name} paddle-qa/fullchain_ce_test
 exit_code=$?
 if [ ${exit_code} == 0 ]; then
@@ -63,4 +63,5 @@ if [ ${exit_code} == 0 ]; then
 fi
 rm -rf ${model_tar_name}
 rm -rf ${upload_dir}
+rm -rf ${output_dir}
 exit $exit_code
