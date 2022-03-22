475d809a8031234a9f4c49d581cc2218aef00620
iamWHTWD <28421344+iamWHTWD@users.noreply.github.com>
2022-03-22 19:46:10 +0800

    Update train.sh

--- models/PaddleSlim/CE/linux/scripts/slim_dy_unstructured_prune_threshold/train.sh
+++ models/PaddleSlim/CE/linux/scripts/slim_dy_unstructured_prune_threshold/train.sh
@@ -46,7 +46,8 @@ if [ "$1" = "linux_st_gpu1" ];then #单卡
     --num_epochs 1 \
     --test_period 1 \
     --model_period 1 \
-    --model_path dy_threshold_models_gpu1 > ${log_path}/$2.log 2>&1
+    --model_path dy_threshold_models_gpu1 \
+    --ce_test True > ${log_path}/$2.log 2>&1
     print_info $? $2
 
 elif [ "$1" = "linux_st_gpu2" ];then #双卡
