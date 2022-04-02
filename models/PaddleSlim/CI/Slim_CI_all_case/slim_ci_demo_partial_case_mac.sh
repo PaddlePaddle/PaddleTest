dd91a5f860010c5ce1e0e4088b7f4bb2bbf08c23
iamWHTWD <28421344+iamWHTWD@users.noreply.github.com>
2022-04-02 11:01:49 +0800

    Update slim_ci_demo_partial_case_mac.sh

--- models/PaddleSlim/CI/Slim_CI_all_case/slim_ci_demo_partial_case_mac.sh
+++ models/PaddleSlim/CI/Slim_CI_all_case/slim_ci_demo_partial_case_mac.sh
@@ -34,6 +34,8 @@ print_info(){
 if [ $1 -ne 0 ];then
     mv ${log_path}/$2 ${log_path}/FAIL_$2.log
     echo -e "\033[31m ${log_path}/FAIL_$2 \033[0m"
+    echo "fail log as blows"
+    cat FAIL_$2.log
 else
     mv ${log_path}/$2 ${log_path}/Sucess_$2.log
     echo -e "\033[32m ${log_path}/Sucess_$2 \033[0m"
