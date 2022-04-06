121c918470beafe14eba7cc109330723e4ee3819
iamWHTWD <28421344+iamWHTWD@users.noreply.github.com>
2022-04-06 19:23:07 +0800

    Update run_slim_mac.sh

--- models/PaddleSlim/CI/mac/run_slim_mac.sh
+++ models/PaddleSlim/CI/mac/run_slim_mac.sh
@@ -1,1 +1,20 @@
-echo "run slim demo task of mac with paramters" $1, $2
+#!/usr/bin/env bash
+#python version、paddle_compile_path、slim_install_method
+slim_prepare_env_mac.sh $1 $2 $3
+
+#下载小数据集及预训练模型
+slim_prepare_data_mac.sh
+
+# cudaid1、cudaid2
+slim_run_case_mac.sh $4 $5
+
+
+cd ${slim_dir}/logs
+FF=`ls *FAIL*|wc -l`
+if [ "${FF}" -gt "0" ];then
+    echo ---failed---
+    exit 1
+else
+    echo ---passed---
+    exit 0
+fi
