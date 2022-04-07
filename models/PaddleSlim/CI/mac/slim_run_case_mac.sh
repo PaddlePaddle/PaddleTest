c8034bd8a23f220f4a271269fd51050c4c6bfa0d
iamWHTWD <28421344+iamWHTWD@users.noreply.github.com>
2022-04-07 14:23:32 +0800

    Update slim_run_case_mac.sh

--- models/PaddleSlim/CI/mac/slim_run_case_mac.sh
+++ models/PaddleSlim/CI/mac/slim_run_case_mac.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 ##################
-echo "run slim demo task of mac with paramters" $1, $2
+echo --- run slim case of mac ---
+
+print_info(){
+if [ $1 -ne 0 ];then
+    mv ${log_path}/$2 ${log_path}/FAIL_$2.log
+    echo -e "\033[31m ${log_path}/FAIL_$2 \033[0m"
+    echo "fail log as belows"
+    cat ${log_path}/$2_FAIL.log
+else
+    mv ${log_path}/$2 ${log_path}/SUCCESS_$2.log
+    echo -e "\033[32m ${log_path}/SUCCESS_$2 \033[0m"
+fi
+}
 
 catchException() {
   echo $1 failed due to exception >> FAIL_Exception.log
