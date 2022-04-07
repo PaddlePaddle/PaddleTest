d0859c3f55eccdcdc747e4cad0d2618bc21e57a2
iamWHTWD <28421344+iamWHTWD@users.noreply.github.com>
2022-04-07 14:04:10 +0800

    Update run_slim_mac.sh

--- models/PaddleSlim/CI/mac/run_slim_mac.sh
+++ models/PaddleSlim/CI/mac/run_slim_mac.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #python version、paddle_compile_path、slim_install_method
-slim_prepare_env_mac.sh $1 $2 $3
+bash slim_prepare_env_mac.sh $1 $2 $3
 
 #下载小数据集及预训练模型
-slim_prepare_data_mac.sh
+bash slim_prepare_data_mac.sh
 
 # cudaid1、cudaid2
-slim_run_case_mac.sh
+bash slim_run_case_mac.sh
 
 
 cd ${slim_dir}/logs
