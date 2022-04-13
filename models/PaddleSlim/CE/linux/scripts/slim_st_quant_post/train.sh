1a952d53a725ce8d9fde639ee775c017b5b6f647
iamWHTWD <28421344+iamWHTWD@users.noreply.github.com>
2022-04-13 13:55:27 +0800

    Update train.sh

--- models/PaddleSlim/CE/linux/scripts/slim_st_quant_post/train.sh
+++ models/PaddleSlim/CE/linux/scripts/slim_st_quant_post/train.sh
@@ -42,16 +42,16 @@ if [ "$1" = "export_model" ];then #单卡
     --data imagenet  > ${log_path}/$2.log 2>&1
     print_info $? $2
 elif [ "$1" = "quant_post" ];then
-    wget -P inference_model https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/inference/ResNet50_infer.tar
+    wget -P inference_model https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/inference/MobileNetV1_infer.tar
     cd inference_model/
-    tar -xf ResNet50_infer.tar
+    tar -xf MobileNetV1_infer.tar
     cd ..
     python quant_post.py --model_path ./inference_model/MobileNetV1_infer/ \
-    --save_path ./quant_model/MobileNet \
+    --save_path ./quant_model/MobileNetV1 \
     --ce_test True > ${log_path}/$2.log 2>&1
     print_info $? $2
 elif [ "$1" = "eval1" ];then
-    python eval.py --model_path ./quant_model/MobileNet/  > ${log_path}/$2.log 2>&1
+    python eval.py --model_path ./quant_model/MobileNetV1  > ${log_path}/$2.log 2>&1
     tail -1 ${log_path}/$2.log | grep top1_acc | tr -d '[' | tr -d ']' | awk -F '=' '{print $2}' | awk -F ' ' '{print "top1:" $1"\ttop5:"$2}' >>${log_path}/$2.log
     print_info $? $2
 elif [ "$1" = "quant_post_bc" ];then
