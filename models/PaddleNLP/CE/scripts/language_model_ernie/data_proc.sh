4032e5a49a1c11bc3677f053f89ab59e1334d737
Pcjmmc <544546992@qq.com>
2022-05-13 12:34:20 +0800

    fix model path,test=model

--- models/PaddleNLP/CE/scripts/language_model_ernie/data_proc.sh
+++ models/PaddleNLP/CE/scripts/language_model_ernie/data_proc.sh
@@ -5,7 +5,7 @@ echo "$model_name 模型数据预处理阶段"
 
 #配置目标数据存储路径
 root_path=$cur_path/../../
-code_path=$cur_path/../../models_repo/examples/language_model/ernie-1.0/
+code_path=$cur_path/../../models_repo/model_zoo/ernie-1.0/
 log_path=$root_path/log/$model_name/
 if [ ! -d $log_path ]; then
   mkdir -p $log_path
