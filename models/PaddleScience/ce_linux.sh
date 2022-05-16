658a16447a1c61ab1243e10fd2378278671dd506
shjNT <88530238+shjNT@users.noreply.github.com>
2022-05-16 15:51:52 +0800

    paddlescience new ce run_mode=model (#999)
    * paddlescience new ce run_mode=model
    
    * paddlescience new ce run_mode=model
    
    * fix distributed run_mode=model

---
+++ models/PaddleScience/ce_linux.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+root_dir=$PWD
+cases=`find ./examples -name "*.py" | sort`
+
+ignore="log.py"
+bug=0
+
+echo "" >  ${root_dir}/result.txt
+echo "========= serial bug file list =========" >> ${root_dir}/result.txt
+for file in ${cases}
+echo ${file}
+do
+file_name=`basename $file`
+file_dir=`dirname $file`
+echo ============================= serial ${file_name} start ============================
+    if [[ ${ignore} =~ ${file_name} ]]; then
+        echo "skip"
+    else
+        cd ${file_dir}
+        python ${file_name} >> ${file_name%.*}.log
+        if [ $? -ne 0 ]; then
+            echo ${file_name} >> ${root_dir}/result.txt
+            bug=`expr ${bug} + 1`
+        fi
+        cat ${file_name%.*}.log
+        cd -
+    fi
+echo ============================= serial ${file_name}  end! =============================
+done
+
+
+echo "========= distributed bug file list =========" >> ${root_dir}/result.txt
+for file in ${cases}
+do
+echo ${file}
+file_name=`basename $file`
+file_dir=`dirname $file`
+echo ============================= distributed ${file_name} start ============================
+    if [[ ${ignore} =~ ${file_name} ]]; then
+        echo "skip"
+    else
+        cd ${file_dir}
+        python -m paddle.distributed.launch --devices=0,1 ${file_name} >> dst_${file_name%.*}.log
+        if [ $? -ne 0 ]; then
+            echo ${file_name} >> ${root_dir}/result.txt
+            bug=`expr ${bug} + 1`
+        fi
+        cat dst_${file_name%.*}.log
+        cd -
+    fi
+echo ============================= distributed ${file_name}  end! =============================
+done
+
+
+echo "total bug: $bug"
+cat ${root_dir}/result.txt
+exit ${bug}
--- models/PaddleScience/examples/darcy2d/darcy2d.py
+++ models/PaddleScience/examples/darcy2d/darcy2d.py
@@ -1,66 +1,60 @@
 """
 darcy2d example test
 """
-
 import paddlescience as psci
 import numpy as np
 import paddle
 
+paddle.seed(1)
+np.random.seed(1)
 
-# Analytical solution
-def DarcyRecSolution(x, y):
-    """
-    DarcyRecSolution
-    """
-    return np.sin(2.0 * np.pi * x) * np.cos(2.0 * np.pi * y)
+paddle.enable_static()
+# paddle.disable_static()
 
+psci.config.set_dtype("float32")
 
-# Generate analytical Solution using Geometry points
-def GenSolution(xy, bc_index):
-    """
-    GenSolution
-    """
-    sol = np.zeros((len(xy), 1)).astype(np.float32)
-    bc_value = np.zeros((len(bc_index), 1)).astype(np.float32)
-    len1 = len(xy)
-    for i in range(len1):
-        sol[i][0] = DarcyRecSolution(xy[i][0], xy[i][1])
+# ref solution
+ref_sol = lambda x, y: np.sin(2.0 * np.pi * x) * np.cos(2.0 * np.pi * y)
 
-    len2 = len(bc_index)
-    for i in range(len2):
-        bc_value[i][0] = sol[bc_index[i]]
+# ref rhs
+ref_rhs = lambda x, y: 8.0 * np.pi ** 2 * np.sin(2.0 * np.pi * x) * np.cos(2.0 * np.pi * y)
 
-    return [sol, bc_value]
+# set geometry and boundary
+geo = psci.geometry.Rectangular(origin=(0.0, 0.0), extent=(1.0, 1.0))
 
+geo.add_boundary(name="top", criteria=lambda x, y: y == 1.0)
+geo.add_boundary(name="down", criteria=lambda x, y: y == 0.0)
+geo.add_boundary(name="left", criteria=lambda x, y: x == 0.0)
+geo.add_boundary(name="right", criteria=lambda x, y: x == 1.0)
 
-# right-hand side
-def RighthandBatch(xy):
-    """
-    RighthandBatch
-    """
-    return [8.0 * 3.1415926 * 3.1415926 * paddle.sin(2.0 * np.pi * xy[:, 0]) * paddle.cos(2.0 * np.pi * xy[:, 1])]
+# discretize geometry
+npoints = 10201
+geo_disc = geo.discretize(npoints=npoints, method="uniform")
 
+# Poisson
+pde = psci.pde.Poisson(dim=2, rhs=ref_rhs)
 
-# Geometry
-geo = psci.geometry.Rectangular(space_origin=(0.0, 0.0), space_extent=(1.0, 1.0))
+# set bounday condition
+bc_top = psci.bc.Dirichlet("u", rhs=ref_sol)
+bc_down = psci.bc.Dirichlet("u", rhs=ref_sol)
+bc_left = psci.bc.Dirichlet("u", rhs=ref_sol)
+bc_right = psci.bc.Dirichlet("u", rhs=ref_sol)
 
-# PDE Laplace
-pdes = psci.pde.Laplace2D()
+# add bounday and boundary condition
+pde.add_bc("top", bc_top)
+pde.add_bc("down", bc_down)
+pde.add_bc("left", bc_left)
+pde.add_bc("right", bc_right)
 
-# Discretization
-pdes, geo = psci.discretize(pdes, geo, space_nsteps=(11, 11))
-
-# bc value
-golden, bc_value = GenSolution(geo.get_space_domain(), geo.get_bc_index())
-pdes.set_bc_value(bc_value=bc_value)
-psci.visu.save_vtk(geo, golden, "golden_darcy_2d")
-np.save("./golden_darcy_2d.npy", golden)
+# discretization pde
+pde_disc = pde.discretize(geo_disc=geo_disc)
 
 # Network
-net = psci.network.FCNet(num_ins=2, num_outs=1, num_layers=5, hidden_size=20, dtype="float32", activation="tanh")
+# TODO: remove num_ins and num_outs
+net = psci.network.FCNet(num_ins=2, num_outs=1, num_layers=5, hidden_size=20, activation="tanh")
 
-# Loss, TO rename
-loss = psci.loss.L2(pdes=pdes, geo=geo, aux_func=RighthandBatch)
+# Loss
+loss = psci.loss.L2()
 
 # Algorithm
 algo = psci.algorithm.PINNs(net=net, loss=loss)
@@ -69,23 +63,25 @@ algo = psci.algorithm.PINNs(net=net, loss=loss)
 opt = psci.optimizer.Adam(learning_rate=0.001, parameters=net.parameters())
 
 # Solver
-solver = psci.solver.Solver(algo=algo, opt=opt)
-solution = solver.solve(num_epoch=30000)
+solver = psci.solver.Solver(pde=pde_disc, algo=algo, opt=opt)
+solution = solver.solve(num_epoch=10000)
 
-# Use solution
-rslt = solution(geo)
-psci.visu.save_vtk(geo, rslt, "rslt_darcy_2d")
-np.save("./rslt_darcy_2d.npy", rslt)
+psci.visu.save_vtk(geo_disc=pde_disc.geometry, data=solution)
 
-# Calculate diff and l2 relative error
-diff = rslt - golden
+# MSE
+# TODO: solution array to dict: interior, bc
+cord = pde_disc.geometry.interior
+ref = ref_sol(cord[:, 0], cord[:, 1])
+mse2 = np.linalg.norm(solution[0][:, 0] - ref, ord=2) ** 2
 
-psci.visu.save_vtk(geo, diff, "diff_darcy_2d")
-np.save("./diff_darcy_2d.npy", diff)
-root_square_error = np.linalg.norm(diff, ord=2)
-mean_square_error = root_square_error * root_square_error / geo.get_domain_size()
-print("mean_sqeare_error: ", mean_square_error)
+n = 1
+for cord in pde_disc.geometry.boundary.values():
+    ref = ref_sol(cord[:, 0], cord[:, 1])
+    mse2 += np.linalg.norm(solution[n][:, 0] - ref, ord=2) ** 2
+    n += 1
 
-assert mean_square_error < 0.01, (
-    "The accuracy of mean_square_error is not enough;" "\n mean_square_error: %f" % mean_square_error
-)
+mse = mse2 / npoints
+
+print("MSE is: ", mse)
+
+assert mse < 3, "The accuracy of mse is not enough " % mse
