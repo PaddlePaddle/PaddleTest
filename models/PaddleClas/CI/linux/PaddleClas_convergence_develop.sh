6d701be386cde92dbe4536d059b95981166bae80
quanxiang-liu <qxliunj@163.com>
2022-05-19 15:45:20 +0800

    fix clas convergence path,  test=model

--- models/PaddleClas/CI/linux/PaddleClas_convergence_develop.sh
+++ models/PaddleClas/CI/linux/PaddleClas_convergence_develop.sh
@@ -3,6 +3,19 @@ python -c "import paddle; print(paddle.__version__,paddle.version.commit)";
 echo '*****************clas_version****'
 git rev-parse HEAD
 
+echo ${model_flag}
+echo ${Data_path}
+echo ${Project_path}
+echo ${paddle_compile}
+
+echo "path before"
+pwd
+if [[ ${model_flag} =~ 'convergence' ]]; then
+    cd ${Project_path}
+    echo "path after"
+    pwd
+fi
+
 unset http_proxy
 unset https_proxy
 python -m pip install --upgrade --force --ignore-installed  paddleslim  -i https://mirror.baidu.com/pypi/simple
