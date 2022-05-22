cef5dca066caddc6fdf30fc3c5e25c29fc87ae2e
Weilong Wu <veyron_wu@163.com>
2022-05-22 01:51:09 +0000

    [Eager] Roll back leagcy for fused_feedforward

--- framework/api/incubate/test_fused_feedforward.py
+++ framework/api/incubate/test_fused_feedforward.py
@@ -10,6 +10,8 @@ from apibase import APIBase
 import paddle
 import pytest
 import numpy as np
+from paddle.fluid.framework import _enable_legacy_dygraph
+_enable_legacy_dygraph()
 
 sys.path.append("../../utils/")
 from interceptor import skip_not_compile_gpu
