67eb16288380de454e1229c7ec78f43d20a0339c
liuhuanling <34302448+Pcjmmc@users.noreply.github.com>
2022-05-23 15:27:57 +0800

    去掉强制更新，test=model (#1041)

--- models/PaddleNLP/CE/scripts/compile_package.sh
+++ models/PaddleNLP/CE/scripts/compile_package.sh
@@ -8,10 +8,9 @@ root_path=$cur_path/../
 modle_path=$cur_path/../models_repo/
 # 编包
 cd $modle_path
-python setup.py bdist_wheel
-python -m pip install --upgrade paddlenlp
 python -m pip uninstall -y paddlenlp
-python -m pip install --ignore-installed ./dist/paddlenlp*.whl
+python setup.py bdist_wheel
+python -m pip install ./dist/paddlenlp*.whl
 # 如果前面编包失败就
 if [ $? -ne 0 ];then
     python -m pip install ./
