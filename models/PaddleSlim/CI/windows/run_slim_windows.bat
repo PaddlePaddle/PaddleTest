7b09073f4d352d63bdab94ca389d4cfd3361aa72
iamWHTWD <28421344+iamWHTWD@users.noreply.github.com>
2022-05-27 17:54:39 +0800

    Update run_slim_windows.bat

--- models/PaddleSlim/CI/windows/run_slim_windows.bat
+++ models/PaddleSlim/CI/windows/run_slim_windows.bat
@@ -67,6 +67,7 @@ if "%3"=="run_P0" (
     	echo ---skip run case with windows---
 )
 
+:: prinf_logs
 cd %root_path%
 cd %log_path%
 for /f "delims=" %%i in (' find /C "FAIL" result.log ') do set result=%%i
@@ -77,5 +78,6 @@ for /f "delims=" %%i in (' echo %result:~-1% ') do set exitcode=%%i
 echo -----fail case:%exitcode%---------
 echo -----exit code:%exitcode%---------
 exit %exitcode%
+goto :eof
 
 goto :eof
