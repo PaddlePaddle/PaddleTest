c9abf86819de56d1f3824c90f9b74243904e70d4
xiegegege <xiege01@baidu.com>
2022-06-06 20:44:20 +0800

    add push result script,run_mode=model

--- models/PaddleSeg/CE/linux_whole_fun/push_result.sh
+++ models/PaddleSeg/CE/linux_whole_fun/push_result.sh
@@ -1,4 +1,11 @@
 bash test_seg_ce.sh 'release'
+if [ $? -ne 0 ];then
+    export status='Failed'
+    export exit_code='8'
+else
+    export status='Passed'
+    export exit_code='0'
+fi
 export build_type_id=${AGILE_PIPELINE_CONF_ID}
 export build_id=${AGILE_PIPELINE_BUILD_ID}
 export repo='Paddle'
