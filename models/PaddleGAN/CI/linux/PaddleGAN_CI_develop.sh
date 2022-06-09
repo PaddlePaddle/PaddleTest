a2e492b1305ca690509ab2d8052be77b1438206f
quanxiang-liu <qxliunj@163.com>
2022-06-09 13:21:02 +0800

    add clas pplc dml,  test=model

--- models/PaddleGAN/CI/linux/PaddleGAN_CI_develop.sh
+++ models/PaddleGAN/CI/linux/PaddleGAN_CI_develop.sh
@@ -126,8 +126,8 @@ if [ -f "/etc/redhat-release" ]; then
     echo "######  ffmpeg"
     yum update -y
     yum install epel-release -y
-    rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
-    rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+    # rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
+    # rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
     yum install boost -y
     yum install opencv -y
     yum install ffmpeg -y
