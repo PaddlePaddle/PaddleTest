bda1ba8e9da71c1df9c0dfa8bd303f1292c117e5
iamWHTWD <28421344+iamWHTWD@users.noreply.github.com>
2022-06-23 20:48:46 +0800

    Update upload_report.py

--- models/upload/upload_report.py
+++ models/upload/upload_report.py
@@ -21,7 +21,7 @@ def send(url):
     models=os.getenv('models_list')
     models_list=[]
     for model in (models.split(',')):
-        if model:
+        if model.strip():
             models_list.append(model.strip())
 
     os.chdir(os.getenv('log_path'))
