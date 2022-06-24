5adff186036b36f09af93bf77794a9c18886b9ef
iamWHTWD <28421344+iamWHTWD@users.noreply.github.com>
2022-06-24 12:39:12 +0800

    Update slim_ci_api_coverage.sh

--- models/PaddleSlim/CI/Slim_CI_all_case/slim_ci_api_coverage.sh
+++ models/PaddleSlim/CI/Slim_CI_all_case/slim_ci_api_coverage.sh
@@ -31,7 +31,8 @@ PADDLE_ROOT=${slim_dir}
 test_num=1
 static_test_num=`ls test_*.py|wc -l`
 dygraph_test_num=`ls dygraph/test_*.py|wc -l`
-all_test_num=`expr ${static_test_num} + ${dygraph_test_num}`
+act_test_num=`ls act/test_*.py|wc -l`
+all_test_num=`expr ${static_test_num} + ${dygraph_test_num} + ${act_test_num}`
 run_api_case(){
 cases=`find ./ -name "test*.py" | sort`
 #ignore="test_analysis_helper.py"
@@ -66,6 +67,23 @@ else
 fi
 }
 run_api_case_dygraph
+
+run_api_case_act(){
+if [ -d ${slim_dir}/tests/act ];then
+cd ${slim_dir}/tests/act
+for line in `ls test_*.py`
+do
+    name=`echo ${line} | cut -d \. -f 1`
+    echo ${test_num}_"/"_${all_test_num}_act_${name}
+    python -m coverage run --source=${source} --branch -p ${line} > ${log_path}/${test_num}_act_${name} 2>&1
+    print_info $? ${test_num}_act_${name}
+    let test_num++
+done
+else
+    echo -e "\033[31m no tests/act \033[0m"
+fi
+}
+run_api_case_act
 
 cd ${slim_dir}/tests
 
