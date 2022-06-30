f79cf4cca2ea40b3d870e954e032cb08b2110694
shjNT <88530238+shjNT@users.noreply.github.com>
2022-06-30 14:17:56 +0800

    new cases, test=model (#1219)

--- framework/api/incubate/test_prim_status.py
+++ framework/api/incubate/test_prim_status.py
@@ -1,10 +1,10 @@
 """
-test prim_status
+test prim status
 """
 import sys
 import pytest
 import paddle
-from paddle.incubate.autograd import enable_prim, prim_enabled, disable_prim
+import paddle.incubate.autograd as augograd
 
 sys.path.append("../../utils/")
 from interceptor import skip_branch_not_develop
@@ -14,22 +14,22 @@ from interceptor import skip_branch_not_develop
 @pytest.mark.api_incubate_autograd_enable_prim
 def test_enable_prim():
     """
-    test_enable_prim
+    test01
     """
     paddle.enable_static()
-    assert prim_enabled() is False
-    enable_prim()
-    assert prim_enabled() is True
+    assert augograd.prim_enabled() is False
+    augograd.enable_prim()
+    assert augograd.prim_enabled() is True
 
 
 @skip_branch_not_develop
 @pytest.mark.api_incubate_autograd_disable_prim
 def test_disable_prim():
     """
-    test_disable_prim
+    test02
     """
     paddle.enable_static()
-    enable_prim()
-    assert prim_enabled() is True
-    disable_prim()
-    assert prim_enabled() is False
+    augograd.enable_prim()
+    assert augograd.prim_enabled() is True
+    augograd.disable_prim()
+    assert augograd.prim_enabled() is False
