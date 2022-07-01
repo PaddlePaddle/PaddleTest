aaaa74809c196f9c9424035025c1a8ca00e4ff99
shjNT <sunhongjiang@baidu.com>
2022-07-01 14:06:39 +0800

    disable backward test

--- framework/api/paddlebase/test_cumsum.py
+++ framework/api/paddlebase/test_cumsum.py
@@ -87,4 +87,5 @@ def test_cumsum_3():
     """
     x_data = np.random.rand(1, 16, 96, 32).astype(np.float32)
     res = np.cumsum(x_data, axis=2)
+    obj.enable_backward = False
     obj.run(res=res, x=x_data, axis=2)
