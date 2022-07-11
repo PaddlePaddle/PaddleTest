d4faa0ac96156434d07d437ab0a764efac84b7a1
wangye707 <39162106+wangye707@users.noreply.github.com>
2022-07-11 10:02:42 +0800

    Update test_ernie_gpu.py

--- inference/python_api_test/test_nlp_model/test_ernie_gpu.py
+++ inference/python_api_test/test_nlp_model/test_ernie_gpu.py
@@ -30,6 +30,7 @@ def check_model_exist():
         tar = tarfile.open("ernie.tgz")
         tar.extractall()
         tar.close()
+        clip_model_extra_op(path_prefix="./ernie/inference", output_model_path="./ernie/inference")
 
 
 def test_config():
