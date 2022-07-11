7437636ecfc32686f3c42c45c0ba159347287417
wangye707 <39162106+wangye707@users.noreply.github.com>
2022-07-11 09:59:46 +0800

    Update test_fast_rcnn_gpu.py

--- inference/python_api_test/test_det_model/test_fast_rcnn_gpu.py
+++ inference/python_api_test/test_det_model/test_fast_rcnn_gpu.py
@@ -30,6 +30,7 @@ def check_model_exist():
         tar = tarfile.open("fast_rcnn.tgz")
         tar.extractall()
         tar.close()
+        clip_model_extra_op(path_prefix="./fast_rcnn/inference", output_model_path="./fast_rcnn/inference")
 
 
 def test_config():
