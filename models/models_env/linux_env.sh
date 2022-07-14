7337c07872ad3d7a996a75656c11173d9d916656
quanxiang-liu <qxliunj@163.com>
2022-07-14 20:02:30 +0800

    add docker_run, test=model

--- models/models_env/linux_env.sh
+++ models/models_env/linux_env.sh
@@ -58,6 +58,7 @@ test_code_conf_path=./task/models/${Repo}/CE/conf  #各个repo自己管理，可
 #迁移下载路径代码和配置到框架指定执行路径 [不用改]
 mkdir -p ${test_code_download_path}/log
 ls ${test_code_download_path}/log;
+cp -r ./task/models/models_env/docker_run.sh  ./${CE_version_name}/src
 cp -r ${test_code_download_path}/.  ./${CE_version_name}/src/task
 cp -r ${test_code_download_path_CI}/.  ./${CE_version_name}/src/task
 cp ${test_code_conf_path}/${Common_name}.py ./${CE_version_name}/src/task/common.py
@@ -99,6 +100,7 @@ cat ./${CE_version_name}/src/task/common.py
 
 #####进入执行路径创建docker容器 [用户改docker创建]
 cd ${CE_version_name}/src
+ls;
 
 if  [[ ! -n "${docker_flag}" ]] ;then
     ####创建docker
@@ -130,82 +132,7 @@ if  [[ ! -n "${docker_flag}" ]] ;then
                 /bin/bash -c "
 
                 ldconfig;
-                if [[ ${Python_env} == 'path_way' ]];then
-                    case ${Python_version} in
-                    36)
-                    export LD_LIBRARY_PATH=/opt/_internal/cpython-3.6.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
-                    export PATH=/opt/_internal/cpython-3.6.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-                    ;;
-                    37)
-                    export LD_LIBRARY_PATH=/opt/_internal/cpython-3.7.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
-                    export PATH=/opt/_internal/cpython-3.7.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-                    ;;
-                    38)
-                    export LD_LIBRARY_PATH=/opt/_internal/cpython-3.8.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
-                    export PATH=/opt/_internal/cpython-3.8.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-                    ;;
-                    39)
-                    export LD_LIBRARY_PATH=/opt/_internal/cpython-3.9.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
-                    export PATH=/opt/_internal/cpython-3.9.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-                    ;;
-                    310)
-                    export LD_LIBRARY_PATH=/opt/_internal/cpython-3.10.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
-                    export PATH=/opt/_internal/cpython-3.10.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-                    ;;
-                    esac
-                elif [[ ${Python_env} == 'ln_way' ]];then
-                    # rm -rf /usr/bin/python2.7
-                    # rm -rf /usr/local/python2.7.15/bin/python
-                    # rm -rf /usr/local/bin/python
-                    # export PATH=/usr/local/bin/python:${PATH}
-                    case ${Python_version} in
-                    36)
-                    # ln -s /usr/local/bin/python3.6 /usr/local/bin/python
-                    mkdir run_env_py36;
-                    ln -s $(which python3.6) run_env_py36/python;
-                    ln -s $(which pip3.6) run_env_py36/pip;
-                    export PATH=$(pwd)/run_env_py36:${PATH};
-                    ;;
-                    37)
-                    # ln -s /usr/local/bin/python3.7 /usr/local/bin/python
-                    mkdir run_env_py37;
-                    ln -s $(which python3.7) run_env_py37/python;
-                    ln -s $(which pip3.7) run_env_py37/pip;
-                    export PATH=$(pwd)/run_env_py37:${PATH};
-                    ;;
-                    38)
-                    # ln -s /usr/local/bin/python3.8 /usr/local/bin/python
-                    mkdir run_env_py38;
-                    ln -s $(which python3.8) run_env_py38/python;
-                    ln -s $(which pip3.8) run_env_py38/pip;
-                    export PATH=$(pwd)/run_env_py38:${PATH};
-                    ;;
-                    39)
-                    # ln -s /usr/local/bin/python3.9 /usr/local/bin/python
-                    mkdir run_env_py39;
-                    ln -s $(which python3.9) run_env_py39/python;
-                    ln -s $(which pip3.9) run_env_py39/pip;
-                    export PATH=$(pwd)/run_env_py39:${PATH};
-                    ;;
-                    310)
-                    # ln -s /usr/local/bin/python3.10 /usr/local/bin/python
-                    mkdir run_env_py310;
-                    ln -s $(which python3.10) run_env_py310/python;
-                    ln -s $(which pip3.10) run_env_py310/pip;
-                    export PATH=$(pwd)/run_env_py310:${PATH};
-                    ;;
-                    esac
-                else
-                    echo unset python version
-                fi
-
-                python -c 'import sys; print(sys.version_info[:])';
-                git --version;
-                if [[ ${CE_version} == 'V2' ]];then
-                    bash main.sh --build_id=${AGILE_PIPELINE_BUILD_ID} --build_type_id=${AGILE_PIPELINE_CONF_ID} --priority=${Priority_version} --compile_path=${Compile_version} --job_build_id=${AGILE_JOB_BUILD_ID}
-                else
-                    bash main.sh --task_type='model' --build_number=${AGILE_PIPELINE_BUILD_NUMBER} --project_name=${AGILE_MODULE_NAME} --task_name=${AGILE_PIPELINE_NAME}  --build_id=${AGILE_PIPELINE_BUILD_ID} --build_type=${AGILE_PIPELINE_UUID} --owner='paddle' --priority=${Priority_version} --compile_path=${Compile_version} --agile_job_build_id=${AGILE_JOB_BUILD_ID}
-                fi
+                bash docker_run.sh
 
     " &
     wait $!
@@ -213,76 +140,5 @@ if  [[ ! -n "${docker_flag}" ]] ;then
 else
     echo docker already build
     ldconfig;
-    if [[ ${Python_env} == 'path_way' ]];then
-        case ${Python_version} in
-        36)
-        export LD_LIBRARY_PATH=/opt/_internal/cpython-3.6.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
-        export PATH=/opt/_internal/cpython-3.6.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-        ;;
-        37)
-        export LD_LIBRARY_PATH=/opt/_internal/cpython-3.7.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
-        export PATH=/opt/_internal/cpython-3.7.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-        ;;
-        38)
-        export LD_LIBRARY_PATH=/opt/_internal/cpython-3.8.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
-        export PATH=/opt/_internal/cpython-3.8.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-        ;;
-        39)
-        export LD_LIBRARY_PATH=/opt/_internal/cpython-3.9.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
-        export PATH=/opt/_internal/cpython-3.9.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-        ;;
-        310)
-        export LD_LIBRARY_PATH=/opt/_internal/cpython-3.10.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
-        export PATH=/opt/_internal/cpython-3.10.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-        ;;
-        esac
-    elif [[ ${Python_env} == 'ln_way' ]];then
-        case ${Python_version} in
-        36)
-        # ln -s /usr/local/bin/python3.6 /usr/local/bin/python
-        mkdir run_env_py36;
-        ln -s $(which python3.6) run_env_py36/python;
-        ln -s $(which pip3.6) run_env_py36/pip;
-        export PATH=$(pwd)/run_env_py36:${PATH};
-        ;;
-        37)
-        # ln -s /usr/local/bin/python3.7 /usr/local/bin/python
-        mkdir run_env_py37;
-        ln -s $(which python3.7) run_env_py37/python;
-        ln -s $(which pip3.7) run_env_py37/pip;
-        export PATH=$(pwd)/run_env_py37:${PATH};
-        ;;
-        38)
-        # ln -s /usr/local/bin/python3.8 /usr/local/bin/python
-        mkdir run_env_py38;
-        ln -s $(which python3.8) run_env_py38/python;
-        ln -s $(which pip3.8) run_env_py38/pip;
-        export PATH=$(pwd)/run_env_py38:${PATH};
-        ;;
-        39)
-        # ln -s /usr/local/bin/python3.9 /usr/local/bin/python
-        mkdir run_env_py39;
-        ln -s $(which python3.9) run_env_py39/python;
-        ln -s $(which pip3.9) run_env_py39/pip;
-        export PATH=$(pwd)/run_env_py39:${PATH};
-        ;;
-        310)
-        # ln -s /usr/local/bin/python3.10 /usr/local/bin/python
-        mkdir run_env_py310;
-        ln -s $(which python3.10) run_env_py310/python;
-        ln -s $(which pip3.10) run_env_py310/pip;
-        export PATH=$(pwd)/run_env_py310:${PATH};
-        ;;
-        esac
-    else
-        echo unset python version
-    fi
-
-    python -c 'import sys; print(sys.version_info[:])';
-    git --version;
-    if [[ ${CE_version} == 'V2' ]];then
-        bash main.sh --build_id=${AGILE_PIPELINE_BUILD_ID} --build_type_id=${AGILE_PIPELINE_CONF_ID} --priority=${Priority_version} --compile_path=${Compile_version} --job_build_id=${AGILE_JOB_BUILD_ID}
-    else
-        bash main.sh --task_type='model' --build_number=${AGILE_PIPELINE_BUILD_NUMBER} --project_name=${AGILE_MODULE_NAME} --task_name=${AGILE_PIPELINE_NAME}  --build_id=${AGILE_PIPELINE_BUILD_ID} --build_type=${AGILE_PIPELINE_UUID} --owner='paddle' --priority=${Priority_version} --compile_path=${Compile_version} --agile_job_build_id=${AGILE_JOB_BUILD_ID}
-    fi
+    bash docker_run.sh
 fi
