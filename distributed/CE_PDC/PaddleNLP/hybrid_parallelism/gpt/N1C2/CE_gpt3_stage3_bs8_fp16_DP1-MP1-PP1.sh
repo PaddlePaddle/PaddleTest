f27b44e05399c0df610e86f336250d7e91f547a9
liujie44 <liujie44@baidu.com>
2022-07-18 15:55:20 +0800

    add distributed/CE_PDC scripts

---
+++ distributed/CE_PDC/PaddleNLP/hybrid_parallelism/gpt/N1C2/CE_gpt3_stage3_bs8_fp16_DP1-MP1-PP1.sh
@@ -0,0 +1,24 @@
+model_item=CE_gpt3_stage3
+dp_degree=1
+mp_degree=1
+pp_degree=1
+bs_item=8
+fp_item=fp16
+run_mode=DP1-MP1-PP1
+device_num=N1C2
+max_iter=20000
+use_sharding=true
+use_recompute=True
+sharding_stage=3
+sharding_offload=True
+eval_freq=20000
+sharding_degree=2
+
+model=gpt
+micro_bs=4
+
+cd ./tests
+bash ./test_tipc/dygraph/hybrid_parallelism/${model}/benchmark_common/prepare.sh
+# run
+bash ./test_tipc/dygraph/hybrid_parallelism/${model}/benchmark_common/run_benchmark.sh ${model_item} ${fp_item} ${dp_degree} ${mp_degree} ${pp_degree} ${micro_bs} ${bs_item} ${run_mode} ${device_num} \
+${max_iter} ${use_sharding} ${use_recompute} ${sharding_stage} ${sharding_offload} ${eval_freq} ${sharding_degree} 2>&1;
---
+++ distributed/CE_PDC/PaddleNLP/hybrid_parallelism/gpt/N1C4/CE_gpt3_bs16_fp16_DP1-MP1-PP4.sh
@@ -0,0 +1,18 @@
+model_item=CE_gpt3
+dp_degree=1
+mp_degree=1
+pp_degree=4
+bs_item=16
+fp_item=fp16
+run_mode=DP1-MP1-PP4
+device_num=N1C4
+max_iter=50000
+
+model=gpt
+micro_bs=2
+
+cd ./tests
+bash ./test_tipc/dygraph/hybrid_parallelism/${model}/benchmark_common/prepare.sh
+# run
+bash ./test_tipc/dygraph/hybrid_parallelism/${model}/benchmark_common/run_benchmark.sh ${model_item} ${fp_item} ${dp_degree} ${mp_degree} ${pp_degree} ${micro_bs} ${bs_item} ${run_mode} ${device_num} \
+${max_iter} 2>&1;
