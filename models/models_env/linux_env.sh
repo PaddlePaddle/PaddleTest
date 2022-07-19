8779094c95ce325bc7084a8e6d42ce592f6efd2e
quanxiang-liu <qxliunj@163.com>
2022-07-19 19:44:50 +0800

    restruct paddleclas, test=model

--- models/models_env/linux_env.sh
+++ models/models_env/linux_env.sh
@@ -5,6 +5,8 @@ pwd;
 rm -rf ce && mkdir ce;
 cd ce;
 
+########TODO：区分下是否使用CE框架、区分下是否单独clone库(用于CI)、步骤Common_name需要再细化一下
+
 export Repo=${Repo:-PaddleClas}
 export Python_env=${Python_env:-path_way}
 export Python_version=${Python_version:-37}
@@ -14,8 +16,11 @@ export Compile_version=${Compile_version:-https://paddle-qa.bj.bcebos.com/paddle
 export Image_version=${Image_version:-registry.baidubce.com/paddlepaddle/paddle_manylinux_devel:cuda10.2-cudnn7}
 export Data_path=${Data_path:-/ssd2/ce_data/PaddleClas}
 export Project_path=${Project_path:-/workspace/task/PaddleClas}
-export Common_name=${Common_name:-cls_common_release}  #CE框架中的执行步骤，名称各异所以需要传入
-export model_flag=${model_flag:-CE}  #clas gan特有，可删除
+export Common_name=${Common_name:-conf/cls_common_release}  #CE框架中的执行步骤，名称各异所以需要传入
+
+export SET_MULTI_CUDA=${SET_MULTI_CUDA:-}  #如果不使用流水线，手动设置卡号 默认不设置，用0 1卡
+export docker_flag=${docker_flag:-}  #是否在docker内的环境 默认不设置，如果在docker中进行设置为False
+#手动设置卡号
 
 ####测试框架下载
 if [[ ${CE_version} == "V2" ]];then
@@ -51,48 +56,50 @@ else
 fi
 
 #通用变量[用户改]
-test_code_download_path=./task/models/${Repo}/CE
-test_code_download_path_CI=./task/models/${Repo}/CI
-test_code_conf_path=./task/models/${Repo}/CE/conf  #各个repo自己管理，可以分类，根据任务类型copy对应的common配置
+test_code_download_path_CI=./task/models/${Repo}
+test_code_conf_path=./task/models/${Repo}/conf  #各个repo自己管理，可以分类，根据任务类型copy对应的common配置
 
 #迁移下载路径代码和配置到框架指定执行路径 [不用改]
 mkdir -p ${test_code_download_path}/log
 ls ${test_code_download_path}/log;
-cp -r ./task/models/models_env/docker_run.sh  ./${CE_version_name}/src
 cp -r ${test_code_download_path}/.  ./${CE_version_name}/src/task
-cp -r ${test_code_download_path_CI}/.  ./${CE_version_name}/src/task
 cp ${test_code_conf_path}/${Common_name}.py ./${CE_version_name}/src/task/common.py
 cat ./${CE_version_name}/src/task/common.py;
 ls;
 
 ####根据agent制定对应卡，记得起agent时文件夹按照release_01 02 03 04名称
-tc_name=`(echo $PWD|awk -F '/' '{print $4}')`
-echo "teamcity path:" $tc_name
-if [ $tc_name == "release_02" ];then
-    echo release_02
-    sed -i "s/SET_CUDA = \"0\"/SET_CUDA = \"2\"/g"  ./${CE_version_name}/src/task/common.py
-    sed -i "s/SET_MULTI_CUDA = \"0,1\"/SET_MULTI_CUDA = \"2,3\"/g" ./${CE_version_name}/src/task/common.py
-    export SET_CUDA=2;
-    export SET_MULTI_CUDA=2,3;
+if  [[ "${SET_MULTI_CUDA}" == " " ]] ;then
+    tc_name=`(echo $PWD|awk -F '/' '{print $4}')`
+    echo "teamcity path:" $tc_name
+    if [ $tc_name == "release_02" ];then
+        echo release_02
+        sed -i "s/SET_CUDA = \"0\"/SET_CUDA = \"2\"/g"  ./${CE_version_name}/src/task/common.py
+        sed -i "s/SET_MULTI_CUDA = \"0,1\"/SET_MULTI_CUDA = \"2,3\"/g" ./${CE_version_name}/src/task/common.py
+        export SET_CUDA=2;
+        export SET_MULTI_CUDA=2,3;
 
-elif [ $tc_name == "release_03" ];then
-    echo release_03
-    sed -i "s/SET_CUDA = \"0\"/SET_CUDA = \"4\"/g"  ./${CE_version_name}/src/task/common.py
-    sed -i "s/SET_MULTI_CUDA = \"0,1\"/SET_MULTI_CUDA = \"4,5\"/g" ./${CE_version_name}/src/task/common.py
-    export SET_CUDA=4;
-    export SET_MULTI_CUDA=4,5;
+    elif [ $tc_name == "release_03" ];then
+        echo release_03
+        sed -i "s/SET_CUDA = \"0\"/SET_CUDA = \"4\"/g"  ./${CE_version_name}/src/task/common.py
+        sed -i "s/SET_MULTI_CUDA = \"0,1\"/SET_MULTI_CUDA = \"4,5\"/g" ./${CE_version_name}/src/task/common.py
+        export SET_CUDA=4;
+        export SET_MULTI_CUDA=4,5;
 
-elif [ $tc_name == "release_04" ];then
-    echo release_04
-    sed -i "s/SET_CUDA = \"0\"/SET_CUDA = \"6\"/g"  ./${CE_version_name}/src/task/common.py
-    sed -i "s/SET_MULTI_CUDA = \"0,1\"/SET_MULTI_CUDA = \"6,7\"/g"  ./${CE_version_name}/src/task/common.py
-    export SET_CUDA=6;
-    export SET_MULTI_CUDA=6,7;
+    elif [ $tc_name == "release_04" ];then
+        echo release_04
+        sed -i "s/SET_CUDA = \"0\"/SET_CUDA = \"6\"/g"  ./${CE_version_name}/src/task/common.py
+        sed -i "s/SET_MULTI_CUDA = \"0,1\"/SET_MULTI_CUDA = \"6,7\"/g"  ./${CE_version_name}/src/task/common.py
+        export SET_CUDA=6;
+        export SET_MULTI_CUDA=6,7;
+    else
+        echo release_01
+        export SET_CUDA=0;
+        export SET_MULTI_CUDA=0,1;
+    fi
 else
-    echo release_01
+    echo already seted CUDA_id
     export SET_CUDA=0;
     export SET_MULTI_CUDA=0,1;
-fi
 
 ####显示执行步骤
 cat ./${CE_version_name}/src/task/common.py
@@ -101,7 +108,7 @@ cat ./${CE_version_name}/src/task/common.py
 cd ${CE_version_name}/src
 ls;
 
-if  [[ ! -n "${docker_flag}" ]] ;then
+if  [[ "${docker_flag}" == " " ]] ;then
     ####创建docker
     set +x;
     docker_name="ce_${Repo}_${Priority_version}_${AGILE_JOB_BUILD_ID}" #AGILE_JOB_BUILD_ID以每个流水线粒度区分docker名称
@@ -118,18 +125,18 @@ if  [[ ! -n "${docker_flag}" ]] ;then
                 -v $(pwd):/workspace \
                 -v /ssd2:/ssd2 \
                 -w /workspace \
-                -e "no_proxy=${no_proxy}" \
-                -e "http_proxy=${http_proxy}" \
-                -e "https_proxy=${http_proxy}" \
-                -e "paddle_compile=${Compile_version}" \
-                -e "Data_path=${Data_path}" \
-                -e "model_flag=${model_flag}" \
-                -e "Project_path=${Project_path}" \
-                -e "cudaid1=${SET_CUDA}" \
-                -e "cudaid2=${SET_MULTI_CUDA}" \
                 ${Image_version}  \
                 /bin/bash -c "
+
                 ldconfig;
+                export no_proxy=${no_proxy};
+                export http_proxy=${http_proxy};
+                export https_proxy=${http_proxy};
+                export Data_path=${Data_path};
+                export Project_path=${Project_path};
+                export SET_CUDA=${SET_CUDA};
+                export SET_MULTI_CUDA=${SET_MULTI_CUDA};
+
                 if [[ ${Python_env} == 'path_way' ]];then
                     case ${Python_version} in
                     36)
@@ -206,11 +213,86 @@ if  [[ ! -n "${docker_flag}" ]] ;then
                 else
                     bash main.sh --task_type='model' --build_number=${AGILE_PIPELINE_BUILD_NUMBER} --project_name=${AGILE_MODULE_NAME} --task_name=${AGILE_PIPELINE_NAME}  --build_id=${AGILE_PIPELINE_BUILD_ID} --build_type=${AGILE_PIPELINE_UUID} --owner='paddle' --priority=${Priority_version} --compile_path=${Compile_version} --agile_job_build_id=${AGILE_JOB_BUILD_ID};
                 fi
-
     " &
     wait $!
     exit $?
 else
-    echo docker already build
-    bash docker_run.sh
+    ldconfig;
+    if [[ ${Python_env} == 'path_way' ]];then
+        case ${Python_version} in
+        36)
+        export LD_LIBRARY_PATH=/opt/_internal/cpython-3.6.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
+        export PATH=/opt/_internal/cpython-3.6.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
+        ;;
+        37)
+        export LD_LIBRARY_PATH=/opt/_internal/cpython-3.7.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
+        export PATH=/opt/_internal/cpython-3.7.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
+        ;;
+        38)
+        export LD_LIBRARY_PATH=/opt/_internal/cpython-3.8.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
+        export PATH=/opt/_internal/cpython-3.8.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
+        ;;
+        39)
+        export LD_LIBRARY_PATH=/opt/_internal/cpython-3.9.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
+        export PATH=/opt/_internal/cpython-3.9.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
+        ;;
+        310)
+        export LD_LIBRARY_PATH=/opt/_internal/cpython-3.10.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64;
+        export PATH=/opt/_internal/cpython-3.10.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
+        ;;
+        esac
+    elif [[ ${Python_env} == 'ln_way' ]];then
+        # rm -rf /usr/bin/python2.7
+        # rm -rf /usr/local/python2.7.15/bin/python
+        # rm -rf /usr/local/bin/python
+        # export PATH=/usr/local/bin/python:${PATH}
+        case ${Python_version} in
+        36)
+        # ln -s /usr/local/bin/python3.6 /usr/local/bin/python
+        mkdir run_env_py36;
+        ln -s $(which python3.6) run_env_py36/python;
+        ln -s $(which pip3.6) run_env_py36/pip;
+        export PATH=$(pwd)/run_env_py36:${PATH};
+        ;;
+        37)
+        # ln -s /usr/local/bin/python3.7 /usr/local/bin/python
+        mkdir run_env_py37;
+        ln -s $(which python3.7) run_env_py37/python;
+        ln -s $(which pip3.7) run_env_py37/pip;
+        export PATH=$(pwd)/run_env_py37:${PATH};
+        ;;
+        38)
+        # ln -s /usr/local/bin/python3.8 /usr/local/bin/python
+        mkdir run_env_py38;
+        ln -s $(which python3.8) run_env_py38/python;
+        ln -s $(which pip3.8) run_env_py38/pip;
+        export PATH=$(pwd)/run_env_py38:${PATH};
+        ;;
+        39)
+        # ln -s /usr/local/bin/python3.9 /usr/local/bin/python
+        mkdir run_env_py39;
+        ln -s $(which python3.9) run_env_py39/python;
+        ln -s $(which pip3.9) run_env_py39/pip;
+        export PATH=$(pwd)/run_env_py39:${PATH};
+        ;;
+        310)
+        # ln -s /usr/local/bin/python3.10 /usr/local/bin/python
+        mkdir run_env_py310;
+        ln -s $(which python3.10) run_env_py310/python;
+        ln -s $(which pip3.10) run_env_py310/pip;
+        export PATH=$(pwd)/run_env_py310:${PATH};
+        ;;
+        esac
+    else
+        echo unset python version;
+    fi
+
+    python -c 'import sys; print(sys.version_info[:])';
+    git --version;
+    if [[ ${CE_version} == 'V2' ]];then
+        bash main.sh --build_id=${AGILE_PIPELINE_BUILD_ID} --build_type_id=${AGILE_PIPELINE_CONF_ID} --priority=${Priority_version} --compile_path=${Compile_version} --job_build_id=${AGILE_JOB_BUILD_ID};
+    else
+        bash main.sh --task_type='model' --build_number=${AGILE_PIPELINE_BUILD_NUMBER} --project_name=${AGILE_MODULE_NAME} --task_name=${AGILE_PIPELINE_NAME}  --build_id=${AGILE_PIPELINE_BUILD_ID} --build_type=${AGILE_PIPELINE_UUID} --owner='paddle' --priority=${Priority_version} --compile_path=${Compile_version} --agile_job_build_id=${AGILE_JOB_BUILD_ID};
+    fi
+
 fi
