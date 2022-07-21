33b33592c468a32043df9684a95ef0eed1a00fe4
iamWHTWD <iamwhtwd@163.com>
2022-07-21 10:31:01 +0800

    fix code format

--- models/upload/upload_report.py
+++ models/upload/upload_report.py
@@ -12,7 +12,7 @@ def grep_logs(key_word, model):
     """
         搜索log文件
     """
-    if platform.system().lower() == 'windows':
+    if platform.system().lower() == "windows":
         cmd_grep = " dir | findstr /i  {} | findstr /i {}".format(key_word, model)
     else:
         # linux and mac
@@ -24,22 +24,22 @@ def send(url):
     """
     以case成功/失败的粒度上传报告
     """
-    models = os.getenv('models_list')
+    models = os.getenv("models_list")
     models_list = []
-    for model in (models.split(',')):
+    for model in (models.split(",")):
         if model.strip():
             models_list.append(model.strip())
 
-    os.chdir(os.getenv('log_path'))
+    os.chdir(os.getenv("log_path"))
     models_result = []
     for model in models_list:
         # 需grep/findstr success、避免model_name没有查询到默认为success；
-        cmd_grep = grep_logs('success', model)
+        cmd_grep = grep_logs("success", model)
         cmd_res = os.system(cmd_grep)
         if cmd_res == 0:
             kpi_status = "Passed"
             # grep/findstr fail log中是否包含model
-            cmd_grep_fail = grep_logs('fail', model)
+            cmd_grep_fail = grep_logs("fail", model)
             cmd_res_fail = os.system(cmd_grep_fail)
             if cmd_res_fail == 0:
                 kpi_status = "Failed"
@@ -58,28 +58,28 @@ def send(url):
             })
 
     params = {
-        "build_type_id": os.getenv('build_type_id'),
-        "build_id": os.getenv('build_id'),
-        "commit_id": os.getenv('build_commit_id'),
-        "commit_time": os.getenv('build_commit_time'),
-        "repo": os.getenv('build_repo_name'),
-        "branch": os.getenv('build_repo_branch'),
+        "build_type_id": os.getenv("build_type_id"),
+        "build_id": os.getenv("build_id"),
+        "commit_id": os.getenv("build_commit_id"),
+        "commit_time": os.getenv("build_commit_time"),
+        "repo": os.getenv("build_repo_name"),
+        "branch": os.getenv("build_repo_branch"),
         "duration": 1,
-        "exit_code": os.getenv('build_exit_code'),
-        "status": os.getenv('build_status'),
+        "exit_code": os.getenv("build_exit_code"),
+        "status": os.getenv("build_status"),
         "case_detail": json.dumps(models_result)
     }
     res = requests.post(url, data=params)
     result = res.json()
-    if result['code'] == 200 and result['message'] == 'success':
+    if result["code"] == 200 and result["message"] == "success":
         print("ok")
     else:
-        print('error')
+        print("error")
     print(params)
     print(result)
 
 
 if __name__ == "__main__":
     # build_url需配置在环境变量中，不允许上传到github；
-    url = os.getenv('build_url')
+    url = os.getenv("build_url")
     send(url)
