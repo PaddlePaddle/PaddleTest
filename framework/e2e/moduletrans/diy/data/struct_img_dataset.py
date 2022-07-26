8577f5579ce4b615be4f8ef97a8caaa7ec41cd52
Zeref996 <825276847@qq.com>
2022-07-26 16:51:47 +0800

    add backbones yaml jit, test=model

--- framework/e2e/moduletrans/diy/data/struct_img_dataset.py
+++ framework/e2e/moduletrans/diy/data/struct_img_dataset.py
@@ -12,38 +12,60 @@ from paddle.io import Dataset, BatchSampler, DataLoader
 import tool
 
 
-class ListImageWithoutLabelDataset(Dataset):
-    """List Image Without Label Dataset, use paddle.io.Dataset"""
-
-    def __init__(self, data_dict, num_samples=1):
-        self.data_dict = data_dict
-        for k, v in self.data_dict.items():
-            self.data_dict[k] = [v, v]
-        self.num_samples = num_samples
-
-    def __getitem__(self, idx):
-        data_dict = self.data_dict
-        return data_dict
-
-    def __len__(self):
-        return self.num_samples
-
-
-class ListImageWithoutLabel:
-    """List Image Without Label Dataset, not use paddle.io.Dataset"""
-
-    def __init__(self, data_dict, num_samples=1):
-        self.data_dict = data_dict
-        for k, v in self.data_dict.items():
-            self.data_dict[k] = [v, v]
-        self.num_samples = num_samples
-
-    def __getitem__(self, idx):
-        data_dict = self.data_dict
-        return data_dict
-
-    def __len__(self):
-        return self.num_samples
+# class ListImageWithoutLabelDataset(Dataset):
+#     """List Image Without Label Dataset, use paddle.io.Dataset"""
+#
+#     def __init__(self, data_dict, num_samples=1):
+#         self.data_dict = data_dict
+#         for k, v in self.data_dict.items():
+#             self.data_dict[k] = [v, v]
+#         self.num_samples = num_samples
+#
+#     def __getitem__(self, idx):
+#         data_dict = self.data_dict
+#         return data_dict
+#
+#     def __len__(self):
+#         return self.num_samples
+#
+#
+# class ListImageWithoutLabel:
+#     """List Image Without Label Dataset, not use paddle.io.Dataset"""
+#
+#     def __init__(self, data_dict, num_samples=1):
+#         self.data_dict = data_dict
+#         for k, v in self.data_dict.items():
+#             self.data_dict[k] = [v, v]
+#         self.num_samples = num_samples
+#
+#     def __getitem__(self, idx):
+#         data_dict = self.data_dict
+#         return data_dict
+#
+#     def __len__(self):
+#         return self.num_samples
+#
+#
+# class ListImageWithoutLabel1:
+#     """List Image Without Label Dataset, not use paddle.io.Dataset"""
+#
+#     def __init__(self, data_dict, num_samples=1):
+#         self.data_dict = data_dict
+#         self.new_data_dict = {}
+#         self.new_data_list = []
+#         self.new_key_list = []
+#         for k, v in self.data_dict.items():
+#             self.new_key_list.append(k)
+#             self.new_data_list.append(v)
+#         self.new_data_dict[self.new_key_list[0]] = self.new_data_list
+#         self.num_samples = num_samples
+#
+#     def __getitem__(self, idx):
+#         data_dict = self.new_data_dict
+#         return data_dict
+#
+#     def __len__(self):
+#         return self.num_samples
 
 
 class DictImageWithoutLabel:
