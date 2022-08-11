5d3d355d8d283771977faafcb1cbdf6e7054e08d
Paddle CI <paddle_ci@example.com>
2022-08-11 13:51:13 +0800

    move ernie-gen to model_zoo test=model

--- models/PaddleNLP/CE/scripts/ernie-gen/data_proc.bat
+++ models/PaddleNLP/CE/scripts/ernie-gen/data_proc.bat
@@ -1,7 +1,7 @@
 @echo off
 cd ../..
 
-cd models_repo\examples\text_generation\ernie-gen\
+cd models_repo\model_zoo\ernie-gen\
 
 python -m wget https://paddlenlp.bj.bcebos.com/datasets/poetry.tar.gz
 tar xvf poetry.tar.gz
