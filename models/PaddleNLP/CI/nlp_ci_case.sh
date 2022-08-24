6578552de69a4b4d41846fccd5091d4d197faa63
zjjlivein <1834792141@qq.com>
2022-08-24 10:55:30 +0800

    fix faster test=model

--- models/PaddleNLP/CI/nlp_ci_case.sh
+++ models/PaddleNLP/CI/nlp_ci_case.sh
@@ -1032,9 +1032,10 @@ get_diff_TO_P0case(){
 for key in $(echo ${!all_P0case_dic[*]});do
     all_P0case_time=`expr ${all_P0case_time} + ${all_P0case_dic[$key]}`
 done
-P0case_list=(waybill_ie msra_ner glue bert skep bigbird electra gpt ernie-1.0 xlnet ofa  squad tinybert lexical_analysis seq2seq \
-pretrained_models word_embedding ernie-ctm distilbert stacl transformer pet simbert ernie-doc transformer-xl pointer_summarizer question_matching ernie-csc \
-nptag ernie-m clue taskflow)
+# P0case_list=(waybill_ie msra_ner glue bert skep bigbird electra gpt ernie-1.0 xlnet ofa  squad tinybert lexical_analysis seq2seq \
+# pretrained_models word_embedding ernie-ctm distilbert stacl transformer pet simbert ernie-doc transformer-xl pointer_summarizer question_matching ernie-csc \
+# nptag ernie-m clue taskflow)
+P0case_list=(gpt ofa transformer clue)
 P0case_time=${all_P0case_time}
 }
 set -e
