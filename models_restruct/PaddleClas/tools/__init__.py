27397c2665ff7fde39f740380ae09a1bb2ccea5d
root <root@yq01-sys-hic-k8s-v100-box-a225-0091.yq01.baidu.com>
2022-09-07 13:01:56 +0000

    add models_restruct, test=model

---
+++ models_restruct/PaddleClas/tools/__init__.py
@@ -0,0 +1,2 @@
+# encoding: utf-8
+from .parse_log import *
---
+++ models_restruct/PaddleClas/tools/end.py
@@ -0,0 +1,47 @@
+# encoding: utf-8
+"""
+执行case后：回收数据避免占用太多存储空间
+"""
+import os
+import sys
+import wget
+import yaml
+import json
+import shutil
+import argparse
+import numpy as np
+
+class PaddleClas_End(object):
+    def __init__(self):
+        self.reponame = os.environ['reponame']
+
+    def remove_data(self):
+        """
+        回收之前下载的数据
+        """
+        print('####', os.listdir(self.reponame))
+        return 0
+
+    def build_prepare(self):
+        """
+        执行准备过程
+        """
+        #进入repo中
+        ret = 0
+        ret = self.remove_data()
+        if ret:
+            print("build remove_data failed")
+            return ret
+        return ret
+
+
+def run():
+    """
+    执行入口
+    """
+    model = PaddleClas_End()
+    model.build_prepare()
+    return 0
+
+if __name__ == '__main__':
+    run()
