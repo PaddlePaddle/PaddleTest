48459408e6406bdf862712b8d00f286dcb1df857
JiabinYang <360788950@qq.com>
2022-09-13 02:31:59 +0000

    fix 2 case in nn

--- framework/api/nn/test_cosinesimilarity.py
+++ framework/api/nn/test_cosinesimilarity.py
@@ -263,7 +263,7 @@ def test_cosinesimilarity8():
     except Exception as e:
         print(e)
         if is_in_eager:
-            if "multiply_ad_func" in e.args[0] and "InvalidArgumentError" in e.args[0]:
+            if (("multiply_ad_func" in e.args[0]) or ("multiply_dygraph_function" in e.args[0])) and "InvalidArgumentError" in e.args[0]:
                 pass
             else:
                 raise Exception
