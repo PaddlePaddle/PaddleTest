cbea97ec10112f16db6c28ae67d4b57386ff1073
quanxiang-liu <qxliunj@163.com>
2022-09-14 06:54:00 +0000

    fix slim prune, test=model

--- models/PaddleClas_restruct/scripts/shell/prepare.sh
+++ models/PaddleClas_restruct/scripts/shell/prepare.sh
@@ -24,6 +24,26 @@ fi
 yum install bc -y >/dev/null 2>&1
 apt-get install bc -y >/dev/null 2>&1
 
+#slim要用develop
+if [[ ${yaml_line} =~ "slim" ]];then
+    # python -m pip install -U paddleslim \
+    #     -i https://mirror.baidu.com/pypi/simple  >/dev/null 2>&1
+    echo "start clone paddleslim"
+    if [[ -d "PaddleSlim" ]];then
+        # git clone -b develop https://github.com/PaddlePaddle/PaddleSlim.git
+        wget -q -c https://xly-devops.bj.bcebos.com/PaddleTest/PaddleSlim.tar.gz --no-proxy
+        tar xf PaddleSlim.tar.gz
+    fi
+    echo "end clone paddleslim"
+    cd PaddleSlim
+    git checkout develop
+    git pull
+    python -m pip install -r requirements.txt
+    python setup.py install
+    cd ..
+    echo "end install paddleslim"
+fi
+
 #取消代理用镜像安装包，安装依赖包
 unset http_proxy
 unset https_proxy
@@ -31,8 +51,6 @@ unset https_proxy
 python -m pip install --upgrade \
     pip -i https://mirror.baidu.com/pypi/simple  >/dev/null 2>&1
 python -m pip install  -r requirements.txt  \
-    -i https://mirror.baidu.com/pypi/simple  >/dev/null 2>&1
-python -m pip install -U paddleslim \
     -i https://mirror.baidu.com/pypi/simple  >/dev/null 2>&1
 
 if [[ ${yaml_line} =~ "face" ]] && [[ ${yaml_line} =~ "metric_learning" ]];then
