02e37dbfc0e9996f7694386205731c856e8943bb
quanxiang-liu <qxliunj@163.com>
2022-09-14 12:37:41 +0000

    fix prepare, test=model

--- models_restruct/models_env/linux_env.sh
+++ models_restruct/models_env/linux_env.sh
@@ -183,6 +183,7 @@ if [[ "${docker_flag}" == "" ]]; then
         nvidia-smi;
         python -c 'import sys; print(sys.version_info[:])';
         git --version;
+        python -m pip install -r requirements.txt #预先安装依赖包
         python main.py --models_list=${models_list:-None} --models_file=${models_file:-None} --system=${system:-linux} --step=${step:-train} --reponame=${reponame:-PaddleClas} --mode=${mode:-function} --use_build=${use_build:-yes} --branch=${branch:-develop} --get_repo=${get_repo:-wget} --paddle_whl=${paddle_whl:-None} --dataset_org=${dataset_org:-None} --dataset_target=${dataset_target:-None}
     " &
     wait $!
@@ -261,5 +262,6 @@ else
     nvidia-smi;
     python -c 'import sys; print(sys.version_info[:])';
     git --version;
+    python -m pip install -r requirements.txt #预先安装依赖包
     python main.py --models_list=${models_list:-None} --models_file=${models_file:-None} --system=${system:-linux} --step=${step:-train} --reponame=${reponame:-PaddleClas} --mode=${mode:-function} --use_build=${use_build:-yes} --branch=${branch:-develop} --get_repo=${get_repo:-wget} --paddle_whl=${paddle_whl:-None} --dataset_org=${dataset_org:-None} --dataset_target=${dataset_target:-None}
 fi
