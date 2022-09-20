2207327161e11e3443e32534dac349f63a3211b8
zjjlivein <1834792141@qq.com>
2022-09-20 17:53:20 +0800

    fix typo run_mode=model

--- models/PaddleNLP/CI/nlp_ci_case.sh
+++ models/PaddleNLP/CI/nlp_ci_case.sh
@@ -1031,7 +1031,7 @@ get_diff_TO_P0case(){
 for key in $(echo ${!all_P0case_dic[*]});do
     all_P0case_time=`expr ${all_P0case_time} + ${all_P0case_dic[$key]}`
 done
-if [[ ${Testcase} == 'null']];then
+if [[ ${Testcase} == 'null' ]];then
     P0case_list=(waybill_ie msra_ner glue bert skep bigbird electra gpt ernie-1.0 xlnet ofa  squad tinybert lexical_analysis seq2seq \
     pretrained_models word_embedding ernie-ctm distilbert stacl transformer pet simbert ernie-doc transformer-xl pointer_summarizer question_matching ernie-csc \
     nptag ernie-m clue taskflow)
