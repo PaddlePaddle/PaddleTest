622c470cbba575012f15dbd50d9370fac29a9829
Zeref996 <825276847@qq.com>
2022-09-23 10:38:43 +0800

    add jit cases, test=model

--- framework/e2e/jit/jittest.py
+++ framework/e2e/jit/jittest.py
@@ -14,7 +14,7 @@ from jittrans import JitTrans
 
 yaml_path = os.path.join(os.path.abspath(os.path.dirname(os.getcwd())), "yaml", "base.yml")
 
-case_name = "shard_index_4"
+case_name = "split_0"
 
 
 # loading yaml
