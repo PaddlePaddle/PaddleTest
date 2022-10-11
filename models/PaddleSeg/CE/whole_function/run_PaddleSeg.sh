85ab36568debfe13782647b2e25b8306bd98d622
xiegegege <xiege01@baidu.com>
2022-10-11 20:37:14 +0800

    add rtformer for seg,test=model

--- models/PaddleSeg/CE/whole_function/run_PaddleSeg.sh
+++ models/PaddleSeg/CE/whole_function/run_PaddleSeg.sh
@@ -148,7 +148,7 @@ EXPORT_DYNAMIC(){
         python export.py \
            --config ${config} \
            --model_path seg_dynamic_pretrain/${model}/model.pdparams \
-           --save_dir ./inference_model/${model} 
+           --save_dir ./inference_model/${model} \
            --input_shape 1 3 512 512 >${log_dir}/log/${model}/${model}_${mode}.log 2>&1
         print_result
     elif [[ -z `echo ${skip_export_model} | grep -w ${model}` ]];then
