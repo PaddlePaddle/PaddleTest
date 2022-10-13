26d6487e006f0ad1413e6be91b856226ad215d1e
贾晓 <jiaxiao243@126.com>
2022-10-13 14:06:41 +0800

    add ModelSeries requirement, run_mode=model (#1532)
    * add ModelSeries requiment, run_mode=model
    
    * update ModelSeries requirement, run_mode=model
    
    * fix code style, run_mode=model

--- models/ModelSeries/run_requirement.sh
+++ models/ModelSeries/run_requirement.sh
@@ -1,4 +1,10 @@
+# python
+rm -rf /usr/local/bin/python;
+ln -s /opt/conda/envs/python35-paddle120-env/bin/python3.7 /usr/local/bin/python;
+
 python -m pip install --no-cache-dir --upgrade pip
+python -m pip install --upgrade parl opencv-python
+
 set -x
 repo_list='PaddleClas PaddleOCR PaddleDetection PaddleSeg PaddleNLP PaddleSpeech PaddleHub'
 for repo in $repo_list
@@ -8,11 +14,11 @@ git clone http://github.com/PaddlePaddle/$repo.git
 cd  $repo
 
 if [ "$repo" != "PaddleSpeech" ];then
-python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt -i https://mirror.baidu.com/pypi/simple
 fi
 
 if  [ "$repo" = "PaddleOCR" ];then
-python -m pip install -r ppstructure/recovery/requirements.txt
+python -m pip install -r ppstructure/recovery/requirements.txt -i https://mirror.baidu.com/pypi/simple
 fi
 
 python -m pip install .
