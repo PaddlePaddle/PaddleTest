bc401517eebd9699b0ec23c57b6f1360b716eabd
quanxiang-liu <qxliunj@163.com>
2022-10-18 16:46:10 +0800

    fix windows trained,  test=model

--- models/PaddleClas/CE/analysis_html.py
+++ models/PaddleClas/CE/analysis_html.py
@@ -10,17 +10,17 @@ lie = 9
 lie_index = 5
 b = []
 
-# url_all = [
-#     "https://xly.bce.baidu.com/ipipe/ipipe-report/report/18334479/result/reportUrl.html",
-#     "https://xly.bce.baidu.com/ipipe/ipipe-report/report/18334453/result/reportUrl.html",
-# ]  # release
-# with open("clas_release", "w", encoding="utf-8") as f:
-
 url_all = [
-    "https://xly.bce.baidu.com/ipipe/ipipe-report/report/19385728/result/reportUrl.html",
-    "https://xly.bce.baidu.com/ipipe/ipipe-report/report/19387433/result/reportUrl.html",
-]  # develop
-with open("clas_develop", "w", encoding="utf-8") as f:
+    "https://xly.bce.baidu.com/ipipe/ipipe-report/report/19417018/result/reportUrl.html",
+    "https://xly.bce.baidu.com/ipipe/ipipe-report/report/19416955/result/reportUrl.html",
+]  # release
+with open("clas_release", "w", encoding="utf-8") as f:
+
+    # url_all = [
+    #     "https://xly.bce.baidu.com/ipipe/ipipe-report/report/19416950/result/reportUrl.html",
+    #     "https://xly.bce.baidu.com/ipipe/ipipe-report/report/19418504/result/reportUrl.html",
+    # ]  # develop
+    # with open("clas_develop", "w", encoding="utf-8") as f:
 
     for i, _ in enumerate(url_all):
         # for i in range(len(url_all)): #code style error
