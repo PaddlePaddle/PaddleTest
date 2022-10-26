3a57d834e2b0a5f3e0da524900920ae548d1cded
zjjlivein <1834792141@qq.com>
2022-10-26 17:25:44 +0800

    fix normal_case param run_mode=model

--- models/PaddleNLP/CI/normal_case.sh
+++ models/PaddleNLP/CI/normal_case.sh
@@ -4,6 +4,9 @@ export device=gpu
 export epoch=1
 export max_steps=2
 export save_steps=2
+export output_dir=./output/
+export export_output_dir=./infer_model/
+
 print_info(){
 if [ $1 -ne 0 ];then
     mv ${log_path}/$2 ${log_path}/$2_FAIL.log
@@ -25,27 +28,27 @@ for exec_file in `ls`;do
         print_info $? ${example}_prepare
     # TRAIN
     elif [[ ${exec_file} == "train.py" ]] || [[ ${exec_file} =~ "run_train" ]] ;then
-        python -m paddle.distributed.launch ${exec_file} --device ${devices} --max_steps ${max_steps} --save_steps ${save_steps}  >${log_path}/${example}_train>>${log_path}/${example}_train 2>&1
+        python -m paddle.distributed.launch ${exec_file} --device ${devices} --epoch ${epoch} --max_steps ${max_steps} --output_dir ${output_dir} >${log_path}/${example}_train>>${log_path}/${example}_train 2>&1
         print_info $? ${example}_train
     # EVAL
     elif [[ ${exec_file} == "eval.py" ]] || [[ ${exec_file} =~ "run_eval" ]] ;then
-        python ${exec_file} --device ${devices}    >${log_path}/${example}_eval >>${log_path}/${example}_eval 2>&1
+        python ${exec_file} --device ${devices}  --init_checkpoint_dir $output_dir}  >${log_path}/${example}_eval >>${log_path}/${example}_eval 2>&1
         print_info $? ${example}_eval
     # PREDICT
     elif [[ ${exec_file} == "predict.py" ]] || [[ ${exec_file} =~ "run_predict" ]] ;then
-        python ${exec_file} --device ${devices}    >${log_path}/${example}_predict >>${log_path}/${example}_predcit 2>&1
+        python ${exec_file} --device ${devices} --init_checkpoint_dir $output_dir}   >${log_path}/${example}_predict >>${log_path}/${example}_predcit 2>&1
         print_info $? ${example}_predict
     # EXPORT MODEL
     elif [[ ${exec_file} == "export_model.py" ]] ;then
-        python ${exec_file} --device ${devices}   >${log_path}/${example}_export_model >>${log_path}/${example}_export_model 2>&1
+        python ${exec_file} --device ${devices} --export_output_dir ${export_output_dir} >${log_path}/${example}_export_model >>${log_path}/${example}_export_model 2>&1
         print_info $? ${example}_export_model
     # INFER
     elif [[ ${exec_file} == "infer.py" ]] || [[ ${exec_file} =~ "run_infer" ]] ;then
-        python ${exec_file} --device ${devices}  >${log_path}/${example}_predict >>${log_path}/${example}_predcit 2>&1
+        python ${exec_file} --device ${devices} --infer_input_dir ${export_output_dir} >${log_path}/${example}_predict >>${log_path}/${example}_predcit 2>&1
         print_info $? ${example}_predict
     # CUSTOM
     elif [[ ${exec_file} =~ "run_" ]] && [[ ${exec_file##*.} == "py" ]];then
-        python ${exec_file}  >${log_path}/${example}_${exec_file} >>${log_path}/${example}_${exec_file} 2>&1
+        python ${exec_file} >${log_path}/${example}_${exec_file} >>${log_path}/${example}_${exec_file} 2>&1
         print_info $? ${example}_${exec_file}
     elif [[ ${exec_file} =~ "run_" ]] && [[ ${exec_file##*.} == "sh" ]];then
         bash ${exec_file}  >${log_path}/${example}_${exec_file} >>${log_path}/${example}_${exec_file} 2>&1
