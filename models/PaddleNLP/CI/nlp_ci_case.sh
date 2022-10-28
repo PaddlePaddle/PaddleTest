8e8f84e36bf2e5858b86bd3e5a0eb8046c6fa29e
zjjlivein <1834792141@qq.com>
2022-10-28 10:40:18 +0800

    remove pretrained_models run_mode=model

--- models/PaddleNLP/CI/nlp_ci_case.sh
+++ models/PaddleNLP/CI/nlp_ci_case.sh
@@ -1036,7 +1036,7 @@ for key in $(echo ${!all_P0case_dic[*]});do
 done
 if [[ ${Testcase} == 'null' ]];then
     P0case_list=(waybill_ie msra_ner glue bert skep bigbird electra gpt ernie-1.0 xlnet ofa  squad tinybert lexical_analysis seq2seq \
-    pretrained_models word_embedding ernie-ctm distilbert stacl transformer pet simbert ernie-doc transformer-xl pointer_summarizer question_matching ernie-csc \
+     word_embedding ernie-ctm distilbert stacl transformer pet simbert ernie-doc transformer-xl pointer_summarizer question_matching ernie-csc \
     nptag ernie-m clue taskflow)
 else
     P0case_list=${Testcase}
