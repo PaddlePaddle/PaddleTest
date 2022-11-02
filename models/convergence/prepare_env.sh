1e6c5c90dc89300e98304f4b6387428fb1ee83e9
JiaXiao243 <jiaxiao243@126.com>
2022-11-02 06:45:56 +0000

    fix code style, run_mode=model

--- models/convergence/prepare_env.sh
+++ models/convergence/prepare_env.sh
@@ -6,10 +6,10 @@ echo python_version;
 echo export LD_LIBRARY_PATH=/opt/_internal/cpython-3.8.0/lib/:/usr/local/ssl/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64 >> /etc/profile
 echo export PATH=/opt/_internal/cpython-3.8.0/bin/:/usr/local/ssl:/usr/local/go/bin:/root/gopath/bin:/usr/local/gcc-8.2/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin >> /etc/profile
 source /etc/profile
-python -c 'import sys; print(sys.version_info[:])';      
+python -c 'import sys; print(sys.version_info[:])';
 
 # paddle
-python -m pip install -U https://paddle-qa.bj.bcebos.com/paddle-pipeline/Release-GpuAll-LinuxCentos-Gcc82-Cuda112-Trton-Py38-Compile/latest/paddlepaddle_gpu-0.0.0-cp38-cp38-linux_x86_64.whl 
+python -m pip install -U https://paddle-qa.bj.bcebos.com/paddle-pipeline/Release-GpuAll-LinuxCentos-Gcc82-Cuda112-Trton-Py38-Compile/latest/paddlepaddle_gpu-0.0.0-cp38-cp38-linux_x86_64.whl
 python -c 'import paddle;print(paddle.__version__,paddle.version.commit)'
 
 # nfs
