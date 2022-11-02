fc63551330e3cd3b0f117be346cefb751c7d0893
zjjlivein <1834792141@qq.com>
2022-11-02 20:22:51 +0800

    add allure report run_mode=model

---
+++ models/PaddleNLP/AIstudio/gen_allure_report.py
@@ -0,0 +1,117 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+"""
+/***************************************************************************
+  *
+  * Copyright (c) 2022 Baidu.com, Inc. All Rights Reserved
+  * @file
+  * @author jiaxiao01
+  * @date 2022/9/6 3:46 PM
+  * @brief  allure config
+  *
+  **************************************************************************/
+"""
+
+import os
+import sys
+import platform
+import subprocess
+import zipfile
+import stat
+import shutil
+import wget
+
+
+def make_tar(source_dir, output_filename):
+    """
+    打包文件夹
+    """
+    zipf = zipfile.ZipFile(output_filename, "w")
+    pre_len = len(os.path.dirname(source_dir))
+    for parent, dirnames, filenames in os.walk(source_dir):
+        for filename in filenames:
+            pathfile = os.path.join(parent, filename)
+            arcname = pathfile[pre_len:].strip(os.path.sep)
+            zipf.write(pathfile, arcname)
+    zipf.close()
+
+
+def gen_allure_report():
+    """
+    install allure
+    """
+    exit_code, output = subprocess.getstatusoutput("allure --version")
+    if exit_code == 0:
+        print("allure version is:{}".format(output))
+        allure_bin = "allure"
+    else:
+        sysstr = platform.system()
+        bin_src = "https://xly-devops.bj.bcebos.com/tools/allure-2.19.0.zip"
+        bin_file = wget.download(bin_src)
+        zip_file = zipfile.ZipFile(bin_file, "a")
+        zip_file.extractall()
+        allure_bin_f = "%s/allure-2.19.0/bin/allure" % (os.getcwd())
+        st = os.stat(allure_bin_f)
+        os.chmod(allure_bin_f, st.st_mode | stat.S_IEXEC)
+        if sysstr == "Linux":
+            # 验证完成分布式/mac/linux物理机已经完成java安装，暂时取消java安装
+            # 没有root权限ln -s会失败
+            allure_bin = "%s/allure-2.19.0/bin/allure" % (os.getcwd())
+            # 额外check下是否安装java
+            exit_code, output = subprocess.getstatusoutput("java -version")
+            if exit_code == 0:
+                print("java version is:{}".format(output))
+            else:
+                plat = platform.dist()[0].lower()
+                retry_num = 0
+                ret = 0
+                while True:
+                    retry_num += 1
+                    if plat == "ubuntu":
+                        ret = os.system(
+                            "apt-get update;apt install -y openjdk-8-jdk >/dev/null")
+                    elif plat == "centos":
+                        ret = os.system(
+                            "yum install java-1.8.0-openjdk-devel.x86_64 -y >/dev/null")
+                    if ret == 0 or retry_num > 3:
+                        break
+        # windows和mac xly均已安装java
+        elif sysstr == "Darwin":
+            allure_bin = "%s/allure-2.19.0/bin/allure" % (os.getcwd())
+        elif sysstr == "Windows":
+            cmd = "set PATH=%s/allure-2.19.0/bin/allure;%%PATH%%" % (
+                os.getcwd())
+            os.system(cmd)
+            # allure_bin = "allure"
+            allure_bin = "%s/allure-2.19.0/bin/allure" % (os.getcwd())
+    exit_code, output = subprocess.getstatusoutput("%s --version" % allure_bin)
+    if exit_code == 0:
+        print("allure version is:{}".format(output))
+        cmd = "%s generate result -o report" % allure_bin
+        ret = os.system(cmd)
+        if ret:
+            print("allure generate report failed")
+        else:
+            print("allure generate report sucess")
+        REPORT_SERVER = "https://xly.bce.baidu.com/ipipe/ipipe-report"
+        os.environ["REPORT_SERVER_USERNAME"] = "paddle"
+        os.environ["REPORT_SERVER_PASSWORD"] = "915eedab953b6f51151f50eb"
+        os.environ["REPORT_SERVER"] = REPORT_SERVER
+        job_build_id = os.getenv("AGILE_JOB_BUILD_ID")
+        if job_build_id:
+            if os.path.exists("result"):
+                make_tar("result", "result.tar")
+                shutil.move("result.tar", "report")
+            cmd = "curl -s {}/report/upload.sh | bash -s ./report {} report".format(
+                REPORT_SERVER, job_build_id)
+            print("upload cmd is {}".format(cmd))
+            ret = os.system(cmd)
+        return ret
+    else:
+        print("allure is not config correctly:{}, please config allure manually!".format(output))
+        return 1
+
+
+if __name__ == "__main__":
+    gen_allure_report()
--- models/PaddleNLP/AIstudio/test_paddlenlp_aistudio.py
+++ models/PaddleNLP/AIstudio/test_paddlenlp_aistudio.py
@@ -9,24 +9,31 @@ import sys
 import subprocess
 import logging
 import allure
+import pytest
+import json
 
 
-def save_log(exit_code, output, model, log_dir=""):
+def exit_check(exit_code, file_name,project_name):
+    """
+    check exit_code
+    """
+    assert exit_code == 0, "ProjectID: %s %s Failed!" % (file_name,project_name)
+
+
+def save_log(exit_code, output, file_name, log_dir=""):
     """
     Save log for disply
     """
     if exit_code == 0:
-        logging.info("%s passed" % (model))
-        log_dir = os.getcwd() + "/log/" + os.path.join(model + "_success.log")
+        log_dir = os.getcwd() + "/log/" + os.path.join(file_name + "_success.log")
         with open(log_dir, "a") as flog:
             flog.write("%s" % (output))
-            # allure.attach.file(log_dir, '执行日志', allure.attachment_type.TEXT)
+            allure.attach.file(log_dir, '执行日志', allure.attachment_type.TEXT)
     else:
-        logging.info("%s failed" % (model))
-        log_dir = os.getcwd() + "/log/" + os.path.join(model + "_err.log")
+        log_dir = os.getcwd() + "/log/" + os.path.join(file_name + "_err.log")
         with open(log_dir, "a") as flog:
             flog.write("%s" % (output))
-            # allure.attach.file(log_dir, '执行日志', allure.attachment_type.TEXT)
+            allure.attach.file(log_dir, '执行日志', allure.attachment_type.TEXT)
 
 
 def download_project_files():
@@ -39,30 +46,43 @@ def download_project_files():
         flog.write("%s" % (output[1]))
     assert output[0] == 0, "download failed !"
 
+    
 
-def test_aistudio_case():
+def get_project_list():
     """
-    EXEC AIstudio main.ipynb
+    get aistudio_projects_files
     """
     download_project_files()
+    project_list = []
     path = os.listdir(os.getcwd() + "/aistudio_projects_files/")
     for file in path:
         file_name = os.path.splitext(file)[0]
-        file_path = os.getcwd() + "/aistudio_projects_files/" + file
-        os.system("cd {}".format(file_path))
-        if os.path.exists(os.path.join(file_path + "/main.ipynb")):
-            exec_name = "main"
-        else:
-            exec_name = file_name
-        if not os.path.exists(os.path.join(file_path + "/data")):
-            os.system("cd %s && mkdir data " % (file_path))
-        os.system("cd %s && jupyter nbconvert --to python %s.ipynb" % (file_path, exec_name))
-        output = subprocess.getstatusoutput("cd %s && ipython %s.py" % (file_path, exec_name))
-        # allure.dynamic.feature(file_name)
-        # allure.dynamic.description(
-        # "启动命令: ipython {exec_name}.py".format(exec_name))
-        save_log(output[0], output[1], file_name)
+        project_list.append(file_name)
+    return project_list
 
 
-if __name__ == "__main__":
-    test_aistudio_case()
+@pytest.mark.parametrize('file_name', get_project_list())
+def test_aistudio_case(file_name):
+    """
+    EXEC AIstudio main.ipynb
+    """
+    file_path = os.getcwd() + "/aistudio_projects_files/" + file_name
+    project_name = json.load(open('project_info.json','r',encoding="utf-8"))[file_name]
+    os.system("cd {}".format(file_path))
+    if os.path.exists(os.path.join(file_path + "/main.ipynb")):
+        exec_name = "main"
+    else:
+        exec_name = file_name
+    if not os.path.exists(os.path.join(file_path + "/data")):
+        os.system("cd %s && mkdir data " % (file_path))
+    os.system("cd %s && jupyter nbconvert --to python %s.ipynb" % (file_path, exec_name))
+    output = subprocess.getstatusoutput("cd %s && ipython %s.py" % (file_path, exec_name))
+
+    allure.dynamic.parent_suite("PaddleNLP AIstudio")
+    allure.dynamic.title("{}".format(file_name))
+    allure.dynamic.feature(project_name)
+    allure.dynamic.description(
+    "启动命令: ipython {}.py".format(exec_name))
+
+    save_log(output[0], output[1], file_name)
+    exit_check(output[0], file_name,project_name)
