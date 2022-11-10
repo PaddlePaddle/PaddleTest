697cb128838148c080ad7dc9a9000d01b2b4602f
root <root@yq01-sys-hic-k8s-v100-box-a225-0091.yq01.baidu.com>
2022-11-10 08:30:16 +0000

    update rec and slim case

--- models_restruct/PaddleSlim/tools/case_start.py
+++ models_restruct/PaddleSlim/tools/case_start.py
@@ -14,21 +14,44 @@ class PaddleSlim_Case_Start(object):
         self.reponame = os.environ["reponame"]
         self.REPO_PATH = os.path.join(os.getcwd(), self.reponame)
         self.step = os.environ["step"]
+        # PaddleSlim: step = 'train:single,multi+eval:single,multi'
+        self.case_step = os.environ["case_step"]
+        self.case_name = os.environ["case_name"]
 
 
-def run(function_name):
+def run():
     paddleslim_case_start = PaddleSlim_Case_Start()
     current_path = os.getcwd()
-    with open(os.path.join(paddleslim_case_start.REPO_PATH, paddleslim_case_start.rd_yaml_path), "r") as f:
+    currnet_step = paddleslim_case_start.case_step
+    current_name = paddleslim_case_start.case_name
+
+    print("*******")
+    print(paddleslim_case_start.step)
+    print(currnet_step)
+    print(current_name)
+    print("*******")
+
+    rd_yaml = os.path.join(paddleslim_case_start.REPO_PATH, paddleslim_case_start.rd_yaml_path)
+    qa_yaml = paddleslim_case_start.qa_yaml_name
+    with open(rd_yaml, "r") as f:
         content = yaml.load(f, Loader=yaml.FullLoader)
 
-    if paddleslim_case_start.step == "eval" and paddleslim_case_start.qa_yaml_name == "example:auto_compression:nlp:configs:ernie3.0:afqmc.yaml":
+    if currnet_step == "eval" and qa_yaml == "example^auto_compression^nlp^configs^ernie3.0^afqmc":
         content["Global"]["model_dir"] = "./save_afqmc_ERNIE_pruned"
-    elif paddleslim_case_start.step == "eval" and paddleslim_case_start.qa_yaml_name == "example:auto_compression:nlp:configs:pp-minilm:auto:afqmc.yaml":
+    elif currnet_step == "eval" and qa_yaml == "example^auto_compression^nlp^configs^pp-minilm^auto^afqmc":
         content["Global"]["model_dir"] = "./save_afqmc_pp_minilm_pruned"
+    elif currnet_step == "eval" and qa_yaml == "example^post_training_quantization^pytorch_yolo_series^configs^yolov6s_fine_tune":
+        if current_name == "single":
+            content["model_dir"] = "region_ptq_out"
+        else:
+            content["model_dir"] = "layer_ptq_out"
+
     
     else:
-        logger.info("### {} no update required".format(paddleslim_case_start.rd_yaml_path))
+        logger.info("### {} no update required".format(rd_yaml))
+
+    with open(rd_yaml, "w") as f:
+        yaml.dump(content, f)
 
 
 if __name__ == "__main__":
--- models_restruct/PaddleSlim/tools/start.py
+++ models_restruct/PaddleSlim/tools/start.py
@@ -42,6 +42,18 @@ class PaddleSlim_Start(object):
                 logger.info("*** {} end download and tar -x ".format(tar_name))
             except:
                 logger.info("*** {} start download or  tar -x failed".format(tar_name))
+
+    def wget_and_files(self,wget_url):
+        file_name = wget_url.split("/")[-1]
+        if os.path.exists(file_name):
+            logger.info("*** {} already download ".format(file_name))
+        else:
+            try:
+                logger.info("*** {} start download ".format(file_name))
+                wget.download(wget_url)
+                logger.info("*** {} end download  ".format(file_name))
+            except:
+                logger.info("*** {} start download failed".format(file_name))
     
     def update_yaml_config(self, file_path, old_str, new_str):
         file_data = ""
@@ -56,72 +68,79 @@ class PaddleSlim_Start(object):
 def run():
     paddleslim_start = PaddleSlim_Start()
     current_path = os.getcwd()
-    with open(os.path.join(paddleslim_start.REPO_PATH, paddleslim_start.rd_yaml_path), "r") as f:
+    rd_yaml = os.path.join(paddleslim_start.REPO_PATH, paddleslim_start.rd_yaml_path)
+    qa_yaml = paddleslim_start.qa_yaml_name
+    with open(rd_yaml, "r") as f:
         content = yaml.load(f, Loader=yaml.FullLoader)
 
-    if paddleslim_start.qa_yaml_name == "example:auto_compression:detection:configs:ppyoloe_l_qat_dis":
+    if qa_yaml == "example^auto_compression^detection^configs^ppyoloe_l_qat_dis":
         paddleslim_start.wget_and_tar("https://bj.bcebos.com/v1/paddle-slim-models/act/ppyoloe_crn_l_300e_coco.tar")
         paddleslim_start.wget_and_zip("https://paddle-qa.bj.bcebos.com/PaddleDetection/coco.zip")
         content["TrainConfig"]["train_iter"] = 20
         content["TrainConfig"]["eval_iter"] = 10
         content["Global"]["model_dir"] = current_path + "/ppyoloe_crn_l_300e_coco"
-    elif paddleslim_start.qa_yaml_name == "example:auto_compression:pytorch_yolo_series:configs:yolov5s_qat_dis":
-        paddleslim_start.wget_and_tar("https://paddle-slim-models.bj.bcebos.com/act/yolov5s.onnx")
+    elif qa_yaml == "example^auto_compression^pytorch_yolo_series^configs^yolov5s_qat_dis":
+        paddleslim_start.wget_and_files("https://paddle-slim-models.bj.bcebos.com/act/yolov5s.onnx")
         paddleslim_start.wget_and_zip("https://paddle-qa.bj.bcebos.com/PaddleDetection/coco.zip")
         content["TrainConfig"]["train_iter"] = 20
         content["TrainConfig"]["eval_iter"] = 10
         content["Global"]["model_dir"] = current_path + "/yolov5s.onnx"
         content["Global"]["coco_dataset_dir"] = current_path + "/coco"
-    elif paddleslim_start.qa_yaml_name == "example:auto_compression:image_classification:configs:MobileNetV1:qat_dis":
+    elif qa_yaml == "example^auto_compression^image_classification^configs^MobileNetV1^qat_dis":
         paddleslim_start.wget_and_tar("https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/inference/MobileNetV1_infer.tar")
         paddleslim_start.wget_and_tar("https://paddle-qa.bj.bcebos.com/PaddleSlim_datasets/ILSVRC2012.tar")
         content["Global"]["data_dir"] = current_path + "/ILSVRC2012"
         content["Global"]["model_dir"] = current_path + "/MobileNetV1_infer"
         content["TrainConfig"]["epochs"] = 1
         content["TrainConfig"]["eval_iter"] = 50
-    elif paddleslim_start.qa_yaml_name == "example:auto_compression:image_classification:configs:ResNet50_vd:qat_dis":
+    elif qa_yaml == "example^auto_compression^image_classification^configs^ResNet50_vd^qat_dis":
         paddleslim_start.wget_and_tar("https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/inference/ResNet50_vd_infer.tar")
         paddleslim_start.wget_and_tar("https://paddle-qa.bj.bcebos.com/PaddleSlim_datasets/ILSVRC2012.tar")
         content["Global"]["data_dir"] = current_path + "/ILSVRC2012"
         content["Global"]["model_dir"] = current_path + "/ResNet50_vd_infer"
         content["TrainConfig"]["epochs"] = 1
         content["TrainConfig"]["eval_iter"] = 50
-    elif paddleslim_start.qa_yaml_name == "example:auto_compression:nlp:configs:ernie3.0:afqmc":
+    elif qa_yaml == "example^auto_compression^nlp^configs^ernie3.0^afqmc":
         paddleslim_start.wget_and_tar("https://bj.bcebos.com/v1/paddle-slim-models/act/NLP/ernie3.0-medium/fp32_models/AFQMC.tar")
         content["Global"]["model_dir"] = current_path + "/AFQMC"
         content["TrainConfig"]["epochs"] = 1
         content["TrainConfig"]["eval_iter"] = 50
-    elif paddleslim_start.qa_yaml_name == "example:auto_compression:nlp:configs:pp-minilm:auto:afqmc.yaml":
+    elif qa_yaml == "example^auto_compression^nlp^configs^pp-minilm^auto^afqmc":
         paddleslim_start.wget_and_tar("https://bj.bcebos.com/v1/paddle-slim-models/act/afqmc.tar")
         content["Global"]["model_dir"] = current_path + "/afqmc"
         content["TrainConfig"]["epochs"] = 1
         content["TrainConfig"]["eval_iter"] = 50
-        paddleslim_start.update_yaml_config(paddleslim_start.rd_yaml_path, "HyperParameterOptimization:", "#HyperParameterOptimization:")
-    elif paddleslim_start.qa_yaml_name == "example:auto_compression:semantic_segmentation:configs:pp_liteseg:pp_liteseg_qat.yaml" or \
-            paddleslim_start.qa_yaml_name == "example:auto_compression:semantic_segmentation:configs:pp_liteseg:pp_liteseg_sparse":
+        paddleslim_start.update_yaml_config(rd_yaml, "HyperParameterOptimization:", "#HyperParameterOptimization:")
+    elif qa_yaml == "example^auto_compression^semantic_segmentation^configs^pp_liteseg^pp_liteseg_qat" or \
+            paddleslim_start.qa_yaml_name == "example^auto_compression^semantic_segmentation^configs^pp_liteseg^pp_liteseg_sparse":
         paddleslim_start.wget_and_tar("https://bj.bcebos.com/v1/paddle-slim-models/data/mini_cityscapes/mini_cityscapes.tar")
         paddleslim_start.wget_and_zip("https://paddleseg.bj.bcebos.com/tipc/easyedge/RES-paddle2-PPLIteSegSTDC1.zip")
         content["Global"]["model_dir"] = current_path + "/RES-paddle2-PPLIteSegSTDC1"
         content["TrainConfig"]["epochs"] = 1
         content["TrainConfig"]["eval_iter"] = 50
-    elif paddleslim_start.qa_yaml_name == "example:full_quantization:image_classification:configs:mobilenetv3_large_qat_dis":
+    elif qa_yaml == "example^full_quantization^image_classification^configs^mobilenetv3_large_qat_dis":
         paddleslim_start.wget_and_tar("https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/inference/MobileNetV3_large_x1_0_infer.tar")
         paddleslim_start.wget_and_tar("https://paddle-qa.bj.bcebos.com/PaddleSlim_datasets/ILSVRC2012.tar")
         content["Global"]["model_dir"] = current_path + "/MobileNetV3_large_x1_0_infer"
         content["Global"]["data_dir"] = current_path + "/ILSVRC2012"
         content["TrainConfig"]["epochs"] = 1
         content["TrainConfig"]["eval_iter"] = 50
-    elif paddleslim_start.qa_yaml_name == " example:full_quantization:picodet:configs:picodet_npu_with_postprocess":
+    elif qa_yaml == "example^full_quantization^picodet^configs^picodet_npu_with_postprocess":
         paddleslim_start.wget_and_tar("https://bj.bcebos.com/v1/paddle-slim-models/act/picodet_s_416_coco_npu.tar")
         paddleslim_start.wget_and_zip("https://paddle-qa.bj.bcebos.com/PaddleDetection/coco.zip")
         content["Global"]["model_dir"] = current_path + "/picodet_s_416_coco_npu"
         content["Global"]["data_dir"] = current_path + "/ILSVRC2012"
         content["TrainConfig"]["train_iter"] = 80
         content["TrainConfig"]["eval_iter"] = 10
+    elif qa_yaml == "example^post_training_quantization^pytorch_yolo_series^configs^yolov6s_fine_tune":
+        paddleslim_start.wget_and_files("https://paddle-slim-models.bj.bcebos.com/act/yolov6s.onnx")
+        paddleslim_start.wget_and_zip("https://paddle-qa.bj.bcebos.com/PaddleDetection/coco.zip")
+        content["model_dir"] = current_path + "/picodet_s_416_coco_npu"
+        content["dataset_dir"] = current_path + "/ILSVRC2012"
     else:
-        logger.info("### no exists {} ".format(paddleslim_start.qa_yaml_name))
+        logger.info("### no exists {} ".format(qa_yaml))
 
-    with open(os.path.join(paddleslim_start.REPO_PATH, paddleslim_start.rd_yaml_path), "w") as f:
+    with open(rd_yaml, "w") as f:
         yaml.dump(content, f)
     
     # example/auto_compression:detection:configs:ppyoloe_l_qat_dis 修改数据路径
@@ -129,11 +148,11 @@ def run():
     paddleslim_start.update_yaml_config(ppyoloe_l_qat_dis_reader, "dataset_dir: dataset/coco/", "dataset_dir: " + current_path + '/coco')
 
     # auto_compression/semantic_segmentation demo 修改数据路径
-    semantic_segmentation_reader = "example/auto_compression/semantic_segmentation/configs/dataset/cityscapes_1024x512_scale1.0.yml"
-    with open(os.path.join(paddleslim_start.REPO_PATH, semantic_segmentation_reader), "r") as f_reader:
+    semantic_segmentation_reader = paddleslim_start.REPO_PATH + "/example/auto_compression/semantic_segmentation/configs/dataset/cityscapes_1024x512_scale1.0.yml"
+    with open(semantic_segmentation_reader, "r") as f_reader:
         content_reader = yaml.load(f_reader, Loader=yaml.FullLoader)
     content_reader["train_dataset"]["dataset_root"] = current_path + "/mini_cityscapes"
-    with open(os.path.join(paddleslim_start.REPO_PATH, semantic_segmentation_reader), "w") as f_reader:
+    with open(semantic_segmentation_reader, "w") as f_reader:
         yaml.dump(content_reader, f_reader)
 
     # full_quantization/picodet demo 修改数据路径
