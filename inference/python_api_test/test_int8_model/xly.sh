d735a418ebcc49a4461075390994ca138fe22e6e
zhengya01 <zhengya01@baidu.com>
2022-11-14 12:38:27 +0000

    add xly file,  test=model

--- inference/python_api_test/test_int8_model/xly.sh
+++ inference/python_api_test/test_int8_model/xly.sh
@@ -32,12 +32,12 @@ nvidia-docker run -i --rm \
     -e "no_proxy=bcebos.com,goproxy.cn,baidu.com,bcebos.com" \
     --net=host \
     ${DOCKER_IMAGE} \
-     /bin/bash -c -x "
+     /bin/bash -c -x '
 
 
 export LD_LIBRARY_PATH=/opt/_internal/cpython-3.8.0/lib/:${LD_LIBRARY_PATH}
 export PATH=/opt/_internal/cpython-3.8.0/bin/:${PATH}
-export PYTHON_FLAGS='-DPYTHON_EXECUTABLE:FILEPATH=/opt/_internal/cpython-3.8.0/bin/python3.8 -DPYTHON_INCLUDE_DIR:PATH=/opt/_internal/cpython-3.8.0/include/python3.8 -DPYTHON_LIBRARIES:FILEPATH=/opt/_internal/cpython-3.8.0/lib/libpython3.so'
+export PYTHON_FLAGS="-DPYTHON_EXECUTABLE:FILEPATH=/opt/_internal/cpython-3.8.0/bin/python3.8 -DPYTHON_INCLUDE_DIR:PATH=/opt/_internal/cpython-3.8.0/include/python3.8 -DPYTHON_LIBRARIES:FILEPATH=/opt/_internal/cpython-3.8.0/lib/libpython3.so"
 
 export LD_LIBRARY_PATH=${PWD}/TensorRT-8.4.0.6/lib/:${LD_LIBRARY_PATH}
 
@@ -50,4 +50,5 @@ pip install -r requirements.txt
 pip install -U ${PADDLE_WHL}
 
 bash run.sh
-"
+
+'
