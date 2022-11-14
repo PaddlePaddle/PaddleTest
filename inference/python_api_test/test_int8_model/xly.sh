8d2f9fc4004f96c5e139a9c407f97bbda54a2f64
zhengya01 <zhengya01@baidu.com>
2022-11-14 12:37:34 +0000

    add xly file,  test=model

--- inference/python_api_test/test_int8_model/xly.sh
+++ inference/python_api_test/test_int8_model/xly.sh
@@ -34,11 +34,16 @@ nvidia-docker run -i --rm \
     ${DOCKER_IMAGE} \
      /bin/bash -c -x "
 
+
 export LD_LIBRARY_PATH=/opt/_internal/cpython-3.8.0/lib/:${LD_LIBRARY_PATH}
 export PATH=/opt/_internal/cpython-3.8.0/bin/:${PATH}
 export PYTHON_FLAGS='-DPYTHON_EXECUTABLE:FILEPATH=/opt/_internal/cpython-3.8.0/bin/python3.8 -DPYTHON_INCLUDE_DIR:PATH=/opt/_internal/cpython-3.8.0/include/python3.8 -DPYTHON_LIBRARIES:FILEPATH=/opt/_internal/cpython-3.8.0/lib/libpython3.so'
 
 export LD_LIBRARY_PATH=${PWD}/TensorRT-8.4.0.6/lib/:${LD_LIBRARY_PATH}
+
+
+python -m pip install --retries 50 --upgrade pip -i https://mirror.baidu.com/pypi/simple
+python -m pip config set global.index-url https://mirror.baidu.com/pypi/simple;
 
 pip install -r requirements.txt
 
