187dc90e994cd3c6ab7205e9cabb15fceef7b22b
zhengya01 <zhengya01@baidu.com>
2022-11-14 12:30:19 +0000

    add xly file,  test=model

--- inference/python_api_test/test_int8_model/xly.sh
+++ inference/python_api_test/test_int8_model/xly.sh
@@ -36,6 +36,7 @@ nvidia-docker run -i --rm \
 
 export LD_LIBRARY_PATH=/opt/_internal/cpython-3.8.0/lib/:${LD_LIBRARY_PATH}
 export PATH=/opt/_internal/cpython-3.8.0/bin/:${PATH}
+export PYTHON_FLAGS="-DPYTHON_EXECUTABLE:FILEPATH=/opt/_internal/cpython-3.8.0/bin/python3.8 -DPYTHON_INCLUDE_DIR:PATH=/opt/_internal/cpython-3.8.0/include/python3.8 -DPYTHON_LIBRARIES:FILEPATH=/opt/_internal/cpython-3.8.0/lib/libpython3.so"
 
 export LD_LIBRARY_PATH=${PWD}/TensorRT-8.4.0.6/lib/:${LD_LIBRARY_PATH}
 
