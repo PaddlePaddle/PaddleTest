9c25d22fb19ac6b5926295784de296edf31869ee
root <root@yq01-sys-hic-k8s-v100-qm-a223-0008.yq01.baidu.com>
2022-11-15 03:52:35 +0000

    update slim case 11.15

--- models_restruct/PaddleSlim/tools/case_start.py
+++ models_restruct/PaddleSlim/tools/case_start.py
@@ -44,13 +44,13 @@ def run():
                 content["model_dir"] = "layer_ptq_out"
         elif qa_yaml == "example^auto_compression^pytorch_yolo_series^configs^yolov5s_qat_dis" and current_name == "single":
             os.environ["CUDA_VISIBLE_DEVICES"] = set_cuda_single_card
-        elif qa_yaml == "case^demo^quant^pact_quant_aware":
-            os.environ["CUDA_VISIBLE_DEVICES"] = set_cuda_single_card
         else:
             logger.info("******* {} no update required".format(rd_yaml))
 
         with open(rd_yaml, "w") as f:
             yaml.dump(content, f)
+    elif qa_yaml == "case^demo^quant^pact_quant_aware":
+            os.environ["CUDA_VISIBLE_DEVICES"] = set_cuda_single_card
     else:
         logger.info("******* yaml：{} no exists".format(rd_yaml))
 
