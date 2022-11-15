e6ef2178c4eaef645b654b454e4001e182558006
zhengya01 <43601548+zhengya01@users.noreply.github.com>
2022-11-15 10:52:17 +0800

    add xly file (#1640)
    * update benchmark log ,  test=model
    
    * update benchmark log ,  test=model
    
    * update benchmark log ,  test=model
    
    * update benchmark log ,  test=model
    
    * update benchmark log ,  test=model
    
    * update benchmark log ,  test=model
    
    * update benchmark log ,  test=model
    
    * update benchmark log ,  test=model
    
    * update benchmark log ,  test=model
    
    * update benchmark log ,  test=model
    
    * add log manage file,  test=model
    
    * add log manage file,  test=model
    
    * add log manage file,  test=model
    
    * add log manage file,  test=model
    
    * add xly file,  test=model
    
    * add xly file,  test=model
    
    * add xly file,  test=model
    
    * add xly file,  test=model
    
    * add xly file,  test=model
    
    * add xly file,  test=model
    
    * add xly file,  test=model
    
    * add xly file,  test=model
    
    * add xly file,  test=model
    
    * add xly file,  test=model

---
+++ inference/python_api_test/test_int8_model/run.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+bash prepare.sh
+
+bash run_trt_int8.sh > eval_trt_int8_acc.log.tmp 2>&1
+bash run_trt_int8.sh > eval_trt_int8_acc.log 2>&1
+
+bash run_trt_fp16.sh > eval_trt_fp16_acc.log.tmp 2>&1
+bash run_trt_fp16.sh > eval_trt_fp16_acc.log 2>&1
+
+bash run_mkldnn_int8.sh > eval_mkldnn_int8_acc.log 2>&1
+
+bash run_mkldnn_fp32.sh > eval_mkldnn_fp32_acc.log 2>&1
+
+python get_benchmark_info.py
