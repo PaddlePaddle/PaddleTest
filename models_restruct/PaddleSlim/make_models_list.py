a2b7310217ed27dc7e2527c0a81c7ccc2129d70e
iamWHTWD <iamwhtwd@163.com>
2022-11-16 06:54:46 +0000

    add slim case,test=model

--- models_restruct/PaddleSlim/make_models_list.py
+++ models_restruct/PaddleSlim/make_models_list.py
@@ -1,11 +1,37 @@
 import os
 import yaml
-path = "/workspace/PaddleTest_wht/models_restruct/PaddleSlim/cases"
-files = os.listdir(path)
 
-file_data = ""
+cases_path = os.getcwd() + "/cases"
+files = os.listdir(cases_path)
+
+file_data_linux = ""
+file_data_windows = ""
+file_data_mac = ""
+
 for file in files:
-	with open(path +"/"+ file, "r", encoding="utf-8") as f:
+	with open(cases_path +"/"+ file, "r", encoding="utf-8") as f:
 		content = yaml.load(f, Loader=yaml.FullLoader)
+		if content["case"].get("windows"):
+			if file_data_windows=="":
+				file_data_windows += file
+			else:
+				file_data_windows += "\n" + file
 		if content["case"].get("mac"):
-			print(file)
+			if file_data_mac=="":
+				file_data_mac += file
+			else:
+				file_data_mac += "\n" + file
+	
+	if file_data_linux=="":
+		file_data_linux += file
+	else:
+		file_data_linux += "\n" + file
+
+with open("models_list_windows.txt", "w", encoding="utf-8") as f:
+	f.write(file_data_windows)
+
+with open("models_list_mac.txt", "w", encoding="utf-8") as f:
+	f.write(file_data_mac)
+
+with open("models_list_linux.txt", "w", encoding="utf-8") as f:
+	f.write(file_data_linux)
