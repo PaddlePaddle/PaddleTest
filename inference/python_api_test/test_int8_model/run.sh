047b50c9e52011d37b84463b13857b17beda4cff
zhengya01 <zhengya01@baidu.com>
2022-11-16 06:06:41 +0000

    add xly file,  test=model

--- inference/python_api_test/test_int8_model/run.sh
+++ inference/python_api_test/test_int8_model/run.sh
@@ -2,11 +2,20 @@
 
 bash prepare.sh
 
+mv models models.bak
+cp -r models.bak models
+
 bash run_trt_int8.sh > eval_trt_int8_acc.log.tmp 2>&1
 bash run_trt_int8.sh > eval_trt_int8_acc.log 2>&1
 
+rm -rf models
+cp -r models.bak models
+
 bash run_trt_fp16.sh > eval_trt_fp16_acc.log.tmp 2>&1
 bash run_trt_fp16.sh > eval_trt_fp16_acc.log 2>&1
+
+rm -rf models
+cp -r models.bak models
 
 bash run_mkldnn_int8.sh > eval_mkldnn_int8_acc.log 2>&1
 
