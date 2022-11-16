d9d1c1bfb780385ba4bb526afa2e05048e9c6032
quanxiang-liu <qxliunj@163.com>
2022-11-16 13:11:51 +0000

    add gan value,  test=model

--- models_restruct/PaddleGAN/tools/start.py
+++ models_restruct/PaddleGAN/tools/start.py
@@ -47,7 +47,6 @@ class PaddleGAN_Start(object):
         下载数据集
         """
         # 调用函数路径已切换至PaddleGAN
-
         tar_name = value.split("/")[-1]
         # if os.path.exists(tar_name) and os.path.exists(tar_name.replace(".tar", "")):
         # 有end回收数据, 只判断文件夹
@@ -76,7 +75,83 @@ class PaddleGAN_Start(object):
         """
         获取模型输出路径
         """
-        pass
+        # 获取kpi 的标签
+        if "singan_sr" in self.qa_yaml_name:
+            self.kpi_value_eval = "scale9/D_gradient_penalty"
+        elif "singan_universal" in self.qa_yaml_name or "singan_animation" in self.qa_yaml_name:
+            self.kpi_value_eval = "scale6/D_gradient_penalty"
+        elif "singan_finetune" in self.qa_yaml_name:
+            self.kpi_value_eval = "scale1/D_gradient_penalty"
+        elif (
+            "firstorder_vox_mobile_256" in self.qa_yaml_name
+            or "firstorder_vox_256" in self.qa_yaml_name
+            or "firstorder_fashion" in self.qa_yaml_name
+        ):
+            self.kpi_value_eval = "perceptual"
+        elif (
+            "basicvsr_reds" in self.qa_yaml_name
+            or "basicvsr++_vimeo90k_BD" in self.qa_yaml_name
+            or "lesrcnn_psnr_x4_div2k" in self.qa_yaml_name
+            or "edvr_l_wo_tsa" in self.qa_yaml_name
+            or "basicvsr++_reds" in self.qa_yaml_name
+            or "pan_psnr_x4_div2k" in self.qa_yaml_name
+            or "iconvsr_reds" in self.qa_yaml_name
+            or "edvr_l_w_tsa" in self.qa_yaml_name
+            or "edvr_m_w_tsa" in self.qa_yaml_name
+            or "esrgan_psnr_x4_div2k" in self.qa_yaml_name
+            or "edvr_m_wo_tsa" in self.qa_yaml_name
+            or "esrgan_psnr_x2_div2k" in self.qa_yaml_name
+        ):
+            self.kpi_value_eval = "loss_pixel"
+        elif (
+            "msvsr_vimeo90k_BD" in self.qa_yaml_name
+            or "realsr_bicubic_noise_x4_df2k" in self.qa_yaml_name
+            or "realsr_kernel_noise_x4_dped" in self.qa_yaml_name
+            or "msvsr_reds" in self.qa_yaml_name
+            or "esrgan_x4_div2k" in self.qa_yaml_name
+        ):
+            self.kpi_value_eval = "loss_pix"
+        elif "drn_psnr_x4_div2k" in self.qa_yaml_name:
+            self.kpi_value_eval = "loss_dual"
+        elif "lapstyle_draft" in self.qa_yaml_name:
+            self.kpi_value_eval = "loss_c"
+        elif "mprnet_denoising" in self.qa_yaml_name or "mprnet_deraining" in self.qa_yaml_name:
+            self.kpi_value_eval = "loss"
+        elif "stylegan_v2_256_ffhq" in self.qa_yaml_name:
+            self.kpi_value_eval = "l_d"
+        elif "animeganv2_pretrain" in self.qa_yaml_name:
+            self.kpi_value_eval = "init_c_loss"
+        elif "cyclegan_cityscapes" in self.qa_yaml_name or "cyclegan_horse2zebra" in self.qa_yaml_name:
+            self.kpi_value_eval = "G_idt_A_loss"
+        elif "photopen" in self.qa_yaml_name:
+            self.kpi_value_eval = "g_featloss"
+        elif "starganv2_celeba_hq" in self.qa_yaml_name or "starganv2_afhq" in self.qa_yaml_name:
+            self.kpi_value_eval = "G/latent_adv"
+        elif "ugatit_selfie2anime_light" in self.qa_yaml_name or "ugatit_photo2cartoon" in self.qa_yaml_name:
+            self.kpi_value_eval = "discriminator_loss"
+        elif "animeganv2" in self.qa_yaml_name:
+            self.kpi_value_eval = "d_loss"
+        elif (
+            "pix2pix_facades" in self.qa_yaml_name
+            or "pix2pix_cityscapes_2gpus" in self.qa_yaml_name
+            or "lapstyle_rev_first" in self.qa_yaml_name
+            or "lapstyle_rev_second" in self.qa_yaml_name
+            or "pix2pix_cityscapes" in self.qa_yaml_name
+        ):
+            self.kpi_value_eval = "D_fake_loss"
+        else:
+            logger.info("### use default kpi_value_eval loss")
+            self.kpi_value_eval = "loss"
+        return 0
+
+    def prepare_env(self):
+        """
+        下载预训练模型, 指定路径
+        """
+        self.get_params()  #  准备参数
+        print("###self.kpi_value_eval", self.kpi_value_eval)
+        input()
+        self.env_dict["kpi_value_eval"] = self.kpi_value_eval
         return 0
 
     def prepare_creat_yaml(self):
@@ -139,6 +214,10 @@ class PaddleGAN_Start(object):
         """
         # 进入repo中
         ret = 0
+        ret = self.prepare_env()
+        if ret:
+            logger.info("build prepare_env failed")
+            return ret
         ret = self.prepare_gpu_env()
         if ret:
             logger.info("build prepare_gpu_env failed")
