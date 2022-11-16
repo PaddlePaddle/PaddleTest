1f4d828053b47bddba650196dcb723fe720e3a18
zhengya01 <zhengya01@baidu.com>
2022-11-16 03:34:49 +0000

    add xly file,  test=model

--- inference/python_api_test/test_int8_model/test_nlp_infer.py
+++ inference/python_api_test/test_int8_model/test_nlp_infer.py
@@ -228,6 +228,8 @@ class Predictor(object):
                 config.collect_shape_range_info(dynamic_shape_file)
                 print("Start collect dynamic shape...")
                 cls.rerun_flag = True
+                if args.precision == "int8":
+                    cls.rerun_flag = False
 
         predictor = paddle.inference.create_predictor(config)
 
