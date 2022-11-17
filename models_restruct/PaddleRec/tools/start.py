0e18ade4734e2bf42327febe5dd9873de6da63fb
iamWHTWD <iamwhtwd@163.com>
2022-11-17 10:26:50 +0000

    update paddlerec case,test=model

--- models_restruct/PaddleRec/tools/start.py
+++ models_restruct/PaddleRec/tools/start.py
@@ -19,13 +19,12 @@ class PaddleRecStart(object):
         self.qa_yaml_name = os.environ["qa_yaml_name"]
         self.rd_yaml_path = os.environ["rd_yaml_path"]
         self.reponame = os.environ["reponame"]
-        self.REPO_PATH = os.path.join(os.getcwd(), self.reponame) 
+        self.REPO_PATH = os.path.join(os.getcwd(), self.reponame)
 
 
 def run():
     paddlerec_start = PaddleRecStart()
     current_path = os.getcwd()
-    rd_yaml = os.path.join(paddlerec_start.REPO_PATH, paddlerec_start.rd_yaml_path)
     qa_yaml = paddlerec_start.qa_yaml_name
     
     datasets_path = paddlerec_start.REPO_PATH + "/datasets"
@@ -41,8 +40,11 @@ def run():
         os.chdir("census")
     elif qa_yaml == "models^recall^ncf^config_bigdata":
         os.chdir("movielens_pinterest_NCF")
+    else:
+        logger.info("******* {} not exists".format(qa_yaml))
+
     os.system("bash run.sh")
     os.chdir(current_path)
-        
+
 if __name__ == "__main__":
     run()
