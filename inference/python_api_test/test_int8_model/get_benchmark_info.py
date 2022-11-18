ac0d1dadd10c4bcea3ca1b871dbb9e92f62f4210
zhengya01 <zhengya01@baidu.com>
2022-11-18 07:10:11 +0000

    add xly file,  test=model

--- inference/python_api_test/test_int8_model/get_benchmark_info.py
+++ inference/python_api_test/test_int8_model/get_benchmark_info.py
@@ -196,19 +196,63 @@ def res_summary(trt_int8, trt_fp16, mkldnn_int8, mkldnn_fp32):
             "g_ratio": 0,
         },
     }
-    gsb_total["jingdu"]["g"] = gsb_trt_int8["jingdu"]["g"] + gsb_trt_fp16["jingdu"]["g"] + gsb_mkldnn_int8["jingdu"]["g"] + gsb_mkldnn_fp32["jingdu"]["g"]
-    gsb_total["jingdu"]["s"] = gsb_trt_int8["jingdu"]["s"] + gsb_trt_fp16["jingdu"]["s"] + gsb_mkldnn_int8["jingdu"]["s"] + gsb_mkldnn_fp32["jingdu"]["s"]
-    gsb_total["jingdu"]["b"] = gsb_trt_int8["jingdu"]["b"] + gsb_trt_fp16["jingdu"]["b"] + gsb_mkldnn_int8["jingdu"]["b"] + gsb_mkldnn_fp32["jingdu"]["b"]
-    gsb_total["jingdu"]["total"] = gsb_trt_int8["jingdu"]["total"] + gsb_trt_fp16["jingdu"]["total"] + gsb_mkldnn_int8["jingdu"]["total"] + gsb_mkldnn_fp32["jingdu"]["total"]
-    gsb_total["jingdu"]["gsb"] = "{}:{}:{}".format(gsb_total["jingdu"]["g"], gsb_total["jingdu"]["s"], gsb_total["jingdu"]["b"])
+    gsb_total["jingdu"]["g"] = (
+        gsb_trt_int8["jingdu"]["g"]
+        + gsb_trt_fp16["jingdu"]["g"]
+        + gsb_mkldnn_int8["jingdu"]["g"]
+        + gsb_mkldnn_fp32["jingdu"]["g"]
+    )
+    gsb_total["jingdu"]["s"] = (
+        gsb_trt_int8["jingdu"]["s"]
+        + gsb_trt_fp16["jingdu"]["s"]
+        + gsb_mkldnn_int8["jingdu"]["s"]
+        + gsb_mkldnn_fp32["jingdu"]["s"]
+    )
+    gsb_total["jingdu"]["b"] = (
+        gsb_trt_int8["jingdu"]["b"]
+        + gsb_trt_fp16["jingdu"]["b"]
+        + gsb_mkldnn_int8["jingdu"]["b"]
+        + gsb_mkldnn_fp32["jingdu"]["b"]
+    )
+    gsb_total["jingdu"]["total"] = (
+        gsb_trt_int8["jingdu"]["total"]
+        + gsb_trt_fp16["jingdu"]["total"]
+        + gsb_mkldnn_int8["jingdu"]["total"]
+        + gsb_mkldnn_fp32["jingdu"]["total"]
+    )
+    gsb_total["jingdu"]["gsb"] = "{}:{}:{}".format(
+        gsb_total["jingdu"]["g"], gsb_total["jingdu"]["s"], gsb_total["jingdu"]["b"]
+    )
     if gsb_total["jingdu"]["total"] > 0:
         gsb_total["jingdu"]["b_ratio"] = gsb_total["jingdu"]["b"] / gsb_total["jingdu"]["total"]
         gsb_total["jingdu"]["g_ratio"] = gsb_total["jingdu"]["g"] / gsb_total["jingdu"]["total"]
-    gsb_total["xingneng"]["g"] = gsb_trt_int8["xingneng"]["g"] + gsb_trt_fp16["xingneng"]["g"] + gsb_mkldnn_int8["xingneng"]["g"] + gsb_mkldnn_fp32["xingneng"]["g"]
-    gsb_total["xingneng"]["s"] = gsb_trt_int8["xingneng"]["s"] + gsb_trt_fp16["xingneng"]["s"] + gsb_mkldnn_int8["xingneng"]["s"] + gsb_mkldnn_fp32["xingneng"]["s"]
-    gsb_total["xingneng"]["b"] = gsb_trt_int8["xingneng"]["b"] + gsb_trt_fp16["xingneng"]["b"] + gsb_mkldnn_int8["xingneng"]["b"] + gsb_mkldnn_fp32["xingneng"]["b"]
-    gsb_total["xingneng"]["total"] = gsb_trt_int8["xingneng"]["total"] + gsb_trt_fp16["xingneng"]["total"] + gsb_mkldnn_int8["xingneng"]["total"] + gsb_mkldnn_fp32["xingneng"]["total"]
-    gsb_total["xingneng"]["gsb"] = "{}:{}:{}".format(gsb_total["xingneng"]["g"], gsb_total["xingneng"]["s"], gsb_total["xingneng"]["b"])
+    gsb_total["xingneng"]["g"] = (
+        gsb_trt_int8["xingneng"]["g"]
+        + gsb_trt_fp16["xingneng"]["g"]
+        + gsb_mkldnn_int8["xingneng"]["g"]
+        + gsb_mkldnn_fp32["xingneng"]["g"]
+    )
+    gsb_total["xingneng"]["s"] = (
+        gsb_trt_int8["xingneng"]["s"]
+        + gsb_trt_fp16["xingneng"]["s"]
+        + gsb_mkldnn_int8["xingneng"]["s"]
+        + gsb_mkldnn_fp32["xingneng"]["s"]
+    )
+    gsb_total["xingneng"]["b"] = (
+        gsb_trt_int8["xingneng"]["b"]
+        + gsb_trt_fp16["xingneng"]["b"]
+        + gsb_mkldnn_int8["xingneng"]["b"]
+        + gsb_mkldnn_fp32["xingneng"]["b"]
+    )
+    gsb_total["xingneng"]["total"] = (
+        gsb_trt_int8["xingneng"]["total"]
+        + gsb_trt_fp16["xingneng"]["total"]
+        + gsb_mkldnn_int8["xingneng"]["total"]
+        + gsb_mkldnn_fp32["xingneng"]["total"]
+    )
+    gsb_total["xingneng"]["gsb"] = "{}:{}:{}".format(
+        gsb_total["xingneng"]["g"], gsb_total["xingneng"]["s"], gsb_total["xingneng"]["b"]
+    )
     if gsb_total["xingneng"]["total"] > 0:
         gsb_total["xingneng"]["b_ratio"] = gsb_total["xingneng"]["b"] / gsb_total["xingneng"]["total"]
         gsb_total["xingneng"]["g_ratio"] = gsb_total["xingneng"]["g"] / gsb_total["xingneng"]["total"]
@@ -220,7 +264,6 @@ def res_summary(trt_int8, trt_fp16, mkldnn_int8, mkldnn_fp32):
         "mkldnn_fp32": gsb_mkldnn_fp32,
         "total": gsb_total,
     }
-    
     
     # 详细数据
     res = {}
@@ -311,10 +354,7 @@ def res2xls(env, res, tongji):
     wb = openpyxl.Workbook()
 
     # table1: 详细数据
-    #sheet_detail = wb.active
-    #sheet_detail.title = "detail"
     sheet_detail = wb.create_sheet(index=0, title="detail")
-    #sheet_detail = wb.get_sheet_by_name("detail")
     sheet_detail = wb["detail"]
 
     # 表头
@@ -434,10 +474,7 @@ def res2xls(env, res, tongji):
     sheet_detail.merge_cells("AD3:AG3")
 
     # table2: gsb统计数据
-    #sheet_gsb = wb.active
-    #sheet_gsb.title = "gsb"
     sheet_gsb = wb.create_sheet(index=1, title="gsb")
-    #sheet_gsb = wb.get_sheet_by_name("gsb")
     sheet_gsb = wb["gsb"]
 
     # 表头
@@ -461,7 +498,7 @@ def res2xls(env, res, tongji):
     column_s = 1
     row_s = 3
     for mode, info in tongji.items():
-        sheet_gsb.cell(row_s, column_s).value = mode 
+        sheet_gsb.cell(row_s, column_s).value = mode
         sheet_gsb.cell(row_s, column_s + 1).value = info["jingdu"]["gsb"]
         sheet_gsb.cell(row_s, column_s + 2).value = "{}({})".format(info["jingdu"]["b"], info["jingdu"]["b_ratio"])
         sheet_gsb.cell(row_s, column_s + 3).value = "{}({})".format(info["jingdu"]["g"], info["jingdu"]["g_ratio"])
@@ -512,7 +549,6 @@ def run():
     mkldnn_fp32 = compare_diff(base_res, benchmark_res)
 
     res, tongji = res_summary(trt_int8, trt_fp16, mkldnn_int8, mkldnn_int8)
-    #print(json.dumps(tongji, indent=4))
 
     env = "环境\n"
     env += "docker: "
