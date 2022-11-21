1f4ab46ef5ab5869ba491feed4cb0651c4009f49
xiegegege <xiege01@baidu.com>
2022-11-21 19:28:56 +0800

    add new ce for detection,test=model

---
+++ models_restruct/PaddleDetection/tools/start.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import json
+import shutil
+import logging
+import wget
+
+logger = logging.getLogger("ce")
+
+
+class PaddleDetection_Start(object):
+    """
+    自定义环境准备
+    """
+
+    def __init__(self):
+        self.qa_yaml_name = os.environ["qa_yaml_name"]
+        self.rd_yaml_path = os.environ["rd_yaml_path"]
+        logger.info("###self.qa_yaml_name: {}".format(self.qa_yaml_name))
+        self.reponame = os.environ["reponame"]
+        self.system = os.environ["system"]
+        self.step = os.environ["step"]
+        self.paddle_whl = os.environ["paddle_whl"]
+        self.mode = os.environ["mode"]  # function or precision
+        self.REPO_PATH = os.path.join(os.getcwd(), self.reponame)
+        self.env_dict = {}
+        self.model = self.qa_yaml_name.split("^")[2]
+        logger.info("###self.model_name: {}".format(self.model))
+        self.env_dict["model"] = self.model
+        os.environ["model"] = self.model
+        
+
+
+    def prepare_gpu_env(self):
+        """
+        根据操作系统获取用gpu还是cpu
+        """
+        if "cpu" in self.system or "mac" in self.system:
+            self.env_dict["set_cuda_flag"] = "cpu"  # 根据操作系统判断
+        else:
+            self.env_dict["set_cuda_flag"] = "gpu"  # 根据操作系统判断
+        return 0
+
+    def build_prepare(self):
+        ret = 0
+        ret = self.prepare_gpu_env()
+        if ret:
+            logger.info("build prepare_gpu_env failed")
+            return ret   
+
+
+def run():
+    """
+    执行入口
+    """
+    model = PaddleDetection_Start()
+    model.build_prepare()
+    return 0
+
+if __name__ == "__main__":
+    run()
