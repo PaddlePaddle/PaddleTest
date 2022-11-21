0d319ae62936f1eb3b01b7421d8f4d62e20f3096
iamWHTWD <iamwhtwd@163.com>
2022-11-21 06:50:37 +0000

    update slim case

--- models_restruct/PaddleSlim/tools/case_start.py
+++ models_restruct/PaddleSlim/tools/case_start.py
@@ -79,7 +79,6 @@ def run():
         qa_yaml == "case^demo^quant^pact_quant_aware^MobileNetV3_use_pact"
         or qa_yaml == "case^demo^quant^pact_quant_aware^MobileNetV3_use_pact_false"
         or qa_yaml == "case^demo^quant^pact_quant_aware^MobileNetV3_use_pact_precision"
-
     ):
         os.environ["CUDA_VISIBLE_DEVICES"] = set_cuda_single_card
     else:
