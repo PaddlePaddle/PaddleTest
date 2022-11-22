aa1132359ee0e16096193d0c647d9dacdc230bd3
zhoutianzi666 <17801055074@163.com>
2022-11-22 10:18:26 +0000

    add new line

--- inference/python_api_test/test_int8_model/run_nv_trt_int8.sh
+++ inference/python_api_test/test_int8_model/run_nv_trt_int8.sh
@@ -7,7 +7,7 @@ PYTHON="python"
 # ResNet_vd trt int8
 echo "[Benchmark] Run ResNet_vd trt int8"
 $PYTHON test_image_classification_infer.py --model_path=models/ResNet50_vd_QAT/model.onnx --deploy_backend=tensorrt --input_name=inputs --precision=int8 --calibration_file=models/ResNet50_vd_QAT/calibration.cache --model_name=ResNet_vd
-rm -rf model_int8_model.trt 
+rm -rf model_int8_model.trt
 # MobileNetV3_large trt int8
 echo "[Benchmark] Run MobileNetV3_large trt int8"
 $PYTHON test_image_classification_infer.py --model_path=models/MobileNetV3_large_x1_0_QAT/model.onnx --deploy_backend=tensorrt --input_name=inputs --precision=int8 --calibration_file=models/MobileNetV3_large_x1_0_QAT/calibration.cache --model_name=MobileNetV3_large
@@ -31,11 +31,11 @@ rm -rf model_int8_model.trt
 # ERNIE 3.0-Medium nv-trt int8
 echo "[Benchmark] Run NV-TRT ERNIE 3.0-Medium trt int8"
 $PYTHON  test_nlp_infer.py --model_path=models/save_ernie3_afqmc_new_cablib/model.onnx --deploy_backend=tensorrt  --task_name='afqmc' --precision=int8 --calibration_file=models/save_ernie3_afqmc_new_cablib/calibration.cache --model_name=ERNIE_3.0-Medium
-rm -rf model_int8_model.trt 
+rm -rf model_int8_model.trt
 # PP-MiniLM nv-trt int8
 echo "[Benchmark] Run NV-TRT PP-MiniLM trt int8"
 $PYTHON test_nlp_infer.py --model_path=models/save_ppminilm_afqmc_new_calib/model.onnx --deploy_backend=tensorrt  --task_name='afqmc' --precision=int8 --calibration_file=models/save_ppminilm_afqmc_new_calib/calibration.cache --model_name=PP-MiniLM
-rm -rf model_int8_model.trt 
+rm -rf model_int8_model.trt
 # BERT Base nv-trt int8
 echo "[Benchmark] Run NV-TRT BERT Base trt int8"
 $PYTHON  test_bert_infer.py --model_path=models/x2paddle_cola_new_calib/model.onnx --precision=int8 --batch_size=1 --deploy_backend=tensorrt  --calibration_file=./models/x2paddle_cola_new_calib/calibration.cache --model_name=BERT_Base
