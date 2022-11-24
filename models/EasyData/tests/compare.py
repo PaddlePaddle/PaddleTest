2fb1629022364cdc995773749641dc97deb240fa
plusNew001 <suijiaxin_job@163.com>
2022-11-24 09:23:10 +0000

    add EasyData CE code

--- models/EasyData/tests/compare.py
+++ models/EasyData/tests/compare.py
@@ -5,18 +5,15 @@ def compare(result, str, standard):
     with open(result, "r", encoding="utf-8") as f:
         for line in f:
             if str in line:
-                index = line[line.find(str):]
+                index = line[line.find(str) :]
                 result_value += index
-    # with open("PPDC1_standard.txt","w",encoding="utf-8") as f:
-        # f.write(result_value)
 
     with open(standard, "r", encoding="utf-8") as f:
         for line in f:
             standard_value += line
-    
     assert result_value==standard_value
 
-
+    
 if __name__ == '__main__':
     obj = compare("test_PPDC1.log","ClasOutput INFO","PPDC1_standard.txt")
 
--- models/EasyData/tests/test_PPDC.py
+++ models/EasyData/tests/test_PPDC.py
@@ -1,4 +1,5 @@
 from easydata import EasyData
-model = EasyData(model="image_orientation", device="cpu", return_res=True, print_res=False)
+
+model = EasyData(model="image_orientation",device="cpu", return_res=True, print_res=False)
 results = model.predict("./demo/image_orientation/")
 print(results)
