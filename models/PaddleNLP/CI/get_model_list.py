e0277f77f131382e2eff9a13923b4e6f3a267f96
zjjlivein <1834792141@qq.com>
2022-11-28 18:40:52 +0800

    fix typo run_mode=model

--- models/PaddleNLP/CI/get_model_list.py
+++ models/PaddleNLP/CI/get_model_list.py
@@ -125,7 +125,7 @@ def get_model_list():
     # save model list for CI pr_env.sh
     with io.open("./model_list.txt", "w", encoding="utf-8") as list:
         for all_model in all_examples_dict:
-            list.write("{}\n".format(all_model))
+            list.write(u'{}\n'.format(all_model))
         list.close()
     return all_examples_dict
 
