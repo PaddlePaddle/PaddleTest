6745b84c3a1b87cb241cd9d73520556ea73460c1
plusNew001 <>
2022-12-05 13:18:54 +0000

    add ce

--- models/PaddleScience/test_deepxde/models/standardization.py
+++ models/PaddleScience/test_deepxde/models/standardization.py
@@ -11,15 +11,19 @@ def parse_args():
     return args
     
 
-def alter(file,old_str,new_str):
+def alter(file,old_str,new_str,flag=True, except_str='model.train(0'):
     """
     replaced the backend 
     """
     file_data = ""
     with open(file, "r", encoding="utf-8") as f:
         for line in f:
-            if old_str in line and new_str not in line:
-                line = line.replace(old_str,new_str)
+            if flag:
+                if old_str in line and new_str not in line and except_str not in line:
+                    line = line.replace(old_str,new_str)
+            else:
+                if old_str in line:
+                    line = line.replace(old_str,new_str)
             file_data += line
     with open(file,"w",encoding="utf-8") as f:
         f.write(file_data)
@@ -47,11 +51,13 @@ case_num, py = yamldir.get_case(0)
 filedir = py
 #alter(filedir, "tf", "paddle")
 #alter("../../deepxde/deepxde/backend/paddle/tensor.py", "2.3.0", "0.0.0")
-alter(filedir, "model.train(", "model.train(display_every=1,")
+alter(filedir, "model.train(", "model.train(display_every=1,", True, "model.train(0")
 alter(filedir, "model.train(", "losshistory, train_state = model.train(")
 alter(filedir, "display_every=1000", "display_every=1")
+alter(filedir, "display_every=500", " ",False)
 add_seed(filedir, "import deepxde", "import paddle\n")
 add_seed(filedir, "import paddle", "paddle.seed(1)\n")
+add_seed(filedir, "paddle.seed(1)", "import numpy as np\n")
 add_seed(filedir, "import numpy as np", "np.random.seed(1)\n")
 with open(filedir, "a") as f:
     f.write("result = np.sum(losshistory.loss_train, axis=1)\n"
