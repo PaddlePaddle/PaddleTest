897ebf089b87c5c48c76dfc1ec4cbb2f1ee4e1f6
yghstill <742925032@qq.com>
2022-12-05 03:08:31 +0000

    fix requiement,test=model

--- inference/python_api_test/test_int8_model/run_trt_fp16.sh
+++ inference/python_api_test/test_int8_model/run_trt_fp16.sh
@@ -7,7 +7,7 @@ echo "[Benchmark] Run PPYOLOE trt fp16"
 $PYTHON test_ppyoloe_infer.py --model_path=models/ppyoloe_crn_l_300e_coco --reader_config=configs/ppyoloe_reader.yml --use_trt=True --precision=fp16 --model_name=PPYOLOE
 # PPYOLOE trt fp16
 echo "[Benchmark] Run PPYOLOE+ trt fp16"
-$PYTHON test_ppyoloe_infer.py --model_path=models/ppyoloe_plus_crn_s_80e_coco_no_nms --reader_config=configs/ppyoloe_plus_reader.yml --use_trt=True --precision=fp16 --model_name=PPYOLOE --exclude_nms
+$PYTHON test_ppyoloe_infer.py --model_path=models/ppyoloe_plus_crn_s_80e_coco_no_nms --reader_config=configs/ppyoloe_plus_reader.yml --use_trt=True --precision=fp16 --model_name=PPYOLOE_PLUS --exclude_nms
 # PicoDet trt fp16
 echo "[Benchmark] Run PicoDet trt fp16"
 $PYTHON test_ppyoloe_infer.py --model_path=models/picodet_s_416_coco_npu --reader_config=configs/picodet_reader.yml --use_trt=True --precision=fp16 --model_name=PicoDet
