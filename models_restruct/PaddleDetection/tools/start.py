bb1501748a0ca855e2a9f703a035b42f60695fbf
xiegegege <xiege01@baidu.com>
2022-12-07 11:42:06 +0000

    skip no model cases for det and seg,test=model

--- models_restruct/PaddleDetection/tools/start.py
+++ models_restruct/PaddleDetection/tools/start.py
@@ -26,6 +26,7 @@ class PaddleDetection_Start(object):
         self.reponame = os.environ["reponame"]
         self.system = os.environ["system"]
         self.step = os.environ["step"]
+        logger.info("###self.step: {}".format(self.step))
         self.paddle_whl = os.environ["paddle_whl"]
         self.mode = os.environ["mode"]  # function or precision
         self.REPO_PATH = os.path.join(os.getcwd(), self.reponame)
