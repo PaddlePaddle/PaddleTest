a00295e05e3d0130ab8cbd4ca940a17d2b0d96b7
贾晓 <jiaxiao243@126.com>
2022-12-23 10:43:55 +0800

    update speech (#1795)
    * fix speech, run_mode=model
    
    * fix speech, run_mode=model
    
    * update for speech, run_mode=model
    
    * update for speech, run_mode=model
    
    * fix speech, run_mode=model
    
    * fix bug, run_mode=model

--- models_restruct/PaddleSpeech/tools/start.py
+++ models_restruct/PaddleSpeech/tools/start.py
@@ -154,8 +154,8 @@ class PaddleSpeech_Start(object):
 
             if self.model == "transformer_tts":
                 self.download_data(
-                    "https://paddlespeech.bj.bcebos.com/Parakeet/released_models/waveflow/ \
-                         waveflow_ljspeech_ckpt_0.3.zip"
+                    "https://paddlespeech.bj.bcebos.com/Parakeet/released_models/waveflow/\
+waveflow_ljspeech_ckpt_0.3.zip"
                 )
             else:
                 self.download_data(
