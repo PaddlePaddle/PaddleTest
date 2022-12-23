693eabec673bb03b27cd6747e7a939dbee62ac76
zjjlivein <1834792141@qq.com>
2022-12-23 16:08:13 +0800

    fix codestyle test=model

--- models/PaddleNLP/CI/test_model_zoo_gpt.py
+++ models/PaddleNLP/CI/test_model_zoo_gpt.py
@@ -18,10 +18,12 @@ import os
 import sys
 import subprocess
 import json
-import pytest
 
 
-def exit_check(exit_code, file_name,):
+def exit_check(
+    exit_code,
+    file_name,
+):
     """
     check exit_code
     """
@@ -29,10 +31,9 @@ def exit_check(exit_code, file_name,):
 
 
 def save_log(exit_code, output, file_name):
-    """
-    """
-    if not os.path.exists('test_log'):
-        os.mkdir('test_log')
+    """ """
+    if not os.path.exists("test_log"):
+        os.mkdir("test_log")
     if exit_code == 0:
         log_file = os.getcwd() + "/test_log/" + os.path.join(file_name + "_success.log")
         with open(log_file, "a") as flog:
@@ -44,28 +45,32 @@ def save_log(exit_code, output, file_name):
 
 
 def test_requirements():
-    file_name = 'requirements'
-    if os.path.exists('requirements.txt'):
-        cmd=f"pip install -r requirements.txt"
+    """"""
+    file_name = "requirements"
+    if os.path.exists("requirements.txt"):
+        cmd = f"pip install -r requirements.txt"
         output = subprocess.getstatusoutput(cmd)
         save_log(output[0], output[1], file_name)
         exit_check(output[0], file_name)
 
 
 def test_prepare_data_files():
-    file_name = 'prepare_data_files'
-    if not os.path.exists('pre_data'):
-        os.mkdir('pre_data')
-    cmd='''cd ./pre_data && 
-    wget -q https://bj.bcebos.com/paddlenlp/models/transformers/gpt/data/gpt_en_dataset_300m_ids.npy && 
-    wget -q https://bj.bcebos.com/paddlenlp/models/transformers/gpt/data/gpt_en_dataset_300m_idx.npz && cd ../'''
+    """"""
+    file_name = "prepare_data_files"
+    if not os.path.exists("pre_data"):
+        os.mkdir("pre_data")
+    cmd = """cd ./pre_data &&
+    wget -q https://bj.bcebos.com/paddlenlp/models/transformers/gpt/data/gpt_en_dataset_300m_ids.npy &&
+    wget -q https://bj.bcebos.com/paddlenlp/models/transformers/gpt/data/gpt_en_dataset_300m_idx.npz && cd ../"""
     output = subprocess.getstatusoutput(cmd)
     save_log(output[0], output[1], file_name)
     exit_check(output[0], file_name)
 
+
 def test_run_pretrain():
-    file_name = 'run_pretrain'
-    cmd="""python -m paddle.distributed.launch run_pretrain.py \
+    """"""
+    file_name = "run_pretrain"
+    cmd = """python -m paddle.distributed.launch run_pretrain.py \
     --model_type gpt \
     --model_name_or_path gpt2-en \
     --input_dir "./pre_data" \
@@ -84,10 +89,13 @@ def test_run_pretrain():
 
 
 def test_run_eval():
-    file_name = 'run_eval'
-    get_data_cmd='wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip && unzip wikitext-103-v1.zip'
+    """"""
+    file_name = "run_eval"
+    get_data_cmd = (
+        "wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip && unzip wikitext-103-v1.zip"
+    )
     os.system(get_data_cmd)
-    cmd="""python run_eval.py --model_name gpt2-en \
+    cmd = """python run_eval.py --model_name gpt2-en \
     --eval_path ./wikitext-103/wiki.valid.tokens \
     --overlapping_eval 32 \
     --init_checkpoint_path ./output/model_2/model_state.pdparams \
@@ -98,9 +106,11 @@ def test_run_eval():
     save_log(output[0], output[1], file_name)
     exit_check(output[0], file_name)
 
+
 def test_export_model():
-    file_name = 'export_model'
-    cmd="""python export_model.py --model_type=gpt-cn \
+    """"""
+    file_name = "export_model"
+    cmd = """python export_model.py --model_type=gpt-cn \
     --model_path=gpt-cpm-large-cn \
     --output_path=./infer_model/model"""
 
@@ -108,9 +118,11 @@ def test_export_model():
     save_log(output[0], output[1], file_name)
     exit_check(output[0], file_name)
 
+
 def test_inference():
-    file_name = 'inference'
-    cmd="""python deploy/python/inference.py --model_type gpt-cn \
+    """"""
+    file_name = "inference"
+    cmd = """python deploy/python/inference.py --model_type gpt-cn \
     --model_path ./infer_model/model"""
 
     output = subprocess.getstatusoutput(cmd)
