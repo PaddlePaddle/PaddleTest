a9e1a51e0af24cfbb6a3a433a2a4507e668e04d7
zjjlivein <1834792141@qq.com>
2022-12-29 18:47:33 +0800

    fix FG path

--- models/PaddleNLP/CI/ci_case.sh
+++ models/PaddleNLP/CI/ci_case.sh
@@ -1046,8 +1046,7 @@ for apicase in `ls`;do
 done
 }
 faster_generation(){
-cd ${nlp_dir}/fast_generation/samples/
-
+cd ${nlp_dir}/faster_generation/samples
 python codegen_sample.py >${log_path}/fast_generation_codegen >>${log_path}/fast_generation_codegen 2>&1
 print_info $? fast_generation_codegen
 
@@ -1063,7 +1062,7 @@ print_info $? fast_generation_plato
 python t5_sample.py --use_faster >${log_path}/fast_generation_t5 >>${log_path}/fast_generation_t5 2>&1
 print_info $? fast_generation_t5
 
-cd ${nlp_dir}/paddlenlp/ops/fast_transformer/sample/
+cd ${nlp_dir}/paddlenlp/ops/faster_transformer/sample/
 python bart_decoding_sample.py >${log_path}/fast_generation_bart >>${log_path}/fast_generation_bart 2>&1
 print_info $? fast_generation_bart
 
