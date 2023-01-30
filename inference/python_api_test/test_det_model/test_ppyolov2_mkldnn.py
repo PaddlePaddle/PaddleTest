2c20a53e92fb329d28e6ccfb83f9ad542fc66b56
Zhang Yulong <35552275+ZhangYulongg@users.noreply.github.com>
2023-01-30 20:45:43 +0800

    Update threshold for detection models (#1883)

--- inference/python_api_test/test_det_model/test_ppyolov2_mkldnn.py
+++ inference/python_api_test/test_det_model/test_ppyolov2_mkldnn.py
@@ -88,6 +88,7 @@ def test_more_bz_mkldnn():
         for batch in range(1, batch_size * 2, 2):
             scale_1 = np.concatenate((scale_1, result[batch].flatten()), axis=0)
 
-        output_data_dict = {"save_infer_model/scale_0.tmp_1": scale_0, "save_infer_model/scale_1.tmp_1": scale_1}
+        # output_data_dict = {"save_infer_model/scale_0.tmp_1": scale_0, "save_infer_model/scale_1.tmp_1": scale_1}
+        output_data_dict = test_suite.get_truth_val(input_data_dict, device="gpu")
         test_suite.load_config(model_file="./ppyolov2/model.pdmodel", params_file="./ppyolov2/model.pdiparams")
         test_suite.mkldnn_test(input_data_dict, output_data_dict, repeat=1, delta=1e-4)
