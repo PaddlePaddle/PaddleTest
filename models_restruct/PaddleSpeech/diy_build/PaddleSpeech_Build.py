7502a178fb79787a4a3c52cc2f0e0678412a0dea
JiaXiao243 <jiaxiao243@126.com>
2023-02-08 06:40:38 +0000

    fix codestyle, run_mode=model

--- models_restruct/PaddleSpeech/diy_build/PaddleSpeech_Build.py
+++ models_restruct/PaddleSpeech/diy_build/PaddleSpeech_Build.py
@@ -84,12 +84,12 @@ class PaddleSpeech_Build(Model_Build):
             if sysstr == "Darwin" and platform.machine() == "x86_64":
                 os.system("python -m pip install -U protobuf==3.19.6")
                 # mac interl: installed in '/var/root/.local/bin' which is not on PATH.
-                os.environ["PATH"] += os.pathsep + '/var/root/.local/bin' 
+                os.environ["PATH"] += os.pathsep + "/var/root/.local/bin"
 
             os.system("python -m pip uninstall -y paddlespeech")
             if sysstr == "Linux":
                 # linux：paddlespeech are installed in '/root/.local/bin' which is not on PATH
-                os.environ["PATH"] += os.pathsep + '/root/.local/bin'  #注意修改你的路径
+                os.environ["PATH"] += os.pathsep + "/root/.local/bin"  # 注意修改你的路径
             # os.system("python -m pip install -U pyinstaller")
             os.system("python -m pip install --user . --ignore-installed")
 
