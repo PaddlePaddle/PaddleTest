a4aff84451388396bdd4dc81c791cdb562a3cc23
JiaXiao243 <jiaxiao243@126.com>
2023-02-09 03:25:07 +0000

    update bce-python-sdk version for speech version, run_mode=model

--- models_restruct/PaddleSpeech/diy_build/PaddleSpeech_Build.py
+++ models_restruct/PaddleSpeech/diy_build/PaddleSpeech_Build.py
@@ -95,10 +95,10 @@ class PaddleSpeech_Build(Model_Build):
                 os.system("python -m pip install jsonlines")
             # os.system("python -m pip install -U pyinstaller")
             os.system("python -m pip install --user . --ignore-installed")
-
             # mac from numba.np.ufunc import _internal
             # os.system("python -m pip install -U numpy<1.24.0")
-
+            # bug: bce-python-sdk==0.8.79
+            os.system("python -m pip install bce-python-sdk==0.8.74")
             os.chdir(path_now)
             print("build paddlespeech wheel!")
 
