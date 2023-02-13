468ce471cf0ef40e05260f40811b4d031154e821
zjjlivein <1834792141@qq.com>
2023-02-13 19:16:24 +0800

    bdist_wheel

--- models_restruct/PaddleNLP/diy_build/PaddleNLP_Build.py
+++ models_restruct/PaddleNLP/diy_build/PaddleNLP_Build.py
@@ -91,10 +91,10 @@ class PaddleNLP_Build(Model_Build):
 
 
         os.chdir("PaddleNLP")  # 执行setup要先切到路径下面
-        # os.system("python setup.py bdist_wheel")
-        # cmd_return = os.system(" python -m pip install -U dist/p****.whl")
+        os.system("python setup.py bdist_wheel")
+        cmd_return = os.system(" python -m pip install -U dist/p****.whl")
 
-        cmd_return = os.system("python setup.py install")
+        # cmd_return = os.system("python setup.py install")
         # cmd_return = os.system("python setup.py install > paddlenlp_install.log 2>&1 ")
         os.chdir(path_now)
 
