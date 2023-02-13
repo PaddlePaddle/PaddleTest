fc5e26cef2412565fbfd034259cf120d93fb5c2e
JiaXiao243 <jiaxiao243@126.com>
2023-02-13 07:52:24 +0000

    fix 3d, run_mode=model

--- models_restruct/Paddle3D/diy_build/Paddle3D_Build.py
+++ models_restruct/Paddle3D/diy_build/Paddle3D_Build.py
@@ -91,7 +91,12 @@ class Paddle3D_Build(Model_Build):
             if sysstr == "Linux":
                 # linux：xx are installed in '/root/.local/bin' which is not on PATH
                 os.environ["PATH"] += os.pathsep + "/root/.local/bin"
+            os.system('python -m pip uninstall -y setuptools')
+            os.system('python -m pip install --upgrade setuptools')
+            os.system('python -m pip install --upgrade pip')
+            os.system("python -m pip install -r requirements.txt -i https://mirror.baidu.com/pypi/simple")
             os.system("python -m pip install . ")
+          
 
             print("build wheel!")
 
