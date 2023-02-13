6d1d8a90b520d553d46bd5c9c9616134e6d8845e
JiaXiao243 <jiaxiao243@126.com>
2023-02-13 08:20:54 +0000

    fix 3d, run_mode=model

--- models_restruct/Paddle3D/diy_build/Paddle3D_Build.py
+++ models_restruct/Paddle3D/diy_build/Paddle3D_Build.py
@@ -92,12 +92,11 @@ class Paddle3D_Build(Model_Build):
                 # linux：xx are installed in '/root/.local/bin' which is not on PATH
                 os.environ["PATH"] += os.pathsep + "/root/.local/bin"
                 # setuptools not found on python3.10
-                os.system('python -m pip uninstall -y setuptools')
-                os.system('python -m pip install --upgrade setuptools')
+                os.system('/usr/bin/python3.10  -m pip uninstall -y setuptools')
+                os.system('/usr/bin/python3.10  -m pip install --upgrade setuptools')
                 os.system('python -m pip install --upgrade pip')
                 os.system('apt-get install -y python3-setuptools')
-                os.system('curl -O https://bootstrap.pypa.io/get-pip.py')
-                os.system('python get-pip.py')
+             
                 os.system("python -m pip install -r requirements.txt -i https://mirror.baidu.com/pypi/simple")
             os.system("python -m pip install . ")
           
