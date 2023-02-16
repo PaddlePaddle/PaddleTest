1b060f1d12c3f251176c7a2b9d990f2af8d17878
iamWHTWD <iamwhtwd@163.com>
2023-02-16 15:05:59 +0800

    update slim case

--- models/upload/prepare_cfs_CI.sh
+++ models/upload/prepare_cfs_CI.sh
@@ -1,11 +1,12 @@
 # $1：挂载点ip $2:挂载目录
 unset http_proxy && unset https_proxy 
 apt-get install nfs-common -y
-if [ -d "$2" ];then
-   rm -rf "$2"
+echo ---cfs_folder:$2
+if [ -d $2 ];then
+   rm -rf $2
 fi
 
-mkdir "$2"
+mkdir $2
 mount -t nfs4 -o minorversion=1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $1:/ $2
 export date_path=${PWD}/$2
 echo "---date_path:${date_path}---"
