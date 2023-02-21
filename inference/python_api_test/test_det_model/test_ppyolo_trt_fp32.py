1dc53d542785b0b76b8688634e0b5ead9e52d0d2
YuBaoku <49938469+EmmonsCurse@users.noreply.github.com>
2023-02-21 10:07:11 +0800

    Fix the case of det model  (#1963)

--- inference/python_api_test/test_det_model/test_ppyolo_trt_fp32.py
+++ inference/python_api_test/test_det_model/test_ppyolo_trt_fp32.py
@@ -15,7 +15,7 @@ import numpy as np
 
 # pylint: disable=wrong-import-position
 sys.path.append("..")
-from test_case import InferenceTest
+from test_case import InferenceTest, clip_model_extra_op
 
 # pylint: enable=wrong-import-position
 
@@ -30,6 +30,10 @@ def check_model_exist():
         tar = tarfile.open("ppyolo.tgz")
         tar.extractall()
         tar.close()
+        clip_model_extra_op(
+            path_prefix="./ppyolo/model",
+            output_model_path="./ppyolo/model",
+        )
 
 
 def test_config():
@@ -38,7 +42,10 @@ def test_config():
     """
     check_model_exist()
     test_suite = InferenceTest()
-    test_suite.load_config(model_file="./ppyolo/model.pdmodel", params_file="./ppyolo/model.pdiparams")
+    test_suite.load_config(
+        model_file="./ppyolo/model.pdmodel",
+        params_file="./ppyolo/model.pdiparams",
+    )
     test_suite.config_test()
 
 
@@ -58,7 +65,10 @@ def test_trt_fp32_more_bz():
     for batch_size in batch_size_pool:
 
         test_suite = InferenceTest()
-        test_suite.load_config(model_file="./ppyolo/model.pdmodel", params_file="./ppyolo/model.pdiparams")
+        test_suite.load_config(
+            model_file="./ppyolo/model.pdmodel",
+            params_file="./ppyolo/model.pdiparams",
+        )
         images_list, images_origin_list, npy_list = test_suite.get_images_npy(
             file_path, images_size, center=False, model_type="det"
         )
@@ -81,7 +91,11 @@ def test_trt_fp32_more_bz():
             im_shape_pool.append(im_shape)
         im_shape_pool = np.array(im_shape_pool).reshape((batch_size, 2))
         input_data_dict = {"im_shape": im_shape_pool, "image": data, "scale_factor": scale_factor_pool}
-        test_suite.collect_shape_info(model_path="./ppyolo/", input_data_dict=input_data_dict, device="gpu")
+        test_suite.collect_shape_info(
+            model_path="./ppyolo/",
+            input_data_dict=input_data_dict,
+            device="gpu",
+        )
 
         scale_0 = []
         for batch in range(0, batch_size * 2, 2):
@@ -92,9 +106,17 @@ def test_trt_fp32_more_bz():
 
         # output_data_dict = {"save_infer_model/scale_0.tmp_1": scale_0, "save_infer_model/scale_1.tmp_1": scale_1}
         del test_suite.pd_config
-        test_suite.load_config(model_file="./ppyolo/model.pdmodel", params_file="./ppyolo/model.pdiparams")
+
+        test_suite.load_config(
+            model_file="./ppyolo/model.pdmodel",
+            params_file="./ppyolo/model.pdiparams",
+        )
         output_data_dict = test_suite.get_truth_val(input_data_dict, device="gpu")
-        test_suite.load_config(model_file="./ppyolo/model.pdmodel", params_file="./ppyolo/model.pdiparams")
+
+        test_suite.load_config(
+            model_file="./ppyolo/model.pdmodel",
+            params_file="./ppyolo/model.pdiparams",
+        )
         test_suite.trt_more_bz_test(
             input_data_dict,
             output_data_dict,
@@ -105,3 +127,5 @@ def test_trt_fp32_more_bz():
             dynamic=True,
             shape_range_file="./ppyolo/shape_range.pbtxt",
         )
+
+        del test_suite
