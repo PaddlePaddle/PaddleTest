b42c4847d19aaa9f34a9b68f9ecbe7c0f23fefe4
xiegegege <xiege01@baidu.com>
2023-02-22 13:13:13 +0000

    fix detection ce,test=model

--- models_restruct/PaddleSeg/tools/start.py
+++ models_restruct/PaddleSeg/tools/start.py
@@ -41,16 +41,10 @@ class PaddleSeg_Start(object):
         """
         if "cityscapes" in self.model:
             if not os.path.exists("seg_dynamic_pretrained/{}/model.pdparams".format(self.model)):
-                os.system(
-                    "wget -P seg_dynamic_pretrained/{} https://bj.bcebos.com/paddleseg/dygraph/cityscapes/{}/
-                    model.pdparams".format(self.model,self.model)
-                )
+                os.system("wget -P seg_dynamic_pretrained/{} https://bj.bcebos.com/paddleseg/dygraph/cityscapes/{}/model.pdparams".format(self.model,self.model))
         if "voc12" in self.model:
             if not os.path.exists("seg_dynamic_pretrained/{}/model.pdparams".format(self.model)):
-                os.system(
-                    "wget -P seg_dynamic_pretrained/{} https://bj.bcebos.com/paddleseg/dygraph/pascal_voc12/{}
-                    /model.pdparams".format(self.model,self.model)
-                )
+                os.system("wget -P seg_dynamic_pretrained/{} https://bj.bcebos.com/paddleseg/dygraph/pascal_voc12/{}/model.pdparams".format(self.model,self.model))
         if "cpu" in self.system or "mac" in self.system:
             self.env_dict["set_cuda_flag"] = "cpu"  # 根据操作系统判断
         else:
