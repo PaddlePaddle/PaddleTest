7a51097d799e1d28e4d50bb10d1c699c4d8ece9b
JiaXiao243 <jiaxiao243@126.com>
2023-03-02 11:04:01 +0000

    fix e2e, run_mode=model

--- models_restruct/PaddleOCR/diy_build/PaddleOCR_Build.py
+++ models_restruct/PaddleOCR/diy_build/PaddleOCR_Build.py
@@ -137,7 +137,7 @@ class PaddleOCR_Build(Model_Build):
                     if sysstr == "Darwin":
                         cmd = "sed -i '' 's/batch_size: 14/batch_size: 1/g' %s" % filename
                     else:
-                        cmd = "sed -i s/batch_size: 14!batch_size: 1/g %s" % filename
+                        cmd = "sed -i s/batch_size: 14/batch_size: 1/g %s" % filename
                         os.system(cmd)
             os.chdir(self.test_root_path)
             print("build dataset!")
