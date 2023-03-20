a7b6003781701c1c8a190a0188ca8181a345c9d0
xiege01 <xiege01@baidu.com>
2023-03-20 12:54:39 +0000

    fix detection ce,test=model

--- models_restruct/PaddleDetection/cases/test.py
+++ models_restruct/PaddleDetection/cases/test.py
@@ -1,6 +1,5 @@
 import os
-with open('PaddleDetection_TIPC_list', 'r') as f:
+with open('models_list_detection_no_model', 'r') as f:
     for line in f:
         temp = line.strip('\n')
-        os.system("cp test_tipc.yml {}".format(temp))
-
+        os.system("cp test_no_model.yml {}".format(temp))
