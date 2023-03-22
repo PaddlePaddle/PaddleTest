4b279095387fb7b069c8b4c3b7c1fae9681ead36
JiaXiao243 <jiaxiao243@126.com>
2023-03-22 08:44:51 +0000

    fix distill, run_mode=model

--- models_restruct/PaddleOCR/tools/start.py
+++ models_restruct/PaddleOCR/tools/start.py
@@ -157,9 +157,9 @@ class PaddleOCR_Start(object):
             os.makedirs("cases")
 
         case_file = os.path.join("cases", self.qa_yaml_name) + ".yml"
-        det_distill = ["det_dml", "det_distill", "det_dml", "det_cml"]
+        ocr_distill = ["rec_distillation", "det_distill", "det_dml", "det_cml"]
         if not os.path.exists(case_file):
-            if any(item in self.qa_yaml_name for item in det_distill):
+            if any(item in self.qa_yaml_name for item in ocr_distill):
                 with open((os.path.join("cases", self.qa_yaml_name) + ".yml"), "w") as f:
                     f.writelines(
                         (
