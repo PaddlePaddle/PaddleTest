e929912dc507523674850e5241f2dadbdf2df9a9
xiege01 <xiege01@baidu.com>
2023-03-24 05:07:28 +0000

    fix detection ce,test=model

--- models_restruct/PaddleDetection/diy_build/PaddleDetection_Build.py
+++ models_restruct/PaddleDetection/diy_build/PaddleDetection_Build.py
@@ -70,7 +70,9 @@ class PaddleDetection_Build(Model_Build):
         os.system("python -m pip install -r requirements.txt")
         os.system("python -m pip install zip --ignore-installed")
         os.system("python -m pip uninstall paddleslim -y")
-        os.system("python -m pip install https://paddle-qa.bj.bcebos.com/PaddleSlim/paddleslim-0.0.0.dev0-py3-none-any.whl")
+        os.system(
+            "python -m pip install https://paddle-qa.bj.bcebos.com/PaddleSlim/paddleslim-0.0.0.dev0-py3-none-any.whl"
+        )
         os.system("python -m pip install onnxruntime")
         logger.info("***start ffmpeg install***")
         os.system("rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro")
