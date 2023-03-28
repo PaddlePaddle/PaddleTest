114d9d5713e3b7070ada34fdd1f18b005971e3dc
iamWHTWD <iamwhtwd@163.com>
2023-03-28 06:05:07 +0000

    add mount func

--- models_restruct/PaddleRec/diy_build/PaddleRec_Build.py
+++ models_restruct/PaddleRec/diy_build/PaddleRec_Build.py
@@ -51,6 +51,31 @@ class PaddleRec_Build(Model_Build):
             for file_name in os.listdir("cases"):
                 if ".yaml" in file_name:
                     self.clas_model_list.append(file_name.strip().replace(":", "/"))
+        
+        # 数据原存储路径
+        self.dataset_org = str(args.dataset_org)
+        # 数据软链的路径
+        self.dataset_target = str(args.dataset_target)
+        # 使用数据服务时，将被用于更新dataset_org参数
+        self.mount_path = str(os.getenv("mount_path"))
+        # linux release 是否用数据服务，默认为False
+        self.use_data_cfs = str(args.use_data_cfs)
+        # windows、mac 系统下使用数据服务
+        if ("Windows" in platform.system() or "Darwin" in platform.system()) and os.path.exists(self.mount_path) \
+            # linux release下使用数据服务
+            or (os.path.exists(self.mount_path) and self.use_data_cfs): 
+            logger.info("#### mount_path diy_build is {}".format(self.mount_path))
+            # 设置dataset_target为mount_path
+            if os.listdir(self.mount_path) != []:
+                self.dataset_org = self.mount_path
+                os.environ["dataset_org"] = self.mount_path
+                self.dataset_target = os.path.join(os.getcwd(), self.reponame)
+                os.environ["dataset_target"] = os.path.join(os.getcwd(), self.reponame)
+
+        # 将dataset_org 软链到dataset_targ
+            exit_code = os.symlink(self.dataset_org, os.path.join(self.reponame, self.dataset_target))
+            if exit_code:
+                logger.info("#### link_dataset failed")
 
     def build_paddlerec(self):
         """
