b8e9aed92c0108ade15b8afb5a1392544f82b83c
liujie44 <liujie44@baidu.com>
2023-03-28 19:57:30 +0800

    update auto_base, test=model

--- distributed/CI/PaddleFleetX/before_hook.sh
+++ distributed/CI/PaddleFleetX/before_hook.sh
@@ -40,9 +40,8 @@ function requirements() {
     export http_proxy=${proxy}
     export https_proxy=${proxy}
     python -m pip install -r requirements.txt --force-reinstall
-    python -m pip install setuptools==50.3.2
     cd ppfleetx/ops && python setup_cuda.py install && cd ../..
-
+        # uninstall padle -y
     python -m pip list|grep paddle
 }
 
