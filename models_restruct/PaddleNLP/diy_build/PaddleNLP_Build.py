4d2f98996a81ced8a0efbc6014b2df162d1901fa
zjjlivein <1834792141@qq.com>
2023-03-31 17:26:52 +0800

    fix linux_conve system

--- models_restruct/PaddleNLP/diy_build/PaddleNLP_Build.py
+++ models_restruct/PaddleNLP/diy_build/PaddleNLP_Build.py
@@ -62,7 +62,7 @@ class PaddleNLP_Build(Model_Build):
         os.environ["no_proxy"] = "bcebos.com,huggingface.co,baidu.com"
         print(os.environ["no_proxy"])
 
-        if platform == "linux":
+        if platform == "linux" or platform=="linux_convergence":
             print('linux')
             os.system("python -m pip install -U setuptools -i https://mirror.baidu.com/pypi/simple")
             os.system("python -m pip install --user -r requirements_nlp.txt -i https://mirror.baidu.com/pypi/simple")
