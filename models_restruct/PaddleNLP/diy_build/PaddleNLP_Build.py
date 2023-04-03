69dbf83c7a2466ebd854776c7a8fd69f1a511fdf
zjjlivein <1834792141@qq.com>
2023-04-03 19:41:47 +0800

    fix codestyle test=model

--- models_restruct/PaddleNLP/diy_build/PaddleNLP_Build.py
+++ models_restruct/PaddleNLP/diy_build/PaddleNLP_Build.py
@@ -98,7 +98,7 @@ class PaddleNLP_Build(Model_Build):
             logger.info("installing develop PaddleNLP")
             os.system("python setup.py bdist_wheel")
             cmd_return = os.system(" python -m pip install -U dist/p****.whl")
-            
+
             if cmd_return:
                 logger.info("repo {} python -m pip install-failed".format(self.reponame))
 
