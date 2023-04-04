a0ed7b0ed7f555833ac0420b8de31cd60a8adeb6
xiegegege <xiege01@baidu.com>
2023-04-04 01:18:28 +0000

    fix detection_ce,test=document_fix

--- models_restruct/PaddleSeg/tools/end.py
+++ models_restruct/PaddleSeg/tools/end.py
@@ -32,7 +32,6 @@ class PaddleDetection_End(object):
         self.log_dir = "logs"
         self.log_name = "train_prim_single.log"
         self.log_path = os.path.join(os.getcwd(), self.log_dir, self.reponame, self.qa_model_name, self.log_name)
-        self.log_path_
         logger.info("log_path:{}".format(log_path))
         self.qa_model_name_base = ""
         self.prim_log_path = ""
