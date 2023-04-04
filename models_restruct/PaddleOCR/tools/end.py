4071d75a03bc0259c8c9c55c78a0ec376f5af4cd
贾晓 <jiaxiao243@126.com>
2023-04-04 10:10:08 +0800

    update ocr (#2111)
    * fix ocr, run_mode=model
    
    * update ocr, run_mode=model
    
    * update ocr, run_mode=model
    
    * update ocr, run_mode=model
    
    * fix codestyle, run_mode=model
    
    * fix codestyle, run_mode=model

--- models_restruct/PaddleOCR/tools/end.py
+++ models_restruct/PaddleOCR/tools/end.py
@@ -170,8 +170,6 @@ class PaddleOCR_End(object):
                     r = re.findall(regexp, line)
                     # 如果解析不到符合格式到指标，默认值设置为-1
                     kpi_value = float(r[0].strip()) if len(r) > 0 else -1
-                    print("kpi_value:{}".format(kpi_value))
-                    logger.info("kpi_value:{}".format(kpi_value))
                     data_list.append(kpi_value)
         return data_list
 
