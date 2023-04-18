6946709d4014e1546d256a50e727baaa65ec55e7
GGBond8488 <33050871+GGBond8488@users.noreply.github.com>
2023-04-18 19:16:01 +0800

    recovery and change test to adapt 0D output, run_mode=framwork (#2158)

--- framework/api/multithreading_case.py
+++ framework/api/multithreading_case.py
@@ -30,7 +30,7 @@ ignore_case_dir = {
     "device": [],
     "fft": [],
     "incubate": [],
-    "linalg": ["test_cond.py", "test_norm.py", "test_slogdet.py"],
+    "linalg": ["test_cond.py", "test_norm.py"],
     "loss": [],
     "nn": [
         "test_functional_celu.py",
