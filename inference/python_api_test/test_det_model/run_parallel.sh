7679e9af03cddddea1582cfb52f23bdfe453ca18
ZhangYulongg <1272816783@qq.com>
2023-04-24 20:56:11 +0800

    update

---
+++ inference/python_api_test/test_det_model/run_parallel.sh
@@ -0,0 +1,41 @@
+[[ -n $1 ]] && export CUDA_VISIBLE_DEVICES=$1
+export FLAGS_call_stack_level=2
+cases="./test_fast_rcnn_mkldnn.py \
+       ./test_fast_rcnn_gpu.py \
+       ./test_fast_rcnn_trt_fp32.py \
+       ./test_ppyolo_gpu.py \
+       ./test_ppyolo_mkldnn.py \
+       ./test_ppyolov2_mkldnn.py \
+       ./test_solov2_gpu.py \
+       ./test_solov2_mkldnn.py \
+       ./test_yolov3_gpu.py \
+       ./test_yolov3_mkldnn.py \
+       ../test_nlp_model/test_bert_gpu.py \
+       ../test_nlp_model/test_bert_mkldnn.py \
+       ../test_nlp_model/test_bert_trt_fp32.py.py \
+       ../test_nlp_model/test_ernie_gpu.py \
+       ../test_nlp_model/test_ernie_mkldnn.py \
+       ../test_nlp_model/test_ernie_trt_fp32.py \
+       ../test_nlp_model/test_lac_gpu.py \
+       ../test_nlp_model/test_lac_trt_fp32.py \
+       ../test_nlp_model/test_lac_trt_fp16.py \
+      "
+bug=0
+
+echo "============ failed cases =============" >> result.txt
+for file in ${cases}
+do
+    echo ${file}
+    if [[ ${ignore} =~ ${file##*/} ]]; then
+        echo "跳过"
+    else
+        python -m pytest -m server --disable-warnings -v ${file}
+        if [ $? -ne 0 ]; then
+            echo ${file} >> result.txt
+            bug=`expr ${bug} + 1`
+        fi
+    fi
+done
+
+echo "total bugs: "${bug} >> result.txt
+exit ${bug}
