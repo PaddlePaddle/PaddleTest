a61a6256018e9f734d5bd239ce9653b7d02d81f9
Zhang Yulong <35552275+ZhangYulongg@users.noreply.github.com>
2023-04-25 14:50:12 +0800

    PR-CE Infer case parallel (#2181)

---
+++ inference/python_api_test/test_det_model/run_parallel.sh
@@ -0,0 +1,48 @@
+[[ -n $1 ]] && export CUDA_VISIBLE_DEVICES=$1
+export FLAGS_call_stack_level=2
+# V100 total:691.19s
+cases="./test_fast_rcnn_mkldnn.py \
+       ./test_fast_rcnn_gpu.py \
+       ./test_fast_rcnn_trt_fp32.py \
+       ./test_ppyolo_gpu.py \
+       ./test_ppyolo_mkldnn.py \
+       ./test_ppyolov2_mkldnn.py \
+       ./test_solov2_gpu.py \
+       ./test_solov2_mkldnn.py \
+       ./test_yolov3_gpu.py \
+       ./test_yolov3_mkldnn.py \
+       ../test_nlp_model/test_bert_gpu.py \
+       ../test_nlp_model/test_bert_mkldnn.py \
+       ../test_nlp_model/test_bert_trt_fp32.py \
+       ../test_nlp_model/test_ernie_gpu.py \
+       ../test_nlp_model/test_ernie_mkldnn.py \
+       ../test_nlp_model/test_ernie_trt_fp32.py \
+       ../test_nlp_model/test_lac_gpu.py \
+       ../test_nlp_model/test_lac_trt_fp32.py \
+       ../test_nlp_model/test_lac_trt_fp16.py \
+      "
+bug=0
+
+now=`date +'%Y-%m-%d %H:%M:%S'`
+start_time=$(date --date="$now" +%s);
+echo "============ failed cases =============" >> result.txt
+for file in ${cases}
+do
+    echo ${file}
+    if [[ ${ignore} =~ ${file##*/} ]]; then
+        echo "跳过"
+    else
+        python -m pytest -m server --disable-warnings -v ${file}
+        if [ $? -ne 0 ]; then
+            echo ${file} >> result.txt
+            bug=`expr ${bug} + 1`
+        fi
+    fi
+done
+
+now=`date +'%Y-%m-%d %H:%M:%S'`
+end_time=$(date --date="$now" +%s);
+echo "card1 used time:"$((end_time-start_time))"s"
+
+echo "total bugs: "${bug} >> result.txt
+exit ${bug}
