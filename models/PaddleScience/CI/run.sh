d4cb5031452ce809ce37427752cb3572dd93c9b9
plusNew001 <suijiaxin_job@163.com>
2023-04-28 05:27:45 +0000

    add CI code,test=model

---
+++ models/PaddleScience/CI/run.sh
@@ -0,0 +1,32 @@
+# 获取当前工作目录
+home=$PWD
+
+# 运行 api 测试脚本，并获取退出码
+cd $home/test_apis && bash ./run.sh && api=$?
+
+# 运行 examples 测试脚本，并获取退出码
+cd $home/test_models && bash ./run.sh && example=$?
+
+# 输出测试结果
+echo "=============== result ================="
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+if [ `expr ${api} + ${example}` -eq 0 ]; then
+  # 如果两个测试脚本的退出码之和为 0，则说明测试全部通过，输出正确结果并退出程序
+  result=`find . -name "result.txt"`
+  for file in ${result}
+    do
+      cat ${file}
+      echo "------------------------"
+    done
+  echo "success!"
+else
+  # 如果两个测试脚本的退出码之和不为 0，则说明有测试未通过，输出错误结果并退出程序
+  result=`find $home/ -name "result.txt"`
+  for file in ${result}
+    do
+      cat ${file}
+      echo "--------------------------"
+    done
+  echo "error!"
+  exit `expr ${api} + ${example}`
+fi
