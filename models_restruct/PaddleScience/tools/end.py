a024310940b29c0fabf084162fd38a088746928f
plusNew001 <suijiaxin_job@163.com>
2023-05-06 11:37:07 +0000

    add ce,test=model

--- models_restruct/PaddleScience/tools/end.py
+++ models_restruct/PaddleScience/tools/end.py
@@ -12,14 +12,14 @@ import logging
 import yaml
 import wget
 import numpy as np
-
+import urllib.request
 # from picture.analysis import analysis, draw
-from picture.analysis import plt_dy2st
+from picture.analysis import plot_loss_from_log_files
 
 logger = logging.getLogger("ce")
 
 
-class PaddleClas_End(object):
+class PaddleScience_End(object):
     """
     回收类
     """
@@ -31,7 +31,7 @@ class PaddleClas_End(object):
         self.reponame = os.environ["reponame"]
         self.qa_yaml_name = os.environ["qa_yaml_name"]
 
-    def dy2st_plt(self):
+    def plot_loss(self):
         """
         绘制动转静图片
         """
@@ -41,75 +41,13 @@ class PaddleClas_End(object):
             path_list = []
             for name in os.listdir(os.path.join("../logs", self.reponame, self.qa_yaml_name)):
                 path_list.append(os.path.join("../logs", self.reponame, self.qa_yaml_name, name))
-            plt_dy2st(path_list, self.qa_yaml_name)
-            # with open("dy2st.yaml", "r", encoding="utf-8") as f:
-            #     content = yaml.load(f, Loader=yaml.FullLoader)
-            # self.dy2st_yaml = content[self.qa_yaml_name]
-            # logger.info("self.dy2st_yaml is {}".format(self.dy2st_yaml))
-            # # train
-            # train_log_info_map = analysis(
-            #     os.path.join("../logs", self.reponame, self.qa_yaml_name), self.dy2st_yaml, "train"
-            # )
-            # draw(train_log_info_map, self.dy2st_yaml, "train")
-            # # eval
-            # if "eval" in self.dy2st_yaml.keys():
-            #     train_log_info_map = analysis(
-            #         os.path.join("../logs", self.reponame, self.qa_yaml_name), self.dy2st_yaml, "eval"
-            #     )
-            #     draw(train_log_info_map, self.dy2st_yaml, "eval")
+            plot_loss_from_log_files(path_list, self.qa_yaml_name)
         except Exception as e:
             logger.info("draw picture failed")
             logger.info("error info : {}".format(e))
         os.chdir(path_now)
         return 0
 
-    def remove_data(self):
-        """
-        回收之前下载的数据
-        """
-        list_dir = os.listdir(self.reponame)
-        path_now = os.getcwd()
-        os.chdir(self.reponame)
-
-        for file_name in list_dir:
-            if ".tar" in file_name:
-                os.remove(file_name)
-                if os.path.exists(file_name.replace(".tar", "")):
-                    shutil.rmtree(file_name.replace(".tar", ""))
-                logger.info("#### clean data _infer: {}".format(file_name))
-
-            if "_pretrained.pdparams" in file_name:
-                os.remove(file_name)
-                logger.info("#### clean data: {}".format(file_name))
-
-            # if file_name == "inference":
-            #     shutil.rmtree("inference")
-            #     logger.info("#### clean data inference: {}".format("inference"))
-
-            if file_name == "output":
-                del_pdparams = glob.glob(r"output/*/*/*.pdparams")
-                del_pdopt = glob.glob(r"output/*/*/*.pdopt")
-                if del_pdparams != [] and del_pdopt != []:
-                    logger.info("#### clean data pdparams: {}".format("pdparams"))
-                    for del_name in del_pdparams:
-                        if "latest" not in del_name:  # 保留最后一个epoch是完整的，其它只保留结构参数
-                            os.remove(del_name)
-                    for del_name in del_pdopt:
-                        if "latest" not in del_name:
-                            os.remove(del_name)
-
-            if file_name == "dataset":
-                del_dataset = glob.glob(r"dataset/*.tar")
-                if del_dataset != []:
-                    logger.info("#### clean data dataset: {}".format("dataset"))
-                    for del_name in del_dataset:
-                        os.remove(del_name)
-                if os.path.exists(os.path.join("dataset", "face")):
-                    shutil.rmtree(os.path.join("dataset", "face"))
-                    logger.info("#### clean data face: {}".format("dataset"))
-
-        os.chdir(path_now)
-        return 0
 
     def build_end(self):
         """
@@ -118,13 +56,17 @@ class PaddleClas_End(object):
         # 进入repo中
         logger.info("build remove_data start")
         ret = 0
-        ret = self.remove_data()
-        if ret:
-            logger.info("build remove_data failed")
-            return ret
-        if "dy2st_convergence" in self.qa_yaml_name:
-            logger.info("dy2st_convergence star draw picture")
-            self.dy2st_plt()
+        # ret = self.remove_data()
+        # if ret:
+        #     logger.info("build remove_data failed")
+        #     return ret
+        if "examples" in self.qa_yaml_name:
+            url = "https://paddle-qa.bj.bcebos.com/suijiaxin/base_log/ldc2d_unsteady_Re10_base.log"
+            file_name = "ldc2d_unsteady_Re10_base.log"
+            urllib.request.urlretrieve(url, file_name)
+            shutil.copy(file_name, "./logs/{}/{}/".format(self.reponame,self.qa_yaml_name))
+            logger.info("plot start！")
+            self.plot_loss()
         logger.info("build remove_data end")
         return ret
 
@@ -133,7 +75,7 @@ def run():
     """
     执行入口
     """
-    model = PaddleClas_End()
+    model = PaddleScience_End()
     model.build_end()
     return 0
 
