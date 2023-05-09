bc9c307208db4d3bd5c37dfa3d0616d03cdb9737
EmmonsCurse <1577972691@qq.com>
2023-05-09 15:51:02 +0800

    [Inference] update_diff_delta

--- inference/python_api_test/test_class_model/test_ViT_large_patch32_384_trt_fp16.py
+++ inference/python_api_test/test_class_model/test_ViT_large_patch32_384_trt_fp16.py
@@ -82,7 +82,7 @@ def test_trt_fp16_more_bz():
         test_suite1.trt_more_bz_test(
             input_data_dict,
             output_data_dict,
-            delta=0.002,
+            delta=0.005,
             max_batch_size=max_batch_size,
             min_subgraph_size=1,
             precision="trt_fp16",
@@ -100,7 +100,7 @@ def test_trt_fp16_more_bz():
         test_suite2.trt_more_bz_test(
             input_data_dict,
             output_data_dict,
-            delta=0.002,
+            delta=0.005,
             max_batch_size=max_batch_size,
             min_subgraph_size=1,
             precision="trt_fp16",
@@ -201,7 +201,7 @@ def test_trt_fp16_bz1_multi_thread():
         input_data_dict,
         output_data_dict,
         min_subgraph_size=1,
-        delta=0.002,
+        delta=0.005,
         precision="trt_fp16",
     )
 
