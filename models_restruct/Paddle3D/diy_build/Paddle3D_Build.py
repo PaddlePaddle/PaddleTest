e7e36edce5d896c57c4c502a782bc2af571737ec
JiaXiao243 <jiaxiao243@126.com>
2023-05-18 10:56:49 +0000

    update 3d, run_mode=model

--- models_restruct/Paddle3D/diy_build/Paddle3D_Build.py
+++ models_restruct/Paddle3D/diy_build/Paddle3D_Build.py
@@ -123,8 +123,8 @@ class Paddle3D_Build(Model_Build):
             for filename in self.test_model_list:
                 print("filename:{}".format(filename))
                 if filename == 'configs/petr/petrv2_vovnet_gridmask_p4_800x320_dn_amp.yml':
+                    os.makedirs("data")
                     if os.path.exists("/ssd2/ce_data/Paddle3D/nuscenes_petrv2"):
-                        os.makedirs("data")
                         os.symlink("/ssd2/ce_data/Paddle3D/nuscenes_petrv2", "data/nuscenes")
                     else:
                         os.symlink("/home/jiaxiao01/Paddle3D/nuscenes", "data/nuscenes")
