a4d6e4efa6cd61e6f4108adbd75371523492c386
JiaXiao243 <jiaxiao243@126.com>
2023-05-26 08:27:47 +0000

    debug

--- models_restruct/models_env/linux_env_dy2st.sh
+++ models_restruct/models_env/linux_env_dy2st.sh
@@ -391,8 +391,7 @@ if [[ "${docker_flag}" == "" ]]; then
         mkdir -p /paddle/build/third_party/CINN/src/external_cinn-build/thirds/install/jitify/stl_headers/algorithm
         python -m pip install --user -U pip  -i https://mirror.baidu.com/pypi/simple #升级pip
         python -m pip install --user -U -r requirements.txt  -i https://mirror.baidu.com/pypi/simple #预先安装依赖包
-        # python main.py --models_list=${models_list:-None} --models_file=${models_file:-None} --system=${system:-linux} --step=${step:-train} --reponame=${reponame:-PaddleClas} --mode=${mode:-function} --use_build=${use_build:-yes} --branch=${branch:-develop} --get_repo=${get_repo:-wget} --paddle_whl=${paddle_whl:-None} --dataset_org=${dataset_org:-None} --dataset_target=${dataset_target:-None} --set_cuda=${set_cuda:-0,1} --timeout=${timeout:-3600}
-        
+        python main.py --models_list=${models_list:-None} --models_file=${models_file:-None} --system=${system:-linux} --step=${step:-train} --reponame=${reponame:-PaddleClas} --mode=${mode:-function} --use_build=${use_build:-yes} --branch=${branch:-develop} --get_repo=${get_repo:-wget} --paddle_whl=${paddle_whl:-None} --dataset_org=${dataset_org:-None} --dataset_target=${dataset_target:-None} --set_cuda=${set_cuda:-0,1} --timeout=${timeout:-3600}
         python tools/end.py
     ' &
     wait $!
