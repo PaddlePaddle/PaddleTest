14b5a5cbd8201f83f7fe706443e70c05cf23b4bd
JiaXiao243 <jiaxiao243@126.com>
2023-06-05 11:55:15 +0000

    update ts, run_mode=model

--- models/PaddleTS/CI/conftest.py
+++ models/PaddleTS/CI/conftest.py
@@ -27,4 +27,5 @@ collect_ignore_glob = [
     "tests/models/representation/dl/test_repr_base.py",
     "tests/models/representation/dl/test_ts2vec.py",
     "tests/models/representation/task/test_repr_cluster.py",
+    "tests/models/forecasting/dl/distributions/test_distributions.py"
 ]
