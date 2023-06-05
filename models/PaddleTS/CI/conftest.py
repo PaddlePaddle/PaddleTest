26dd7dba937d44c9649218149265a40a088e3327
JiaXiao243 <jiaxiao243@126.com>
2023-06-05 08:52:59 +0000

    update ts, run_mode=model

---
+++ models/PaddleTS/CI/conftest.py
@@ -0,0 +1,12 @@
+'''
+collect_ignore_glob
+'''
+collect_ignore_glob = ['tests/automl/test_autots.py', 'tests/automl/test_optimize_runner.py', \
+'tests/automl/test_seacher.py', 'tests/automl/test_search_space_configer.py', 'tests/datasets/repository/test_base.py',\
+'tests/models/forecasting/dl/test_nhits.py', 'tests/models/forecasting/dl/test_nlinear.py', \
+'tests/models/representation/task/test_repr_classifier.py', 'tests/models/representation/task/test_repr_forecasting.py', \
+'tests/pipeline/test_pipeline.py', 'tests/transform/test_base.py', 'tests/transform/test_sklearn_transform_base.py', \
+'tests/transform/utils/test_make_ts_transform.py', 'tests/utils/test_cross_validate.py', 'tests/utils/test_utils.py', \
+'tests/xai/post_hoc/test_deep_paddle.py', 'tests/xai/post_hoc/test_shap_explainer.py', \
+'models/classify/dl/test_inception_time.py', 'models/anomaly/dl/test_vae.py', \
+'models/forecasting/dl/test_transformer.py']
