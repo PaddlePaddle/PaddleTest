9c6b720ce85864218c347db4841b8570d0276b06
plusNew001 <suijiaxin_job@163.com>
2023-06-14 11:51:27 +0000

    change deepxde ce code,test=model

--- models/PaddleScience/test_deepxde/models/run.sh
+++ models/PaddleScience/test_deepxde/models/run.sh
@@ -4,15 +4,15 @@ cases=`find ../config/$1 -name "*.yaml" | sort`
 echo $cases
 ignoe=""
 bug=0
-python3.7 change_model.py
+#python${py_version} change_model.py
 echo "============ failed cases =============" > result.txt
 for file_dir in ${cases}
 do
     name=`basename -s .yaml $file_dir`
     echo ${name}
-    python3.7 standardization.py -f ${file_dir}
-    python3.7 generate.py -f ${name} -a $1
-    python3.7 test_${name}.py
+    python${py_version} standardization.py -f ${file_dir}
+    python${py_version} generate.py -f ${name} -a $1
+    python${py_version} test_${name}.py
     if [ $? -ne 0 ]; then
         echo test_${name} >> result.txt
         bug=`expr ${bug} + 1`
