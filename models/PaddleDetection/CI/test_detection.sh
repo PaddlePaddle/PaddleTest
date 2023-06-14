d64f861fca5ab57563be480a8a2085a7bdea5a0a
Pcjmmc <544546992@qq.com>
2023-06-14 16:30:00 +0800

    open WITH_KEYPOINT,test=model

--- models/PaddleDetection/CI/test_detection.sh
+++ models/PaddleDetection/CI/test_detection.sh
@@ -39,15 +39,11 @@ wget ${paddle_inference}
 tar xvf paddle_inference.tgz
 # 跟镜像有关
 sed -i "s|WITH_GPU=OFF|WITH_GPU=ON|g" scripts/build.sh
+sed -i "s|WITH_KEYPOINT=OFF|WITH_KEYPOINT=ON|g" scripts/build.sh
+sed -i "s|WITH_MOT=OFF|WITH_MOT=ON|g" scripts/build.sh
 sed -i "s|CUDA_LIB=/path/to/cuda/lib|CUDA_LIB=/usr/local/cuda/lib64|g" scripts/build.sh
 sed -i "s|/path/to/paddle_inference|../paddle_inference|g" scripts/build.sh
 sed -i "s|CUDNN_LIB=/path/to/cudnn/lib|CUDNN_LIB=/usr/lib/x86_64-linux-gnu|g" scripts/build.sh
-# sed -i "s|WITH_GPU=OFF|WITH_GPU=ON|g" scripts/build.sh
-# sed -i "s|WITH_KEYPOINT=OFF|WITH_KEYPOINT=ON|g" scripts/build.sh
-# sed -i "s|WITH_MOT=OFF|WITH_MOT=ON|g" scripts/build.sh
-# sed -i "s|CUDA_LIB=/path/to/cuda/lib|CUDA_LIB=/usr/local/cuda/lib64|g" scripts/build.sh
-# sed -i "s|/path/to/paddle_inference|../paddle_inference|g" scripts/build.sh
-# sed -i "s|CUDNN_LIB=/path/to/cudnn/lib|CUDNN_LIB=/usr/lib/x86_64-linux-gnu|g" scripts/build.sh
 sh scripts/build.sh
 cd ../..
 #compile op
