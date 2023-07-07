6cbbb622e5165482ebb9364464d662ce966be489
liuhuanling <34302448+Pcjmmc@users.noreply.github.com>
2023-07-07 15:22:06 +0800

    add auto kill (#2348)
    * disable backward test
    
    * 新增slim case test=model
    
    * 删除临时支持，test=model
    
    * 去掉临时支持 (#2208)
    
    * disable backward test
    
    * 新增slim case test=model
    
    * 去掉临时支持，test=model
    
    * fix bug, test=model
    
    ---------
    
    Co-authored-by: shjNT <sunhongjiang@baidu.com>
    
    * 新增PaddleSlim 量化新格式case, test=model
    
    * fix style, test=model
    
    * fix style, test=model
    
    * 修改slim数据集问题，test=model
    
    * fix style， test=model
    
    * fix style， test=model
    
    * fix slim case bug, test=model
    
    * fix slim data buf, test=model
    
    * 修改seg安装方式，test=model
    
    * test=model,支持det新增模型
    
    * fix slim dependence，test=model
    
    * fix code style, test=model
    
    * fix denpendent, test=model
    
    * 先保证产物传递，test=model
    
    * 去掉重复代码，test=model
    
    * fix det slim,test=model
    
    * 指标通过后，先上传，test=model
    
    * fix bug, test=model
    
    * fix test=model
    
    * 更新setuptools，test=model
    
    * 修复检测ext_op问题，test=model
    
    * 修改seg ci，test=model
    
    * update seg, test=model
    
    * fix install seg，test=model
    
    * xiufu det cpp, test=model
    
    * 适配det编译，test=model
    
    * 去掉seg pyhton，test=model
    
    * update seg compile, test=model
    
    * det ci，test=model
    
    * 去掉，test=model
    
    * 关闭det ci阶段的cpp case,test=model
    
    * open WITH_KEYPOINT,test=model
    
    * open cpp, test=model
    
    * 不更新setuptools, test=model
    
    * 更新PaddleDet infer，test=model
    
    * 修复detcpp预测问题 test=model
    
    * fix det style, test=model
    
    * fix style, test=model
    
    * fix style，test=model
    
    * 支持Det检测，test=model
    
    * add nlp act, test=model
    
    * add nlp model, test=model
    
    * 修复paddle infer的兼容性，test=model
    
    * fix params, test=model
    
    * fix style,test=model
    
    * fix, test=model
    
    * print env value, test=model
    
    * 固定det依赖包版本，test=model
    
    * change det format，test=model
    
    * 上传原始模型, test=model
    
    * 适配非正常退出，test=model
    
    * 规范风格，test=model
    
    * fix style, test=model
    
    ---------
    
    Co-authored-by: shjNT <sunhongjiang@baidu.com>

---
+++ models_restruct/PaddleDetection/tools/end.py
@@ -0,0 +1,57 @@
+# encoding: utf-8
+"""
+执行case后执行的文件
+"""
+import os
+import logging
+
+logger = logging.getLogger("ce")
+
+
+class PaddleDetection_End(object):
+    """
+    case执行结束后
+    """
+
+    def __init__(self):
+        """
+        初始化
+        """
+        self.reponame = os.environ["reponame"]
+        self.model = os.environ["model"]
+        self.qa_model_name = os.environ["qa_yaml_name"]
+        self.log_dir = "logs"
+        self.log_name = "train_prim_single.log"
+        self.log_path = os.path.join(os.getcwd(), self.log_dir, self.reponame, self.qa_model_name, self.log_name)
+        logger.info("log_path:{}".format(self.log_path))
+        self.qa_model_name_base = ""
+        self.prim_log_path = ""
+        self.standard_log_path = ""
+
+    def build_end(self):
+        """
+        执行准备过程
+        """
+        # kill遗留程序
+        logger.info("PID before is {}".format(os.system(f"ps aux| grep '{self.qa_model_name}'| grep -v 'main.py'")))
+        cmd_kill = os.system(
+            f"ps aux | grep '{self.qa_model_name}' | grep -v 'main.py' | awk '{{print $2}}' | xargs kill -9"
+        )
+        # cmd_kill = os.system(f"pkill -f '{self.qa_model_name}'| grep -v 'main.py'") #这样会先执行kill再执行grep没有生效
+        logger.info("PID after is {}".format(os.system(f"ps aux| grep '{self.qa_model_name}'| grep -v 'main.py'")))
+        # 异常256
+        logger.info("cmd_kill is {}".format(cmd_kill))
+        return 0
+
+
+def run():
+    """
+    执行入口
+    """
+    model = PaddleDetection_End()
+    model.build_end()
+    return 0
+
+
+if __name__ == "__main__":
+    run()
