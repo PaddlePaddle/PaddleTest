151375ebb9ec589460c685823dcf32d2a4e41e77
Pcjmmc <544546992@qq.com>
2023-07-07 15:18:23 +0800

    fix style, test=model

--- models_restruct/PaddleDetection/tools/end.py
+++ models_restruct/PaddleDetection/tools/end.py
@@ -22,8 +22,7 @@ class PaddleDetection_End(object):
         self.qa_model_name = os.environ["qa_yaml_name"]
         self.log_dir = "logs"
         self.log_name = "train_prim_single.log"
-        self.log_path = os.path.join(
-            os.getcwd(), self.log_dir, self.reponame, self.qa_model_name, self.log_name)
+        self.log_path = os.path.join(os.getcwd(), self.log_dir, self.reponame, self.qa_model_name, self.log_name)
         logger.info("log_path:{}".format(self.log_path))
         self.qa_model_name_base = ""
         self.prim_log_path = ""
@@ -34,14 +33,12 @@ class PaddleDetection_End(object):
         执行准备过程
         """
         # kill遗留程序
-        logger.info("PID before is {}".format(
-            os.system(f"ps aux| grep '{self.qa_model_name}'| grep -v 'main.py'")))
+        logger.info("PID before is {}".format(os.system(f"ps aux| grep '{self.qa_model_name}'| grep -v 'main.py'")))
         cmd_kill = os.system(
             f"ps aux | grep '{self.qa_model_name}' | grep -v 'main.py' | awk '{{print $2}}' | xargs kill -9"
         )
         # cmd_kill = os.system(f"pkill -f '{self.qa_model_name}'| grep -v 'main.py'") #这样会先执行kill再执行grep没有生效
-        logger.info("PID after is {}".format(
-            os.system(f"ps aux| grep '{self.qa_model_name}'| grep -v 'main.py'")))
+        logger.info("PID after is {}".format(os.system(f"ps aux| grep '{self.qa_model_name}'| grep -v 'main.py'")))
         # 异常256
         logger.info("cmd_kill is {}".format(cmd_kill))
         return 0
