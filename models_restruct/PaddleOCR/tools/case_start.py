1b29c196de26c721a3c6c7ca6e2a0ddb5df7d82b
JiaXiao243 <jiaxiao243@126.com>
2023-07-19 07:38:22 +0000

    fix ocr, run_mode=model

--- models_restruct/PaddleOCR/tools/case_start.py
+++ models_restruct/PaddleOCR/tools/case_start.py
@@ -31,7 +31,9 @@ class PaddleOCR_Case_Start(object):
         sysstr = platform.system()
         if sysstr == "Linux":
             logger.info("###kill python process")
-            os.system("ps aux| grep python | awk '{print $2}' | xargs kill -9")
+            pid = os.getpid()
+            cmd = """ps aux| grep python | grep -v main.py |grep -v %s | awk '{print $2}'| xargs kill -9;""" % pid
+            os.system(cmd)
 
 
 def run():
