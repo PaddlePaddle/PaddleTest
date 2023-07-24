f7032a2a61dce0bad9f47b47bcc2d3330a4e5c07
Zeref996 <825276847@qq.com>
2023-07-24 16:48:44 +0800

    fix jelly_v2 1, test=model

--- framework/e2e/api_benchmark_new/jelly/jelly_v2.py
+++ framework/e2e/api_benchmark_new/jelly/jelly_v2.py
@@ -1,8 +1,4 @@
-#!/bin/env python
-# -*- coding: utf-8 -*-
-# @author DDDivano
-# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
-#!/bin/env python
+#!/bin/env python3
 # -*- coding: utf-8 -*-
 # encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
 """
