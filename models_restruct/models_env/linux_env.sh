57698217f4d5017e8dfa60e9944e463fe2320c2e
jiaxiao243 <jiaxiao243@126.com>
2023-08-21 03:42:37 +0000

    fix bug ,run_mode=model

--- models_restruct/models_env/linux_env.sh
+++ models_restruct/models_env/linux_env.sh
@@ -601,6 +601,13 @@ else
     export mount_path="/home/paddleqa/cfs/${reponame}"
     ls ${mount_path}
     echo "@@@mount_path: ${mount_path}"
+   
+    # FLAGS_enable_new_ir_in_executor bug  
+    if [ $FLAGS_enable_new_ir_in_executor == 0 ];then
+    echo FLAGS_enable_new_ir_in_executor
+    echo $FLAGS_enable_new_ir_in_executor
+    unset FLAGS_enable_new_ir_in_executor
+    fi
 
     nvidia-smi;
     python -c "import sys; print(sys.version_info[:])";
