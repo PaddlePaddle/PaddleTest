87eb7af52e096e672e7cc90284a86f1a4a2ab42e
mengmingooo <mengminglxl@163.com>
2023-09-15 17:45:20 +0800

    fix blip2 prepare bug

--- models/PaddleMIX/CE/paddlemix/blip2/prepare.sh
+++ models/PaddleMIX/CE/paddlemix/blip2/prepare.sh
@@ -4,7 +4,7 @@ rm -rf /root/.paddlemix/datasets/coco
 wget https://bj.bcebos.com/v1/paddlenlp/models/community/paddlemix/benchmark/blip2/coco.tar.gz -P /root/.paddlemix/datasets/
 cd /root/.paddlemix/datasets/
 tar -zxvf coco.tar.gz
-cd ${root_path}
+cd ${root_path}/PaddleMIX/
 
 export http_proxy=${proxy}
 export https_proxy=${proxy}
