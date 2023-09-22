14d1070e941d9eabac781d4f0cec967b2b8fc5cd
mengmingooo <mengminglxl@163.com>
2023-09-22 22:36:20 +0800

    fix some bug

--- models/PaddleMIX/CE/ppdiffusers/autoencoder_vae/prepare.sh
+++ models/PaddleMIX/CE/ppdiffusers/autoencoder_vae/prepare.sh
@@ -5,3 +5,7 @@ export https_proxy=${proxy}
 pip install -r requirements.txt
 unset http_proxy
 unset https_proxy
+
+rm -rf data/
+wget https://paddlenlp.bj.bcebos.com/models/community/junnyu/develop/laion400m_demo_data.tar.gz
+tar -zxvf laion400m_demo_data.tar.gz
