1ae48f2350aea30f51704a1a41302e53844ddfd3
mengmingooo <mengminglxl@163.com>
2023-09-23 14:21:19 +0800

    fix prepare req

--- models/PaddleMIX/CE/ppdiffusers/fire.sh
+++ models/PaddleMIX/CE/ppdiffusers/fire.sh
@@ -13,9 +13,9 @@ echo ${work_path2}/
 cd ${work_path2}
 export http_proxy=${proxy}
 export https_proxy=${proxy}
-pip install -e .
-pip install -r requirements.txt
 python3.10 -m pip install --upgrade pip
+pip install -r requirements.txt
+pip install -e .
 unset http_proxy
 unset https_proxy
 pip install pytest safetensors ftfy fastcore opencv-python einops parameterized requests-mock -i http://pip.baidu.com/root/baidu/+simple/ --trusted-host pip.baidu.com
