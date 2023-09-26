4232e52742a722502c2f7c30c2457a66f718a9a2
mengmingooo <mengminglxl@163.com>
2023-09-26 15:02:01 +0800

    change step

--- models/PaddleMIX/CE/ppdiffusers/t2i_adapter/infer.py
+++ models/PaddleMIX/CE/ppdiffusers/t2i_adapter/infer.py
@@ -1,6 +1,6 @@
 from ppdiffusers import StableDiffusionAdapterPipeline, T2IAdapter
 from ppdiffusers.utils import load_image
-adapter = T2IAdapter.from_pretrained("./sd15_openpose/checkpoint-12000/adapter")
+adapter = T2IAdapter.from_pretrained("./sd15_openpose/checkpoint-100/adapter")
 pipe = StableDiffusionAdapterPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", adapter = adapter, safety_checker=None)
 pose_image = load_image("https://paddlenlp.bj.bcebos.com/models/community/westfish/t2i-adapter/test/man-openpose.png")
 img = pipe(prompt="a beautiful girl", image=pose_image, guidance_scale=9, num_inference_steps=50).images[0]
