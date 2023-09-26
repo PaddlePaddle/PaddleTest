af05dd1e061e328e2c3d8495cb0aa609100b668b
mengmingooo <mengminglxl@163.com>
2023-09-26 15:16:40 +0800

    change bs and step

--- models/PaddleMIX/CE/ppdiffusers/text_to_image_laion400m/infer_mscoco.sh
+++ models/PaddleMIX/CE/ppdiffusers/text_to_image_laion400m/infer_mscoco.sh
@@ -15,14 +15,14 @@ python generate_pipelines.py \
 python generate_images.py \
     --model_name_or_path ./ldm_pipelines \
     --file coco1k \
-    --batch_size 16 \
+    --batch_size 1 \
     --save_path ./outputs \
     --guidance_scales 3 \
     --seed 42 \
     --scheduler_type ddim \
     --height 256 \
     --width 256 \
-    --num_inference_steps 50 \
+    --num_inference_steps 3 \
     --device gpu
 
 # 检查命令是否成功执行
