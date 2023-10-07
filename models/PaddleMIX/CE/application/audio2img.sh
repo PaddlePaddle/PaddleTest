f7aa3a850caea4ab9d8bf325a6d08698827718d9
mengmingooo <mengminglxl@163.com>
2023-10-07 14:25:41 +0800

    fix app code

--- models/PaddleMIX/CE/application/audio2img.sh
+++ models/PaddleMIX/CE/application/audio2img.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python applications/Audio2Img/audio2img_imagebind.py \
---model_name_or_path The dir name of imagebind checkpoint. \
---stable_unclip_model_name_or_path The dir name of StableUnCLIPImg2ImgPipeline pretrained checkpoint. \
---input_audio bird_audio.wav
+--model_name_or_path imagebind-1.2b/ \
+--stable_unclip_model_name_or_path stabilityai/stable-diffusion-2-1-unclip \
+--input_audio https://paddlenlp.bj.bcebos.com/models/community/paddlemix/audio-files/bird_audio.wav
