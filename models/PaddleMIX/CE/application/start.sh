44c908f25f54ff665e9399708c140fd69548ac9f
mengmingooo <mengminglxl@163.com>
2023-10-10 20:17:40 +0800

    fix some bug

--- models/PaddleMIX/CE/application/start.sh
+++ models/PaddleMIX/CE/application/start.sh
@@ -24,7 +24,7 @@ bash prepare.sh
 cd ${work_path}
 
 echo "*******application grounded_sam begin***********"
-(python grounded_sam.py) | tee ${log_dir}/grounded_sam.log
+(python grounded_sam.py) 2>&1 | tee ${log_dir}/grounded_sam.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -38,7 +38,7 @@ echo "*******application grounded_sam end***********"
 
 
 echo "*******application automatic_label begin***********"
-(python automatic_label.py) | tee ${log_dir}/automatic_label.log
+(python automatic_label.py) 2>&1 | tee ${log_dir}/automatic_label.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -50,8 +50,8 @@ else
 fi
 echo "*******application automatic_label end***********"
 
-echo "*******application grounded_sam_inpainting begin***********"
-(python grounded_sam_inpainting.py) | tee ${log_dir}/grounded_sam_inpainting.log
+echo "*******application grounded_sam begin***********"
+(python grounded_sam.py) 2>&1 | tee ${log_dir}/grounded_sam.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -64,7 +64,7 @@ fi
 echo "*******application grounded_sam_inpainting end***********"
 
 echo "*******application grounded_sam_inpainting begin***********"
-(python grounded_sam_inpainting.py) | tee ${log_dir}/grounded_sam_inpainting.log
+(python grounded_sam_inpainting.py) 2>&1 | tee ${log_dir}/grounded_sam_inpainting.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -77,7 +77,7 @@ fi
 echo "*******application grounded_sam_inpainting end***********"
 
 echo "*******application grounded_sam_chatglm begin***********"
-(python grounded_sam_chatglm.py) | tee ${log_dir}/grounded_sam_chatglm.log
+(python grounded_sam_chatglm.py) 2>&1 | tee ${log_dir}/grounded_sam_chatglm.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -90,7 +90,7 @@ fi
 echo "*******application grounded_sam_chatglm end***********"
 
 echo "*******application text_guided_image_inpainting begin***********"
-(python text_guided_image_inpainting.py) | tee ${log_dir}/text_guided_image_inpainting.log
+(python text_guided_image_inpainting.py) 2>&1 | tee ${log_dir}/text_guided_image_inpainting.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -103,7 +103,7 @@ fi
 echo "*******application text_guided_image_inpainting end***********"
 
 echo "*******application text_to_image_generation begin***********"
-(python text_to_image_generation.py) | tee ${log_dir}/text_to_image_generation.log
+(python text_to_image_generation.py) 2>&1 | tee ${log_dir}/text_to_image_generation.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -116,7 +116,7 @@ fi
 echo "*******application text_to_image_generation end***********"
 
 echo "*******application text_guided_image_upscaling begin***********"
-(python text_guided_image_upscaling.py) | tee ${log_dir}/text_guided_image_upscaling.log
+(python text_guided_image_upscaling.py) 2>&1 | tee ${log_dir}/text_guided_image_upscaling.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -142,7 +142,7 @@ echo "*******application text_guided_image_upscaling end***********"
 # echo "*******application dual_text_image_guided_generation end***********"
 
 echo "*******application image2image_text_guided_generation begin***********"
-(python image2image_text_guided_generation.py) | tee ${log_dir}/image2image_text_guided_generation.log
+(python image2image_text_guided_generation.py) 2>&1 | tee ${log_dir}/image2image_text_guided_generation.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -155,7 +155,7 @@ fi
 echo "*******application image2image_text_guided_generation end***********"
 
 echo "*******application text2video_generation begin***********"
-(python text2video_generation.py) | tee ${log_dir}/text2video_generation.log
+(python text2video_generation.py) 2>&1 | tee ${log_dir}/text2video_generation.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -168,7 +168,7 @@ fi
 echo "*******application text2video_generation end***********"
 
 echo "*******application audio2caption_generation begin***********"
-(python audio2caption_generation.py) | tee ${log_dir}/audio2caption_generation.log
+(python audio2caption_generation.py) 2>&1 | tee ${log_dir}/audio2caption_generation.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -181,7 +181,7 @@ fi
 echo "*******application audio2caption_generation end***********"
 
 echo "*******application audio2chat_generation begin***********"
-(python audio2chat_generation.py) | tee ${log_dir}/audio2chat_generation.log
+(python audio2chat_generation.py) 2>&1 | tee ${log_dir}/audio2chat_generation.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -194,7 +194,7 @@ fi
 echo "*******application audio2chat_generation end***********"
 
 echo "*******application music_generation begin***********"
-(python music_generation.py) | tee ${log_dir}/music_generation.log
+(python music_generation.py) 2>&1 | tee ${log_dir}/music_generation.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -207,7 +207,7 @@ fi
 echo "*******application music_generation end***********"
 
 echo "*******application audio2img begin***********"
-(bash audio2img.sh) | tee ${log_dir}/audio2img.log
+(bash audio2img.sh) 2>&1 | tee ${log_dir}/audio2img.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -220,7 +220,7 @@ fi
 echo "*******application audio2img end***********"
 
 echo "*******application audio_text2img begin***********"
-(bash audio_text2img.sh) | tee ${log_dir}/audio_text2img.log
+(bash audio_text2img.sh) 2>&1 | tee ${log_dir}/audio_text2img.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -233,7 +233,7 @@ fi
 echo "*******application audio_text2img end***********"
 
 echo "*******application auodio_image2image begin***********"
-(bash auodio_image2image.sh) | tee ${log_dir}/auodio_image2image.log
+(bash auodio_image2image.sh) 2>&1 | tee ${log_dir}/auodio_image2image.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
