3166bd38cc9e372c111d7a36473871b8ecb4f210
mengmingooo <mengminglxl@163.com>
2023-10-12 13:17:32 +0800

    fix some bug

--- models/PaddleMIX/CE/paddlemix/eva02/prepare.sh
+++ models/PaddleMIX/CE/paddlemix/eva02/prepare.sh
@@ -2,7 +2,7 @@
 
 export http_proxy=${proxy}
 export https_proxy=${proxy}
-cd paddlemix/external_ops/
+cd ${root_path}/PaddleMIX/paddlemix/external_ops/
 python setup.py install
 unset http_proxy
 unset https_proxy
