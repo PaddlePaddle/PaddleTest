6995399c537a26ad9c41fe8075e237e7b20de185
jiaxiao01 <jiaxiao01@baidu.com>
2023-10-30 19:09:08 +0800

    fix bug, run_mode=model

--- models_restruct/models_env/linux_env.sh
+++ models_restruct/models_env/linux_env.sh
@@ -170,7 +170,7 @@ elif [[ ${AGILE_PIPELINE_NAME} =~ "Cuda116" ]] && [[ ${AGILE_PIPELINE_NAME} =~ "
     fi
 elif [[ ${AGILE_PIPELINE_NAME} =~ "Cuda116" ]] && [[ ${AGILE_PIPELINE_NAME} =~ "Python310" ]];then
     if [[ ${AGILE_PIPELINE_NAME} =~ "Develop" ]];then
-        linux_env_info_main get_wheel_url Cuda116 Python39 Develop ON
+        linux_env_info_main get_wheel_url Cuda116 Python310 Develop ON
         # export paddle_inference=${paddle_inference:-"https://paddle-inference-lib.bj.bcebos.com/develop/cxx_c/Linux/GPU/x86-64_gcc8.2_avx_mkl_cuda11.6_cudnn8.4.0-trt8.4.0.6/paddle_inference.tgz"}
         export TENSORRT_DIR=${TENSORRT_DIR:-"/usr/local/TensorRT-8.4.0.6"}
     else
