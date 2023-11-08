db78da0cb2f1eb1762d180e115c4734bcf01b820
mengmingooo <mengminglxl@163.com>
2023-11-08 14:47:41 +0800

    update ci run logic

--- models/PaddleMIX/CI/test.sh
+++ models/PaddleMIX/CI/test.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-export no_proxy=localhost,bj.bcebos.com,su.bcebos.com;
-rm -rf run_env_py310;
-mkdir run_env_py310;
-ln -s $(which python3.10) run_env_py310/python;
-ln -s $(which pip3.10) run_env_py310/pip;
-export PATH=$(pwd)/run_env_py310:${PATH};
-python --version
-
 export http_proxy=${proxy};
 export https_proxy=${proxy};
 bash prepare.sh
 unset http_proxy
 unset https_proxy
-
-export RUN_SLOW=False
-export RUN_NIGHTLY=False
-export FROM_HF_HUB=False
-export FROM_DIFFUSERS=False
-export TO_DIFFUSERS=False
-export HF_HUB_OFFLINE=False
-export CUDA_VISIBLE_DEVICES=$cudaid1
 
 work_path=`pwd`
 echo ${work_path}
