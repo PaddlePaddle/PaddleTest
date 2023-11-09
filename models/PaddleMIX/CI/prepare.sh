0a07cd4bc6937bf083c32f5c553a240e45a80396
mengmingooo <mengminglxl@163.com>
2023-11-09 19:38:25 +0800

    remove python version

--- models/PaddleMIX/CI/prepare.sh
+++ models/PaddleMIX/CI/prepare.sh
@@ -2,7 +2,7 @@
 
 cd ${root_path}/PaddleMIX
 pip install -r requirements.txt
-python3.10 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 pip install -e .
 pip install -r paddlemix/appflow/requirements.txt
 
