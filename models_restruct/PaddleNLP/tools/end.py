33ba73bf57b907efc2d94791032aa3f07844e239
zjjlivein <1834792141@qq.com>
2023-11-16 11:26:50 +0800

    test llama loss corve

--- models_restruct/PaddleNLP/tools/end.py
+++ models_restruct/PaddleNLP/tools/end.py
@@ -101,13 +101,11 @@ class PaddleNLP_End(object):
             if re.compile("baseline").findall(file):
                 baseline_info["baseline_loss"] = self.get_metrics(self.TRAIN_LOG_PATH + "/" + file, "loss")
                 baseline_info["baseline_ips"] = self.get_metrics(self.TRAIN_LOG_PATH + "/" + file, "ips")
-            elif re.compile("dy2st").findall(file):
+            else:
                 strategy_loss = file.split("train_")[-1].replace(".log", "") + "_loss"
                 strategy_ips = file.split("train_")[-1].replace(".log", "") + "_ips"
                 strategy_info[strategy_loss] = self.get_metrics(self.TRAIN_LOG_PATH + "/" + file, "loss")
                 strategy_info[strategy_ips] = self.get_metrics(self.TRAIN_LOG_PATH + "/" + file, "ips")
-            else:
-                logger.info("this log file not convergence task ")
 
         self.drow_picture(self.qa_yaml_name, baseline_info, strategy_info, metric="loss")
         self.drow_picture(self.qa_yaml_name, baseline_info, strategy_info, metric="ips")
