8c95de572ab496d901aa1f52d6ac407aa7a11dae
mengmingooo <mengminglxl@163.com>
2023-12-13 14:33:38 +0800

    tmp remove blip2 case

--- models/PaddleMIX/CI/test.sh
+++ models/PaddleMIX/CI/test.sh
@@ -23,9 +23,9 @@ cd ${work_path}/ut/
 bash fire.sh
 exit_code=$(($exit_code + $?))
 
-cd ${work_path}/paddlemix/
-bash fire.sh
-exit_code=$(($exit_code + $?))
+# cd ${work_path}/paddlemix/
+# bash fire.sh
+# exit_code=$(($exit_code + $?))
 
 log_dir=${root_path}/log
 cat ${log_dir}/res.log
