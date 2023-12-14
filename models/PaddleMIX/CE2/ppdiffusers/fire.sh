90474b04fae889e88e17d9005bf9eeb3e27d8d5d
mengmingooo <mengminglxl@163.com>
2023-12-14 10:31:00 +0800

    tmp skip ce2 ppdiffusers test ut case

--- models/PaddleMIX/CE2/ppdiffusers/fire.sh
+++ models/PaddleMIX/CE2/ppdiffusers/fire.sh
@@ -14,6 +14,12 @@ bash prepare.sh
 
 for subdir in */; do
   if [ -d "$subdir" ]; then
+
+    # 检查子目录是否为"ut_case"，如果是，则跳过(该目录需要的权重较大，暂时跳过运行)
+    if [ "$subdir" == "ut_case/" ]; then
+      continue
+    fi
+     
     start_script_path="$subdir/start.sh"
     if [ -f "$start_script_path" ]; then
       cd $subdir
