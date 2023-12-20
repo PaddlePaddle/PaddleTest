e18105dd5675157457f2f604672776a7e70d4ea8
mengmingooo <mengminglxl@163.com>
2023-12-20 16:24:00 +0800

    change res log name

--- models/PaddleMIX/CE2/env_info.sh
+++ models/PaddleMIX/CE2/env_info.sh
@@ -6,15 +6,15 @@ if [ ! -d "$log_dir" ]; then
     mkdir -p "$log_dir"
 fi
 
-echo "mix version:" >> ${log_dir}/res.log
+echo "mix version:" >> ${log_dir}/ce_ce_res.log
 cd ${root_path}/PaddleMIX
-echo "mix branch:" >> ${log_dir}/res.log
-git branch >> ${log_dir}/res.log
-echo "mix commit:" >> ${log_dir}/res.log
-git rev-parse HEAD >> ${log_dir}/res.log
+echo "mix branch:" >> ${log_dir}/ce_res.log
+git branch >> ${log_dir}/ce_res.log
+echo "mix commit:" >> ${log_dir}/ce_res.log
+git rev-parse HEAD >> ${log_dir}/ce_res.log
 cd ${root_path}
 
 log_dir=${root_path}/log
-echo "paddle version:" >> ${log_dir}/res.log
-echo "mix commit:" >> ${log_dir}/res.log
-python -c "import paddle;print(paddle.__git_commit__)" >> ${log_dir}/res.log
+echo "paddle version:" >> ${log_dir}/ce_res.log
+echo "mix commit:" >> ${log_dir}/ce_res.log
+python -c "import paddle;print(paddle.__git_commit__)" >> ${log_dir}/ce_res.log
