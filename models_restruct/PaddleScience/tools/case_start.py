92df41d49470059d0f312e7135929cf9afaf4d7a
plusNew001 <suijiaxin_job@163.com>
2024-01-16 03:19:48 +0000

    add ce case,test=model

--- models_restruct/PaddleScience/tools/case_start.py
+++ models_restruct/PaddleScience/tools/case_start.py
@@ -49,6 +49,9 @@ class PaddleScience_Case_Start(object):
                 logger.info("set org FLAGS_use_cinn as {}".format(os.getenv("FLAGS_use_cinn")))
         elif "deepcfd" in self.case_name:
             os.environ["FLAGS_cudnn_deterministic"] = "True"
+        elif "viv" in self.case_name:
+            os.system("wget -nc https://paddle-org.bj.bcebos.com/paddlescience/models/viv/viv_pretrained.pdeqn")
+            os.system("wget -nc https://paddle-org.bj.bcebos.com/paddlescience/models/viv/viv_pretrained.pdparams")
         else:
             return 0
 
