189cb95416934f21cc4df31e36c77af0ff4aaa3a
LQX <38774750+quanxiang-liu@users.noreply.github.com>
2024-01-17 19:36:23 +0800

    add SOT_EXPORT、,  test=model (#2615)

--- models_restruct/PaddleClas/tools/case_start.py
+++ models_restruct/PaddleClas/tools/case_start.py
@@ -35,6 +35,9 @@ class PaddleClas_Case_Start(object):
         """
         执行准备过程
         """
+        os.environ["SOT_EXPORT"] = "Layer_cases/{self.case_name}_{self.case_step}"
+        logger.info("set org SOT_EXPORT as {}".format(os.getenv("SOT_EXPORT")))
+
         if "dy2st_convergence" in self.qa_yaml_name:
             logger.info("dy2st_convergence tag is: {}".format(self.case_name.split("train_")[-1]))
 
--- models_restruct/PaddleGAN/tools/case_start.py
+++ models_restruct/PaddleGAN/tools/case_start.py
@@ -41,6 +41,9 @@ class PaddleClas_Case_Start(object):
         """
         下载预训练模型, 指定路径
         """
+        os.environ["SOT_EXPORT"] = "Layer_cases/{self.case_name}_{self.case_step}"
+        logger.info("set org SOT_EXPORT as {}".format(os.getenv("SOT_EXPORT")))
+
         if "dy2st_convergence" in self.qa_yaml_name:
             logger.info("dy2st_convergence tag is: {}".format(self.case_name.split("train_")[-1]))
 
