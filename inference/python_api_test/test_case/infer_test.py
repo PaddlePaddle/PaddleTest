b8b3ca9c727370f8e2c5240c439268aeefd84050
EmmonsCurse <1577972691@qq.com>
2024-01-18 11:35:41 +0800

    fix

--- inference/python_api_test/test_case/infer_test.py
+++ inference/python_api_test/test_case/infer_test.py
@@ -100,7 +100,6 @@ class InferenceTest(object):
         for _, output_data_name in enumerate(output_names):
             output_handle = predictor.get_output_handle(output_data_name)
             output_data = output_handle.copy_to_cpu()
-            # output_data = output_data.flatten()
             output_data_dict[output_data_name] = output_data
         return output_data_dict
 
@@ -316,7 +315,6 @@ class InferenceTest(object):
         for i, output_data_name in enumerate(output_names):
             output_handle = predictor.get_output_handle(output_data_name)
             output_data = output_handle.copy_to_cpu()
-            # output_data = output_data.flatten()
             output_data_truth_val = output_data_dict[output_data_name]
             print("output_data_shape:", output_data.shape)
             print("truth_value_shape:", output_data_truth_val.shape)
@@ -374,10 +372,8 @@ class InferenceTest(object):
         for _, input_data_name in enumerate(input_names):
             input_handle = predictor.get_input_handle(input_data_name)
             input_handle.copy_from_cpu(input_data_dict[input_data_name])
-
         for i in range(repeat):
             predictor.run()
-
         output_names = predictor.get_output_names()
         print("output_names:", output_names)
         print("truth_value_names:", list(output_data_dict.keys()))
@@ -426,6 +422,8 @@ class InferenceTest(object):
         for i in range(repeat):
             predictor.run()
         output_names = predictor.get_output_names()
+        if use_pir:
+            paddle.set_flags({"FLAGS_enable_pir_in_executor": False})
         print("output_names:", output_names)
         print("truth_value_names:", list(output_data_dict.keys()))
         for i, output_data_name in enumerate(output_names):
@@ -450,30 +448,25 @@ class InferenceTest(object):
         test enable_use_gpu() in mixed_precision
         Args:
             input_data_dict(dict): input data constructed as dictionary
-            output_data_dict(dict): output data constructed as dictionary
-            repeat(int): inference repeat time, set to catch gpu mem
-            delta(float): difference threshold between inference outputs and thruth value
         Returns:
             None
         """
         self.pd_config.enable_use_gpu(gpu_mem, 0)
         if use_new_executor:
-            print("use_new_executor!!!")
             self.pd_config.enable_new_executor()
         if use_pir:
             print("use_pir!!!")
             paddle.set_flags({"FLAGS_enable_pir_in_executor": True})
         predictor = paddle_infer.create_predictor(self.pd_config)
-
         input_names = predictor.get_input_names()
         for _, input_data_name in enumerate(input_names):
             input_handle = predictor.get_input_handle(input_data_name)
             input_handle.copy_from_cpu(input_data_dict[input_data_name])
-
         for i in range(repeat):
             predictor.run()
         output_names = predictor.get_output_names()
-        # Change the accuracy check to sequential comparison
+        if use_pir:
+            paddle.set_flags({"FLAGS_enable_pir_in_executor": False})
         truth_value_names = list(output_data_dict.keys())
         print("output_names:", output_names)
         print("truth_value_names:", list(output_data_dict.keys()))
@@ -777,7 +770,6 @@ class InferenceTest(object):
         for i, output_data_name in enumerate(output_names):
             output_handle = predictor.get_output_handle(output_data_name)
             output_data = output_handle.copy_to_cpu()
-            # output_data = output_data.flatten()
             output_data_truth_val = output_data_dict[output_data_name]
             print("output_data_shape:", output_data.shape)
             print("truth_value_shape:", output_data_truth_val.shape)
@@ -958,7 +950,6 @@ class InferenceTest(object):
         for i, output_data_name in enumerate(output_names):
             output_handle = predictor.get_output_handle(output_data_name)
             output_data = output_handle.copy_to_cpu()
-            # output_data = output_data.flatten()
             output_data_truth_val = output_data_dict[output_data_name]
             print("output_data_shape:", output_data.shape)
             print("truth_value_shape:", output_data_truth_val.shape)
