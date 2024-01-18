d25b3f780fe2e29572eacdaa4bc4cd8e51bf3f63
plusNew001 <suijiaxin_job@163.com>
2024-01-18 12:03:39 +0000

    add ce case,test=model

--- models_restruct/PaddleScience/tools/case_start.py
+++ models_restruct/PaddleScience/tools/case_start.py
@@ -53,7 +53,8 @@ class PaddleScience_Case_Start(object):
             os.system("wget -nc https://paddle-org.bj.bcebos.com/paddlescience/models/viv/viv_pretrained.pdeqn")
             os.system("wget -nc https://paddle-org.bj.bcebos.com/paddlescience/models/viv/viv_pretrained.pdparams")
         elif "amgnet" in self.case_name:
-            os.system("python3 --version")
+            py_v = os.system("python3 --version")
+            print(py_v)
             os.system("python3.10 -m pip install -U pgl")
         else:
             return 0
