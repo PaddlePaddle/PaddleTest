172b1f6566c7d8c99800390ed4056152ca7b17ef
zhouwei25 <zhouwei25@baidu.com>
2024-01-24 09:46:27 +0000

    fix paddle.linalg.norm

--- framework/api/multithreading_case.py
+++ framework/api/multithreading_case.py
@@ -30,7 +30,7 @@ ignore_case_dir = {
     "device": [],
     "fft": [],
     "incubate": ["test_jvp.py"],
-    "linalg": [],
+    "linalg": ["test_norm.py"],
     "loss": [],
     "nn": [
         "test_functional_celu.py",
