08738d49d1a0ba9989fdc1be506aa486d2d17d7a
mengmingooo <mengminglxl@163.com>
2024-01-24 17:35:34 +0800

    change prepare download logic

--- models/PaddleMIX/CE/application/prepare.sh
+++ models/PaddleMIX/CE/application/prepare.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export http_proxy=${proxy}
-export https_proxy=${proxy}
-
 # cd ${root_path}/PaddleMIX/ppdiffusers
 # pip install -e .
 # pip install -r requirements.txt
@@ -16,11 +13,12 @@ pip install -r requirements.txt
 python -m pip install --upgrade pip
 pip install -e .
 pip install -r paddlemix/appflow/requirements.txt
+
+export http_proxy=${proxy}
+export https_proxy=${proxy}
 python nltk_data_download.py
-
 pip install git+https://github.com/PaddlePaddle/PaddleSpeech.git
-
-wget https://paddlenlp.bj.bcebos.com/models/community/paddlemix/audio-files/zh.wav
-
 unset http_proxy
 unset https_proxy
+
+wget https://paddlenlp.bj.bcebos.com/models/community/paddlemix/audio-files/zh.wav
