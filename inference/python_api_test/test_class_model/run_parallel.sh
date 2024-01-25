c3e995ef4367cf6bef0d6539ff2f89faa1e401dc
EmmonsCurse <1577972691@qq.com>
2024-01-25 23:23:46 +0800

    fix ppyolo and yolov3

--- inference/python_api_test/test_class_model/run_parallel.sh
+++ inference/python_api_test/test_class_model/run_parallel.sh
@@ -7,7 +7,6 @@ cases="./test_resnet50_gpu.py \
        ./test_resnet50_trt_fp32.py \
        ./test_resnet50_slim.py \
        ./test_swin_transformer_gpu.py \
-       ./test_swin_transformer_mkldnn.py \
        ./test_swin_transformer_trt_fp32.py \
        ./test_tnt_small_gpu.py \
        ./test_tnt_small_trt_fp32.py \
@@ -15,9 +14,6 @@ cases="./test_resnet50_gpu.py \
        ./test_vgg11_mkldnn.py \
        ./test_vgg11_trt_fp32.py \
        ./test_ViT_base_patch16_224_trt_fp32.py \
-       ../test_ocr_model/test_ocr_det_mv3_db_gpu.py \
-       ../test_ocr_model/test_ocr_det_mv3_db_mkldnn.py \
-       ../test_ocr_model/test_ocr_det_mv3_db_trt_fp32.py \
       "
 ignore=""
 bug=0
--- inference/python_api_test/test_det_model/run_parallel.sh
+++ inference/python_api_test/test_det_model/run_parallel.sh
@@ -18,6 +18,9 @@ cases="./test_fast_rcnn_mkldnn.py \
        ../test_nlp_model/test_ernie_gpu.py \
        ../test_nlp_model/test_ernie_mkldnn.py \
        ../test_nlp_model/test_ernie_trt_fp32.py \
+       ../test_ocr_model/test_ocr_det_mv3_db_gpu.py \
+       ../test_ocr_model/test_ocr_det_mv3_db_mkldnn.py \
+       ../test_ocr_model/test_ocr_det_mv3_db_trt_fp32.py \
       "
 bug=0
 
