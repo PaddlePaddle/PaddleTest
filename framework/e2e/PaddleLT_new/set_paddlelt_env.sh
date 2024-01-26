f88ebaa08271c8f67cb3074b8771c1d70934111e
Zeref996 <53218160+Zeref996@users.noreply.github.com>
2024-01-26 16:23:35 +0800

    Layer tt 06 (#2641)
    * fix run and start, test=model
    
    * fix run and start 1, test=model
    
    * fix plt, test=model
    
    * fix plt init, test=model
    
    * fix layertest and run, test=model
    
    * fix set_plt_env and run, test=model
    
    * update set plt env, test=model

--- framework/e2e/PaddleLT_new/set_paddlelt_env.sh
+++ framework/e2e/PaddleLT_new/set_paddlelt_env.sh
@@ -2,7 +2,7 @@
 
 export CASE_URL="${CASE_URL:-https://paddle-qa.bj.bcebos.com/PaddleLT/LayerCase/layercase.tar}"
 export CASE_DIR=$(echo $(basename $CASE_URL) | cut -d '.' -f 1) # 设定子图case根目录
-wget -q ${CASE_URL} --no-proxy && tar -xzf ${CASE_DIR}.tar
+wget ${CASE_URL} --no-proxy && tar -xzf ${CASE_DIR}.tar
 export TESTING="${TESTING:-yaml/dy^dy2stcinn_eval.yml}" # 设定测试项目配置yaml
 export docker_type="${docker_type:-Ubuntu}"
 export cuda_ver="${cuda_ver:-cuda11.2}"
@@ -79,5 +79,6 @@ fi
 
 echo "wheel_url is: ${wheel_url}"
 echo "python_ver is: ${python_ver}"
+echo "CASE_URL is: ${CASE_URL}"
 echo "CASE_DIR is: ${CASE_DIR}"
 echo "TESTING is: ${TESTING}"
