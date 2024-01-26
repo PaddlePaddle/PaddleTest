e5ea7d8e8bdb4350bc5c7bfd20c6b327c80c98f9
Zeref996 <825276847@qq.com>
2024-01-26 17:30:40 +0800

    fix set env, test=model

--- framework/e2e/PaddleLT_new/scene/set_demo_env.sh
+++ framework/e2e/PaddleLT_new/scene/set_demo_env.sh
@@ -4,8 +4,8 @@ export CASE_URL="${CASE_URL:-https://paddle-qa.bj.bcebos.com/PaddleLT/LayerCase/
 export CASE_DIR=$(echo $(basename $CASE_URL) | cut -d '.' -f 1) # 设定子图case根目录
 wget ${CASE_URL} --no-proxy && tar -xzf ${CASE_DIR}.tar
 export TESTING="${TESTING:-yaml/dy^dy2stcinn_eval.yml}" # 设定测试项目配置yaml
-export wheel_url="${wheel_url:-https://paddle-qa.bj.bcebos.com/paddle-pipeline/Develop-GpuAll-LinuxCentos-Gcc82-Cuda112-Trton-Py38-Compile/latest/paddlepaddle_gpu-0.0.0-cp38-cp38-linux_x86_64.whl}"
-export python_ver="${python_ver:-python3.8}"
+#export wheel_url="${wheel_url:-https://paddle-qa.bj.bcebos.com/paddle-pipeline/Develop-GpuAll-LinuxCentos-Gcc82-Cuda112-Trton-Py38-Compile/latest/paddlepaddle_gpu-0.0.0-cp38-cp38-linux_x86_64.whl}"
+#export python_ver="${python_ver:-python3.8}"
 
 echo "wheel_url is: ${wheel_url}"
 echo "python_ver is: ${python_ver}"
