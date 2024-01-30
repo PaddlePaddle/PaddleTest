7c0b7f08d67acfe7177a710dfd390ee0d093be72
Zeref996 <825276847@qq.com>
2024-01-30 10:51:43 +0800

    fix start.sh 5, test=model

--- framework/e2e/PaddleLT_new/start.sh
+++ framework/e2e/PaddleLT_new/start.sh
@@ -3,7 +3,7 @@
 set -x
 # 最外层执行脚本
 test_scene=$1
-source ./PTSTools/tools/set_env/set_env.sh ${PTS_ENV_VARS}  # 设定PTS环境变量
+source ./PTSTools/tools/set_env/set_env.sh $PTS_ENV_VARS  # 设定PTS环境变量
 source ./set_docker_env.sh # 设定docker环境相关参数
 
 docker_name="PaddleLayerTest_${AGILE_PIPELINE_BUILD_NUMBER}"
@@ -21,7 +21,7 @@ nvidia-docker run --rm -i --name ${docker_name} --privileged --shm-size=128g --n
 set -x
 ldconfig;
 
-source ./PTSTools/tools/set_env/set_env.sh ${PTS_ENV_VARS}  # 设定PTS环境变量
+source ./PTSTools/tools/set_env/set_env.sh $PTS_ENV_VARS  # 设定PTS环境变量
 source ./${test_scene}
 ${python_ver} -m pip install -r requirement.txt
 ${python_ver} -m pip install ${wheel_url}
