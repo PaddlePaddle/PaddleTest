a4030295c4a5e652c7438fc95fca53dafd6ca670
Zeref996 <825276847@qq.com>
2024-01-31 15:11:59 +0800

    add plt tool, test=model

---
+++ framework/e2e/PaddleLT_new/tools/init_gen.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# 获取当前文件夹路径
+cur_path=`pwd`
+case_root=layercase_all
+clas_case=layercase_all/Clas_cases
+det_case=layercase_all/Det_cases
+seg_case=layercase_all/Seg_cases
+ocr_case=layercase_all/Ocr_cases
+
+echo case_root is: ${case_root}
+cd ${case_root}
+
+if [ -f "__init__.py" ]; then
+    echo "delete~~~"
+    rm "__init__.py"
+fi
+
+# 遍历当前文件夹下的子文件夹
+for subdir in */; do
+  # 构建 import 语句
+  import_statement="import ${case_root/\//.}.${subdir%/}"
+
+  # 将 import 语句写入 __init__.py 文件
+  echo "$import_statement" >> "__init__.py"
+#   echo "$import_statement"
+
+done
+
+cd ${cur_path}
+
+function generateRepoInit(){
+repo_name=$1
+root_dir=$2
+cd ${repo_name}
+if [ -f "__init__.py" ]; then
+    echo "delete clas init ~~~"
+    rm "__init__.py"
+fi
+
+# 遍历当前文件夹下的子文件夹
+for subdir in */; do
+  # 构建 import 语句
+  tmp0=${repo_name/\//.}.${subdir%/}
+  import_statement="import ${tmp0}"
+
+  # 将 import 语句写入 __init__.py 文件
+  echo "$import_statement" >> "__init__.py"
+
+  cd ${subdir}
+  if [ -f "__init__.py" ]; then
+    echo "delete ${subdir} init ~~~"
+    rm "__init__.py"
+  fi
+  find ./ -type f -name "*.py" | while read file; do
+    # echo file is: ${file}
+    tmp1=$(echo "$file" | sed 's/.\///g; s/.py//g')
+    import_statement="import ${tmp0}.${tmp1}"
+    echo "$import_statement" >> "__init__.py"
+  done
+  cd ..
+
+done
+
+cd ${root_dir}
+
+}
+
+generateRepoInit ${clas_case} ${cur_path}
+generateRepoInit ${det_case} ${cur_path}
+generateRepoInit ${seg_case} ${cur_path}
+generateRepoInit ${ocr_case} ${cur_path}
---
+++ framework/e2e/PaddleLT_new/tools/modelname_change.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# 获取当前文件夹路径
+cur_path=`pwd`
+case_root=layercase_all
+clas_case=layercase_all/Clas_cases
+det_case=layercase_all/Det_cases
+seg_case=layercase_all/Seg_cases
+ocr_case=layercase_all/Ocr_cases
+
+echo case_root is: ${case_root}
+
+cd ${clas_case}
+
+# 遍历当前文件夹下的子文件夹
+for subdir in */; do
+  echo ${subdir%/}
+  tmp0=${subdir%/}
+  tmp1=$(echo "$tmp0" | sed 's/ppcls\^configs\^ImageNet\^//g; s/_single_dy2st_train//g')
+#   echo ${tmp1}
+  tmp2=$(echo "$tmp1" | sed 's/\^/_/g; s/-/_/g')
+#   echo ${tmp2}
+  mv ${subdir} ${tmp2}
+
+done
+
+cd ${cur_path}
+
+cd ${det_case}
+
+# 遍历当前文件夹下的子文件夹
+for subdir in */; do
+  echo ${subdir%/}
+  tmp0=${subdir%/}
+  tmp1=$(echo "$tmp0" | sed 's/configs\^//g; s/_single_dy2st_train//g')
+#   echo ${tmp1}
+  tmp2=$(echo "$tmp1" | sed 's/\^/_/g; s/-/_/g')
+#   echo ${tmp2}
+  mv ${subdir} ${tmp2}
+
+done
+
+cd ${cur_path}
+
+cd ${seg_case}
+
+# 遍历当前文件夹下的子文件夹
+for subdir in */; do
+  echo ${subdir%/}
+  tmp0=${subdir%/}
+  tmp1=$(echo "$tmp0" | sed 's/configs\^//g; s/_single_dy2st_train//g')
+#   echo ${tmp1}
+  tmp2=$(echo "$tmp1" | sed 's/\^/_/g; s/-/_/g')
+#   echo ${tmp2}
+  mv ${subdir} ${tmp2}
+
+done
+
+cd ${cur_path}
+
+cd ${ocr_case}
+
+# 遍历当前文件夹下的子文件夹
+for subdir in */; do
+  echo ${subdir%/}
+  tmp0=${subdir%/}
+  tmp1=$(echo "$tmp0" | sed 's/configs\^//g; s/_single_dy2st_train//g')
+#   echo ${tmp1}
+  tmp2=$(echo "$tmp1" | sed 's/\^/_/g; s/-/_/g')
+#   echo ${tmp2}
+  mv ${subdir} ${tmp2}
+
+done
+
+cd ${cur_path}
