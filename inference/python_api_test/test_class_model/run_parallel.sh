f115a58778cc0ea0393954a3e7330a6491bc7b2c
YuBaoku <49938469+EmmonsCurse@users.noreply.github.com>
2024-03-05 16:19:39 +0800

    update for pr_ce_framework (#2686)
    * update the model_version of important ce case
    
    * fix ci case list
    
    * fix ppyolo and yolov3
    
    * fix
    
    * fix pytest error of DarkNet53
    
    * update ci&ce and reopen tnt
    
    * set resnet50 gpu_fp16 in pir
    
    * update for pr_ce_framework, run_mode=model

--- inference/python_api_test/test_class_model/run_parallel.sh
+++ inference/python_api_test/test_class_model/run_parallel.sh
@@ -9,6 +9,7 @@ cases="./test_resnet50_gpu.py \
        ./test_swin_transformer_gpu.py \
        ./test_swin_transformer_trt_fp32.py \
        ./test_tnt_small_gpu.py \
+       ./test_tnt_small_trt_fp32.py \
        ./test_vgg11_gpu.py \
        ./test_vgg11_mkldnn.py \
        ./test_ViT_base_patch16_224_trt_fp32.py \
