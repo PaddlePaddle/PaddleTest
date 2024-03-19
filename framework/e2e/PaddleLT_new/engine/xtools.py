ab0f63772a73370efa5444824cb99befdfa247d5
Zeref996 <53218160+Zeref996@users.noreply.github.com>
2024-03-19 15:57:52 +0800

    add PaddleLT benchmark engine (#2720)
    * update plt ignore case, test=model
    
    * add nlp sot_export_flag, test=model
    
    * add plt benchmark, test=model
    
    * add plt benchmark 1, test=model
    
    * add plt benchmark 2, test=model
    
    * add plt benchmark 3, test=model

--- framework/e2e/PaddleLT_new/engine/xtools.py
+++ framework/e2e/PaddleLT_new/engine/xtools.py
@@ -20,3 +20,46 @@ def reset(seed):
     paddle.seed(seed)
     np.random.seed(seed)
     np.set_printoptions(threshold=5, edgeitems=3)
+
+
+# 多种统计学计算策略
+def trimmean(data_list, ratio=0.2):
+    """
+    掐头去尾求平均
+    :param data_list: 输入的data list, 多次试验的结果集合
+    """
+    head = int(len(data_list) * ratio)
+    tail = int(len(data_list) - len(data_list) * ratio)
+    res = sum(sorted(data_list)[head:tail]) / (tail - head)
+    return res
+
+
+def mean(data_list):
+    """
+    求平均值
+    :param data_list:
+    :return:
+    """
+    res = sum(data_list) / len(data_list)
+    return res
+
+
+def best(data_list):
+    """
+    找出耗时最少的一次试验结果
+    :param data_list: 输入的data list, 多次试验的结果集合
+    :return: 最少的时间
+    """
+    # print('data_list is: ', data_list)
+    res = min(data_list)
+    return res
+
+
+def best_top_k(data_list, ratio=0.2):
+    """
+    求最优top k的平均值，默认ratio=0.2
+    :param data_list: 输入的data list, 多次试验的结果集合
+    """
+    head = int(len(data_list) * ratio)
+    res = sum(sorted(data_list)[:head]) / head
+    return res
