84492f5ab14fe2dc9a363173753879988208088a
Zeref996 <825276847@qq.com>
2024-03-19 20:00:35 +0800

    fix plt benchmark 4, test=model

--- framework/e2e/PaddleLT_new/tools/res_save.py
+++ framework/e2e/PaddleLT_new/tools/res_save.py
@@ -13,11 +13,27 @@ def xlsx_save(sublayer_dict):
     """
     子图保存到excel
     """
-    data = [
-        {"sub_layer": key, "time(s)": value}
-        for key, sublayer_dict in sublayer_dict.items()
-        for value in sublayer_dict.values()
-    ]
+    # data = [
+    #     {"sub_layer": key, "time(s)": value}
+    #     for key, sublayer_dict in sublayer_dict.items()
+    #     for value in sublayer_dict.values()
+    # ]
+    # # 创建 DataFrame
+    # df = pd.DataFrame(data)
+
+    # # 将数据写入 Excel 文件
+    # excel_file = "output.xlsx"  # 输出的 Excel 文件名
+    # df.to_excel(excel_file, index=False)
+
+    data = []
+
+    # 遍历嵌套字典，提取数据并添加到列表中
+    for key, sub_dict in sublayer_dict.items():
+        row = {"Key": key}
+        for subkey, value in sub_dict.items():
+            row[subkey] = value
+        data.append(row)
+
     # 创建 DataFrame
     df = pd.DataFrame(data)
 
