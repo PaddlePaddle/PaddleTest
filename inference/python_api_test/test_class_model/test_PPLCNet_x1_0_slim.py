563a4e3bed88f3703038fe172c0a27e7586cdc6d
ZhangYulongg <1272816783@qq.com>
2024-03-22 17:51:39 +0800

    update

--- inference/python_api_test/test_class_model/test_PPLCNet_x1_0_slim.py
+++ inference/python_api_test/test_class_model/test_PPLCNet_x1_0_slim.py
@@ -139,7 +139,6 @@ def test_trt_int8_more_bz():
             min_subgraph_size=30,
             dynamic=True,
             shape_range_file="./PPLCNet_x1_0_act_qat/shape_range.pbtxt",
-            # use_calib_mode=True,
             with_benchmark=True,
             base_latency_ms=0.74,
             benchmark_threshold=5e-2,
