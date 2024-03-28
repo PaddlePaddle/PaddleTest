7ad1f172c4ecb1f595434fbf0b6fa28801231754
HertHan <>
2024-03-28 09:16:12 +0000

    softmax and layernorm with the original rmsnorm

--- framework/e2e/PaddleLT_new/layercase/perf_monitor/manual_subgraph/rms_norm.py
+++ framework/e2e/PaddleLT_new/layercase/perf_monitor/manual_subgraph/rms_norm.py
@@ -6,7 +6,7 @@
 
 import unittest
 import numpy as np
-import utils
+# import utils
 
 import paddle
 
@@ -34,48 +34,48 @@ def create_paddle_inputs():
     return inputs
 
 
-class PaddleRMSNormSubGraph(paddle.nn.Layer):
-    def __init__(self):
-        super().__init__()
-        self.variance_epsilon = 1e-6
+# class PaddleRMSNormSubGraph(paddle.nn.Layer):
+#     def __init__(self):
+#         super().__init__()
+#         self.variance_epsilon = 1e-6
 
-    def forward(self, hidden_states, weight):
-        return paddle.incubate.nn.functional.fused_rms_norm(
-            x=hidden_states,
-            norm_weight=weight,
-            norm_bias=None,
-            epsilon=self.variance_epsilon,
-            begin_norm_axis=2,
-        )
-
-
-class TestRMSNormSubGraph(unittest.TestCase):
-    def setUp(self):
-        paddle.seed(2022)
-        self.prepare_data()
-
-    def prepare_data(self):
-        self.x, self.weight = create_paddle_inputs()
-
-    def train(self, use_cinn):
-        if use_cinn:
-            net = LayerCase()
-        else:
-            net = PaddleRMSNormSubGraph()
-        net.eval()
-        net = utils.apply_to_static(net, use_cinn)
-        for i in range(10000):
-            out = net(self.x, self.weight)
-        return out
-
-    def test_train(self):
-        cinn_out = self.train(use_cinn=True)
-
-        dy_out = self.train(use_cinn=False)
-        np.testing.assert_allclose(
-            cinn_out.numpy(), dy_out.numpy(), atol=1e-6, rtol=1e-6
-        )
+#     def forward(self, hidden_states, weight):
+#         return paddle.incubate.nn.functional.fused_rms_norm(
+#             x=hidden_states,
+#             norm_weight=weight,
+#             norm_bias=None,
+#             epsilon=self.variance_epsilon,
+#             begin_norm_axis=2,
+#         )
 
 
-if __name__ == '__main__':
-    unittest.main()
+# class TestRMSNormSubGraph(unittest.TestCase):
+#     def setUp(self):
+#         paddle.seed(2022)
+#         self.prepare_data()
+
+#     def prepare_data(self):
+#         self.x, self.weight = create_paddle_inputs()
+
+#     def train(self, use_cinn):
+#         if use_cinn:
+#             net = LayerCase()
+#         else:
+#             net = PaddleRMSNormSubGraph()
+#         net.eval()
+#         net = utils.apply_to_static(net, use_cinn)
+#         for i in range(10000):
+#             out = net(self.x, self.weight)
+#         return out
+
+#     def test_train(self):
+#         cinn_out = self.train(use_cinn=True)
+
+#         dy_out = self.train(use_cinn=False)
+#         np.testing.assert_allclose(
+#             cinn_out.numpy(), dy_out.numpy(), atol=1e-6, rtol=1e-6
+#         )
+
+
+# if __name__ == '__main__':
+#     unittest.main()
