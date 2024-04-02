a851da0125cb5745f65983a507a1b96536cf1dd6
HertHan <>
2024-04-02 03:06:37 +0000

    add shape to name

---
+++ framework/e2e/PaddleLT_new/layercase/perf_monitor/manual_subgraph/softmax_1_13_4096.py
@@ -0,0 +1,67 @@
+'''test softmax subgraph '''
+# FLAGS_pir_apply_shape_optimization_pass=0 FLAGS_enable_pir_api=1 
+# FLAGS_prim_enable_dynamic=true FLAGS_prim_all=true 
+# FLAGS_cinn_new_group_scheduler=1 FLAGS_group_schedule_tiling_first=1 FLAGS_cinn_bucket_compile=True 
+# FLAGS_cinn_compile_with_nvrtc=True FLAGS_nvrtc_compile_to_cubin=True 
+# FLAGS_support_reduce_stride_read=1
+
+import unittest
+import numpy as np
+# import utils
+
+import paddle
+
+class LayerCase(paddle.nn.Layer):
+    '''this is the softmax layercase'''
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, axis=-1):
+        output = paddle.nn.functional.softmax(x, axis = axis)
+        return output
+
+def create_paddle_inputs():
+    shape = [1, 13, 4096]
+    x = paddle.uniform(shape, dtype="float32", min=-0.5, max=0.5)
+    x.stop_gradient = False
+    return x
+
+
+# class PaddleSoftmaxSubGraphNet(paddle.nn.Layer):
+#     def __init__(self):
+#         super().__init__()
+#         self.fn = paddle.nn.functional.softmax
+
+#     def forward(self, x, axis=-1):
+#         out = self.fn(x, axis=axis)
+#         return out
+
+
+# class TestSoftmaxSubGraph(unittest.TestCase):
+#     def setUp(self):
+#         paddle.seed(2022)
+#         self.prepare_data()
+
+#     def prepare_data(self):
+#         self.x = create_paddle_inputs()
+
+#     def train(self, use_cinn):
+#         if use_cinn:
+#             net = LayerCase()
+#         else:
+#             net = PaddleSoftmaxSubGraphNet()
+#         net.eval()
+#         net = utils.apply_to_static(net, use_cinn)
+#         for i in range(10000):
+#             out = net(self.x)
+#         return out
+
+#     def test_train(self):
+#         cinn_out = self.train(use_cinn=True)
+#         dy_out = self.train(use_cinn=False)
+#         np.testing.assert_allclose(
+#             cinn_out.numpy(), dy_out.numpy(), atol=1e-6, rtol=1e-6
+#         )
+
+# if __name__ == '__main__':
+#     unittest.main()
