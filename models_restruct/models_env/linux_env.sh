c1b27a7f924c24b732e78b8c3a6229929fb36f14
plusNew001 <suijiaxin_job@163.com>
2024-04-16 07:55:02 +0000

    fix bugs,test=model

--- models_restruct/models_env/linux_env.sh
+++ models_restruct/models_env/linux_env.sh
@@ -440,6 +440,7 @@ if [[ "${docker_flag}" == "" ]]; then
     trap 'docker_del' SIGTERM
     ## 使用修改之前的set_cuda_back
     if [[ "${AGILE_PIPELINE_NAME}" =~ "DCU" ]];then
+        echo "=========start DCU test==============="
         docker run -i  --rm \
             --name=${docker_name} --network=host --cap-add=SYS_ADMIN \
             --shm-size=128G \
@@ -554,7 +555,6 @@ if [[ "${docker_flag}" == "" ]]; then
             python -m pip install --ignore-installed six -i https://mirror.baidu.com/pypi/simple
             python -m pip uninstall -y opencv-python
             python -m pip config list
-            python -c "import paddle; paddle.utils.run_check()"
             python main.py --models_list=${models_list:-None} --models_file=${models_file:-None} --system=${system:-linux} --step=${step:-train} --reponame=${reponame:-PaddleClas} --mode=${mode:-function} --use_build=${use_build:-yes} --branch=${branch:-develop} --get_repo=${get_repo:-wget} --paddle_whl=${paddle_whl:-None} --dataset_org=${dataset_org:-None} --dataset_target=${dataset_target:-None} --set_cuda=${set_cuda:-0,1} --timeout=${timeout:-3600} --binary_search_flag=${binary_search_flag:-False} --is_analysis_logs=${is_analysis_logs:-False} --use_data_cfs=${use_data_cfs:-False} --plot=${plot:-False} --c_plus_plus_predict=${c_plus_plus_predict:-False} --paddle_inference=${paddle_inference:-None} --TENSORRT_DIR=${TENSORRT_DIR:-None} --PaddleX=${PaddleX:-None}
         ' &
     else
