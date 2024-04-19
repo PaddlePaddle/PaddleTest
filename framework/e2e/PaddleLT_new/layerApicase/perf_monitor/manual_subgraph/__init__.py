24dbcc1229f229facd9cba2d3778dfb3c46eba6e
Zeref996 <825276847@qq.com>
2024-04-19 10:32:10 +0800

    add layer api case, test=model

--- framework/e2e/PaddleLT_new/layerApicase/perf_monitor/manual_subgraph/__init__.py
+++ framework/e2e/PaddleLT_new/layerApicase/perf_monitor/manual_subgraph/__init__.py
@@ -1,3 +1,4 @@
 import layerApicase.perf_monitor.manual_subgraph.rms_norm_fp32_shape_1_13_4096
+import layerApicase.perf_monitor.manual_subgraph.rope_fp32_shape_1_13_4096
 import layerApicase.perf_monitor.manual_subgraph.layernorm_fp32_shape_1_13_4096
 import layerApicase.perf_monitor.manual_subgraph.softmax_fp32_shape_1_13_4096
