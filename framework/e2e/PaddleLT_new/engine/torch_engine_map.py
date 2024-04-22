a93ba7c88f5f2ae7efdd3b20f657e576029eb767
Zeref996 <825276847@qq.com>
2024-04-22 15:09:47 +0800

    add plt competition frame, test=model

--- framework/e2e/PaddleLT_new/engine/torch_engine_map.py
+++ framework/e2e/PaddleLT_new/engine/torch_engine_map.py
@@ -11,6 +11,6 @@ from engine.torch_eval import TorchLayerEval
 # from interpreter.testing_trans import TrainTrans, EvalTrans
 
 
-engine_map = {
+torch_engine_map = {
     "dy_eval": TorchLayerEval,
 }
