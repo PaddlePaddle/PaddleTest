e0bed788e64b0e2e0accfdfd056ae9e1cae12ac9
plusNew001 <suijiaxin_job@163.com>
2024-05-07 12:53:05 +0000

    debug html,test=model

--- models/Modulus/test_framework/test_xly.py
+++ models/Modulus/test_framework/test_xly.py
@@ -27,7 +27,8 @@ def test_dynamic(model_name):
         test_data[model_name]["dynamic"]["L4"] ={}
     test_data[model_name]["dynamic"]["L2"]["status"] = "Failed"
     test_data[model_name]["dynamic"]["L4"]["status"] = "Failed"
-
+    with open('./test_data.json', 'w', encoding='utf-8') as file:
+        json.dump(test_data, file, ensure_ascii=False, indent=4)
     # 执行torch
     pytorch_exit = run_model(model_name, 'pytorch')
     assert pytorch_exit == 0
@@ -84,11 +85,13 @@ def test_dy2st(model_name):
         test_data[model_name]["dy2st"]["L4"] ={}
     test_data[model_name]["dy2st"]["L2"]["status"] = "Failed"
     test_data[model_name]["dy2st"]["L4"]["status"] = "Failed"
+    with open('./test_data.json', 'w', encoding='utf-8') as file:
+        json.dump(test_data, file, ensure_ascii=False, indent=4)
     # 执行dy2st
     dy2st_exit = run_model(model_name, 'dy2st')
-    if dy2st_exit != 0:
-        test_data[model_name]["dy2st"]["L2"]["status"] = "Failed"
-        test_data[model_name]["dy2st"]["L4"]["status"] = "Failed"
+    # if dy2st_exit != 0:
+    #     test_data[model_name]["dy2st"]["L2"]["status"] = "Failed"
+    #     test_data[model_name]["dy2st"]["L4"]["status"] = "Failed"
     assert dy2st_exit == 0
     kpi_loss = {}
     # 解析pytorch的日志文件，提取损失值
@@ -135,11 +138,13 @@ def test_dy2st_prim(model_name):
         test_data[model_name]["dy2st_prim"]["L4"] ={}
     test_data[model_name]["dy2st_prim"]["L2"]["status"] = "Failed"
     test_data[model_name]["dy2st_prim"]["L4"]["status"] = "Failed"
+    with open('./test_data.json', 'w', encoding='utf-8') as file:
+        json.dump(test_data, file, ensure_ascii=False, indent=4)
     # 执行dy2st+prim
     dy2st_prim_exit = run_model(model_name, 'dy2st_prim')
-    if dy2st_prim_exit != 0:
-        test_data[model_name]["dy2st_prim"]["L2"]["status"] = "Failed"
-        test_data[model_name]["dy2st_prim"]["L4"]["status"] = "Failed"
+    # if dy2st_prim_exit != 0:
+    #     test_data[model_name]["dy2st_prim"]["L2"]["status"] = "Failed"
+    #     test_data[model_name]["dy2st_prim"]["L4"]["status"] = "Failed"
     assert dy2st_prim_exit == 0
     kpi_loss = {}
     # 解析pytorch的日志文件，提取损失值
@@ -172,6 +177,51 @@ def test_dy2st_prim(model_name):
         json.dump(test_data, file, ensure_ascii=False, indent=4)
     np.testing.assert_allclose(dy2st_prim_kpi[0], pytorch_kpi[0], atol=1e-5, rtol=1.3e-6)
     np.testing.assert_allclose(dy2st_prim_kpi_avg, pytorch_kpi_avg, atol=1e-5, rtol=1.3e-6)
+
+@pytest.mark.parametrize("model_name", model_list)
+def test_dy2st_prim_cinn(model_name):
+    if model_name not in test_data:
+        test_data[model_name]={}
+    if "dy2st_prim_cinn"not in test_data[model_name]:
+        test_data[model_name]["dy2st_prim_cinn"]={}
+    if "L2" not in test_data[model_name]["dy2st_prim_cinn"]:
+        test_data[model_name]["dy2st_prim_cinn"]["L2"] ={}
+    if "L4" not in test_data[model_name]["dy2st_prim_cinn"]:
+        test_data[model_name]["dy2st_prim_cinn"]["L4"] ={}
+    test_data[model_name]["dy2st_prim_cinn"]["L2"]["status"] = "Failed"
+    test_data[model_name]["dy2st_prim_cinn"]["L4"]["status"] = "Failed"
+    with open('./test_data.json', 'w', encoding='utf-8') as file:
+        json.dump(test_data, file, ensure_ascii=False, indent=4)
+    # 执行dy2st+prim+cinn
+    dy2st_prim_cinn_exit = run_model(model_name, 'dy2st_prim_cinn')
+    # if dy2st_prim_cinn_exit != 0:
+    #     test_data[model_name]["dy2st_prim_cinn"]["L2"]["status"] = "Failed"
+    #     test_data[model_name]["dy2st_prim_cinn"]["L4"]["status"] = "Failed"
+    assert dy2st_prim_cinn_exit == 0
+    kpi_loss = {}
+    # 解析pytorch的日志文件，提取损失值
+    pytorch_kpi = log_parse(f'./logs/{model_name}/{model_name}_pytorch.log', 'Loss')
+    assert pytorch_kpi[0] != -1
+    kpi_loss['pytorch'] = pytorch_kpi
+    dy2st_prim_cinn_kpi = log_parse(f'./logs/{model_name}/{model_name}_dy2st_prim_cinn.log', 'Loss')
+    assert dy2st_prim_cinn_kpi[0] != -1
+    kpi_loss['dy2st_prim_cinn'] = dy2st_prim_cinn_kpi
+    # 绘制KPI曲线图
+    plot_kpi_curves(model_name, kpi_loss, 'dy2st_prim_cinn')
+    dy2st_prim_cinn_kpi_avg = np.mean(dy2st_prim_cinn_kpi)
+    pytorch_kpi_avg = np.mean(pytorch_kpi)
+    test_data[model_name]["dy2st_prim_cinn"]["L2"]["atol"] = np.abs(dy2st_prim_cinn_kpi[0] - pytorch_kpi[0])/pytorch_kpi[0]
+    test_data[model_name]["dy2st_prim_cinn"]["L2"]["rtol"] = np.abs(dy2st_prim_cinn_kpi[0] - pytorch_kpi[0])
+    test_data[model_name]["dy2st_prim_cinn"]["L4"]["atol"] = np.abs(dy2st_prim_cinn_kpi_avg - pytorch_kpi_avg)/pytorch_kpi_avg
+    test_data[model_name]["dy2st_prim_cinn"]["L4"]["rtol"] = np.abs(dy2st_prim_cinn_kpi_avg - pytorch_kpi_avg)
+    if test_data[model_name]["dy2st_prim_cinn"]["L2"]["atol"] < 1e-5 and test_data[model_name]["dy2st_prim_cinn"]["L2"]["rtol"] < 1.3e-6:
+        test_data[model_name]["dy2st_prim_cinn"]["L2"]["status"] = "Success"
+    if test_data[model_name]["dy2st_prim_cinn"]["L4"]["atol"] < 1e-5 and test_data[model_name]["dy2st_prim_cinn"]["L4"]["rtol"] < 1.3e-6:
+        test_data[model_name]["dy2st_prim_cinn"]["L4"]["status"] = "Success"
+    with open('./test_data.json', 'w', encoding='utf-8') as file:
+        json.dump(test_data, file, ensure_ascii=False, indent=4)
+    np.testing.assert_allclose(dy2st_prim_cinn_kpi[0], pytorch_kpi[0], atol=1e-5, rtol=1.3e-6)
+    np.testing.assert_allclose(dy2st_prim_cinn_kpi_avg, pytorch_kpi_avg, atol=1e-5, rtol=1.3e-6)
 
 if __name__ == "__main__":
     current_date = datetime.now()
