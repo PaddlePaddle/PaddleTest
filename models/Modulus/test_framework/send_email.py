a01dd0c50be138a0d7ce54d20d3171e1ab07324c
plusNew001 <suijiaxin_job@163.com>
2024-05-11 03:08:12 +0000

    add timeout,test=model

--- models/Modulus/test_framework/send_email.py
+++ models/Modulus/test_framework/send_email.py
@@ -19,7 +19,7 @@ def email_send(sender, receivers, subject, content):
     except smtplib.SMTPException as e:
         return e
 if __name__ == '__main__':
-    sender = 'suijiaxin@baidu.com'
+    sender = 'auto_send@baidu.com'
     receivers = ['suijiaxin@baidu.com', 'hesensen@baidu.com', 'chenxiaoxu02@baidu.com', 'chenxi67@baidu.com', 'chenyaowen02@baidu.com', 'chenzhuo13@baidu.com', 'gaotiezhu@baidu.com', 'liuhongyu02@baidu.com', 'tianchao04@baidu.com', 'wanghao107@baidu.com', 'xiongkun03@baidu.com', 'xuxiaojian@baidu.com', 'zhangliujie@baidu.com', 'zhangyanbo02@baidu.com', 'huxiaoguang@baidu.com']
     subject = 'Modulus  CE Test'
     with open('./html_result/index.html', 'r', encoding='utf-8') as f:
