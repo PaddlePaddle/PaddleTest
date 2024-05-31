0343ba9fd82e56ea1a2d555ca0781e43dd9c670f
Aurelius84 <zhangliujie@baidu.com>
2024-05-31 08:25:43 +0000

    fix codestyle

--- framework/e2e/PaddleLT_new/layercase/sublayer1000/Det_cases/yolox_yolox_crn_s_300e_coco/SIR_6.py
+++ framework/e2e/PaddleLT_new/layercase/sublayer1000/Det_cases/yolox_yolox_crn_s_300e_coco/SIR_6.py
@@ -7,9 +7,10 @@ import numpy as np
 class LayerCase(paddle.nn.Layer):
     def __init__(self):
         super().__init__()
+
     def forward(
         self,
-        var_0,    # (shape: [2], dtype: paddle.int64, stop_gradient: True)
+        var_0,  # (shape: [2], dtype: paddle.int64, stop_gradient: True)
     ):
         var_0.stop_gradient = True
         var_1 = var_0.__getitem__(0)
@@ -20,24 +21,18 @@ class LayerCase(paddle.nn.Layer):
         return var_5, var_2, var_4
 
 
-
-def create_inputspec(): 
-    inputspec = ( 
-        paddle.static.InputSpec(shape=(-1,), dtype=paddle.int64, stop_gradient=True), 
-    )
+def create_inputspec():
+    inputspec = (paddle.static.InputSpec(shape=(-1,), dtype=paddle.int64, stop_gradient=True),)
     return inputspec
 
+
 def create_tensor_inputs():
-    inputs = (
-        paddle.randint(low=0, high=10, shape=[2], dtype=paddle.int64),
-    )
+    inputs = (paddle.randint(low=0, high=10, shape=[2], dtype=paddle.int64),)
     return inputs
 
 
 def create_numpy_inputs():
-    inputs = (
-        np.random.randint(low=0, high=10, size=[2], dtype='int64'),
-    )
+    inputs = (np.random.randint(low=0, high=10, size=[2], dtype="int64"),)
     return inputs
 
 
@@ -45,9 +40,10 @@ class TestLayer(unittest.TestCase):
     def setUp(self):
         self.inputs = create_tensor_inputs()
         self.net = LayerCase()
+
     def train(self, net, to_static, with_prim=False, with_cinn=False):
         if to_static:
-            paddle.set_flags({'FLAGS_prim_all': with_prim})
+            paddle.set_flags({"FLAGS_prim_all": with_prim})
             if with_cinn:
                 build_strategy = paddle.static.BuildStrategy()
                 build_strategy.build_cinn_pass = True
@@ -57,6 +53,7 @@ class TestLayer(unittest.TestCase):
         paddle.seed(123)
         outs = net(*self.inputs)
         return outs
+
     def test_ast_prim_cinn(self):
         st_out = self.train(self.net, to_static=True)
         cinn_out = self.train(self.net, to_static=True, with_prim=True, with_cinn=True)
@@ -64,5 +61,5 @@ class TestLayer(unittest.TestCase):
             np.testing.assert_allclose(st.numpy(), cinn.numpy(), atol=1e-8)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
