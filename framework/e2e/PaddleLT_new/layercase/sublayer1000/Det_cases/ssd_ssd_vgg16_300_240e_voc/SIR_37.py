2a6863cb9e4125ecc20b451267a88fa7e723fd18
Aurelius84 <zhangliujie@baidu.com>
2024-06-03 19:17:31 +0800

    [CINN]Fix lable Tensor last dims must be 1 (#2820)

--- framework/e2e/PaddleLT_new/layercase/sublayer1000/Det_cases/ssd_ssd_vgg16_300_240e_voc/SIR_37.py
+++ framework/e2e/PaddleLT_new/layercase/sublayer1000/Det_cases/ssd_ssd_vgg16_300_240e_voc/SIR_37.py
@@ -7,34 +7,35 @@ import numpy as np
 class LayerCase(paddle.nn.Layer):
     def __init__(self):
         super().__init__()
+
     def forward(
         self,
-        var_0,    # (shape: [1, 8732, 4], dtype: paddle.float32, stop_gradient: False)
-        var_1,    # (shape: [1, 8732, 4], dtype: paddle.bool, stop_gradient: True)
-        var_2,    # (shape: [1, 8732, 4], dtype: paddle.float32, stop_gradient: True)
-        var_3,    # (shape: [1, 8732, 21], dtype: paddle.float32, stop_gradient: False)
-        var_4,    # (shape: [1, 8732, 1], dtype: paddle.int64, stop_gradient: True)
+        var_0,  # (shape: [1, 8732, 4], dtype: paddle.float32, stop_gradient: False)
+        var_1,  # (shape: [1, 8732, 4], dtype: paddle.bool, stop_gradient: True)
+        var_2,  # (shape: [1, 8732, 4], dtype: paddle.float32, stop_gradient: True)
+        var_3,  # (shape: [1, 8732, 21], dtype: paddle.float32, stop_gradient: False)
+        var_4,  # (shape: [1, 8732, 1], dtype: paddle.int64, stop_gradient: True)
     ):
         var_5 = paddle.tensor.search.masked_select(var_0, var_1)
         var_6 = paddle.tensor.search.masked_select(var_2, var_1)
-        var_7 = paddle.nn.functional.loss.smooth_l1_loss(var_5, var_6, reduction='sum')
+        var_7 = paddle.nn.functional.loss.smooth_l1_loss(var_5, var_6, reduction="sum")
         var_8 = var_7.__mul__(1.0)
-        var_9 = paddle.nn.functional.loss.cross_entropy(var_3, var_4, reduction='none')
+        var_9 = paddle.nn.functional.loss.cross_entropy(var_3, var_4, reduction="none")
         var_10 = var_9.squeeze(-1)
         var_11 = var_4.squeeze(-1)
         return var_10, var_11, var_9, var_8
 
 
-
-def create_inputspec(): 
-    inputspec = ( 
-        paddle.static.InputSpec(shape=(-1, -1, -1), dtype=paddle.float32, stop_gradient=False), 
-        paddle.static.InputSpec(shape=(-1, -1, -1), dtype=paddle.bool, stop_gradient=False), 
-        paddle.static.InputSpec(shape=(-1, -1, -1), dtype=paddle.float32, stop_gradient=False), 
-        paddle.static.InputSpec(shape=(-1, -1, -1), dtype=paddle.float32, stop_gradient=False), 
-        paddle.static.InputSpec(shape=(-1, -1, -1), dtype=paddle.int64, stop_gradient=False), 
+def create_inputspec():
+    inputspec = (
+        paddle.static.InputSpec(shape=(-1, -1, -1), dtype=paddle.float32, stop_gradient=False),
+        paddle.static.InputSpec(shape=(-1, -1, -1), dtype=paddle.bool, stop_gradient=False),
+        paddle.static.InputSpec(shape=(-1, -1, -1), dtype=paddle.float32, stop_gradient=False),
+        paddle.static.InputSpec(shape=(-1, -1, -1), dtype=paddle.float32, stop_gradient=False),
+        paddle.static.InputSpec(shape=(-1, -1, 1), dtype=paddle.int64, stop_gradient=False),
     )
     return inputspec
+
 
 def create_tensor_inputs():
     inputs = (
@@ -49,11 +50,11 @@ def create_tensor_inputs():
 
 def create_numpy_inputs():
     inputs = (
-        np.random.random(size=[1, 8732, 4]).astype('float32'),
-        np.random.random(size=[1, 8732, 4]).astype('bool'),
-        np.random.random(size=[1, 8732, 4]).astype('float32'),
-        np.random.random(size=[1, 8732, 21]).astype('float32'),
-        np.random.randint(low=0, high=10, size=[1, 8732, 1], dtype='int64'),
+        np.random.random(size=[1, 8732, 4]).astype("float32"),
+        np.random.random(size=[1, 8732, 4]).astype("bool"),
+        np.random.random(size=[1, 8732, 4]).astype("float32"),
+        np.random.random(size=[1, 8732, 21]).astype("float32"),
+        np.random.randint(low=0, high=10, size=[1, 8732, 1], dtype="int64"),
     )
     return inputs
 
@@ -62,9 +63,10 @@ class TestLayer(unittest.TestCase):
     def setUp(self):
         self.inputs = create_tensor_inputs()
         self.net = LayerCase()
+
     def train(self, net, to_static, with_prim=False, with_cinn=False):
         if to_static:
-            paddle.set_flags({'FLAGS_prim_all': with_prim})
+            paddle.set_flags({"FLAGS_prim_all": with_prim})
             if with_cinn:
                 build_strategy = paddle.static.BuildStrategy()
                 build_strategy.build_cinn_pass = True
@@ -74,6 +76,7 @@ class TestLayer(unittest.TestCase):
         paddle.seed(123)
         outs = net(*self.inputs)
         return outs
+
     def test_ast_prim_cinn(self):
         st_out = self.train(self.net, to_static=True)
         cinn_out = self.train(self.net, to_static=True, with_prim=True, with_cinn=True)
@@ -81,5 +84,5 @@ class TestLayer(unittest.TestCase):
             np.testing.assert_allclose(st.numpy(), cinn.numpy(), atol=1e-8)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
