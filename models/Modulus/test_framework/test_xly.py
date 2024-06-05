592fdf283063125282243e585e320857e72c9cb2
plusNew001 <suijiaxin_job@163.com>
2024-06-05 09:04:04 +0000

    debug ce,test=model

--- models/Modulus/test_framework/test_xly.py
+++ models/Modulus/test_framework/test_xly.py
@@ -15,7 +15,7 @@ from tools.send_email import send_email
 
 test_data = {}
 # model_list = ["limerock^limerock_transfer_learning^limerock_flow", "darcy^darcy_FNO", "darcy^darcy_PINO", "darcy^darcy_DeepO"]
-model_list = ["darcy^darcy_DeepO"]
+model_list = ["limerock^limerock_transfer_learning^limerock_flow"]
 project_dir = os.getcwd()
 # with open('./model_daily.json', 'r', encoding='utf-8') as f:
 #     # 读取JSON数据
