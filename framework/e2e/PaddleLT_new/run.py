419b9ac80e5c9ee13cb860ed97c23c6ba90a2bd7
Zeref996 <825276847@qq.com>
2024-06-06 11:43:53 +0800

    update cinn inputspec run, test=model

--- framework/e2e/PaddleLT_new/run.py
+++ framework/e2e/PaddleLT_new/run.py
@@ -234,6 +234,7 @@ class Run(object):
                     os.environ.get("TESTING") == "yaml/dy^dy2stcinn_train_inputspec.yml"
                     or os.environ.get("TESTING") == "dy^dy2stcinn_eval_inputspec.yml"
                 ):
+                    self.logger.get_log().info(f"{py_file} 进入subprocess执行模式, 加入超市限制")
                     cmd = (
                         "cp -r PaddleLT.py {}.py && "
                         "{} -m pytest {}.py --title={} --layerfile={} --testing={} --alluredir={} --timeout={}"
