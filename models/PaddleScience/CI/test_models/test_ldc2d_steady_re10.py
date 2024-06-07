08921dc83879bfe6ba8b4259df14bdbde457ab60
plusNew001 <suijiaxin_job@163.com>
2024-06-07 10:40:22 +0000

    add ci,test=model

--- models/PaddleScience/CI/test_models/test_ldc2d_steady_re10.py
+++ models/PaddleScience/CI/test_models/test_ldc2d_steady_re10.py
@@ -20,11 +20,12 @@ import sys
 import subprocess
 
 import pytest
+import allure
 import numpy as np
 
 from tools.log_analysis import get_last_epoch_loss, get_last_eval_metric
 
-
+@allure.title("功能测试")
 def test_ldc2d_steady_re10_exit_code():
     """
     测试函数：测试 ldc2d_steady_Re10.py 脚本的退出码是否为 0 以保证可视化文件的正常保存
@@ -46,7 +47,7 @@ def test_ldc2d_steady_re10_exit_code():
     # 断言退出码为 0
     assert exit_code == 0
 
-
+@allure.title("训练精度测试")
 def test_ldc2d_steady_re10_loss():
     """
     test loss
@@ -62,7 +63,7 @@ def test_ldc2d_steady_re10_loss():
     # 断言最后一轮迭代的损失值与基准
     assert np.allclose(float(last_loss), base_loss, rtol=1e-6)
 
-
+@allure.title("评估精度测试")
 def test_ldc2d_steady_re10_metric():
     """
     测试函数：测试 ldc2d_steady_Re10.py 的评估值
@@ -77,6 +78,7 @@ def test_ldc2d_steady_re10_metric():
 
     assert np.allclose(float(last_metric), base_metric, rtol=1e-6)
 
+@allure.title("导出功能测试")
 def test_ldc2d_steady_re10_export():
     """
     测试函数：测试 ldc2d_steady_re10.py 脚本的export功能
@@ -94,6 +96,7 @@ def test_ldc2d_steady_re10_export():
     # 断言退出码为 0
     assert exit_code == 0
 
+@allure.title("推理功能测试")
 def test_ldc2d_steady_re10_infer():
     """
     测试函数：测试 ldc2d_steady_re10.py 脚本的infer功能
@@ -113,5 +116,5 @@ def test_ldc2d_steady_re10_infer():
 
 if __name__ == "__main__":
     # 使用 pytest 模块运行测试函数
-    code = pytest.main([sys.argv[0]])
+    code = pytest.main([--alluredir=./allure", sys.argv[0]])
     sys.exit(code)
