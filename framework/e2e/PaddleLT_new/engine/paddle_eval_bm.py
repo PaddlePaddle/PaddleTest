1d3bebb5987adf98e68d1c78fcd22da6d1b5e23e
Zeref996 <825276847@qq.com>
2024-06-19 17:09:56 +0800

    add plt perf set flag and del flag, test=model

--- framework/e2e/PaddleLT_new/engine/paddle_eval_bm.py
+++ framework/e2e/PaddleLT_new/engine/paddle_eval_bm.py
@@ -14,6 +14,7 @@ from generator.builder_layer import BuildLayer
 from generator.builder_data import BuildData
 from tools.res_save import save_pickle
 from tools.statistics import trimmean, mean, best, best_top_k, perf_by_step
+from tools.logger import Logger
 
 
 class LayerEvalBM(object):
@@ -45,6 +46,7 @@ class LayerEvalBM(object):
 
         self.layerfile = layerfile
         self.data = BuildData(layerfile=self.layerfile).get_single_tensor()
+        self.logger = Logger("LayerEvalBM")
 
     def _net_instant(self):
         """get net and data"""
@@ -52,18 +54,67 @@ class LayerEvalBM(object):
         net = BuildLayer(layerfile=self.layerfile).get_layer()
         return net
 
+    def _set_cinn_flags(self):
+        """
+        set cinn flags
+        """
+        os.environ["FLAGS_prim_all"] = "true"
+        os.environ["FLAGS_prim_enable_dynamic"] = "true"
+
+        os.environ["FLAGS_use_cinn"] = "true"
+        os.environ["FLAGS_cinn_bucket_compile"] = "1"
+        os.environ["FLAGS_cinn_new_group_scheduler"] = "1"
+        os.environ["FLAGS_group_schedule_tiling_first"] = "1"
+
+        os.environ["FLAGS_enable_pir_api"] = "1"
+        os.environ["FLAGS_enable_pir_in_executor"] = "1"
+        os.environ["MIN_GRAPH_SIZE"] = "0"
+
+        self.logger.get_log().info("_set_cinn_flags 性能测试过程中, 成功追加设定cinn相关FLAGS~~")
+
+    def _del_cinn_flags(self):
+        """
+        del cinn flags
+        """
+        if "FLAGS_prim_all" in os.environ:
+            del os.environ["FLAGS_prim_all"]
+        if "FLAGS_prim_enable_dynamic" in os.environ:
+            del os.environ["FLAGS_prim_enable_dynamic"]
+
+        if "FLAGS_use_cinn" in os.environ:
+            del os.environ["FLAGS_use_cinn"]
+        if "FLAGS_cinn_bucket_compile" in os.environ:
+            del os.environ["FLAGS_cinn_bucket_compile"]
+        if "FLAGS_cinn_new_group_scheduler" in os.environ:
+            del os.environ["FLAGS_cinn_new_group_scheduler"]
+        if "FLAGS_group_schedule_tiling_first" in os.environ:
+            del os.environ["FLAGS_group_schedule_tiling_first"]
+
+        if "FLAGS_enable_pir_api" in os.environ:
+            del os.environ["FLAGS_enable_pir_api"]
+        if "FLAGS_enable_pir_in_executor" in os.environ:
+            del os.environ["FLAGS_enable_pir_in_executor"]
+        if "MIN_GRAPH_SIZE" in os.environ:
+            del os.environ["MIN_GRAPH_SIZE"]
+
+        self.logger.get_log().info("_del_cinn_flags 性能测试过程中, 取消cinn相关FLAGS~~")
+
     def dy_eval_perf(self):
         """dygraph eval"""
+        self._del_cinn_flags()
+
         net = self._net_instant()
         net.eval()
 
         def _perf(input_data):
             logit = net(*input_data)
+            paddle.core._cuda_synchronize(paddle.CUDAPlace(0))
             return logit
 
         total_time_list = []
         # 预热
-        timeit.timeit(lambda: _perf(self.data), number=int(self.perf_repeat * self.timeit_num * 0.2))
+        timeit.timeit(lambda: _perf(self.data), number=10)
+        # timeit.timeit(lambda: _perf(self.data), number=int(self.perf_repeat * self.timeit_num * 0.2))
         for i in range(self.perf_repeat):
             total_time = timeit.timeit(lambda: _perf(self.data), number=self.timeit_num)
             total_time_list.append(total_time)
@@ -78,10 +129,14 @@ class LayerEvalBM(object):
 
         time_res = eval(self.perf_statis)(data_list=total_time_list)
         time_res = round(time_res * self.statis_times, self.statis_round)
+
+        self._set_cinn_flags()
         return time_res
 
     def dy2st_eval_perf(self):
         """dygraph eval"""
+        self._del_cinn_flags()
+
         net = self._net_instant()
         st_net = paddle.jit.to_static(net, full_graph=True)
         st_net.eval()
@@ -92,7 +147,8 @@ class LayerEvalBM(object):
 
         total_time_list = []
         # 预热
-        timeit.timeit(lambda: _perf(self.data), number=int(self.perf_repeat * self.timeit_num * 0.2))
+        timeit.timeit(lambda: _perf(self.data), number=10)
+        # timeit.timeit(lambda: _perf(self.data), number=int(self.perf_repeat * self.timeit_num * 0.2))
         for i in range(self.perf_repeat):
             total_time = timeit.timeit(lambda: _perf(self.data), number=self.timeit_num)
             total_time_list.append(total_time)
@@ -107,10 +163,14 @@ class LayerEvalBM(object):
 
         time_res = eval(self.perf_statis)(data_list=total_time_list)
         time_res = round(time_res * self.statis_times, self.statis_round)
+
+        self._set_cinn_flags()
         return time_res
 
     def dy2st_eval_cinn_perf(self):
         """dy2st eval"""
+        self._set_cinn_flags()
+
         net = self._net_instant()
 
         build_strategy = paddle.static.BuildStrategy()
@@ -125,7 +185,8 @@ class LayerEvalBM(object):
 
         total_time_list = []
         # 预热
-        timeit.timeit(lambda: _perf(self.data), number=int(self.perf_repeat * self.timeit_num * 0.2))
+        timeit.timeit(lambda: _perf(self.data), number=10)
+        # timeit.timeit(lambda: _perf(self.data), number=int(self.perf_repeat * self.timeit_num * 0.2))
         for i in range(self.perf_repeat):
             total_time = timeit.timeit(lambda: _perf(self.data), number=self.timeit_num)
             total_time_list.append(total_time)
@@ -140,4 +201,6 @@ class LayerEvalBM(object):
 
         time_res = eval(self.perf_statis)(data_list=total_time_list)
         time_res = round(time_res * self.statis_times, self.statis_round)
+
+        self._set_cinn_flags()
         return time_res
