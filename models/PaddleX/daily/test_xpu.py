8345633cf9c51a29454698e94ff589eb4f45d8e1
plusNew001 <suijiaxin_job@163.com>
2024-06-27 12:09:08 +0000

    add  case,test=model

--- models/PaddleX/daily/test_xpu.py
+++ models/PaddleX/daily/test_xpu.py
@@ -4,20 +4,20 @@ import json
 
 def run_model(model_name, model_cls, data_dir, env, concurrency=1):
     print(f"start to run {model_name}")
-    os.system(f"python main.py -c paddlex/configs/{model_cls}/{model_name}.yaml -o Global.mode=train -o Global.dataset_dir={data_dir} -o Global.device={env}:12,13,14,15 >test_${model_name}.log 2>&1")
+    os.system(f"python main.py -c paddlex/configs/{model_cls}/{model_name}.yaml -o Global.mode=train -o Global.dataset_dir={data_dir} -o Global.device={env}:4,5,6,7 >test_${model_name}.log 2>&1")
     return 0
 
 
 
 def test_npu():
-    with open('./model_npu.json', 'r', encoding='utf-8') as f:
+    with open('./model_xpu.json', 'r', encoding='utf-8') as f:
     # 读取JSON数据
         test_json = json.load(f)
         # 遍历 JSON 对象获取所有的键
     for key in test_json:
         model_name = key
         model_cls = test_json[key]
-        env = "npu"
+        env = "xpu"
         if model_cls == "image_classification":
             if not os.path.exists("cls_flowers_examples.tar"):
                 os.system("wget https://paddle-model-ecology.bj.bcebos.com/paddlex/data/cls_flowers_examples.tar")
@@ -38,6 +38,11 @@ def test_npu():
                 os.system("wget https://paddle-model-ecology.bj.bcebos.com/paddlex/data/ts_anomaly_examples.tar")
                 os.system("tar -xvf ts_anomaly_examples.tar")
             data_dir = "./ts_anomaly_examples"
+        elif model_cls == "structure_analysis":
+            if not os.path.exists("det_layout_examples.tar"):
+                os.system("wget https://paddle-model-ecology.bj.bcebos.com/paddlex/data/det_layout_examples.tar")
+                os.system("tar -xvf det_layout_examples.tar")
+            data_dir = "./det_layout_examples"
         else :
             raise ValueError(f"{model_cls} is not supported yet.")
         run_model(model_name, model_cls, data_dir, env)
