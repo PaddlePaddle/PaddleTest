3fe467bf9afe09058ad7982d5e87d9eb0bdeb8c5
tianlef <1095012807@qq.com>
2024-07-11 11:06:17 +0800

    fix ipadapter log path

--- models/PaddleMIX/CE/ppdiffusers/deploy/ipadapter/sd15_infer.sh
+++ models/PaddleMIX/CE/ppdiffusers/deploy/ipadapter/sd15_infer.sh
@@ -15,7 +15,7 @@ export USE_PPXFORMERS=False
     --scheduler "ddim" \
     --backend paddle \
     --device gpu \
-    --task_name text2img) 2>&1 | tee ${log_dir}/deploy_ipadapter_sd15_inference_text2img.log
+    --task_name text2img) 2>&1 | tee ${log_dir}/ipadapter_sd15_inference_text2img.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -47,7 +47,7 @@ echo "*******ppdiffusers/deploy/sd15 ipadapter_sd15_inference_img2img end*******
     --scheduler "ddim" \
     --backend paddle \
     --device gpu \
-    --task_name inpaint) 2>&1 | tee ${log_dir}/sd15_inference_inpaint.log
+    --task_name inpaint) 2>&1 | tee ${log_dir}/ipadapter_sd15_inference_inpaint.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -57,54 +57,54 @@ else
 fi
 echo "*******ppdiffusers/deploy/sd15 ipadapter_sd15_inference_inpaint end***********"
 
-# # tensorrt
-# # text2img
-# (python infer.py \
-#     --model_dir static_model/stable-diffusion-v1-5-ipadapter/ \
-#     --scheduler "ddim" \
-#     --backend paddle_tensorrt \
-#     --device gpu \
-#     --task_name text2img) 2>&1 | tee ${log_dir}/sd15_inference_tensorrt_text2img.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_text2img success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_text2img fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_text2img end***********"
+# tensorrt
+# text2img
+(python infer.py \
+    --model_dir static_model/stable-diffusion-v1-5-ipadapter/ \
+    --scheduler "ddim" \
+    --backend paddle_tensorrt \
+    --device gpu \
+    --task_name text2img) 2>&1 | tee ${log_dir}/ipadapter_sd15_inference_tensorrt_text2img.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_text2img success" >>"${log_dir}/ce_res.log"
+else
+    echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_text2img fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_text2img end***********"
 
-# # img2img
-# (python infer.py \
-#     --model_dir static_model/stable-diffusion-v1-5-ipadapter/ \
-#     --scheduler "ddim" \
-#     --backend paddle_tensorrt \
-#     --device gpu \
-#     --task_name img2img) 2>&1 | tee ${log_dir}/sd15_inference_tensorrt_img2img.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_img2img success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_img2img fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_img2img end***********"
+# img2img
+(python infer.py \
+    --model_dir static_model/stable-diffusion-v1-5-ipadapter/ \
+    --scheduler "ddim" \
+    --backend paddle_tensorrt \
+    --device gpu \
+    --task_name img2img) 2>&1 | tee ${log_dir}/ipadapter_sd15_inference_tensorrt_img2img.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_img2img success" >>"${log_dir}/ce_res.log"
+else
+    echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_img2img fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_img2img end***********"
 
-# # inpaint
-# (python infer.py \
-#     --model_dir static_model/stable-diffusion-v1-5-ipadapter/ \
-#     --scheduler "ddim" \
-#     --backend paddle_tensorrt \
-#     --device gpu \
-#     --task_name inpaint) 2>&1 | tee ${log_dir}/sd15_inference_tensorrt_inpaint.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_inpaint success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_inpaint fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_inpaint end***********"
+# inpaint
+(python infer.py \
+    --model_dir static_model/stable-diffusion-v1-5-ipadapter/ \
+    --scheduler "ddim" \
+    --backend paddle_tensorrt \
+    --device gpu \
+    --task_name inpaint) 2>&1 | tee ${log_dir}/ipadapter_sd15_inference_tensorrt_inpaint.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_inpaint success" >>"${log_dir}/ce_res.log"
+else
+    echo "ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_inpaint fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******ppdiffusers/deploy/sd15 ipadapter_sd15_inference_tensorrt_inpaint end***********"
 
 echo exit_code:${exit_code}
 exit ${exit_code}
