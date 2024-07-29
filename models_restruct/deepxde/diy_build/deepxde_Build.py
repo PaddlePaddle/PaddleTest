4fb868e7373f7edd97979696380024ebf3674f97
plusNew001 <suijiaxin_job@163.com>
2024-07-29 12:38:47 +0000

    fix deepxde bug,test=model

---
+++ models_restruct/deepxde/diy_build/deepxde_Build.py
@@ -0,0 +1,110 @@
+
+# encoding: utf-8
+"""
+自定义环境准备
+"""
+import os
+import sys
+import time
+import logging
+import platform
+import tarfile
+import argparse
+import numpy as np
+import yaml
+import wget
+import shutil
+from Model_Build import Model_Build
+
+logger = logging.getLogger("ce")
+
+
+class deepxde_Build(Model_Build):
+    """
+    自定义环境准备
+    """
+
+    def __init__(self, args):
+        """
+        初始化变量
+        """
+        self.paddle_whl = args.paddle_whl
+        self.get_repo = args.get_repo
+        self.branch = args.branch
+        self.system = args.system
+        self.set_cuda = args.set_cuda
+        self.dataset_org = args.dataset_org
+        self.dataset_target = args.dataset_target
+
+        self.REPO_PATH = os.path.join(
+            os.getcwd(), args.reponame)  # 所有和yaml相关的变量与此拼接
+        self.test_root_path = os.getcwd()
+
+        self.reponame = args.reponame
+        self.models_list = args.models_list
+        self.models_file = args.models_file
+        self.test_model_list = []
+        self.mount_path = str(os.getenv("mount_path"))
+        self.use_data_cfs = str(args.use_data_cfs)
+
+        if str(self.models_list) != "None":
+            for line in self.models_list.split(","):
+                if ".yaml" or ".yml" in line:
+                    self.test_model_list.append(line.strip().replace("^", "/"))
+                    print("self.test_model_list:{}".format(self.test_model_list))
+        elif str(self.models_file) != "None":  # 获取要执行的yaml文件列表
+            for file_name in self.models_file.split(","):
+                for line in open(file_name):
+                    if ".yaml" or ".yml" in line:
+                        self.test_model_list.append(
+                            line.strip().replace("^", "/"))
+        else:
+            for file_name in os.listdir("cases"):
+                if ".yaml" or ".yml" in file_name:
+                    self.test_model_list.append(
+                        file_name.strip().replace("^", "/"))
+
+    def build_deepxde(self):
+        """
+        安装依赖包
+        """
+        os.system("python -m pip install numpy==1.23.1")
+        return 0
+
+    def download_data(self, data_link, destination_folder=".", dir_name=""):
+        """
+        下载数据集和使用的文件
+        """
+        pass
+
+    def build_env(self):
+        """
+        使用父类实现好的能力
+        """
+        super(deepxde_Build, self).build_env()
+        ret = 0
+        ret = self.build_deepxde()
+        if ret:
+            logger.info("build env whl failed")
+            return ret
+        return ret
+
+
+if __name__ == "__main__":
+
+    def parse_args():
+        """
+        接收和解析命令传入的参数
+            最好尽可能减少输入给一些默认参数就能跑的示例!
+        """
+        parser = argparse.ArgumentParser("Tool for running CE task")
+        parser.add_argument("--models_file", help="模型列表文件",
+                            type=str, default=None)
+        parser.add_argument("--reponame", help="输入repo",
+                            type=str, default=None)
+        args = parser.parse_args()
+        return args
+
+    args = parse_args()
+    model = deepxde_Build(args)
+    model.build_deepxde()
