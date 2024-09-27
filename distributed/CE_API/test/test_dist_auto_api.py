106948094cc354f94bb3268cf25c5f601a17b58c
LixinGuo <18510030324@126.com>
2024-09-27 15:24:41 +0800

    remove api cases not suport in pir mode,test=model (#2960)
    * set FLAGS_use_stride_kernel true
    
    * update dnn threshold
    
    * update dnn threshold,test=model
    
    * test=model,update base_value because of Paddle PR##53584
    
    * fix code_style and pandas=1.5.3,test=model
    
    * fix dy2st run error
    
    * fix dy2st run error,test=model
    
    * fix rec, test=model
    
    * test=model,fix xly list
    
    * add PTS list;test=model
    
    * add PTS list;test=model
    
    * fix step name;test=model
    
    * fix step name;test=model
    
    * fix step name;test=model
    
    * fix step name;test=model
    
    * fix step name;test=model
    
    * fix step name;test=model
    
    * fix step name;test=model
    
    * remove cuda102 py37
    
    * Update dssm base value due to cuda102 to cuda112
    
    * Update models^contentunderstanding^textcnn^config_bigdata.yaml
    
    * Update PaddleRec_XLY_list
    
    * add paddlerec ce, test=model
    
    * fix bert train dir,test=model
    
    * fix distributed api error
    
    * fix nlp pip error,test=model
    
    * remove api cases not suport in pir mode

--- distributed/CE_API/test/test_dist_auto_api.py
+++ distributed/CE_API/test/test_dist_auto_api.py
@@ -22,51 +22,51 @@ os.system("export CUDA_VISIBLE_DEVICES=0,1")
 class TestDistAutoApi(object):
     """TestDistAutoApi"""
 
-    def test_auto_process_mesh(self):
-        """test_auto_process_mesh"""
-        cmd = "python -m paddle.distributed.launch --devices 0,1 --job_id auto_process_mesh dist_auto_process_mesh.py"
-        pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = pro.communicate()
-        print(out)
-        pro.wait()
-        pro.returncode == 0
-        assert str(out).find("Error") == -1
-        assert str(err).find("Error") == -1
+    # def test_auto_process_mesh(self):
+    #     """test_auto_process_mesh"""
+    #     cmd = "python -m paddle.distributed.launch --devices 0,1 --job_id auto_process_mesh dist_auto_process_mesh.py"
+    #     pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #     out, err = pro.communicate()
+    #     print(out)
+    #     pro.wait()
+    #     pro.returncode == 0
+    #     assert str(out).find("Error") == -1
+    #     assert str(err).find("Error") == -1
 
-    def test_auto_process_mesh_v2(self):
-        """test_auto_process_mesh"""
-        cmd = (
-            "python -m paddle.distributed.launch --devices 0,1 --job_id auto_process_mesh dist_auto_process_mesh_v2.py"
-        )
-        pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = pro.communicate()
-        print(out)
-        pro.wait()
-        pro.returncode == 0
-        assert str(out).find("Error") == -1
-        assert str(err).find("Error") == -1
+    # def test_auto_process_mesh_v2(self):
+    #     """test_auto_process_mesh"""
+    #     cmd = (
+    #         "python -m paddle.distributed.launch --devices 0,1 --job_id auto_process_mesh dist_auto_process_mesh_v2.py"
+    #     )
+    #     pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #     out, err = pro.communicate()
+    #     print(out)
+    #     pro.wait()
+    #     pro.returncode == 0
+    #     assert str(out).find("Error") == -1
+    #     assert str(err).find("Error") == -1
 
-    def test_auto_shard_op(self):
-        """test_auto_shard_op"""
-        cmd = "python -m paddle.distributed.launch --devices 0,1 --job_id auto_shard_op dist_auto_shard_op.py"
-        pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = pro.communicate()
-        print(out)
-        pro.wait()
-        pro.returncode == 0
-        assert str(out).find("Error") == -1
-        assert str(err).find("Error") == -1
+    # def test_auto_shard_op(self):
+    #     """test_auto_shard_op"""
+    #     cmd = "python -m paddle.distributed.launch --devices 0,1 --job_id auto_shard_op dist_auto_shard_op.py"
+    #     pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #     out, err = pro.communicate()
+    #     print(out)
+    #     pro.wait()
+    #     pro.returncode == 0
+    #     assert str(out).find("Error") == -1
+    #     assert str(err).find("Error") == -1
 
-    def test_auto_shard_tensor(self):
-        """test_auto_shard_tensor"""
-        cmd = "python -m paddle.distributed.launch --devices 0,1 --job_id auto_shard_tensor dist_auto_shard_tensor.py"
-        pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = pro.communicate()
-        print(out)
-        pro.wait()
-        pro.returncode == 0
-        assert str(out).find("Error") == -1
-        assert str(err).find("Error") == -1
+    # def test_auto_shard_tensor(self):
+    #     """test_auto_shard_tensor"""
+    #     cmd = "python -m paddle.distributed.launch --devices 0,1 --job_id auto_shard_tensor dist_auto_shard_tensor.py"
+    #     pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #     out, err = pro.communicate()
+    #     print(out)
+    #     pro.wait()
+    #     pro.returncode == 0
+    #     assert str(out).find("Error") == -1
+    #     assert str(err).find("Error") == -1
 
     def test_auto_converter(self):
         """test_auto_converter"""
@@ -79,17 +79,17 @@ class TestDistAutoApi(object):
         assert str(out).find("Error") == -1
         assert str(err).find("Error") == -1
 
-    def test_auto_new_cost_model(self):
-        """test_auto_new_cost_model"""
-        cmd = "python -m paddle.distributed.launch --devices 0,1 --job_id auto_new_cost_model \
-             dist_auto_new_cost_model.py"
-        pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = pro.communicate()
-        print(out)
-        pro.wait()
-        pro.returncode == 0
-        assert str(out).find("Error") == -1
-        assert str(err).find("Error") == -1
+    # def test_auto_new_cost_model(self):
+    #     """test_auto_new_cost_model"""
+    #     cmd = "python -m paddle.distributed.launch --devices 0,1 --job_id auto_new_cost_model \
+    #          dist_auto_new_cost_model.py"
+    #     pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #     out, err = pro.communicate()
+    #     print(out)
+    #     pro.wait()
+    #     pro.returncode == 0
+    #     assert str(out).find("Error") == -1
+    #     assert str(err).find("Error") == -1
 
     def test_auto_comm_cost(self):
         """test_auto_comm_cost"""
@@ -113,13 +113,13 @@ class TestDistAutoApi(object):
         assert str(out).find("Error") == -1
         assert str(err).find("Error") == -1
 
-    def test_auto_base_cost(self):
-        """test_auto_base_cost"""
-        cmd = "python -m paddle.distributed.launch --devices 0,1 --job_id auto_base_cost dist_auto_base_cost.py"
-        pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = pro.communicate()
-        print(out)
-        pro.wait()
-        pro.returncode == 0
-        assert str(out).find("Error") == -1
-        assert str(err).find("Error") == -1
+    # def test_auto_base_cost(self):
+    #     """test_auto_base_cost"""
+    #     cmd = "python -m paddle.distributed.launch --devices 0,1 --job_id auto_base_cost dist_auto_base_cost.py"
+    #     pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #     out, err = pro.communicate()
+    #     print(out)
+    #     pro.wait()
+    #     pro.returncode == 0
+    #     assert str(out).find("Error") == -1
+    #     assert str(err).find("Error") == -1
