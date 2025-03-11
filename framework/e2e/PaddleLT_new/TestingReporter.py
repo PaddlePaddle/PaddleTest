857ab276c5830a81c7ac841fa4496b0dd15a1322
Zeref996 <825276847@qq.com>
2025-03-11 08:05:59 +0000

    fix plt bs, test=model

--- framework/e2e/PaddleLT_new/TestingReporter.py
+++ framework/e2e/PaddleLT_new/TestingReporter.py
@@ -193,7 +193,6 @@ if __name__ == "__main__":
     reporter = TestingReporter(date_interval=args.date_interval)  # date_interval=2024-11-13,2024-11-14
     # 打印出相对失败case信息
     relative_fail_dict, absolute_fail_dict = reporter.get_fail_case_info()
-    # exit(0)
     # print(f"relative_fail_dict:{relative_fail_dict}")
     relative_fail_num_dict = reporter.get_fail_case_num(fail_dict=relative_fail_dict)
     # print(f"relative_fail_num_dict:{relative_fail_num_dict}")
