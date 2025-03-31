e7ffeab601e0d8484069d9f6569447a4f0d0f749
liujie44 <liujie44@baidu.com>
2025-03-31 10:15:38 +0800

    fix path

--- models_restruct/PaddleLLM/train/grpo.sh
+++ models_restruct/PaddleLLM/train/grpo.sh
@@ -54,8 +54,8 @@ python -u -m paddle.distributed.launch --devices "0,1,2,3,4,5,6,7" run_ppo.py ..
     --actor_model_name_or_path ${model_name_or_path} \
     --reward_model_name_or_path "" \
     --output_dir ${output_dir} \
-    --max_steps 5 \
-    --save_steps 5 \
+    --max_steps 2 \
+    --save_steps 2 \
     --tensor_parallel_degree 4 \
     --per_device_prompt_batch_size 1 \
     --per_device_train_batch_size 4 \
@@ -74,7 +74,7 @@ python -u -m paddle.distributed.launch --devices "0,1,2,3,4,5,6,7" run_ppo.py ..
     --actor_model_name_or_path ${model_name_or_path} \
     --reward_model_name_or_path "" \
     --output_dir ${output_dir} \
-    --max_steps 2 \
+    --max_steps 1 \
     --save_steps 11 \
     --tensor_parallel_degree 4 \
     --per_device_prompt_batch_size 1 \
--- models_restruct/PaddleLLM/train/predict.sh
+++ models_restruct/PaddleLLM/train/predict.sh
@@ -4,7 +4,7 @@ ngpus=${2:-8}
 step_name=${3:-"grpo"}
 # 1.设置模型路径
 if [ "$step_name" == "ppo" ] || [ "$step_name" == "grpo" ]; then 
-    model_name_or_path=./checkpoints/$model_name/${step_name}/policy/checkpoint-5
+    model_name_or_path=./checkpoints/$model_name/${step_name}/policy/checkpoint-2
 else
     model_name_or_path=./checkpoints/$model_name/${step_name}
 fi
