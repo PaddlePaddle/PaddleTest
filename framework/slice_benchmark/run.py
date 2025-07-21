b56b887976472b2f6b6168e989e97501438e3ccb
Zeref996 <53218160+Zeref996@users.noreply.github.com>
2025-07-21 14:04:20 +0800

    fix slice bm 01 (#3104)
    * add slice bm
    
    * fix slice bm
    
    * fix slice run

--- framework/slice_benchmark/run.py
+++ framework/slice_benchmark/run.py
@@ -3,7 +3,7 @@
 # @author Zeref996
 # encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
 """
-slice 测试启动
+slice 测试启动 
 """
 
 import os
@@ -151,13 +151,13 @@ class SliceTestRun(object):
         if isinstance(res, str):
             grade = res
         else:
-            if res <= -0.3:
+            if res <= -0.2:
                 grade = "worse"
-            elif -0.3 < res <= -0.15:
+            elif -0.2 < res <= -0.1:
                 grade = "doubt"
-            elif -0.15 < res <= 0.15:
+            elif -0.1 < res <= 0.1:
                 grade = "equal"
-            elif res > 0.15:
+            elif res > 0.1:
                 grade = "better"
         return grade
 
