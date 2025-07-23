4fa4f3c4ba191c8308169d21ecbee15ecaae84dc
Divano <dddivano@outlook.com>
2025-07-23 14:09:43 +0800

    add fd test framework (#3108)

---
+++ framework/ServeTest/core/__init__.py
@@ -0,0 +1,10 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# @author DDDivano
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+"""
+core
+"""
+
+from .logprob import LogProbHandler
+from .route import ROUTE
