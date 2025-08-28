b35f08fac8d5ba93598bb39f8788a68393e9fd1c
Difer <707065510@qq.com>
2025-08-28 20:33:39 +0800

    fix some dtype (#3136)

--- framework/api/paddlebase/test_zeros.py
+++ framework/api/paddlebase/test_zeros.py
@@ -391,14 +391,15 @@ def test_zeros32():
     obj.run(res=res, shape=shape, dtype=dtype)
 
 
-@pytest.mark.api_base_zeros_exception
+@pytest.mark.api_base_zeros_parameters
 def test_zeros33():
     """
     shape_type=list,dtype='BOOL'
     """
     shape = [1, 2, 3, 4]
     dtype = "BOOL"
-    obj.exception(mode="python", etype=TypeError, shape=shape, dtype=dtype)
+    res = np.zeros(shape)
+    obj.run(res=res, shape=shape, dtype=dtype)
 
 
 @pytest.mark.api_base_zeros_exception
