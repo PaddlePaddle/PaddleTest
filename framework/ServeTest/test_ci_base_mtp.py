97200e404dc46fa3cf94b05253a7dffe9fc45238
Zhang Yulong <35552275+ZhangYulongg@users.noreply.github.com>
2025-12-03 15:48:07 +0800

    Update test_ci_base_mtp.py (#3178)

--- framework/ServeTest/test_ci_base_mtp.py
+++ framework/ServeTest/test_ci_base_mtp.py
@@ -309,7 +309,7 @@ def test_non_stream_with_logprobs():
 
     assert resp_json["usage"]["prompt_tokens"] == 52
     assert resp_json["usage"]["completion_tokens"] == 3
-    assert resp_json["usage"]["total_tokens"] == 53
+    assert resp_json["usage"]["total_tokens"] == 55
 
 
 def test_stream_with_logprobs():
