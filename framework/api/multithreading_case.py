246baee09d2962cdbf3bce1906296474a3b5f1f6
Divano <dddivano@outlook.com>
2025-12-09 11:01:59 +0800

    Update multithreading_case.py (#3184)

--- framework/api/multithreading_case.py
+++ framework/api/multithreading_case.py
@@ -37,6 +37,7 @@ ignore_case_dir = {
         "test_CELU.py",
         "test_initializer_truncated_normal_new.py",
         "test_initializer_truncated_normal.py",
+        "test_birnn.py",
     ],
     "paddlebase": [
         "test_is_grad_enabled.py",
